a7570537feb9b41f9ec6a0b89f3ad6c769dd8170
Mike Jarvis <michael@jarvis.net>
2012-02-24 16:20:31 -0500

    added GalSim.h in include dir

---
+++ include/GalSim.h
@@ -0,0 +1,19 @@
+// This just includes the relevant header files from the galsim directory.
+
+#ifndef GALSIM_H
+#define GALSIM_H
+
+#include "galsim/SBProfile.h"
+#include "galsim/SBDeconvolve.h"
+#include "galsim/SBParse.h"
+#include "galsim/SBPixel.h"
+
+#include "galsim/Image.h"
+#include "galsim/FITSImage.h"
+
+#include "galsim/FFT.h"
+
+#include "galsim/Random.h"
+#include "galsim/Poisson.h"
+
+#endif
