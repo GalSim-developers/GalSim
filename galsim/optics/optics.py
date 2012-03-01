44a34dac6b847e09f55bc3e1d6b2f00f55b13e86
Barnaby Rowe <barnaby.t.p.rowe@gmail.com>
2012-03-01 09:54:40 -0800

    starting optics module code here..

---
+++ galsim/optics/optics.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+"""
+Module containing the optical PSF generation routines
+"""
+
+
+def image(defocus=0., astig1=0., astig2=0., coma1=0., coma2=0., lambda_over_D=5., npix=32):
+	"""Construct a square image of an aberrated PSF.
+
+	
+
+	"""
