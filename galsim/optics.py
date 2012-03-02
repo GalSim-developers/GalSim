d542e67d3b5ae7659590cde680f818bdf704ea59
Barnaby Rowe <barnaby.t.p.rowe@gmail.com>
2012-03-02 10:17:04 -0800

    very basic funcionality seems to be working, need to add asecondary mirror obstruction function, and maybe support struts... (#13)

--- galsim/optics.py
+++ galsim/optics.py
@@ -2,12 +2,10 @@
 """
 import numpy as np
 
-def roll2d(image, nroll):
-    """Perform a 2D roll (circular shift) on a supplied 2D image, concisely.
-    
-    Will probably not do what you want it to if the supplied image is not square!
+def roll2d(image, (iroll, jroll)):
+    """Perform a 2D roll (circular shift) on a supplied 2D image, conveniently.
     """
-    return np.roll(np.roll(image, nroll, axis=1), nroll, axis=0)
+    return np.roll(np.roll(image, jroll, axis=1), iroll, axis=0)
 
 def kxky(shape=(256, 256)):
     """Output the tuple kx, ky corresponding to the DFT of a unit integer-sampled array of input
@@ -17,7 +15,7 @@ def kxky(shape=(256, 256)):
     Uses the most common DFT element ordering conventions (and those of FFTW), so that (0, 0) array
     element corresponds to (kx, ky) = (0, 0).
     
-    Also adopts C/Python array ordering so that the trailing axis corresponds to kx, rather than the
+    Adopts Numpy array index ordering so that the trailing axis corresponds to kx, rather than the
     leading axis as would be expected in IDL/Fortran.  See docstring for numpy.meshgrid which also
     uses this convention.
     """
@@ -30,7 +28,8 @@ def kxky(shape=(256, 256)):
 
 def wavefront(shape=(256, 256), defocus=0., astig1=0., astig2=0., coma1=0., coma2=0., spher=0.,
 			  kmax=np.pi, circular_pupil=True):
-    """Construct a complex, aberrated wavefront across a circular pupil (default) or full array.
+    """Construct a complex, aberrated wavefront across a circular pupil (default) or square
+	array extent.
     
     Outputs a complex image (shape=shape) of a circular pupil wavefront of radius kmax.  We adopt the
     conventions of SBProfile so that the Nyquist frequency of an image with unit integer pixel
@@ -56,9 +55,8 @@ def wavefront(shape=(256, 256), defocus=0., astig1=0., astig2=0., coma1=0., coma
     if circular_pupil:
         in_pupil = (rho <= 1.)
     else:
-        in_pupil = np.ones(shape, dtype=bool)
+        in_pupil = (np.abs(kx) <= kmax) * (np.abs(ky) <= kmax)
 	# Then make wavefront image
-	print shape
     wf = np.zeros(shape, dtype=complex)
     wf[in_pupil] = 1.
 	# Defocus
@@ -77,10 +75,38 @@ def wavefront(shape=(256, 256), defocus=0., astig1=0., astig2=0., coma1=0., coma
     wf[in_pupil] *= np.exp(2j * pi * spher * (6. * rho[in_pupil]**4 - 6. * rho[in_pupil]**2 + 1.))
     return wf
 
-#def cobstruct(pupil, kobs=None):
-#	"""Place a circular obstruction (e.g. secondary mirror) in the middle of a supplied image.
-#
-#	Sets all regions of supplied pupil with |k| < kobs [assumed to be laid out in s
+def psf(shape=(256, 256), defocus=0., astig1=0., astig2=0., coma1=0., coma2=0., spher=0.,
+		kmax=np.pi, circular_pupil=True, secondary=None):
+	"""Generate an image of a circular (default) or square pupil PSF with specified low-order
+	wavefront abberations.
+
+	Image has unit total flux, and is centred on the image[shape[0] / 2, shape[1] / 2] pixel,
+	by default.
+	"""
+	if secondary == None:  # TODO: Build a secondary mirror obstruction function!
+	    wf = wavefront(shape=shape, defocus=defocus, astig1=astig1, astig2=astig2, coma1=coma1,
+					   coma2=coma2, spher=spher, kmax=kmax, circular_pupil=circular_pupil)
+	else:
+		raise NotImplementedError('Secondar mirror obstruction not yet implemented')
+	ftwf = np.fft.fft2(wf)  # I think this (and the below) is quicker than np.abs(ftwf)**2
+	im = roll2d((ftwf * ftwf.conj()).real, (shape[0] / 2, shape[1] / 2)) 
+	return im / im.sum()
+
+def mtf(shape=(256, 256), defocus=0., astig1=0., astig2=0., coma1=0., coma2=0., spher=0.,
+		kmax=np.pi, circular_pupil=True, secondary=None):
+	"""Generate the complex MTF of a circular (default) or square pupil with specified low-order
+	wavefront abberations.
+
+	MTF has mtf[0, 0] = 1 by default, and array elements follow the DFT standard of kxky(shape).
+	"""
+	if secondary == None:  # TODO: Build a secondary mirror obstruction function!
+	    wf = wavefront(shape=shape, defocus=defocus, astig1=astig1, astig2=astig2, coma1=coma1,
+					   coma2=coma2, spher=spher, kmax=kmax, circular_pupil=circular_pupil)
+	else:
+		raise NotImplementedError('Secondar mirror obstruction not yet implemented')
+	ftwf = np.fft.fft2(wf)  # I think this (and the below) is quicker than np.abs(ftwf)**2
+	mtf = np.fft.ifft2((ftwf * ftwf.conj()).real)
+	return mtf / mtf[0, 0].real
 
 
 
