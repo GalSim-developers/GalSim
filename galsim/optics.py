urgh corrected missing factor of two bug

galsim/optics.py
@@ -22,8 +22,8 @@ def kxky(shape=(256, 256)):
     leading axis as would be expected in IDL/Fortran.  See docstring for numpy.meshgrid which also
     uses this convention.
     """
-    k_xaxis = np.fft.fftfreq(shape[1]) * np.pi
-    k_yaxis = np.fft.fftfreq(shape[0]) * np.pi
+    k_xaxis = np.fft.fftfreq(shape[1]) * 2. * np.pi
+    k_yaxis = np.fft.fftfreq(shape[0]) * 2. * np.pi
     return np.meshgrid(k_xaxis, k_yaxis)
 
 def wavefront(shape=(256, 256), defocus=0., astig1=0., astig2=0., coma1=0., coma2=0., spher=0.,
@@ -53,8 +53,10 @@ def wavefront(shape=(256, 256), defocus=0., astig1=0., astig2=0., coma1=0., coma
     array ordering.
     """
     pi = np.pi # minor but saves Python checking the entire np. namespace every time I need pi
-	# Build kx, ky coords
+    # Build kx, ky coords
     kx, ky = kxky(shape)
+    #    kx *= 2.
+    #ky *= 2.
 	# Then define unit disc rho and theta pupil coords for Zernike polynomials
     rho = np.sqrt((kx**2 + ky**2) / (.5 * kmax)**2)
     theta = np.arctan2(ky, kx)
@@ -80,7 +82,7 @@ def wavefront(shape=(256, 256), defocus=0., astig1=0., astig2=0., coma1=0., coma
                            * np.sin(theta[in_pupil]))
 	# Spherical abberation
     wf[in_pupil] *= np.exp(2j * pi * spher * (6. * rho[in_pupil]**4 - 6. * rho[in_pupil]**2 + 1.))
-    return wf
+    return wf, rho
 
 def psf(shape=(256, 256), defocus=0., astig1=0., astig2=0., coma1=0., coma2=0., spher=0.,
         kmax=np.pi, circular_pupil=True, secondary=None):

tests/test_optics.py
@@ -63,8 +63,8 @@ def test_kxky():
     """Test that the basic properties of kx and ky are right.
     """
     kx, ky = galsim.optics.kxky((4, 4))
-    kxref = np.array([0., 0.25, -0.5, -0.25]) * np.pi
-    kyref = np.array([0., 0.25, -0.5, -0.25]) * np.pi
+    kxref = np.array([0., 0.25, -0.5, -0.25]) * 2. * np.pi
+    kyref = np.array([0., 0.25, -0.5, -0.25]) * 2. * np.pi
     for i in xrange(4):
         np.testing.assert_array_almost_equal(kx[i, :], kxref, decimal=decimal,
                                              err_msg='failed kx equivalence on row i = '+str(i))
@@ -77,8 +77,8 @@ def test_kxky_plusone():
     But increment testshape used in test_kxky by one to test both odd and even cases.
     """
     kx, ky = galsim.optics.kxky((4 + 1, 4 + 1))
-    kxref = np.array([0., 0.2, 0.4, -0.4, -0.2]) * np.pi
-    kyref = np.array([0., 0.2, 0.4, -0.4, -0.2]) * np.pi
+    kxref = np.array([0., 0.2, 0.4, -0.4, -0.2]) * 2. * np.pi
+    kyref = np.array([0., 0.2, 0.4, -0.4, -0.2]) * 2. * np.pi
     for i in xrange(4 + 1):
         np.testing.assert_array_almost_equal(kx[i, :], kxref, decimal=decimal,
                                              err_msg='failed kx equivalence on row i = '+str(i))