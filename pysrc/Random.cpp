ff3bf3dc287dd7da93d256aacb1056949bd1bb98
Barnaby Rowe <barnaby.t.p.rowe@gmail.com>
2012-03-22 17:58:18 -0700

    First attempt at wrapping Random::UniformDeviate into Python (#31_RNG_wrapper)

---
+++ pysrc/Random.cpp
@@ -0,0 +1,43 @@
+#include "boost/python.hpp"
+#include "Random.h"
+
+namespace bp = boost::python;
+
+namespace galsim {
+namespace {
+
+struct PyUniformDeviate {
+
+    static void wrap() {
+        static char const * doc = 
+            "\n"
+            "Pseudo-random number generator with uniform distribution in interval [0.,1.).\n"
+            "\n"
+            "UniformDeviate is foundation of the Random.h classes: other distributions take a\n"
+            "UniformDeviate as construction argument and execute some transformation of the\n" 
+            "distribution. Can be seeded with a long int, or by default will be seeded by the\n"
+            "system microsecond counter.\n" 
+            "Copy constructor and assignment operator are kept private since you probably do not\n"
+            "want two 'random' number generators producing the same sequence of numbers in your\n"
+            "code!\n"
+            ;
+
+        bp::class_<UniformDeviate,boost::noncopyable>("UniformDeviate", init<>())
+            .def(init<long>(bp::arg("lseed")))
+            .def("operator", (double)()&UniformDeviate::operator, 
+                 "Draw a new random number from the distribution.")
+            .def("seed", (void)()&UniformDeviate::seed, "Re-seed the PRNG using current time.")
+            .def("seed", (void)(long)&UniformDeviate::seed, (bp::arg("lseed")), 
+                 "Re-seed the PRNG using specified seed.")
+            ;
+    }
+
+};
+
+} // anonymous
+
+void pyExportRandom() {
+    PyUniformDeviate::wrap();
+}
+
+} // namespace galsim
