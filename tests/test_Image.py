ab0308a8289ed80a60bb107f785624c80f6c2c03
Barnaby Rowe <barnaby.t.p.rowe@gmail.com>
2012-03-27 23:36:17 -0700

    Some interesting results, getting to the bottom of failures, will bring this up with Jim...

--- tests/test_Image.py
+++ tests/test_Image.py
@@ -25,13 +25,13 @@ Images are in S, I, F & D flavours.
 """
 
 # Setup info for tests, not likely to change
-testshape = (512, 512)  # shape of image arrays for all tests
+testshape = (4, 4)  # shape of image arrays for all tests
 ntypes = 4
 types = [np.int16, np.int32, np.float32, np.float64]
 tchar = ['S', 'I', 'F', 'D']
 
 ref_array = np.array([[00, 10, 20, 30], [01, 11, 21, 31], [02, 12, 22, 32],
-                      [03, 31, 32, 33]]).astype(types[0])
+                      [03, 13, 23, 33]]).astype(types[0])
 
 datadir = os.path.join(".", "data")
 
@@ -46,19 +46,45 @@ def test_Image_XYmin_XYMax():
         assert image.getXMax() == testshape[0]
         assert image.getYMax() == testshape[1]
 
+def test_FITS_IO():
+    """Test that all four FITS reference images are correctly read in by both PyFITS and our Image 
+    wrappers.
+    """
+    for i in xrange(ntypes):
+        # First try PyFITS for sanity
+        testfile = os.path.join(datadir, "test"+tchar[i]+".fits")
+        test_array = pyfits.getdata(testfile)
+        np.testing.assert_array_equal(ref_array.astype(types[i]), test_array,
+                                      err_msg="PyFITS failing to read reference image.")
+        # Then use the Image methods... Note this also relies on the array look working too
+        image_init_func = eval("galsim.Image"+tchar[i]) # Use handy eval() mimics use of ImageSIFD
+        # First give ImageSIFD.read() a PyFITS PrimaryHDU
+        hdu = pyfits.open(testfile)
+        test_image = image_init_func.read(hdu)
+        np.testing.assert_array_equal(ref_array.astype(types[i]), test_image.array, 
+                                      err_msg="Image"+tchar[i]+".read() failed reading from PyFITS"
+                                              +"PrimaryHDU input.")
+        # Then try an ImageSIFD.read() with the filename itself as input
+        test_image = image_init_func.read(testfile)
+        np.testing.assert_array_equal(ref_array.astype(types[i]), test_image.array, 
+                                      err_msg="Image"+tchar[i]+".read() failed reading from string"
+                                              +"filename input.")
+        # TODO: test reading from an HDU list (e.g. for multi-extension FITS).
+
+
 def test_Image_array_view():
     """Test that all four types of supported arrays correctly provide a view on an input array.
     """
     for i in xrange(ntypes):
-        test_array = pyfits.getdata(os.path.join(datadir, "test"+tchar[i]+".fits"))
-        # First try using the dictionary in Image
-        image = galsim.Image[types[i]](test_array)
-        np.testing.assert_array_equal(test_array, image.array,
+        # First try using the dictionary-type Image init
+        image = galsim.Image[types[i]](ref_array.astype(types[i]))
+        np.testing.assert_array_equal(ref_array.astype(types[i]), image.array,
                                       err_msg="Array look into Image class (dictionary call) does"
                                               +" not match input for dtype = "+str(types[i]))
-        #Then try using the exec command to mimic use via ImageD, ImageF etc.
-        exec("image = galsim.Image"+tchar[i]+"(test_array)")
-        np.testing.assert_array_equal(test_array, image.array,
+        #Then try using the eval command to mimic use via ImageD, ImageF etc.
+        image_init_func = eval("galsim.Image"+tchar[i])
+        image = image_init_func(ref_array.astype(types[i]))
+        np.testing.assert_array_equal(ref_array.astype(types[i]), image.array,
                                       err_msg="Array look into Image class does not match input"
                                               +" for dtype = "+str(types[i]))
  
