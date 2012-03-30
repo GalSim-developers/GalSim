ec42ceedd22b56612f7c6a7fb7d9730496920a67
Rachel Mandelbaum <rmandelb@astro.princeton.edu>
2012-03-29 20:08:30 -0400

    changed SBProfile unit tests in response to Joerg, Barney comments on issue #29 (#29_SBP_unit_tests)

--- tests/test_SBProfile.py
+++ tests/test_SBProfile.py
@@ -1,15 +1,16 @@
 import galsim
 import numpy as np
+import os
 
-imgdir = "./SBProfile_comparison_images/" # Directory containing the reference images.
+imgdir = os.path.join(".","SBProfile_comparison_images") # Directory containing the reference images.
 
 # define a series of tests
 def test_sbprofile_gaussian():
-    """Test the generation of a specific Gaussian profile using SBProfile again a known result.
+    """Test the generation of a specific Gaussian profile using SBProfile against a known result.
     """
     myGauss = galsim.SBGaussian(1)
     myImg = myGauss.draw(dx=0.2)
-    testImg = galsim.fits.read(imgdir+"gauss_1.fits")
+    testImg = galsim.fits.read(os.path.join(imgdir,"gauss_1.fits"))
     #np.testing.assert_almost_equal(np.max(myImg.array),np.max(testImg.array),5,
     #                               err_msg="Peak value of Gaussian disagrees with expected result")
     np.testing.assert_array_almost_equal(myImg.array,testImg.array,5,
