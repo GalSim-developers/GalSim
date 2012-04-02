dc69628b1317519e0f18da3d7e8f161cc9031bb7
Rachel Mandelbaum <rmandelb@astro.princeton.edu>
2012-04-02 19:44:22 -0400

    more fixed spacing for easier readability in test_SBProfile.py (#29_SBP_unit_tests)

--- tests/test_SBProfile.py
+++ tests/test_SBProfile.py
@@ -21,11 +21,11 @@ def printval(image1, image2):
 def getmoments(image1):
     xgrid, ygrid = np.meshgrid(np.arange(np.shape(image1.array)[0])+image1.getXMin(), 
                                np.arange(np.shape(image1.array)[1])+image1.getYMin())
-    mx = np.mean(xgrid*image1.array)/np.mean(image1.array)
-    my = np.mean(ygrid*image1.array)/np.mean(image1.array)
-    mxx = np.mean(((xgrid-mx)**2)*image1.array)/np.mean(image1.array)
-    myy = np.mean(((ygrid-my)**2)*image1.array)/np.mean(image1.array)
-    mxy = np.mean((xgrid-mx)*(ygrid-my)*image1.array)/np.mean(image1.array)
+    mx = np.mean(xgrid*image1.array) / np.mean(image1.array)
+    my = np.mean(ygrid*image1.array) / np.mean(image1.array)
+    mxx = np.mean(((xgrid-mx)**2)*image1.array) / np.mean(image1.array)
+    myy = np.mean(((ygrid-my)**2)*image1.array) / np.mean(image1.array)
+    mxy = np.mean((xgrid-mx)*(ygrid-my)*image1.array) / np.mean(image1.array)
     print "    ", mx-image1.getXMin(), my-image1.getYMin(), mxx, myy, mxy
 
 def test_sbprofile_gaussian():
