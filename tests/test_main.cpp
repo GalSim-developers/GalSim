ebc16ca3b3620e9263bef113c75cfd17a7cfc0f7
Mike Jarvis <michael@jarvis.net>
2012-04-03 21:14:04 -0400

    Switched test_main to using single-header variant of Boost UTF (#23)

--- tests/test_main.cpp
+++ tests/test_main.cpp
@@ -1,5 +1,5 @@
 #define BOOST_TEST_DYN_LINK
 #define BOOST_TEST_MODULE Main
-#include <boost/test/unit_test.hpp>
+#include <boost/test/included/unit_test.hpp>
 
 //JAZ Nothing needs to go here - the test module definitions above create a main function.
