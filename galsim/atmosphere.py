10e603269fbe3b9c9d88108745b0f998b41628b2
Jörg Dietrich <astro@joergdietrich.com>
2012-04-04 00:56:30 -0400

    Create DoubleGaussian class for atmospheric PSF derived from GSAdd, which is a python interface to SBAdd.

--- galsim/atmosphere.py
+++ galsim/atmosphere.py
@@ -6,15 +6,31 @@ import galsim
 """\file atmosphere.py Simple atmospheric PSF generation routines
 """
 
-def gaussian(flux=1.0, size=1.0):
-    """Initialize a Gaussian PSF. In this case simply return an SBGaussian object with given flux 
-    and size.
+class GSAdd:
+    """Base class for defining the python interface to the SBAdd C++ class.
     """
-    return galsim.SBGaussian(flux, size)
+    def __init__(self, SBAdd):
+        self.SBAdd = SBAdd
 
-def moffat(beta, truncationFWHM=2.0, flux=1.0, re=1.0):
-    """Initialize a Moffat PSF. In this case simply return an SBMoffat object with exponent beta,
-    flux, and effective radius re. The Moffat profile is truncated at truncationFWHM times the 
-    FWHM.
+    def add(self, profile, scale=1.):
+        self.SBAdd.add(profile, scale)
+
+    def draw(self, dx=0., wmult=1):
+        if type(wmult) != int:
+            raise TypeError("Input wmult must be an int")
+        if type(dx) != float:
+            raise Warning("Input dx not a float, converting...")
+            dx = float(dx)
+        return self.SBAdd.draw(dx=dx, wmult=wmult)
+
+    # many more methods exposed to python not yet implemented
+    
+
+class DoubleGaussian(GSAdd):
+    """Double Gaussian, which is the sum of two SBProfile Gaussian profiles, using the GSAdd
+    interface to SBProfile's SBAdd.
     """
-    return galsim.SBMoffat(beta, truncationFWHM, flux, re)
+    def __init__(self, flux1=1., sigma1=1., flux2=1., sigma2=1.):
+        GSAdd.__init__(self, galsim.SBAdd(galsim.SBGaussian(flux1, sigma1)))
+        GSAdd.add(self, galsim.SBGaussian(flux2, sigma2))
+
