7715d992a02a6e4fe4544838490c2af0f0bb0867
Mike Jarvis <michael@jarvis.net>
2012-04-03 20:48:51 -0400

    Updated test_Image.cpp to use new method names in Image.h (#23)

--- tests/test_Image.cpp
+++ tests/test_Image.cpp
@@ -14,10 +14,10 @@ BOOST_AUTO_TEST_CASE( image_pixel_range_definition )
 	int nrow=100;
 	int ncol=150;
 	galsim::Image<int> img(nrow,ncol);
-    BOOST_CHECK(img.XMin()==1);
-    BOOST_CHECK(img.XMax()==nrow);
-    BOOST_CHECK(img.YMin()==1);
-    BOOST_CHECK(img.YMax()==ncol);
+    BOOST_CHECK(img.getXMin()==1);
+    BOOST_CHECK(img.getXMax()==nrow);
+    BOOST_CHECK(img.getYMin()==1);
+    BOOST_CHECK(img.getYMax()==ncol);
 }
 
 BOOST_AUTO_TEST_CASE( image_creation )
@@ -30,7 +30,7 @@ BOOST_AUTO_TEST_CASE( image_data )
 	int nrow=100;
 	int ncol=150;
 	galsim::Image<int> img(nrow,ncol);
-	BOOST_CHECK(img.data() != NULL);
+	BOOST_CHECK(img.getData() != NULL);
 }
 
 
