ac6acbee8d69c9ee16962454e12be6205d346054
Barnaby Rowe <barnaby.t.p.rowe@gmail.com>
2012-04-04 02:15:41 -0700

    Am getting puzzling SEGFAULTs using the wgalsim.Optics __init__ method followed by draw(), and yet not getting these constructing the SBPixel manually using copy paseted commands.  Have added a tmp script demonstrating this, would love ideas. (#34_optics_wrapper)

---
+++ tests/optics_segfault_demo_tmp.py
@@ -0,0 +1,28 @@
+import numpy as np
+import galsim
+
+lod = 0.2   # lamda / D = .2, defining whatever physical scale I'm pretending to adopt for this test
+
+# Then copy paste code from base.Optics()...
+ALIAS_THRESHOLD = 0.005
+
+maxk = 2. * np.pi / lod
+dx = np.pi / maxk  # Nyquist sample rate
+stepk = min(ALIAS_THRESHOLD * .5 * np.pi**3 / lod, np.pi / 5. / lod)
+
+npix = np.ceil(2. * maxk / stepk).astype(int)
+optimage = galsim.optics.psf_image(array_shape=(npix, npix), kmax=dx*maxk)
+l5 = galsim.Lanczos(5, True, 1.e-4)
+interpolant2d = galsim.InterpolantXY(l5)
+
+sbpixel_test = galsim.SBPixel(optimage, interpolant2d, dx=dx)
+image_test = sbpixel_test.draw()#.write('airy_sb_test.fits')
+
+
+#OK, so image_test works, now uncomment this and see if a segfault/bus error happens
+
+#opt_test = galsim.Optics(lod)
+#image_opt_test = opt_test.draw()
+
+
+
