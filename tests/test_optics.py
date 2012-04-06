f3b82d11b6be8c773b6695914c746889849d0652
Barnaby Rowe <barnaby.t.p.rowe@gmail.com>
2012-04-05 23:34:48 -0700

    added SBAiry Optics comparison test (#34_optics_wrapper)

--- tests/test_optics.py
+++ tests/test_optics.py
@@ -208,12 +208,27 @@ def test_Optics_flux():
     """Compare an unaberrated Optics() PSF flux to unity.
     """
     lod = 8.   # lambda / D: Don't choose unity in case symmetry hides something
-    D = 1. / lod
-    airy_test = galsim.Airy(D=D, obs=0., flux=1.)
     optics_test = galsim.Optics(lod)
-    airy_array = airy_test.draw(dx=1.).array
     optics_array = optics_test.draw(dx=1.).array 
     np.testing.assert_almost_equal(optics_array.sum(), 1., decimal_dft, 
                                    err_msg="Optics flux not nearly 1")
 
+def test_Optics_Airy():
+    """Compare the array view on an unaberrated Optics() PSF to that of an SBAiry.
+    """
+    lod = 8.   # lambda / D: Don't choose unity in case symmetry hides something
+    D = 1. / lod
+    nlook = 100 # size of array region at the centre of each image to compare
+    airy_test = galsim.Airy(D=D, obs=0., flux=1.)
+    optics_test = galsim.Optics(lod)
+    airy_array = airy_test.draw(dx=1.).array
+    airy_array_test = airy_array[airy_array.shape[0]/2 - nlook/2:airy_array.shape[0]/2 + nlook/2,   
+                                 airy_array.shape[1]/2 - nlook/2:airy_array.shape[1]/2 + nlook/2]
+    optics_array = optics_test.draw(dx=1.).array 
+    optics_array_test = optics_array[optics_array.shape[0]/2 - nlook/2:
+                                     optics_array.shape[0]/2 + nlook/2, 
+                                     optics_array.shape[1]/2 - nlook/2:
+                                     optics_array.shape[1]/2 + nlook/2]
+    np.testing.assert_array_almost_equal(optics_array_test, airy_array_test, decimal_dft, 
+                                         err_msg="Unaberrated Optics not quite equal to SBAiry")
 
