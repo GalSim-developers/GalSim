30e487fdc0abce55cef7ba4a2dbd6d7e695ad67a
Barnaby Rowe <barnaby.t.p.rowe@gmail.com>
2012-04-06 00:10:27 -0700

    moved dx down the arg list as I don't want users to think it is something they need to set often when initializing (#34_optics_wrapper)

--- galsim/base.py
+++ galsim/base.py
@@ -172,16 +172,14 @@ class Optics(GSObject):
 
     Initialization
     --------------
-    >>> optical_psf = galsim.Optics(lod, dx=None, defocus=0., astig1=0., astig2=0., coma1=0.,
-                                    coma2=0., spher=0., circular_pupil=True, interpolantxy=None,
+    >>> optical_psf = galsim.Optics(lod, defocus=0., astig1=0., astig2=0., coma1=0., coma2=0.,
+                                    spher=0., circular_pupil=True, interpolantxy=None, dx=1.,
                                     oversampling=2.)
 
     Initializes optical_psf as a galsim.Optics() instance.
 
     @param lod             lambda / D in the physical units adopted (user responsible for 
                            consistency).
-    @param dx              optional specifier for pixel scale of aberration lookup table images
-                           [default just samples PSF well]
     @param defocus         defocus in units of incident light wavelength.
     @param astig1          first component of astigmatism (like e1) in units of incident light
                            wavelength.
@@ -194,6 +192,8 @@ class Optics(GSObject):
     @param obs             add a central obstruction due to secondary mirror?
     @param interpolantxy   optional keyword for specifiying the interpolation scheme [default = 
                            galsim.InterpolantXY(galsim.Lanczos(5, True, 1.e-4))]
+    @param dx              optional specifier for pixel scale of aberration lookup table images
+                           [default just samples PSF well]
     @param oversampling    optional oversampling factor for the SBPixel table [default = 3.],
                            ignored if dx is set. Setting oversampling < 1 is silly.
     """
