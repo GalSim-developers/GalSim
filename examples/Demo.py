ebd05dfeab6216103bdc39b283a573de1c18b105
Barnaby Rowe <barnaby.t.p.rowe@gmail.com>
2012-04-06 11:58:29 -0700

    some edits on Mike's todos, added a check (and build) for the output directory, and some OCD whitespace to match LSST/PEP8 style. (#27)

--- examples/Demo.py
+++ examples/Demo.py
@@ -25,20 +25,20 @@ def Script1():
     """
 
     # Define the galaxy profile
-    gal = galsim.Gaussian(flux = 100, sigma = 2.)
+    gal = galsim.Gaussian(flux=100, sigma=2.)
 
     # Define the PSF profile
-    psf = galsim.Gaussian(flux = 1., sigma = 1.) # psf flux should always = 1
+    psf = galsim.Gaussian(flux=1., sigma=1.) # psf flux should always = 1
 
     # Final profile is the convolution of these
     # TODO: Are we ever going to convolve more than 2 things at a time?
     #       The syntax would be cleaner without the brackets [].
     #       Also, should remove the GS prefix.
-    final = galsim.GSConvolve([gal,psf])
+    final = galsim.GSConvolve([gal, psf])
 
     # Draw the image with a particular pixel scale
     # TODO: How do we specify the size of the image that gets created?
-    image = final.draw(dx = 0.2)
+    image = final.draw(dx=0.2)
 
     # Add some noise to the image
     # First we need to set up a random number generator:
@@ -48,29 +48,13 @@ def Script1():
     # TODO: Not sure if we want mean as an option to addGaussian.
     #       Furthermore, we probably want to have sigma be required so we don't 
     #       accidentally forget to specify it.
-    # TODO: I can't get this to work.  This seems to be the right signature 
-    #       from noise.py, but here is what I get from running it:
-    # Traceback (most recent call last):
-    #   File "./Demo.py", line 123, in <module>
-    #     main(sys.argv)
-    #   File "./Demo.py", line 113, in main
-    #     Script1()
-    #   File "./Demo.py", line 60, in Script1
-    #     addGaussian(image, rng, sigma=0.1)
-    #   NameError: global name 'addGaussian' is not defined
-
-    # Barney's note: the way it is currently set up is this:
     galsim.noise.addGaussian(image, rng, sigma=0.1)
-    # ... I actually like going to the .noise. submodule namespace here as it differentiates the
-    # Gaussian in question from a Gaussian profile...
 
     # Write the image to a file
+    if not os.path.isdir('output'):
+        os.mkdir('output')
     fileName = os.path.join('output','demo1.fits')
-    # TODO: It should be easier than this to specify that we want to overwrite
-    #       an existing file.
-    if os.path.exists(fileName):
-        os.remove(fileName)
-    image.write(fileName)
+    image.write(fileName, clobber=True)
 
 
 # Script 2: Sheared, exponential galaxy, Moffat PSF, Poisson noise
@@ -83,45 +67,50 @@ def Script2():
     """
 
     # Define the galaxy profile
-    gal = galsim.Exponential(flux = 1.e5, r0 = 2.7)
+    gal = galsim.Exponential(flux=1.e5, r0=2.7)
 
     # Shear the galaxy by some value:
     # TODO: Double check.  These are called e1,e2 in the signature.
     #       Are they distortions or shears?
     g1 = 0.1
     g2 = 0.2
-    gal.applyShear(g1,g2)
+    gal.applyShear(g1, g2)
 
     # Define the PSF profile
     # TODO: re is not very clear for the half-light radius.
     #       Should switch this to something more verbose, like half_light_radius
-    psf = galsim.Moffat(beta = 5, flux = 1., re = 1.)
+    psf = galsim.Moffat(beta=5, flux=1., re=1.)
 
     # Final profile is the convolution of these
-    final = galsim.GSConvolve([gal,psf])
+    final = galsim.GSConvolve([gal, psf])
 
     # Draw the image with a particular pixel scale
     # TODO: Again, how do we specify the size of the image that gets created?
-    #       This image is much larger than the one for script 1.
-    image = final.draw(dx = 0.2)
+    #       This image is much larger than the one for script 1 (Barney: I'm guessing this is
+    #       the default stepK ends up being a lot smaller due to the wings of the exp).
+    image = final.draw(dx=0.2)
 
     # Add some noise to the image
 
     # For Poisson noise, we want to have a sky level as well.
     skyLevel = 1.e4
-    # TODO: How do we add a constant sky level to the image?
+    # Barney: here's how we add a constant sky level to the image:
+    sky_image = galsim.ImageF(bounds=image.getBounds(), initValue=skyLevel)
+    image += sky_image
 
     # This time use a particular seed, so it the image is deterministic
     rng = galsim.UniformDeviate(1534225)
     # Use this to add Poisson noise
-    # TODO: This also doesn't work.  Same problem as above for addGaussian.
-    #addPoisson(image, rng, gain = 1.)
+    galsim.noise.addPoisson(image, rng, gain=1.)
+
+    # Barney: here's how we subtract the background again...
+    image -= sky_image
 
     # Write the image to a file
-    fileName = os.path.join('output','demo2.fits')
-    if os.path.exists(fileName):
-        os.remove(fileName)
-    image.write(fileName)
+    if not os.path.isdir('output'):
+        os.mkdir('output')
+    fileName = os.path.join('output', 'demo2.fits')
+    image.write(fileName, clobber=True)
 
     
 def main(argv):
@@ -140,8 +129,6 @@ def main(argv):
     if scriptNum == 0 or scriptNum == 2:
         Script2()
         
-
-
 
 if __name__ == "__main__":
     main(sys.argv)
