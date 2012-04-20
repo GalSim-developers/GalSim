39422260278f3c329bb16e7b410efc19a96412ef
Mike Jarvis <michael@jarvis.net>
2012-04-20 17:22:05 -0400

    Added guard around pragma GCC line for icpc

--- tests/test_main.cpp
+++ tests/test_main.cpp
@@ -2,7 +2,9 @@
 #define BOOST_TEST_MODULE Main
 
 // The boost unit tests have some unused variables, so suppress the warnings about that.
+#ifdef __GNUC__
 #pragma GCC diagnostic ignored "-Wunused-variable"
+#endif
 
 #include <boost/test/included/unit_test.hpp>
 
