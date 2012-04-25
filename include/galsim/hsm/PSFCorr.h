2e510590d4fdbd2a890d7166ece3203373cf8a04
Rachel Mandelbaum <rmandelb@astro.princeton.edu>
2012-04-25 14:04:02 -0400

    changing form of HSMShapeData constructor, moving to .h (#38)

--- include/galsim/hsm/PSFCorr.h
+++ include/galsim/hsm/PSFCorr.h
@@ -116,8 +116,9 @@ namespace hsm {
         float resolution_factor;
 
         /// @brief Constructor, setting defaults
-        HSMShapeData();
-    }
+    HSMShapeData() : observed_shape(galsim::Shear()), correct_shape(galsim::Shear()), correction_method("None"), moment_status(-1), moment_sigma(-1.), moment_n_iter(0), correction_status(-1), resolution_factor(-1.)
+        {}
+    };
 
     /* functions that the user will want to call from outside */
 
