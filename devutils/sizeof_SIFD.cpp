3d0199e1424f6eacead0bb22c31e150badc8bf9b
Barnaby Rowe <barnaby.t.p.rowe@gmail.com>
2012-05-02 12:06:51 -0700

    added super short C++ function for printing short, int, float and double sizes to stdout

---
+++ devutils/sizeof_SIFD.cpp
@@ -0,0 +1,11 @@
+/// @file sizeof_SIFD.cpp @brief Prints short, int, float and double sizes to stdout. 
+#include <cstdio>
+
+int main(){
+
+    printf("Sizeof short (bits) = %zu\n", sizeof(short) * 8);
+    printf("Sizeof int (bits) = %zu\n", sizeof(int) * 8);
+    printf("Sizeof float (bits) = %zu\n", sizeof(float) * 8);
+    printf("Sizeof double (bits) = %zu\n", sizeof(double) * 8);
+
+}
