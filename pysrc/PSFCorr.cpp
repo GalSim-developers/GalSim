710b7d1a5ea877a8d9ea7763f4b01e90cc1f2c5c
Rachel Mandelbaum <rmandelb@astro.princeton.edu>
2012-05-01 20:27:07 -0700

    fixed python wrapping for HSMShapeData to make data accessible via python (#38)

--- pysrc/PSFCorr.cpp
+++ pysrc/PSFCorr.cpp
@@ -11,11 +11,24 @@ struct PyHSMShapeData {
 
     static void wrap() {
         static char const * doc = 
-            "HSMShapeData object represents information from the HSM moments and PSF-correction functions.\n" 
-            "See C++ docs for more detail.\n"
+            "HSMShapeData object represents information from the HSM moments and PSF-correction\n"
+            "functions.  See C++ docs for more detail.\n"
             ;
 
-        bp::class_<HSMShapeData>("HSMShapeData", doc, bp::init<HSMShapeData &>());
+        bp::class_<HSMShapeData>("HSMShapeData", doc, bp::init<>())
+            .def_readwrite("image_bounds", &HSMShapeData::image_bounds)
+            .def_readwrite("moments_status", &HSMShapeData::moments_status)
+            .def_readwrite("observed_shape", &HSMShapeData::observed_shape)
+            .def_readwrite("moments_sigma", &HSMShapeData::moments_sigma)
+            .def_readwrite("moments_amp", &HSMShapeData::moments_amp)
+            .def_readwrite("moments_centroid", &HSMShapeData::moments_centroid)
+            .def_readwrite("moments_n_iter", &HSMShapeData::moments_n_iter)
+            .def_readwrite("correction_status", &HSMShapeData::correction_status)
+            .def_readwrite("corrected_shape", &HSMShapeData::corrected_shape)
+            .def_readwrite("corrected_shape_err", &HSMShapeData::corrected_shape_err)
+            .def_readwrite("correction_method", &HSMShapeData::correction_method)
+            .def_readwrite("resolution_factor", &HSMShapeData::resolution_factor)
+            ;
     }
 };
 
