4f51123e4335e6c689f27ee28980b85965b74b2b
Mike Jarvis <michael@jarvis.net>
2012-05-03 09:17:56 -0400

    Added size of long to get_sysinfo

--- examples/sizeof_SIFD.cpp
+++ examples/sizeof_SIFD.cpp
@@ -5,6 +5,7 @@ int main(){
 
     printf("Sizeof short (bits) = %zu\n", sizeof(short) * 8);
     printf("Sizeof int (bits) = %zu\n", sizeof(int) * 8);
+    printf("Sizeof long (bits) = %zu\n", sizeof(long) * 8);
     printf("Sizeof float (bits) = %zu\n", sizeof(float) * 8);
     printf("Sizeof double (bits) = %zu\n", sizeof(double) * 8);
 
