3d78ebaf19a4e69252575b6b1ef4665a573bfe72
Rachel Mandelbaum <rmandelb@astro.princeton.edu>
2012-05-03 20:53:42 -0700

    fixed bug = typo (#38)

--- src/hsm/PSFCorr.cpp
+++ src/hsm/PSFCorr.cpp
@@ -377,7 +377,7 @@ namespace hsm {
             // repackage outputs from find_ellipmom_2 to the output HSMShapeData struct
             results.moments_amp = 2.0*amp;
             results.moments_sigma = std::pow(m_xx*m_yy-m_xy*m_xy, 0.25);
-            results.observed_shape.setE1E2((m_xx-m_yy)/(m_xx+m_yy), 2.*m_xy/(m_xx+m_xy));
+            results.observed_shape.setE1E2((m_xx-m_yy)/(m_xx+m_yy), 2.*m_xy/(m_xx+m_yy));
             results.moments_status = 0;
         }
         catch (char *err_msg) {
