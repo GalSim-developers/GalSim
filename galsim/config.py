5aab03dacd84b17a32905192afbd98fec0fe11f5
Barnaby Rowe <browe@star.ucl.ac.uk>
2012-05-07 17:18:37 -0700

    improved control flow in config.py (#101)

--- galsim/config.py
+++ galsim/config.py
@@ -3,8 +3,8 @@ import os
 import logging
 
 class AttributeDict(object):
-    """@brief Dictionary class that allows for easy initialization and refs to key values via
-    attributes.
+    """Dictionary class that allows for easy initialization and refs to key values via attributes.
+
 
     NOTE: Modified a little from Jim's bot.git AttributeDict class  (Jim, please review!) so that...
 
@@ -49,19 +49,25 @@ class AttributeDict(object):
     __str__ = __repr__
 
 
-def load(config_file=None):
-    """@brief Function for loading in configuration settings from the specified config file, and
+def load(config_file=None, include_default=True):
+    """Function for loading in configuration settings from the specified config file, and
     using this to augment/update values in GalSim/config/galsim_default.
     """
     config = AttributeDict()
-    if (config_file != None) and (not os.path.exists(default_config_file)):
+    if (config_file != None) and (not os.path.exists(config_file)):
         raise IOError("User input config file "+str(config_file)+" not found")
     config.user_config_file = config_file
-    thisdir, modfile = os.path.split(__file__)
-    default_config_file = os.path.join(thisdir, "..", "config", "galsim_default")
-    if not os.path.exists(default_config_file):
-        raise IOError("Default config file galsim_default not found.")
-    files = [default_config_file]
+    
+    if include_default:
+        thisdir, modfile = os.path.split(__file__)
+        default_config_file = os.path.join(thisdir, "..", "config", "galsim_default")
+        if not os.path.exists(default_config_file):
+            raise IOError("Default config file galsim_default not found.")
+        files = [default_config_file]
+    elif config_file == None:
+        raise IOError("Neither default nor user input config file specified.")    
+    else:
+        files =[]
     if config_file != None:
         file.append(config_file)
     for f in reversed(files):
