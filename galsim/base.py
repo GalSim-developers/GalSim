be016631d7eae32eb8e76458285e4d28d44fa16d
Barnaby Rowe <barnaby.t.p.rowe@gmail.com>
2012-05-07 20:04:20 -0700

    moved Attribute dict to base module (want to use it for catalogue handling too), added default Pixel flux = 1.

--- galsim/base.py
+++ galsim/base.py
@@ -1,3 +1,4 @@
+import collections
 import numpy as np
 import galsim
 
@@ -347,4 +348,52 @@ class Convolve(GSObject):
 
     def add(self, obj):
         self.SBProfile.add(obj.SBProfile)
+
+
+class AttributeDict(object):
+    """Dictionary class that allows for easy initialization and refs to key values via attributes.
+
+    NOTE: Modified a little from Jim's bot.git AttributeDict class  (Jim, please review!) so that...
+
+    ...Tab completion now works in ipython (it didn't with the bot.git version on my build) since
+    attributes are actually added to __dict__.
+    
+    HOWEVER this means I have redefined the __dict__ attribute to be a collections.defaultdict()
+    so that Jim's previous default attrbiute behaviour is also replicated.
+
+    I prefer this, as a newbie who uses ipython and the tab completion function to aid development,
+    but does it potentially break something down the line or add undesirable behaviour? (I guessed
+    not, since collections.defaultdict objects have all the usual dict() methods, but I cannot be
+    sure.)
+    """
+    def __init__(self):
+        object.__setattr__(self, "__dict__", collections.defaultdict(AttributeDict))
+
+    def __getattr__(self, name):
+        return self.__dict__[name]
+
+    def __setattr__(self, name, value):
+        self.__dict__[name] = value
+
+    def merge(self, other):
+        self.__dict__.update(other.__dict__)
+
+    def _write(self, output, prefix=""):
+        for k, v in self.__dict__.iteritems():
+            if isinstance(v, AttributeDict):
+                v._write(output, prefix="{0}{1}.".format(prefix, k))
+            else:
+                output.append("{0}{1} = {2}".format(prefix, k, repr(v)))
+
+    def __nonzero__(self):
+        return not not self.__dict__
+
+    def __repr__(self):
+        output = []
+        self._write(output, "")
+        return "\n".join(output)
+
+    __str__ = __repr__
+
+
 
--- galsim/config.py
+++ galsim/config.py
@@ -1,59 +1,11 @@
-import collections
 import os
-import logging
-
-class AttributeDict(object):
-    """Dictionary class that allows for easy initialization and refs to key values via attributes.
-
-
-    NOTE: Modified a little from Jim's bot.git AttributeDict class  (Jim, please review!) so that...
-
-    ...Tab completion now works in ipython (it didn't with the bot.git version on my build) since
-    attributes are actually added to __dict__.
-    
-    HOWEVER this means I have redefined the __dict__ attribute to be a collections.defaultdict()
-    so that Jim's previous default attrbiute behaviour is also replicated.
-
-    I prefer this, as a newbie who uses ipython and the tab completion function to aid development,
-    but does it potentially break something down the line or add undesirable behaviour? (I guessed
-    not, since collections.defaultdict objects have all the usual dict() methods, but I cannot be
-    sure.)
-    """
-    def __init__(self):
-        object.__setattr__(self, "__dict__", collections.defaultdict(AttributeDict))
-
-    def __getattr__(self, name):
-        return self.__dict__[name]
-
-    def __setattr__(self, name, value):
-        self.__dict__[name] = value
-
-    def merge(self, other):
-        self.__dict__.update(other.__dict__)
-
-    def _write(self, output, prefix=""):
-        for k, v in self.__dict__.iteritems():
-            if isinstance(v, AttributeDict):
-                v._write(output, prefix="{0}{1}.".format(prefix, k))
-            else:
-                output.append("{0}{1} = {2}".format(prefix, k, repr(v)))
-
-    def __nonzero__(self):
-        return not not self.__dict__
-
-    def __repr__(self):
-        output = []
-        self._write(output, "")
-        return "\n".join(output)
-
-    __str__ = __repr__
-
+import galsim
 
 def load(config_file=None, include_default=True):
     """Function for loading in configuration settings from the specified config file, and
     using this to augment/update values in GalSim/config/galsim_default.
     """
-    config = AttributeDict()
+    config = galsim.AttributeDict()
     if (config_file != None) and (not os.path.exists(config_file)):
         raise IOError("User input config file "+str(config_file)+" not found")
     config.user_config_file = config_file
