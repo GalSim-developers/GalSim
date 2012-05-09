311b5923f38d2c2494f169e8cf72911743d56af8
Mike Jarvis <michael@jarvis.net>
2012-05-09 12:02:31 -0400

    Fix manner of setting default yw for Pixel equal to xw. (#103)

--- galsim/base.py
+++ galsim/base.py
@@ -239,7 +239,9 @@ class Airy(GSObject):
 class Pixel(GSObject):
     """GalSim Pixel, which has an SBBox in the SBProfile attribute.
     """
-    def __init__(self, xw=1., yw=xw, flux=1.):
+    def __init__(self, xw=1., yw=None, flux=1.):
+        if yw is None:
+            yw = xw
         GSObject.__init__(self, galsim.SBBox(xw=xw, yw=yw, flux=flux))
     # Ditto!
 
