1f7e47209d5c94c7b1167d306d9851247179ea80
Rachel Mandelbaum <rmandelb@astro.princeton.edu>
2012-05-09 19:11:46 -0400

    made checking for radii in wrappers for SBBox, SBDeVauc (#65)

--- pysrc/SBProfile.cpp
+++ pysrc/SBProfile.cpp
@@ -349,13 +349,27 @@ struct PySBAiry {
 };
 
 struct PySBBox {
+
+    static SBBox * construct(
+                             const bp::object & xw,
+                             const bp::object & yw,
+                             double flux
+    ) {
+        if (xw.ptr() == Py_None || yw.ptr() == Py_None) {
+            PyErr_SetString(PyExc_TypeError, "SBBox requires x and y width parameters");
+            bp::throw_error_already_set();
+        }
+        return new SBBox(bp::extract<double>(xw), bp::extract<double>(yw), flux);
+    }
+
     static void wrap() {
-        bp::class_<SBBox,bp::bases<SBProfile>,boost::noncopyable>(
-            "SBBox",
-            bp::init<double,double,double>(
-                (bp::arg("xw")=1., bp::arg("yw")=0., bp::arg("flux")=1.)
-            )
-        );
+        bp::class_<SBBox,bp::bases<SBProfile>,boost::noncopyable>("SBBox", bp::no_init)
+            .def("__init__",
+                 bp::make_constructor(
+                                      &construct, bp::default_call_policies(),
+                                      (bp::arg("xw")=bp::object(), bp::arg("yw")=bp::object(), bp::arg("flux")=1.)
+                                      )
+                 );
     }
 };
 
@@ -399,11 +413,26 @@ struct PySBMoffat {
 };
 
 struct PySBDeVaucouleurs {
+    static SBDeVaucouleurs * construct(
+                                       double flux, const bp::object & half_light_radius
+                                       ) {
+        if (half_light_radius.ptr() == Py_None) {
+            PyErr_SetString(PyExc_TypeError, "No radius parameter given");
+            bp::throw_error_already_set();
+        }
+        return new SBDeVaucouleurs(flux, bp::extract<double>(half_light_radius));
+    }
+
     static void wrap() {
         bp::class_<SBDeVaucouleurs,bp::bases<SBProfile>,boost::noncopyable>(
-            "SBDeVaucouleurs",
-            bp::init<double,double>((bp::arg("flux")=1., bp::arg("half_light_radius")=1.))
-        );
+                                                                            "SBDeVaucouleurs",bp::no_init)
+            .def("__init__",
+                 bp::make_constructor(
+                                      &construct, bp::default_call_policies(),
+                                      (bp::arg("flux")=1., bp::arg("half_light_radius")=bp::object())
+                                      )
+                 )
+            ;
     }
 };
 
