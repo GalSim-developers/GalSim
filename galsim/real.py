ee56851c800cd9037c23bcb1e61210a0b4af561a
Rachel Mandelbaum <rmandelb@astro.princeton.edu>
2012-05-10 17:20:07 -0400

    began outlining SimReal function (#104)

--- galsim/real.py
+++ galsim/real.py
@@ -5,8 +5,8 @@ from . import _galsim
 The RealGalaxyCatalog class can be used to represent a catalog containing information about real
 galaxies. 
 
-There will also be a function that can manipulate a RealGalaxy to simulate some data with a given
-shear and target PSF, a la SHERA - tentative name SimReal, to be fleshed out later
+There is also a function called SimReal that can manipulate a RealGalaxy to simulate some data with
+a given shear, target PSF, and pixel scale (similar in purpose to SHERA).
 """
 
 class RealGalaxyCatalog:
@@ -35,3 +35,16 @@ class RealGalaxyCatalog:
         # note: am assuming that pyfits takes care of error handling, e.g., if the file does not
         # exist, there's no field with that name, etc.
         # also note: will be adding bits of information, like noise properties and galaxy fit params
+
+def SimReal(real_galaxy, target_PSF, target_pixel_scale, g1 = 0.0, g2 = 0.0, rand_rotate = True,
+            target_flux = 1.0):
+    # put some documentation here and above for RealGalaxyCatalog
+    # do some checking of arguments and so on
+    # check SHERA code for some more keywords that might be useful
+
+    # rotate
+    # deconvolve
+    # shear
+    # convolve, resample
+    # return simulated image
+
