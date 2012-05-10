d35af34c200094bd7ec473d20ebad0f626cd4760
Rachel Mandelbaum <rmandelb@astro.princeton.edu>
2012-05-09 22:44:38 -0400

    First stab at RealGalaxy class (#104)

--- galsim/base.py
+++ galsim/base.py
@@ -248,7 +248,7 @@ class OpticalPSF(GSObject):
                                         coma2=0., spher=0., circular_pupil=True, interpolantxy=None,
                                         dx=1., oversampling=2., pad_factor=2)
 
-    Initializes optical_psf as a galsim.Optics() instance.
+    Initializes optical_psf as a galsim.optics() instance.
 
     @param lod             lambda / D in the physical units adopted (user responsible for 
                            consistency).
@@ -262,7 +262,7 @@ class OpticalPSF(GSObject):
     @param spher           spherical aberration in units of incident light wavelength.
     @param circular_pupil  adopt a circular pupil?
     @param obs             add a central obstruction due to secondary mirror?
-    @param interpolantxy   optional keyword for specifiying the interpolation scheme [default = 
+    @param interpolantxy   optional keyword for specifying the interpolation scheme [default =
                            galsim.InterpolantXY(galsim.Lanczos(5, True, 1.e-4))].
     @param oversampling    optional oversampling factor for the SBInterpolatedImage table 
                            [default = 2.], setting oversampling < 1 will produce aliasing in the 
@@ -296,6 +296,92 @@ class OpticalPSF(GSObject):
             self.Interpolant2D = galsim.InterpolantXY(l5)
         GSObject.__init__(self, galsim.SBInterpolatedImage(optimage, self.Interpolant2D, dx=dx))
 
+
+class RealGalaxy(GSObject):
+    """@brief Class describing real galaxies from some training dataset.
+
+    This class uses a catalog describing galaxies in some training data to read in data about
+    realistic galaxies that can be used for simulations based on those galaxies.  Also included in
+    the class is additional information that might be needed to make or interpret the simulations,
+    e.g., the noise properties of the training data.
+
+    Initialization
+    --------------
+    >>> real_galaxy = galsim.RealGalaxy(real_galaxy_catalog, index = None, ID = None, ID_string =
+                                        None, random = False, interpolantxy = None)
+
+    Initializes real_galaxy with two SBInterpolatedImage objects (one for the galaxy and one for the
+    PSF). Note that there are multiple keywords for choosing a galaxy; exactly one must be set.  In
+    future we may add more such options, e.g., to choose at random but accounting for the
+    non-constant weight factors (probabilities for objects to make it into the training sample).
+
+    @param real_galaxy_catalog  A RealGalaxyCatalog object with basic information about where to
+                                find the data, etc.
+    @param index                Index of the desired galaxy in the catalog.
+    @param ID                   Object ID for the desired galaxy in the catalog.
+    @param ID_string            ID string for the desired galaxy in the catalog.
+    @param random               If true, then just select a completely random galaxy from the
+                                catalog.
+    @param interpolantxy        optional keyword for specifying the interpolation scheme [default = 
+                                galsim.InterpolantXY(galsim.Lanczos(5, True, 1.e-4))].
+    """
+    def __init__(self, real_galaxy_catalog, index = None, ID = None, ID_string = None, random =
+                                False, interpolantxy = None):
+
+        # Code block below will be for galaxy selection;
+        # implement exactly one for now, then gradually others.  All of them need to return an index
+        # within the real_galaxy_catalog.
+        use_index = -1
+        if index != None:
+            use_index = index
+        elif ID != None:
+            raise NotImplementedError('Selecting galaxy based on its ID not implemented')
+        elif ID_string != None:
+            raise NotImplementedError('Selecting galaxy based on its ID string not implemented')
+        elif random == True:
+            raise NotImplementedError('Selecting a random galaxy not implemented')
+        else:
+            raise RuntimeError('No method specified for selecting a galaxy!')
+
+        # read in the galaxy, PSF images
+        hdu_list = pyfits.open(real_galaxy_catalog.gal_filename[use_index])
+        n_hdu = len(hdu_list)
+        ## note, we assume that the file containing the image has n_hdu HDUs containing
+        ## images in it
+        if real_galaxy_catalog.gal_hdu[use_index] > n_hdu:
+            raise RuntimeError('No HDU corresponding to the one that should contain this galaxy!')
+        use_hdu_list = pyfits.HDUList()
+        use_hdu_list.append(hdu_list[real_galaxy_catalog.gal_hdu[use_index]])
+        gal_image = galsim.fits.read(use_hdu_list)
+
+        if (real_galaxy_catalog.gal_filename[use_index] !=real_galaxy_catalog.PSF_filename[use_index]):
+            hdu_list = pyfits.open(real_galaxy_catalog.PSF_filename[use_index])
+            n_hdu = len(hdu_list)
+        if real_galaxy_catalog.PSF_hdu[use_index] > n_hdu:
+            raise RuntimeError('No HDU corresponding to the one that should contain this PSF!')
+        use_hdu_list = pyfits.HDUList()
+        use_hdu_list.append(hdu_list[real_galaxy_catalog.PSF_hdu[use_index]])
+        PSF_image = galsim.fits.read(use_hdu_list)
+
+        # choose proper interpolant
+        if interpolantxy == None:
+            l5 = galsim.Lanczos(5, True, 1.e-4) # Conserve flux=True and 1.e-4 copied from Shera.py!
+            self.Interpolant2D = galsim.InterpolantXY(l5)
+
+        # read in data about galaxy from FITS binary table; store as members of RealGalaxy
+
+        # save any other relevant information
+        # self.catalog = ?? (need some way to know which catalog it is from??)
+        self.index = use_index
+        self.pixel_scale = real_galaxy_catalog.pixel_scale[use_index]
+        self.i_mag = real_galaxy_catalog.i_mag[use_index]
+        self.weight = real_galaxy_catalog.weight[use_index]
+        # note: will be adding more parameters here about noise properties etc., but let's be basic
+        # for now
+
+        self.PSF = galsim.SBInterpolatedImage(PSF_image, self.Interpolant2D, dx=self.pixel_scale)
+        GSObject.__init__(self, galsim.SBInterpolatedImage(gal_image, self.Interpolant2D,
+                                                           dx=self.pixel_scale))
 
 class Add(GSObject):
     """Base class for defining the python interface to the SBAdd C++ class.
