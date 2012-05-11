016dd5fbdae8dd7da075506e0d2885faa2dc88b3
Rachel Mandelbaum <rmandelb@astro.princeton.edu>
2012-05-10 22:50:36 -0400

    added pixel response into target PSF (#104)

--- examples/DemoReal.py
+++ examples/DemoReal.py
@@ -36,18 +36,21 @@ print 'Made real galaxy from catalog index ',real_galaxy.index
 # make a target PSF object
 good_psf = galsim.Moffat(psf_beta, fwhm = good_psf_fwhm)
 bad_psf = galsim.Moffat(psf_beta, fwhm = bad_psf_fwhm)
+pixel = galsim.Pixel(xw = pixel_scale, yw = pixel_scale)
+good_epsf = galsim.Convolve([good_psf, pixel])
+bad_epsf = galsim.Convolve([bad_psf, pixel])
 
 # simulate some nice ground-based data, e.g., Subaru/CFHT with good seeing; with and without shear
 print "Simulating unsheared galaxy in good seeing..."
-sim_image_good_noshear = galsim.simReal(real_galaxy, good_psf, pixel_scale, rand_rotate = False)
+sim_image_good_noshear = galsim.simReal(real_galaxy, good_epsf, pixel_scale, rand_rotate = False)
 print "Simulating sheared galaxy in good seeing..."
-sim_image_good_shear = galsim.simReal(real_galaxy, good_psf, pixel_scale, g1 = g1, g2 = g2, rand_rotate = False)
+sim_image_good_shear = galsim.simReal(real_galaxy, good_epsf, pixel_scale, g1 = g1, g2 = g2, rand_rotate = False)
 
 # simulate some crappy ground-based data, e.g., a bad night at SDSS; with and without shear
 print "Simulating unsheared galaxy in bad seeing..."
-sim_image_bad_noshear = galsim.simReal(real_galaxy, bad_psf, pixel_scale, rand_rotate = False)
+sim_image_bad_noshear = galsim.simReal(real_galaxy, bad_epsf, pixel_scale, rand_rotate = False)
 print "Simulating sheared galaxy in bad seeing..."
-sim_image_bad_shear = galsim.simReal(real_galaxy, bad_psf, pixel_scale, g1 = g1, g2 = g2, rand_rotate = False)
+sim_image_bad_shear = galsim.simReal(real_galaxy, bad_epsf, pixel_scale, g1 = g1, g2 = g2, rand_rotate = False)
 
 # write to files: original galaxy, original PSF, 2 target PSFs, 4 simulated images
 # note: will differ each time it is run, because we chose a random image
@@ -57,11 +60,11 @@ orig_gal_img.write(os.path.join(image_dir, 'demoreal.orig_gal.fits'), clobber = 
 orig_psf_img = real_galaxy.PSF.draw(dx = real_galaxy.pixel_scale)
 orig_psf_img.write(os.path.join(image_dir, 'demoreal.orig_PSF.fits'), clobber = True)
 
-good_psf_img = good_psf.draw(dx = pixel_scale)
-good_psf_img.write(os.path.join(image_dir, 'demoreal.good_target_PSF.fits'), clobber = True)
+good_epsf_img = good_epsf.draw(dx = pixel_scale)
+good_epsf_img.write(os.path.join(image_dir, 'demoreal.good_target_PSF.fits'), clobber = True)
 
-bad_psf_img = bad_psf.draw(dx = pixel_scale)
-bad_psf_img.write(os.path.join(image_dir, 'demoreal.bad_target_PSF.fits'), clobber = True)
+bad_epsf_img = bad_epsf.draw(dx = pixel_scale)
+bad_epsf_img.write(os.path.join(image_dir, 'demoreal.bad_target_PSF.fits'), clobber = True)
 
 sim_image_good_noshear.write(os.path.join(image_dir, 'demoreal.good_simulated_image.noshear.fits'), clobber = True)
 sim_image_good_shear.write(os.path.join(image_dir, 'demoreal.good_simulated_image.shear.fits'), clobber = True)
