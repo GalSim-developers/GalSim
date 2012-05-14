6715473c8efef1719c6c07e034f58be628a0bf98
Rachel Mandelbaum <rmandelb@astro.princeton.edu>
2012-05-13 20:24:44 -0400

    minor rearrangement and simplification in MeasMoments.py calculations

--- examples/MeasMoments.py
+++ examples/MeasMoments.py
@@ -39,14 +39,15 @@ image = galsim.fits.read(image_file)
 if sky_level > 0.:
     image -= sky_level
 result = galsim.FindAdaptiveMom(image, guess_sig = guess_sigma)
-aval = (1.0 + result.observed_shape.getE1()) / (1.0 - result.observed_shape.getE1())
-bval = np.sqrt(aval - (0.5*(1.0+aval)*result.observed_shape.getE2())**2)
-mxx = aval * (result.moments_sigma**2) / bval
-myy = (result.moments_sigma**2) / bval
-mxy = 0.5 * result.observed_shape.getE2()*(mxx + myy)
+e1_val = result.observed_shape.getE1()
+e2_val = result.observed_shape.getE2()
+a_val = (1.0 + e1_val) / (1.0 - e1_val)
+b_val = np.sqrt(a_val - (0.5*(1.0+a_val)*e2_val)**2)
+mxx = a_val * (result.moments_sigma**2) / b_val
+myy = (result.moments_sigma**2) / b_val
+mxy = 0.5 * e2_val * (mxx + myy)
 
 print '%d   %12.6f   %12.6f   %12.6f   %12.6f   %12.6f   %03d    %12.6f   %12.6f %12.6f' % \
-        (result.moments_status, mxx, myy, mxy,
-         result.observed_shape.getE1(), result.observed_shape.getE2(), result.moments_n_iter,
+        (result.moments_status, mxx, myy, mxy, e1_val, e2_val, result.moments_n_iter,
          result.moments_amp, result.moments_centroid.x-result.image_bounds.getXMin(),
          result.moments_centroid.y-result.image_bounds.getYMin())
