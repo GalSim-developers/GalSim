f24490e415788dcba4e3ad029ad393e789c53ef8
Rachel Mandelbaum <rmandelb@astro.princeton.edu>
2012-05-15 11:21:01 -0400

    updated double Gaussian radius treatment per #152 (#104)

--- galsim/atmosphere.py
+++ galsim/atmosphere.py
@@ -10,9 +10,19 @@ import galsim
 class DoubleGaussian(galsim.Add):
     """Double Gaussian, which is the sum of two SBProfile Gaussian profiles
     """
-    def __init__(self, flux1=1., sigma1=1., flux2=1., sigma2=1.):
+    def __init__(self, flux1=1., flux2=1., sigma1=None, sigma2=None, fwhm1=None, fwhm2=None):
         sblist = []
-        sblist.append(galsim.Gaussian(flux1, sigma=sigma1))
-        sblist.append(galsim.Gaussian(flux2, sigma=sigma2))
+        if fwhm1!=None and sigma1!=None:
+            raise RuntimeError("Both FWHM and sigma specified for first Gaussian!")
+        if fwhm2!=None and sigma2!=None:
+            raise RuntimeError("Both FWHM and sigma specified for first Gaussian!")
+        if fwhm1==None:
+            sblist.append(galsim.Gaussian(flux1, sigma=sigma1))
+        else:
+            sblist.append(galsim.Gaussian(flux1, fwhm=fwhm1))
+        if fwhm2==None:
+            sblist.append(galsim.Gaussian(flux2, sigma=sigma2))
+        else:
+            sblist.append(galsim.Gaussian(flux2, fwhm=fwhm2))
         galsim.Add.__init__(self, sblist)
 
