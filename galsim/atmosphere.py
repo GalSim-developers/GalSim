71018b22ab7bbaf4ae6bc2fe2f8de64b3cf116da
Rachel Mandelbaum <rmandelb@astro.princeton.edu>
2012-05-17 14:20:39 -0400

    simplified DoubleGaussian to rely on C++-level arg checks (#104)

--- galsim/atmosphere.py
+++ galsim/atmosphere.py
@@ -12,17 +12,9 @@ class DoubleGaussian(galsim.Add):
     """
     def __init__(self, flux1=1., flux2=1., sigma1=None, sigma2=None, fwhm1=None, fwhm2=None):
         sblist = []
-        if fwhm1!=None and sigma1!=None:
-            raise RuntimeError("Both FWHM and sigma specified for first Gaussian!")
-        if fwhm2!=None and sigma2!=None:
-            raise RuntimeError("Both FWHM and sigma specified for first Gaussian!")
-        if fwhm1==None:
-            sblist.append(galsim.Gaussian(flux1, sigma=sigma1))
-        else:
-            sblist.append(galsim.Gaussian(flux1, fwhm=fwhm1))
-        if fwhm2==None:
-            sblist.append(galsim.Gaussian(flux2, sigma=sigma2))
-        else:
-            sblist.append(galsim.Gaussian(flux2, fwhm=fwhm2))
+        # Note: we do not have to check for improper args (0 or 2 radii specified) because this is
+        # done in the C++
+        sblist.append(galsim.Gaussian(flux1, sigma=sigma1, fwhm=fwhm1))
+        sblist.append(galsim.Gaussian(flux2, sigma=sigma2, fwhm=fwhm2))
         galsim.Add.__init__(self, sblist)
 
