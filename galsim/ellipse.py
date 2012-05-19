bea9b876f1fc7d925730bea71e9bc26036c7264c
Barnaby Rowe <barnaby.t.p.rowe@gmail.com>
2012-05-18 17:13:21 -0700

    added trial versions of shear and ellipse repr and str methods, noticed a couple of things... (#129)

---
+++ galsim/ellipse.py
@@ -0,0 +1,20 @@
+"""@brief A few adjustments to the Ellipse class at the Python layer.
+"""
+
+from .import _galsim
+
+def Ellipse_repr(self):
+    shear = self.getS()  # extract the e1 and e2 from the Shear instance
+    x0 = self.getX0()    # extract the x0 and y0 from a Position instance
+    return (self.__class__.__name__+"(e1="+str(shear.getE1())+", e2="+str(shear.getE2())+
+            ", m="+str(self.getMu())+", x="+str(x0.x)+", y="+str(x0.y)+")")
+
+def Ellipse_str(self):
+    shear = self.getS()  # extract the e1 and e2 from the Shear instance
+    x0 = self.getX0()    # extract the x0 and y0 from a Position instance
+    return ("(e1="+str(shear.getE1())+", e2="+str(shear.getE2())+", m="+str(self.getMu())+
+            ", x="+str(x0.x)+", y="+str(x0.y)+")")
+
+_galsim.Ellipse.__repr__ = Ellipse_repr
+_galsim.Ellipse.__str__ = Ellipse_str
+
