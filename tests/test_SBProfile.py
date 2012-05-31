Improve Moffat's setupFT() calculation to automatically stop the calculation at a good spot.  Also fix a bug in maxK value. (#141)

tests/test_SBProfile.py
@@ -401,7 +401,10 @@ def test_sbprofile_moffat_properties():
     cen = galsim.PositionD(0, 0)
     np.testing.assert_equal(psf.centroid(), cen)
     # Check Fourier properties
-    np.testing.assert_almost_equal(psf.maxK(), 10.181157840063987)
+    # MJ: I'm not sure how useful it is to check these values (for any of the profiles).
+    #     What matters is that the images are convolved correctly not that the particular
+    #     stepk and maxk matches a previous calculation.
+    np.testing.assert_almost_equal(psf.maxK(), 11.569262763913111)
     np.testing.assert_almost_equal(psf.stepK(), 1.0695706520648969)
     #np.testing.assert_almost_equal(psf.maxK(), 34.226260866076707)
     #np.testing.assert_almost_equal(psf.stepK(), 0.53478529889112425)