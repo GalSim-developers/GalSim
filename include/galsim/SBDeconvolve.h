76ef2e62b9696a2f16509520847218d921514b53
Mike Jarvis <michael@jarvis.net>
2012-06-03 02:45:59 -0400

    Convert SBDeconvolve to pimpl idiom (#107)

--- include/galsim/SBDeconvolve.h
+++ include/galsim/SBDeconvolve.h
@@ -23,98 +23,98 @@ namespace galsim {
     {
     public:
         /// @brief Constructor.
-        SBDeconvolve(const SBProfile& adaptee) : _adaptee(adaptee.duplicate()) 
-        { _maxksq = pow(maxK(),2.); }
+        SBDeconvolve(const SBProfile& adaptee) :
+            SBProfile(new SBDeconvolveImpl(adaptee)) {}
 
         /// @brief Copy constructor.
-        SBDeconvolve(const SBDeconvolve& rhs) : _adaptee(rhs._adaptee->duplicate()) 
-        { _maxksq = pow(maxK(),2.); }
-
-        /// @brief Operator (TODO: ask Gary about this bit...)
-        SBDeconvolve& operator=(const SBDeconvolve& rhs)
-        {
-            if (&rhs == this) return *this;
-            if (_adaptee) {
-                delete _adaptee; 
-                _adaptee = 0;
-            }
-            _adaptee = rhs._adaptee->duplicate();
-            _maxksq = rhs._maxksq;
-            return *this;
-        }
+        SBDeconvolve(const SBDeconvolve& rhs) : SBProfile(rhs) {}
 
         /// @brief Destructor.
-        ~SBDeconvolve() { delete _adaptee; }
-
-        SBProfile* duplicate() const { return new SBDeconvolve(*this); }
-
-        // These are all the base class members that must be implemented:
-
-        /// @brief xValue() not implemented for SBDeconvolve.
-        double xValue(const Position<double>& p) const 
-        { throw SBError("SBDeconvolve::xValue() not implemented"); }
-
-        std::complex<double> kValue(const Position<double>& k) const 
-        {
-            return (k.x*k.x+k.y*k.y) <= _maxksq ?
-                1./_adaptee->kValue(k) :
-                std::complex<double>(0.,0.); 
-        }
-
-        double maxK() const { return _adaptee->maxK(); }
-        double stepK() const { return _adaptee->stepK(); }
-
-        bool isAxisymmetric() const { return _adaptee->isAxisymmetric(); }
-        bool isAnalyticX() const { return false; }
-        bool isAnalyticK() const { return true; }
-
-        Position<double> centroid() const { return -_adaptee->centroid(); }
-
-        double getFlux() const { return 1./_adaptee->getFlux(); }
-        void setFlux(double flux=1.) { _adaptee->setFlux(1./flux); }
-
-        PhotonArray shoot(int N, UniformDeviate& u) const 
-        {
-            throw SBError("SBDeconvolve::shoot() not implemented");
-            return PhotonArray(N);
-        }
+        ~SBDeconvolve() {}
 
     protected:
-
-        // Override for better efficiency if adaptee has it:
-        void fillKGrid(KTable& kt) const 
+        class SBDeconvolveImpl : public SBProfileImpl
         {
-            _adaptee->fillKGrid(kt);
-            // Flip or clip:
-            int N = kt.getN();
-            int maxiksq = _maxksq / (kt.getDk()*kt.getDk());
-            // Only need ix>=0 because it's Hermitian, but also
-            // don't want to repeat the ix=0, N/2 twice:
-            for (int iy = -N/2; iy < N/2; iy++) {
-                if (iy>=0) {
-                    int ix=0;
-                    if (ix*ix+iy*iy <= maxiksq) 
-                        kt.kSet(ix,iy,1./kt.kval(ix,iy));
-                    else
-                        kt.kSet(ix,iy,std::complex<double>(0.,0.));
-                    ix=N/2;
-                    if (ix*ix+iy*iy <= maxiksq) 
-                        kt.kSet(ix,iy,1./kt.kval(ix,iy));
-                    else
-                        kt.kSet(ix,iy,std::complex<double>(0.,0.));
-                }
-                for (int ix = 0; ix <= N/2; ix++) {
-                    if (ix*ix+iy*iy <= maxiksq) 
-                        kt.kSet(ix,iy,1./kt.kval(ix,iy));
-                    else
-                        kt.kSet(ix,iy,std::complex<double>(0.,0.));
+        public:
+            SBDeconvolveImpl(const SBProfile& adaptee) : _adaptee(adaptee)
+            { _maxksq = pow(maxK(),2.); }
+
+            ~SBDeconvolveImpl() {}
+
+            // xValue() not implemented for SBDeconvolve.
+            double xValue(const Position<double>& p) const 
+            { throw SBError("SBDeconvolve::xValue() not implemented"); }
+
+            std::complex<double> kValue(const Position<double>& k) const 
+            {
+                return (k.x*k.x+k.y*k.y) <= _maxksq ?
+                    1./_adaptee.kValue(k) :
+                    std::complex<double>(0.,0.); 
+            }
+
+            double maxK() const { return _adaptee.maxK(); }
+            double stepK() const { return _adaptee.stepK(); }
+
+            bool isAxisymmetric() const { return _adaptee.isAxisymmetric(); }
+            bool isAnalyticX() const { return false; }
+            bool isAnalyticK() const { return true; }
+
+            Position<double> centroid() const { return -_adaptee.centroid(); }
+
+            double getFlux() const { return 1./_adaptee.getFlux(); }
+
+            PhotonArray shoot(int N, UniformDeviate& u) const 
+            {
+                throw SBError("SBDeconvolve::shoot() not implemented");
+                return PhotonArray(N);
+            }
+
+        protected:
+
+            // Override for better efficiency if adaptee has it:
+            void fillKGrid(KTable& kt) const 
+            {
+                assert(_adaptee._pimpl.get());
+                _adaptee._pimpl->fillKGrid(kt);
+                // Flip or clip:
+                int N = kt.getN();
+                int maxiksq = _maxksq / (kt.getDk()*kt.getDk());
+                // Only need ix>=0 because it's Hermitian, but also
+                // don't want to repeat the ix=0, N/2 twice:
+                for (int iy = -N/2; iy < N/2; iy++) {
+                    if (iy>=0) {
+                        int ix=0;
+                        if (ix*ix+iy*iy <= maxiksq) 
+                            kt.kSet(ix,iy,1./kt.kval(ix,iy));
+                        else
+                            kt.kSet(ix,iy,std::complex<double>(0.,0.));
+                        ix=N/2;
+                        if (ix*ix+iy*iy <= maxiksq) 
+                            kt.kSet(ix,iy,1./kt.kval(ix,iy));
+                        else
+                            kt.kSet(ix,iy,std::complex<double>(0.,0.));
+                    }
+                    for (int ix = 0; ix <= N/2; ix++) {
+                        if (ix*ix+iy*iy <= maxiksq) 
+                            kt.kSet(ix,iy,1./kt.kval(ix,iy));
+                        else
+                            kt.kSet(ix,iy,std::complex<double>(0.,0.));
+                    }
                 }
             }
-        }
+
+        private:
+            SBProfile _adaptee;
+            double _maxksq;
+ 
+            // Copy constructor and op= are undefined.
+            SBDeconvolveImpl(const SBDeconvolveImpl& rhs);
+            void operator=(const SBDeconvolveImpl& rhs);
+        };
 
     private:
-        SBProfile* _adaptee;
-        double _maxksq;
+        // op= is undefined
+        void operator=(const SBDeconvolve& rhs);
     };
 
 }
