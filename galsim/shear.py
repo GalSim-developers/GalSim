7e0624fa8d30e5a8b7a16cbe03313ed6a25dd015
Rachel Mandelbaum <rmandelb@astro.princeton.edu>
2012-06-06 12:40:18 -0400

    propagated C++ methods for Shear class into python Shear class (#134)

--- galsim/shear.py
+++ galsim/shear.py
@@ -15,11 +15,11 @@ distortion e = (a - b)/(a + b)
 conformal shear eta, with a/b = exp(eta)
 axis ratio q = b/a
 
-These can be thought of as a magnitude and a position angle theta, or as two components e.g., g1 and
+These can be thought of as a magnitude and a position angle beta, or as two components e.g., g1 and
 g2, with
 
-g1 = g cos(2*theta)
-g2 = g sin(2*theta)
+g1 = g cos(2*beta)
+g2 = g sin(2*beta)
 
 The following are all examples of valid calls to initialize a Shear object:
 @code
@@ -27,15 +27,15 @@ s = galsim.Shear(g1=0.05, g2=0.05)
 s = galsim.Shear(g1=0.05) # assumes g2=0
 s = galsim.Shear(e1=0.05, e2=0.05)
 s = galsim.Shear(e2=0.05) # assumes e1=0
-s = galsim.Shear(eta=0.05, theta=galsim.Angle(45.0*galsim.degrees))
-s = galsim.Shear(q=0.5, theta=galsim.Angle(0.0*galsim.radians))
+s = galsim.Shear(eta=0.05, beta=galsim.Angle(45.0*galsim.degrees))
+s = galsim.Shear(q=0.5, beta=galsim.Angle(0.0*galsim.radians))
 @endcode
 
 There can be no mixing and matching, e.g., specifying g1 and e2.  It is permissible to only specify
 one of two components, with the other assumed to be zero.  If a magnitude such as e, g, eta, or q is
-specified, then theta is also required to be specified.
+specified, then beta is also required to be specified.
 """
-class Shear(_galsim._Shear):
+class Shear:
     def __init__(self, **kwargs):
         # note, no *args because we're not allowing Shear to be initialized with unnamed args!
         import numpy as np
@@ -55,7 +55,7 @@ class Shear(_galsim._Shear):
         e2 = kwargs.pop('e2', None)
         eta1 = kwargs.pop('eta1', None)
         eta2 = kwargs.pop('eta2', None)
-        theta = kwargs.pop('theta', None)
+        beta = kwargs.pop('beta', None)
         g = kwargs.pop('g', None)
         e = kwargs.pop('e', None)
         eta = kwargs.pop('eta', None)
@@ -65,7 +65,7 @@ class Shear(_galsim._Shear):
             raise RuntimeError(
                 "Shear constructor got unexpected argument(s): %s"%kwargs.keys())
 
-        # Now go through the possibilities
+        # Now go through the possibilities for combinations of args
         use_shear = None
         if g1 != None or g2 != None:
             if g1 == None:
@@ -100,10 +100,10 @@ class Shear(_galsim._Shear):
             use_shear = _galsim._Shear()
             use_shear.setEta1Eta2(eta1, e2)
         # from here on, we need a magnitude and position angle, so check the PA
-        elif theta == None:
+        elif beta == None:
             raise RuntimeError(
                 "Shear constructor did not get 2 components, OR a magnitude and position angle!")
-        elif not isinstance(theta, _galsim.Angle) :
+        elif not isinstance(beta, _galsim.Angle) :
             raise RuntimeError("The position angle that was supplied is not an Angle instance!")
         elif g != None:
             if use_shear != None:
@@ -119,12 +119,12 @@ class Shear(_galsim._Shear):
             if abs(e) > 1:
                 raise ValueError("Requested distortion exceeds 1: %f"%e)
             use_shear = _galsim._Shear()
-            use_shear.setEBeta(e, theta)
+            use_shear.setEBeta(e, beta)
         elif eta != None:
             if use_shear != None:
                 raise RuntimeError("Tried to initialize Shear in too many ways!")
             use_shear = _galsim._Shear()
-            use_shear.setEtaBeta(eta, theta)
+            use_shear.setEtaBeta(eta, beta)
         elif q != None:
             if use_shear != None:
                 raise RuntimeError("Tried to initialize Shear in too many ways!")
@@ -132,9 +132,78 @@ class Shear(_galsim._Shear):
                 raise ValueError("Cannot use requested axis ratio of %f!"%q)
             use_shear = _galsim._Shear()
             eta = -np.log(q)
-            use_shear.setEtaBeta(eta, theta)
+            use_shear.setEtaBeta(eta, beta)
 
-        self = use_shear
+        self.Shear = use_shear
+
+    #### propagate through all the methods from C++
+    # define all the methods for setting shear values
+    def setE1E2(self, e1=0.0, e2=0.0):
+        return self.Shear.setE1E2(e1, e2)
+    def setEBeta(self, e=0.0, beta=None):
+        return self.Shear.setEBeta(e, beta)
+    def setEta1Eta2(self, eta1=0.0, eta2=0.0):
+        return self.Shear.setEta1Eta2(eta1, eta2)
+    def setEtaBeta(self, eta=0.0, beta=None):
+        return self.Shear.setEtaBeta(eta, beta)
+    def setG1G2(self, g1=0.0, g2=0.0):
+        return self.Shear.setG1G2(g1, g2)
+    # define all the methods to get shear values
+    def getE1(self):
+        return self.Shear.getE1()
+    def getE2(self):
+        return self.Shear.getE2()
+    def getE(self):
+        return self.Shear.getE()
+    def getESq(self):
+        return self.Shear.getESq()
+    def getBeta(self):
+        return self.Shear.getBeta()
+    def getEta(self):
+        return self.Shear.getEta()
+    def getG(self):
+        return self.Shear.getG()
+    # make it possible to access g, e, etc. of some Shear object called name using name.g, name.e
+    e1 = property(getE1)
+    e2 = property(getE2)
+    e = property(getE)
+    esq = property(getESq)
+    beta = property(getBeta)
+    eta = property(getEta)
+    g = property(getG)
+    # define all the various operators on Shear objects
+    def __neg__(self):
+        return -self.Shear
+    def __add__(self, other):
+        return self.Shear + other.Shear
+    def __sub__(self, other):
+        return self.Shear - other.Shear
+    def __iadd__(self, other):
+        self.Shear += other.Shear
+    def __isub__(self, other):
+        self.Shear -= other.Shear
+    def rotationWith(self, other):
+        return self.Shear.rotationWith(other)
+    def __eq__(self, other):
+        return self.Shear == other.Shear
+    def __ne__(self, other):
+        return self.Shear != other.Shear
+    def __mul__(self, val):
+        return self.Shear * val
+    def __rmul__(self, val):
+        return self.Shear * val
+    def __div__(self, val):
+        return self.Shear / val
+    def __imul__(self, val):
+        self.Shear *= val
+    def __idiv__(self, val):
+        self.Shear /= val
+    def fwd(self, p):
+        return self.Shear.fwd(p)
+    def inv(self, p):
+        return self.Shear.inv(p)
+    def getMatrix(self, a, b, c):
+        self.Shear.getMatrix(a, b, c)
 
 def Shear_repr(self):
     return (self.__class__.__name__+"(e1="+str(self.getE1())+", e2="+str(self.getE2())+")")
