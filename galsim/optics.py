6e2d42bd5bf34ac1820131f412d2d4ba4ea4472d
Barnaby Rowe <barnaby.t.p.rowe@gmail.com>
2012-06-06 22:25:22 -0700

    and deleted in optics (#179)

--- galsim/optics.py
+++ galsim/optics.py
@@ -18,41 +18,7 @@ PTF = phase transfer function = p, where OTF = MTF * exp(i * p)
 Wavefront = the amplitude and phase of the incident light on the telescope pupil, encoded as a
 complex number. The OTF is the autocorrelation function of the wavefront.
 """
-
-def roll2d(image, (iroll, jroll)):
-    """Perform a 2D roll (circular shift) on a supplied 2D numpy array, conveniently.
-
-    Parameters
-    ----------
-    @param image            the numpy array to be circular shifted.
-    @param (iroll, jroll)   the roll in the i and j dimensions, respectively.
-
-    @returns the rolled image.
-    """
-    return np.roll(np.roll(image, jroll, axis=1), iroll, axis=0)
-
-def kxky(array_shape=(256, 256)):
-    """Return the tuple kx, ky corresponding to the DFT of a unit integer-sampled array of input
-    shape.
-    
-    Uses the SBProfile conventions for Fourier space, so k varies in approximate range (-pi, pi].
-    Uses the most common DFT element ordering conventions (and those of FFTW), so that `(0, 0)`
-    array element corresponds to `(kx, ky) = (0, 0)`.
-
-    See also the docstring for np.fftfreq, which uses the same DFT convention, and is called here,
-    but misses a factor of pi.
-    
-    Adopts Numpy array index ordering so that the trailing axis corresponds to kx, rather than the
-    leading axis as would be expected in IDL/Fortran.  See docstring for numpy.meshgrid which also
-    uses this convention.
-
-    Parameters
-    ----------
-    @param array_shape   the Numpy array shape desired for `kx, ky`. 
-    """
-    k_xaxis = np.fft.fftfreq(array_shape[1]) * 2. * np.pi
-    k_yaxis = np.fft.fftfreq(array_shape[0]) * 2. * np.pi
-    return np.meshgrid(k_xaxis, k_yaxis)
+import utilities
 
 def generate_pupil_plane(array_shape=(256, 256), dx=1., lam_over_D=2., circular_pupil=True,
                          obscuration=0.):
