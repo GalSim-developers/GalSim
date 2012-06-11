267693a88827ee4883fc9d362ee6f1cffeaa9607
Rachel Mandelbaum <rmandelb@astro.princeton.edu>
2012-06-10 22:42:39 -0400

    Shear constructor restructuring based on Mike suggestions (#134)

--- galsim/shear.py
+++ galsim/shear.py
@@ -55,91 +55,91 @@ class Shear:
                 raise TypeError("No keywords given to initialize Shear!")
             if len(kwargs) > 2:
                 raise TypeError(
-                    "Shear constructor received too many keyword arguments (max 2): %s"%kwargs.keys())
-            g1 = kwargs.pop('g1', None)
-            g2 = kwargs.pop('g2', None)
-            e1 = kwargs.pop('e1', None)
-            e2 = kwargs.pop('e2', None)
-            eta1 = kwargs.pop('eta1', None)
-            eta2 = kwargs.pop('eta2', None)
-            beta = kwargs.pop('beta', None)
-            g = kwargs.pop('g', None)
-            e = kwargs.pop('e', None)
-            eta = kwargs.pop('eta', None)
-            q = kwargs.pop('q', None)
-            # make sure there is no other random keyword arg provided
-            if kwargs:
-                raise TypeError(
-                    "Shear constructor got unexpected argument(s): %s"%kwargs.keys())
+                    "Shear constructor received >2 keyword arguments: %s"%kwargs.keys())
 
-            # Now go through the possibilities for combinations of args
-            use_shear = None
-            if g1 != None or g2 != None:
-                if g1 == None:
-                    g1 = 0.0
-                if g2 == None:
-                    g2 = 0.0
+            if 'g1' in kwargs or 'g2' in kwargs:
+                g1 = kwargs.pop('g1', 0.)
+                g2 = kwargs.pop('g2', 0.)
                 g = np.sqrt(g1**2 + g2**2)
                 if g < 1:
                     use_shear = _galsim._Shear(g1, g2)
                 else:
                     raise ValueError("Requested shear exceeds 1: %f"%g)
-            elif e1 != None or e2 != None:
-                if use_shear != None:
-                    raise TypeError("Tried to initialize Shear in too many ways!")
-                if e1 == None:
-                    e1 = 0.0
-                if e2 == None:
-                    e2 = 0.0
+            elif 'e1' in kwargs or 'e2' in kwargs:
+                e1 = kwargs.pop('e1', 0.)
+                e2 = kwargs.pop('e2', 0.)
                 e = np.sqrt(e1**2 + e2**2)
                 if e < 1:
                     use_shear = _galsim._Shear()
                     use_shear.setE1E2(e1, e2)
                 else:
                     raise ValueError("Requested distortion exceeds 1: %s"%e)
-            elif eta1 != None or eta2 != None:
-                if use_shear != None:
-                    raise TypeError("Tried to initialize Shear in too many ways!")
-                if eta1 == None:
-                    eta1 = 0.0
-                if eta2 == None:
-                    eta2 = 0.0
+            elif 'eta1' in kwargs or 'eta2' in kwargs:
+                eta1 = kwargs.pop('eta1', 0.)
+                eta2 = kwargs.pop('eta2', 0.)
                 use_shear = _galsim._Shear()
                 use_shear.setEta1Eta2(eta1, e2)
-            # from here on, we need a magnitude and position angle, so check the PA
-            elif beta == None:
-                raise TypeError(
-                    "Shear constructor did not get 2 components, OR a magnitude and position angle!")
-            elif not isinstance(beta, _galsim.Angle) :
-                raise TypeError("The position angle that was supplied is not an Angle instance!")
-            elif g != None:
-                if use_shear != None:
-                    raise TypeError("Tried to initialize Shear in too many ways!")
+            elif 'g' in kwargs:
+                if 'beta' not in kwargs:
+                    raise TypeError(
+                        "Shear constructor requires position angle when g is specified!") 
+                beta = kwargs.pop('beta')
+                if not isinstance(beta, _galsim.Angle):
+                    raise TypeError(
+                        "The position angle that was supplied is not an Angle instance!")
+                g = kwargs.pop('g')
                 if abs(g) > 1:
                     raise ValueError("Requested shear exceeds 1: %f"%g)
                 g1 = np.cos(beta.rad())*g
                 g2 = np.sin(beta.rad())*g
                 use_shear = _galsim._Shear(g1, g2)
-            elif e != None:
-                if use_shear != None:
-                    raise TypeError("Tried to initialize Shear in too many ways!")
+            elif 'e' in kwargs:
+                if 'beta' not in kwargs:
+                    raise TypeError(
+                        "Shear constructor requires position angle when e is specified!")
+                beta = kwargs.pop('beta')
+                if not isinstance(beta, _galsim.Angle):
+                    raise TypeError(
+                        "The position angle that was supplied is not an Angle instance!")
+                e = kwargs.pop('e')
                 if abs(e) > 1:
                     raise ValueError("Requested distortion exceeds 1: %f"%e)
                 use_shear = _galsim._Shear()
                 use_shear.setEBeta(e, beta)
-            elif eta != None:
-                if use_shear != None:
-                    raise TypeError("Tried to initialize Shear in too many ways!")
+            elif 'eta' in kwargs:
+                if 'beta' not in kwargs:
+                    raise TypeError(
+                        "Shear constructor requires position angle when eta is specified!")
+                beta = kwargs.pop('beta')
+                if not isinstance(beta, _galsim.Angle):
+                    raise TypeError(
+                        "The position angle that was supplied is not an Angle instance!")
+                eta = kwargs.pop('eta')
                 use_shear = _galsim._Shear()
                 use_shear.setEtaBeta(eta, beta)
-            elif q != None:
-                if use_shear != None:
-                    raise TypeError("Tried to initialize Shear in too many ways!")
+            elif 'q' in kwargs:
+                if 'beta' not in kwargs:
+                    raise TypeError(
+                        "Shear constructor requires position angle when q is specified!")
+                beta = kwargs.pop('beta')
+                if not isinstance(beta, _galsim.Angle):
+                    raise TypeError(
+                        "The position angle that was supplied is not an Angle instance!")
+                q = kwargs.pop('q')
                 if q <= 0 or q > 1:
                     raise ValueError("Cannot use requested axis ratio of %f!"%q)
                 use_shear = _galsim._Shear()
                 eta = -np.log(q)
                 use_shear.setEtaBeta(eta, beta)
+            elif 'beta' in kwargs:
+                raise RuntimeError("beta provided to Shear constructor, but not g/e/eta/q")
+            else:
+                raise RuntimeError(
+                    "No appropriate shear specification given to Shear constructor")
+
+            if kwargs:
+                raise TypeError(
+                    "Shear constructor got unexpected extra argument(s): %s"%kwargs.keys())
 
             self._shear = use_shear
 
