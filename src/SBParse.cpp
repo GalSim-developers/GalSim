81160457ad9243f3c30f2c0e299fa412af5a6cf0
Barnaby Rowe <barnaby.t.p.rowe@gmail.com>
2012-06-12 17:30:05 -0700

    fixed SBParse rType=FWHM issue for good, phew :) (#142)

--- src/SBParse.cpp
+++ src/SBParse.cpp
@@ -407,11 +407,10 @@ namespace galsim {
             double beta = dargs[0];
             double trunc = dargs[1];
             double fwhm = (nargs>2) ? dargs[2] : 1.;
-	    SBMoffat::RadiusType rtype = (SBMoffat::RadiusType)(0); //FWHM first element of enum
-            //^ Barney: having a C++ fail with the above, looking for assistance!... Is this OK?
+	    SBMoffat::RadiusType rType = SBMoffat::FWHM;
             dbg << "**Returning moffat with beta, trunc, flux, fwhm " << beta
                 << " " << trunc << " " << flux << " " << fwhm << std::endl;
-            return SBMoffat(beta, fwhm, rtype, trunc, flux);
+            return SBMoffat(beta, fwhm, rType, trunc, flux);
 
         } else if (nocaseEqual(sbtype, "laguerre")) {
             // Laguerre: args are [filename]
