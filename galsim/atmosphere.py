dd035f65d2ea6d09e454a227571ea71eed0e0083
Barnaby Rowe <barnaby.t.p.rowe@gmail.com>
2012-06-13 00:18:21 -0700

    fixed atmosphere and tests, all tests now pass (#142)

--- galsim/atmosphere.py
+++ galsim/atmosphere.py
@@ -10,12 +10,12 @@ import utilities
 class DoubleGaussian(galsim.Add):
     """Double Gaussian, which is the sum of two SBProfile Gaussian profiles
     """
-    def __init__(self, flux1=1., flux2=1., sigma1=None, sigma2=None, fwhm1=None, fwhm2=None):
+    def __init__(self, sigma1=None, sigma2=None, fwhm1=None, fwhm2=None, flux1=1., flux2=1.):
         sblist = []
         # Note: we do not have to check for improper args (0 or 2 radii specified) because this is
         # done in the C++
-        sblist.append(galsim.Gaussian(flux1, sigma=sigma1, fwhm=fwhm1))
-        sblist.append(galsim.Gaussian(flux2, sigma=sigma2, fwhm=fwhm2))
+        sblist.append(galsim.Gaussian(sigma=sigma1, fwhm=fwhm1, flux=flux1))
+        sblist.append(galsim.Gaussian(sigma=sigma2, fwhm=fwhm2, flux=flux2))
         galsim.Add.__init__(self, sblist)
 
 def kolmogorov_mtf(array_shape=(256, 256), dx=1., lam_over_r0=1.):
