b7b3e2bc70dc73aeafd2e2900342a941f429605a
Barnaby Rowe <barnaby.t.p.rowe@gmail.com>
2012-06-22 23:01:25 -0700

    finished Gaussian timing test script (#163)

--- devutils/external/time_photon_shooting/time_gaussian.py
+++ devutils/external/time_photon_shooting/time_gaussian.py
@@ -1,7 +1,6 @@
-
 #!/usr/bin/env python
-"""
-Some example scripts to evaluate timing for shooting photons through a Gaussian distribution 
+
+"""Some example scripts to evaluate timing for shooting photons through a Gaussian distribution 
 with the GalSim library.
 """
 
@@ -21,25 +20,32 @@ except ImportError:
     sys.path.append(os.path.abspath(os.path.join(path, "..")))
     import galsim
 
-NDRAWS = 100
-NPHOTONS = 10000         # Number of photons per draw
+NIMAGES = 1000
+NPHOTONS = 500000         # Number of photons per draw
 PIXEL_SCALE = 1.0        # arcsec  (size units in input catalog are pixels)
 IMAGE_XMAX = 64          # pixels
 IMAGE_YMAX = 64          # pixels
 GAUSSIAN_SIGMA = 5.
 
-def time_gaussian():
+RANDOM_SEED = 3231139901
+
+def time_gaussian_shoot():
     """Shoot photons through a Gaussian profile recording times for comparison between USE_COS_SIN
     method in SBProfile.cpp and the unit circle rejection method.
     """
     logger = logging.getLogger("time_gaussian")
 
     # Initialize the random number generator we will be using.
-    rng = galsim.UniformDeviate(random_seed)
+    rng = galsim.UniformDeviate(RANDOM_SEED)
 
+    # Build the image for drawing the galaxy into
+    image = galsim.ImageF(IMAGE_XMAX, IMAGE_YMAX)
+    image.setScale(PIXEL_SCALE)
+
+    # Start the timer
     t1 = time.time()
 
-    for i in range(NDRAWS):
+    for i in range(NIMAGES):
         # Build the galaxy
         gal = galsim.Gaussian(sigma=GAUSSIAN_SIGMA)
         # Build the image for drawing the galaxy into
@@ -47,7 +53,18 @@ def time_gaussian():
         # Shoot the galaxy
         gal.drawShoot(image, NPHOTONS) 
 
+    # Get the time
+    t2 = time.time()
+    logger.info(
+        'time_gaussian_shoot: NIMAGES = %d, NPHOTONS = %d, total time = %f sec', NIMAGES, 
+        NPHOTONS, t2-t1
+    )
 
 
-
-
+if __name__ == "__main__":
+    logging.basicConfig(
+        format="%(message)s",
+        level=logging.DEBUG,
+        stream=sys.stdout
+    )
+    time_gaussian_shoot()
