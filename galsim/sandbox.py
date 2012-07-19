1923a11858067e556c044e0906b1fff2f9040aaa
Jim Bosch <jbosch@astro.princeton.edu>
2012-07-19 13:14:13 -0400

    added/replaced two simple descriptors in sandbox.py (#195)

--- galsim/sandbox.py
+++ galsim/sandbox.py
@@ -541,18 +541,60 @@ class GSObject(object):
 
 
 class SimpleParam(object):
-    def __init__(self, cls, name):
-        self.name = name
-        cls.params[self.name] = self
+    """
+    Descriptor that just gets/sets a value with no additional checking or logic.
 
-    def __get__(self, instance, owner):
-        if self.name not in owner.params:
-            raise AttributeError, self.name
-        return instance._data[self.name]
+    Use it like this:
+
+    class MyProfile(GSObject):
+        flux = SimpleParam("flux")
+    """
+
+    def __init__(self, name, default=0.0):
+        self.name = name
+        self.default = default
+
+    def __get__(self, instance, cls):
+        if instance is not None:
+            # dict.setdefault will return the item in the dict if present, or set and return the default
+            # otherwise
+            return instance._data.setdefault(self.name, self.default)
+        return self
 
     def __set__(self, instance, value):
-        print "Setting"
         instance._data[self.name] = value
+
+class GetSetParam(object):
+    """
+    Descriptor that uses user-supplied functions to get/set values, intended for
+    defining "derived" quantities.
+
+    Use it like this:
+
+    class MyProfile(GSObject):
+    
+        half_light_radius = SimpleParam("half_light_radius")
+
+        def _get_fwhm(self):
+            return self.half_light_radius * RADIUS_CONVERSION_FACTOR
+        def _set_fwhm(self, value):
+            self.half_light_radius = value / RADIUS_CONVERSION_FACTOR
+        fwhm = GetSetParam(_get_fwhm, _set_fwhm)
+    """
+
+    def __init__(self, getter, setter=None):
+        self.getter = getter
+        self.setter = setter
+    
+    def __get__(self, instance, cls):
+        if instance is not None:
+            return self.getter(instance)
+        return self
+
+    def __set__(self, instance, value):
+        if not self.setter:
+            raise TypeError("Cannot set parameter")
+        self.setter(instance, value)
 
 
 class Gaussian0(GSObject):
