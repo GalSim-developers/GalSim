1c7f56196d091fe26275949ed44e6aa72ef2b375
Barnaby Rowe <barnaby.t.p.rowe@gmail.com>
2012-08-06 14:43:04 -0700

    added GSObject parameter test script (#195)

---
+++ tests/test_GSObject.py
@@ -0,0 +1,13 @@
+import os
+import sys
+import numpy as np
+try:
+    import galsim
+except ImportError:
+    path, filename = os.path.split(__file__)
+    sys.path.append(os.path.abspath(os.path.join(path, "..")))
+    import galsim
+
+imgdir = os.path.join(".", "SBProfile_comparison_images")
+
+
