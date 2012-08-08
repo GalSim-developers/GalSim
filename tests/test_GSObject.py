d16ffe1c924e70f052a1964182b8287237448e4e
Barnaby Rowe <barnaby.t.p.rowe@gmail.com>
2012-08-08 15:48:26 +0100

    minor additions, just committing because I want to checkout master (#195)

--- tests/test_GSObject.py
+++ tests/test_GSObject.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import numpy as np
+# import galsim even if path not yet added to PYTHONPATH env variable (e.g. by full install)
 try:
     import galsim
 except ImportError:
@@ -9,5 +10,19 @@ except ImportError:
     import galsim
 
 imgdir = os.path.join(".", "SBProfile_comparison_images")
+
+# Test values taken from test_SBProfile.py... and modified slightly.
+# for radius tests - specify half-light-radius, FHWM, sigma to be compared with high-res image (with
+# pixel scale chosen iteratively until convergence is achieved, beginning with test_dx)
+test_hlr = 1.9
+test_fwhm = 1.9
+test_sigma = 1.9
+test_scale = 1.9
+test_sersic_n = [1.4, 2.6]
+
+# for flux normalization tests
+test_flux = 1.9
+
+
 
 
