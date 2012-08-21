ced3d7c60cbfed853aa10dc92eba45f6a7e60d01
Barnaby Rowe <barnaby.t.p.rowe@gmail.com>
2012-08-21 12:57:11 +0100

    first attempt at a function for adding GeneratableField attributes to the GSObjectNode, commented out previous individually defined derived classes (#148)

--- galsim/config/definition.py
+++ galsim/config/definition.py
@@ -8,7 +8,7 @@ from .. import _galsim
 
 class GSObjectNode(machinery.NodeBase):
     flux = generators.GeneratableField(default=1.)
-    target = None  # GSObject class returned by apply; should be set by derived classes.
+    target = None  # GSObject class returned by apply; updated by _update_gsobject_node_fields
 
     def apply(self, row):
         """
@@ -21,52 +21,73 @@ class GSObjectNode(machinery.NodeBase):
         kwds = generators.makeDict(self, row, self.fields.keys())
         return self.target(**kwds)
 
-class EllipticalObjectNode(GSObjectNode):
-    g1 = generators.GeneratableField(default=None)
-    g2 = generators.GeneratableField(default=None)
 
-    def apply(self, row):
+def _update_gsobject_node_fields(node, target):
+    """Update the GSObjectNode class with the required fields for the target GSOjbect class.
+
+    Takes a GSObjectNode class (the node input) and returns a new class with one
+    generators.GeneratableField() attribute for each of the parameter descriptors of the GSObject
+    supplied on input.
+    """
+    
+    # First check that we're dealing with a GSObject in the input target class
+    if not issubclass(target, base.GSObject):
+        raise TypeError("Supplied target class must be a GSObject-derived class.")
+        
+    # Then introspect the class to find which parameter descriptors it uses, find out which ones are
+    # GSObject parameter descriptors, and set these as appropriately-named attributes of the node:
+    for keyname, entry in target.__dict__.iteritems():
+        if entry.__module__ is "galsim.decriptors":
+            node.__setattr__(keyname, generators.GeneratableField(default=None))
+
+    node.target = target
+    return node
+
+#class EllipticalObjectNode(GSObjectNode):
+#    g1 = generators.GeneratableField(default=None)
+#    g2 = generators.GeneratableField(default=None)
+
+#    def apply(self, row):
         # could remove this implementation if we add ellipticity kwds to GSObject constructors
-        kwds = generators.makeDict(self, row, self.fields.keys())
-        g1 = kwds.pop("g1", 0.)
-        g2 = kwds.pop("g2", 0.)
-        result = self.target(**kwds)
-        if g1 != 0. or g2 != 0.:
-            result.applyShear(g1=g1, g2=g2)
-        return result
+        #kwds = generators.makeDict(self, row, self.fields.keys())
+        #g1 = kwds.pop("g1", 0.)
+        #g2 = kwds.pop("g2", 0.)
+        #result = self.target(**kwds)
+        #if g1 != 0. or g2 != 0.:
+        #    result.applyShear(g1=g1, g2=g2)
+        #return result
 
-class MoffatNode(EllipticalObjectNode):
-    beta = generators.GeneratableField(default=None)
-    truncationFWHM = generators.GeneratableField(default=2.)
-    fwhm = generators.GeneratableField(default=None)
-    half_light_radius = generators.GeneratableField(default=None)
-    scale_radius = generators.GeneratableField(default=None)
-    target = base.Moffat
+#class MoffatNode(EllipticalObjectNode):
+    #beta = generators.GeneratableField(default=None)
+    #truncationFWHM = generators.GeneratableField(default=2.)
+    #fwhm = generators.GeneratableField(default=None)
+    #half_light_radius = generators.GeneratableField(default=None)
+    #scale_radius = generators.GeneratableField(default=None)
+    #target = base.Moffat
 
-class DeVaucouleursNode(EllipticalObjectNode):
-    half_light_radius = generators.GeneratableField(default=None)
-    target = base.DeVaucouleurs
+#class DeVaucouleursNode(EllipticalObjectNode):
+    #half_light_radius = generators.GeneratableField(default=None)
+    #target = base.DeVaucouleurs
 
-class ExponentialNode(EllipticalObjectNode):
-    half_light_radius = generators.GeneratableField(default=None)
-    scale_radius = generators.GeneratableField(default=None)
-    target = base.Exponential
+#class ExponentialNode(EllipticalObjectNode):
+    #half_light_radius = generators.GeneratableField(default=None)
+    #scale_radius = generators.GeneratableField(default=None)
+    #target = base.Exponential
 
-class GaussianNode(EllipticalObjectNode):
-    half_light_radius = generators.GeneratableField(default=None)
-    sigma = generators.GeneratableField(default=None)
-    fwhm = generators.GeneratableField(default=None)
-    target = base.Gaussian
+#class GaussianNode(EllipticalObjectNode):
+    #half_light_radius = generators.GeneratableField(default=None)
+    #sigma = generators.GeneratableField(default=None)
+    #fwhm = generators.GeneratableField(default=None)
+    #target = base.Gaussian
 
-class SersicNode(EllipticalObjectNode):
-    n = generators.GeneratableField(default=None)
-    half_light_radius = generators.GeneratableField(default=None)
-    target = base.Sersic
+#class SersicNode(EllipticalObjectNode):
+    #n = generators.GeneratableField(default=None)
+    #half_light_radius = generators.GeneratableField(default=None)
+    #target = base.Sersic
 
-class PixelNode(GSObjectNode):
-    xw = machinery.Field(default=None) # but see RootNode.finish()
-    yw = machinery.Field(default=None)
-
+#class PixelNode(GSObjectNode):
+    #xw = machinery.Field(default=None) # but see RootNode.finish()
+    #yw = machinery.Field(default=None)
 
 # TODO: more PSF component nodes
 
