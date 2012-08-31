21f8cdbae33dbfca208e5dcc4901cc0302e432ed
Mike Jarvis <michael@jarvis.net>
2012-08-31 02:27:48 -0400

    Made InputCatalog a real class, rather than an AttributeDict (#148)

--- galsim/io.py
+++ galsim/io.py
@@ -1,62 +1,58 @@
 import galsim
 
-def ReadInputCat(config, cat_file_name=None, filetype="ASCII", comments="#"):
-    """@brief Read in an input catalog for object-by-object parameter specification.
-
-    @param config         A configuration galsim.Config instance containing all the required
-                          information for running GalSim in Multi-Object mode.
-    @param cat_file_name  Filename of the input catalog.
-    @param filetype       Either 'ASCII' (currently the only, default, option) or (soon) 'FITS'.
-    @param comments       The character used to indicate the start of a comment in an ASCII catalog
-                          (default='#').
-
-    @returns A galsim.Config instance, each attribute of which is a vector of parameter values of
-             length equal to the number of valid rows in the input catalog.
-
-    Does some checking for sensible inputs, unlike the functions it calls (ReadAsciiInputCat()
-    and ReadFitsInputCat()).
+class InputCatalog(object):
+    """@brief A class storing the data from an input catalog where each row corresponds to 
+    a different object to be built, and each column stores some item of information about
+    that object (e.g. flux or half_light_radius).
     """
-    # First check for sensible inputs
-    if cat_file_name == None:
-        raise IOError("No filename given!")
+    def __init__(self, config):
+        """
+        @param config   A configuration dict containing the parameters required for
+                        reading in the input cat. 
+    
+        The configuration paramters that may be set:
 
-    # Raise an apologetic exception for FITS input-wanting users
-    if filetype.upper() == "FITS":
-        raise NotImplementedError("FITS catalog inputs not yet implemented, sorry!")
-    # Then read in from the ASCII-type catalogs
-    elif filetype.upper() ==  "ASCII":
-        input_cat = ReadAsciiInputCat(cat_file_name=cat_file_name, comments=comments)
-    else:
-        raise AttributeError("User must specify input catalog file type as either 'ASCII' "+
-                             "or 'FITS' (case-insensitive).")
-    # Return catalog to the user
-    return input_cat
+        dir           Directory catalog is in. (default = .)
+        file_name     Filename of the input catalog. (Required)
+        file_type     Either 'ASCII' (currently the only, default, option) or (soon) 'FITS'.
+                    (default = determine from extension or, if that fails, ASCII)
+        comments      The character used to indicate the start of a comment in an ASCII catalog.
+                    (default='#').
 
-def ReadAsciiInputCat(cat_file_name=None, comments="#"):
-    """@brief Read in an input catalog from an ASCII file.
+        Does some checking for sensible inputs, unlike the functions it calls (ReadAsciiInputCat()
+        and ReadFitsInputCat()).
+        """
+        # First check for sensible inputs
+        if 'file_name' not in config:
+            raise AttributeError("file_name is required for input catalog")
+        self.file_name = config['file_name']
+        if 'dir' in config:
+            import os
+            dir = config['dir']
+            self.file_name = os.path.join(dir,self.file_name)
+    
+        # Raise an apologetic exception for FITS input-wanting users
+        self.file_type = config.get('file_type','ASCII')
+        if self.file_type.upper() == 'FITS':
+            raise NotImplementedError("FITS catalog inputs not yet implemented, sorry!")
+        # Then read in from the ASCII-type catalogs
+        elif self.file_type.upper() ==  'ASCII':
+            self.read_ascii(config)
+        else:
+            raise AttributeError("User must specify input catalog file type as either 'ASCII' "+
+                                "or 'FITS' (case-insensitive).")
 
-    @param cat_file_name  Filename of the input catalog.
-    @param comments       The character used to indicate the start of a comment in an ASCII catalog
-                          (default='#').
+    def read_ascii(self, config):
+        """@brief Read in an input catalog from an ASCII file.
 
-    @returns A galsim.Config instance, containing useful metadata and the ASCII table itself stored
-             as a NumPy array in the .data attribute.
-
-    Does not check for sensible inputs, leaving this up to the wrapper function ReadInputCat().
-    """
-    from numpy import loadtxt
-    # Initialize the Config() ready for storing the field values
-    input_cat = galsim.Config()
-    # Store basic meta data
-    input_cat.cat_file_name = cat_file_name
-    # Read in the data using the numpy convenience function
-    input_cat.data = loadtxt(cat_file_name, comments=comments)
-    # Also store the number of objects as input_cat.nobjects for easy access by other routines
-    input_cat.nobjects = input_cat.data.shape[0]
-    input_cat.ncols = input_cat.data.shape[1]
-    # Store that this is an ASCII catalog
-    input_cat.type = 'ASCII'
-    # Return catalog to the user
-    return input_cat
-
-
+        Does not check for sensible inputs, leaving this up to the wrapper function read.
+        """
+        from numpy import loadtxt
+        # Read in the data using the numpy convenience function
+        comments = config.get('comments','#')
+        self.data = loadtxt(self.file_name, comments=comments)
+        # Also store the number of objects as nobjects for easy access by other routines
+        self.nobjects = self.data.shape[0]
+        self.ncols = self.data.shape[1]
+        # Store that this is an ASCII catalog
+        self.file_type = 'ASCII'
