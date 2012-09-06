74eaf34cf18ffb901365dee89024dd418b937825
Rachel Mandelbaum <rmandelb@astro.princeton.edu>
2012-09-06 15:54:09 -0400

    added unit tests for random shears from a power spectrum (#216)

--- tests/test_lensing.py
+++ tests/test_lensing.py
@@ -38,16 +38,103 @@ def test_nfwhalo():
 
     # check internal correctness:
     # g1 = gamma1/(1-kappa), and g2 = 0
-    np.testing.assert_array_equal(g1, gamma1/(1-kappa),  err_msg="Computation of reduced shear g incorrect.")
-    np.testing.assert_array_equal(g2, np.zeros_like(g2),  err_msg="Computation of reduced shear g2 incorrect.")
+    np.testing.assert_array_equal(g1, gamma1/(1-kappa),
+                                  err_msg="Computation of reduced shear g incorrect.")
+    np.testing.assert_array_equal(g2, np.zeros_like(g2),
+                                  err_msg="Computation of reduced shear g2 incorrect.")
 
     # comparison to reference:
     # tangential shear in x-direction is purely negative in g1
-    np.testing.assert_allclose(-ref[:,2], gamma1,  rtol=1e-4, err_msg="Computation of shear deviates from reference.")
-    np.testing.assert_allclose(-ref[:,3], g1,  rtol=1e-4, err_msg="Computation of reduced shear deviates from reference.")
-    np.testing.assert_allclose(ref[:,4], kappa,  rtol=1e-4, err_msg="Computation of convergence deviates from reference.")
+    np.testing.assert_allclose(-ref[:,2], gamma1,  rtol=1e-4,
+                                err_msg="Computation of shear deviates from reference.")
+    np.testing.assert_allclose(-ref[:,3], g1,  rtol=1e-4,
+                                err_msg="Computation of reduced shear deviates from reference.")
+    np.testing.assert_allclose(ref[:,4], kappa,  rtol=1e-4,
+                               err_msg="Computation of convergence deviates from reference.")
     t2 = time.time()
     print 'time for %s = %.2f'%(funcname(),t2-t1)
-    
+
+def test_shear_flatps():
+    """Test that shears from power spectrum P(k)=const have the expected statistical properties"""
+    # make a flat power spectrum for E, B modes
+    test_ps = galsim.lensing.PowerSpectrum(E_power_function=galsim.lensing.pkflat,
+                                           B_power_function=galsim.lensing.pkflat)
+    # get shears on 500x500 grid
+    g1, g2 = test_ps.getShear(grid_spacing=1.0, grid_nx=500)
+    # check: are shears consistent with unit variance?
+    var1 = np.var(g1)
+    var2 = np.var(g2)
+    np.testing.assert_almost_equal(var1, 0.01, decimal=3,
+                                   err_msg="Non-unit shear variance(1) from flat power spectrum!")
+    np.testing.assert_almost_equal(var2, 0.01, decimal=3,
+                                   err_msg="Non-unit shear variance(2) from flat power spectrum!")
+    # check: are g1, g2 uncorrelated with each other?
+    top= np.sum((g1-np.mean(g1))*(g2-np.mean(g2)))
+    bottom1 = np.sum((g1-np.mean(g1))**2)
+    bottom2 = np.sum((g2-np.mean(g2))**2)
+    corr = top / np.sqrt(bottom1*bottom2)
+    np.testing.assert_almost_equal(corr, 0., decimal=2,
+                                   err_msg="Shear components should be uncorrelated with each other!")
+
+def test_shear_seeds():
+    """Test that shears from lensing engine behave appropriate when given same/different seeds"""
+    # make a power spectrum for some E, B power function
+    test_ps = galsim.lensing.PowerSpectrum(E_power_function=galsim.lensing.pk2,
+                                           B_power_function=galsim.lensing.pkflat)
+
+    # get shears on a grid w/o specifying seed
+    g1, g2 = test_ps.getShear(grid_spacing=1.0, grid_nx = 10)
+    # do it again, w/o specifying seed: should differ
+    g1new, g2new = test_ps.getShear(grid_spacing=1.0, grid_nx = 10)
+    ## I'm not actually sure how to use numpy.testing to require inequality, so I'm going to just
+    ## fake it by explicitly testing first element and using a ridiculous assertion
+    if (g1[0,0]==g1new[0,0]) or (g2[0,0]==g2new[0,0]):
+        np.testing.assert_equal(0,1,err_msg="New shear field is same as previous!")
+        np.testing.assert_equal(0,1,err_msg="New shear field is same as previous!")
+
+    # get shears on a grid w/ specified seed
+    g1, g2 = test_ps.getShear(grid_spacing=1.0, grid_nx = 10, seed = 13796)
+    # get shears on a grid w/ same specified seed: should be same
+    g1new, g2new = test_ps.getShear(grid_spacing=1.0, grid_nx = 10, seed = 13796)
+    np.testing.assert_array_equal(g1, g1new,
+                                  err_msg="New shear field differs from previous (same seed)!")
+    np.testing.assert_array_equal(g2, g2new,
+                                  err_msg="New shear field differs from previous (same seed)!")
+    # get shears on a grid w/ diff't specified seed: should differ
+    g1new, g2new = test_ps.getShear(grid_spacing=1.0, grid_nx = 10, seed = 1379)
+    if (g1[0,0]==g1new[0,0]) or (g2[0,0]==g2new[0,0]):
+        np.testing.assert_equal(0,1,err_msg="New shear field is same as previous!")
+        np.testing.assert_equal(0,1,err_msg="New shear field is same as previous!")
+
+def test_shear_reference():
+    """Test shears from lensing engine compared to stored reference values"""
+    # read input data
+    ref = np.loadtxt(refdir + '/shearfield_reference.dat')
+    g1_in = ref[:,0]
+    g2_in = ref[:,1]
+
+    # set up params
+    seed = 14136
+    n = 10
+    dx = 1.
+
+    # define power spectrum
+    ps = galsim.lensing.PowerSpectrum(E_power_function=galsim.lensing.pkflat,
+                                      B_power_function=galsim.lensing.pkflat)
+    # get shears
+    g1, g2 = ps.getShear(grid_spacing = dx, grid_nx = n, seed = seed)
+
+    # put in same format as data that got read in
+    g1vec = g1.reshape(n*n)
+    g2vec = g2.reshape(n*n)
+    # compare input vs. calculated values
+    np.testing.assert_almost_equal(g1_in, g1vec, 9,
+                                   err_msg = "Shear field differs from reference shear field!")
+    np.testing.assert_almost_equal(g2_in, g2vec, 9,
+                                   err_msg = "Shear field differs from reference shear field!")
+
 if __name__ == "__main__":
     test_nfwhalo()
+    test_shear_flatps()
+    test_shear_seeds()
+    test_shear_reference()
