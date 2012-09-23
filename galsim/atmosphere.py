a7c1eae12e0b74291b218efb55865cf7ede4f14f
barnabytprowe <barnaby.t.p.rowe@gmail.com>
2012-09-23 17:27:31 +0100

    removed @brief calls and updated NumPy capitaliztion in atmoshpere.py

--- galsim/atmosphere.py
+++ galsim/atmosphere.py
@@ -23,10 +23,10 @@ MTF = modulation transfer function = |FT{PSF}|
 """
 
 def kolmogorov_mtf(array_shape=(256, 256), dx=1., lam_over_r0=1.):
-    """@brief Return the atmospheric modulation transfer function for long exposures with 
-    Kolmogorov turbulence as a numpy array. 
+    """Return the atmospheric modulation transfer function for long exposures with Kolmogorov 
+    turbulence as a NumPy array. 
 
-    @param array_shape     the Numpy array shape desired for the array view of the ImageViewD.
+    @param array_shape     the NumPy array shape desired for the array view of the ImageViewD.
     @param dx              grid spacing of PSF in real space units
     @param lam_over_r0     lambda / r0 in the physical units adopted for dx (user responsible for 
                            consistency), where r0 is the Fried parameter. The FWHM of the Kolmogorov
@@ -43,14 +43,14 @@ def kolmogorov_mtf(array_shape=(256, 256), dx=1., lam_over_r0=1.):
     return amtf
 
 def kolmogorov_mtf_image(array_shape=(256, 256), dx=1., lam_over_r0=1.):
-    """@brief Return the atmospheric modulation transfer function for long exposures with 
-    Kolmogorov turbulence as an ImageViewD.
+    """Return the atmospheric modulation transfer function for long exposures with Kolmogorov 
+    turbulence as an ImageViewD.
 
     The ImageView output can be used to directly instantiate an SBInterpolatedImage, and its 
     .getScale() method will reflect the spacing of the output grid in the system of units adopted.
     for lam_over_r0.
 
-    @param array_shape     the Numpy array shape desired for the array view of the ImageViewD.
+    @param array_shape     the NumPy array shape desired for the array view of the ImageViewD.
     @param dx              grid spacing of PSF in real space units
     @param lam_over_r0     lambda / r0 in the physical units adopted for dx (user responsible for 
                            consistency), where r0 is the Fried parameter. The FWHM of the Kolmogorov
@@ -64,9 +64,9 @@ def kolmogorov_mtf_image(array_shape=(256, 256), dx=1., lam_over_r0=1.):
     return galsim.ImageViewD(amtf.astype(np.float64))
 
 def kolmogorov_psf(array_shape=(256,256), dx=1., lam_over_r0=1., flux=1.):
-    """@brief Return numpy array containing long exposure Kolmogorov PSF.
+    """Return NumPy array containing long exposure Kolmogorov PSF.
     
-    @param array_shape     the Numpy array shape desired for the array view of the ImageViewD.
+    @param array_shape     the NumPy array shape desired for the array view of the ImageViewD.
     @param dx              grid spacing of PSF in real space units
     @param lam_over_r0     lambda / r0 in the physical units adopted for dx (user responsible for 
                            consistency), where r0 is the Fried parameter. The FWHM of the Kolmogorov
@@ -85,13 +85,13 @@ def kolmogorov_psf(array_shape=(256,256), dx=1., lam_over_r0=1., flux=1.):
     return im * (flux / (im.sum() * dx**2))
     
 def kolmogorov_psf_image(array_shape=(256, 256), dx=1., lam_over_r0=1., flux=1.):
-    """@brief Return long exposure Kolmogorov PSF as an ImageViewD.
+    """Return long exposure Kolmogorov PSF as an ImageViewD.
 
     The ImageView output can be used to directly instantiate an SBInterpolatedImage, and its 
     .getScale() method will reflect the spacing of the output grid in the system of units adopted.
     for lam_over_diam.
 
-    @param array_shape     the Numpy array shape desired for the array view of the ImageViewD.
+    @param array_shape     the NumPy array shape desired for the array view of the ImageViewD.
     @param dx              grid spacing of PSF in real space units
     @param lam_over_r0     lambda / r0 in the physical units adopted for dx (user responsible for 
                            consistency). r0 is the Fried parameter. Typical values for the 
