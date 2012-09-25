978989beacd2be997f94dfd167281b76c34c1894
Rachel Mandelbaum <rmandelb@andrew.cmu.edu>
2012-09-25 10:22:03 -0400

    removed wrong additions to drawShoot dox re: wmult (#257_2)

--- galsim/base.py
+++ galsim/base.py
@@ -568,13 +568,7 @@ class GSObject(object):
                       numbers output from a CCD).  (Default `gain =  1.`)
 
         @param wmult  A factor by which to make an automatically-sized image larger than 
-                      it would normally be made.  This factor also applies to any intermediate 
-                      images during Fourier calculations.  The size of the intermediate images
-                      are normally automatically chosen to reach some preset accuracy targets 
-                      (see include/galsim/SBProfile.h); however, if you see strange artifacts 
-                      in the image, you might try using wmult > 1.  This will take longer of 
-                      course, but it will produce more accurate images, since they will have 
-                      less "folding" in Fourier space. (Default `wmult = 1.`)
+                      it would normally be made.
 
         @param normalization    Two options for the normalization:
                                 "flux" or "f" means that the sum of the output pixels is normalized
