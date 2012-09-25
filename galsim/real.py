0ffd6ffd1c75be0a3feabb289d94d5e6da7ad3b0
barnabytprowe <barnaby.t.p.rowe@gmail.com>
2012-09-25 12:39:07 +0100

    (#280) Used underscore to make it clearer that get_index_for_id is an internal function, stressed that in docstring too, updated call in base.py

--- galsim/real.py
+++ galsim/real.py
@@ -86,8 +86,8 @@ class RealGalaxyCatalog(object):
         # i.e. (dataset, ID within dataset)
         # also note: will be adding bits of information, like noise properties and galaxy fit params
 
-    def get_index_for_id(self, id):
-        """Find which index number corresponds to the value ID in the ident field.
+    def _get_index_for_id(self, id):
+        """Internal function to find which index # corresponds to the value ID in the ident field.
         """
         # Just to be completely consistent, convert id to a string in the same way we
         # did above for the ident array:
