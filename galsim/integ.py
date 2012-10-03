c7a0460aa647026adf15781fe671c624cd1673d0
Rachel Mandelbaum <rmandelb@andrew.cmu.edu>
2012-10-02 21:40:10 -0400

    added an example to docstring for integ.py (#288)

--- galsim/integ.py
+++ galsim/integ.py
@@ -7,6 +7,16 @@ from . import _galsim
 def int1d(func, min, max, rel_err=1.e-6, abs_err=1.e-12):
     """Integrate a 1-dimensional function from min to max.
 
+    Example usage:
+
+        >>> def func(x): return x**2
+        >>> galsim.int1d(func, 0, 1)
+        0.33333333333333337
+        >>> galsim.int1d(func, 0, 2)
+        2.666666666666667
+        >>> galsim.int1d(func, -1, 1)
+        0.66666666666666674
+
     @param func     The function to be integrated.  y = func(x) should be valid.
     @param min      The lower end of the integration bounds (anything < -1.e10 is treated as
                     negative infinity).
