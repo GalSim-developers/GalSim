fca7851e59b40dd78a1370fe0e2ac1b0db0c3888
Mike Jarvis <michael@jarvis.net>
2012-10-10 08:41:25 -0400

    Attempt to fix Reiko's double np.int32 problem. (#293)

--- galsim/image.py
+++ galsim/image.py
@@ -3,6 +3,23 @@ A few adjustments to the galsim.Image classes at the Python layer, including the
 docstrings.
 """
 from . import _galsim
+import numpy
+
+# Sometimes there are two numpy.int32 types.  This can lead to some confusion when doing
+# arithmetic with images.  So just make sure both of them point to ImageI in the Image dict.
+# One of them is what you get when you just write numpy.int32.  The other is what numpy 
+# decides an int32 value + 1 is.  The first one is usually the one that's already in the
+# Image dict, but we assign both versions just to be sure.
+
+_galsim.Image[numpy.int32] = _galsim.ImageI
+_galsim.ImageView[numpy.int32] = _galsim.ImageViewI
+_galsim.ConstImageView[numpy.int32] = _galsim.ConstImageViewI
+
+alt_int32 = ( numpy.array([0]).astype(numpy.int32) + 1 ).dtype.type
+_galsim.Image[alt_int32] = _galsim.ImageI
+_galsim.ImageView[alt_int32] = _galsim.ImageViewI
+_galsim.ConstImageView[alt_int32] = _galsim.ConstImageViewI
+
 
 # First of all we add docstrings to the Image, ImageView and ConstImageView classes for each of the
 # S, F, I & D datatypes
