caa577a5713f2e225134334685d2be7f080a04a1
Barnaby Rowe <barnaby.t.p.rowe@gmail.com>
2012-11-09 17:14:51 +0000

    put some more stuff into the NoiseCorrFunc header (#297)

--- include/galsim/CorrelatedNoise.h
+++ include/galsim/CorrelatedNoise.h
@@ -14,11 +14,81 @@
 
 namespace galsim {
 
-    class NoiseCorrFunc
+    /**
+     * @brief Class for storing 2D correlation functions represented by interpolation over a data
+     * table/image.
+     *
+     * This class inherits much from SBInterpolatedImage to store the 2D correlation function.
+     * The NoiseCorrFunc and SBInterpolatedImage classes represent a profile (supplied as an image
+     * an image), including rules for how to interpolate the profile between the supplied pixel
+     * values.  Many of the SBProfile methods are, however, disabled.
+     *
+     * NoiseCorrFunc also imposes two-fold rotational symmetry: any pixels in the negative region 
+     * of the input image below the line y = 0 will be ignored.
+     *
+     * It is assumed that the input image oversamples the correlation function profile they 
+     * represent.  maxK() is set at the Nyquist frequency of the input image, although it should be
+     * noted that interpolants other than the ideal sinc function may make the max frequency higher
+     * than this.  The output is required to be periodic on a scale > original image extent + kernel
+     * footprint, and stepK() is set accordingly. 
+     *
+     * The normal way to make an SBInterpolatedImage is to provide the image to interpolate
+     * and the interpolation scheme.  See Interpolant.h for more about the different 
+     * kind of interpolation.  
+     *
+     * You can provide different interpolation schemes for real and fourier space
+     * (passed as xInterp and kInterp respectively).  If either one is omitted, the 
+     * defaults are:
+     *
+     * xInterp = Lanczos(5, fluxConserve=true, tol=kvalue_accuracy)
+     *
+     * kInterp = Quintic(tol=kvalue_accuracy)
+     *
+     * The ideal k-space interpolant is a sinc function; however, the quintic interpolant is the
+     * default, based on detailed investigations on the tradeoffs between accuracy and speed.  Note
+     * that, as in Bernstein & Gruen (2012), the accuracy achieved by this interpolant is dependent
+     * on our choice of 4x pad factor.  Users who do not wish to pad the arrays to this degree may
+     * need to use a higher-order Lanczos interpolant instead, but this is not the recommended
+     * usage.
+     *
+     * There are also optional arguments for the pixel size (default is to get it from
+     * the image), and a factor by which to pad the image (default = 4).
+     */
+    class NoiseCorrFunc: public SBInterpolatedImage
     {
     public:
+        /** 
+         * @brief Initialize internal quantities and allocate data tables based on a supplied 2D 
+         * image of the Correlation function.
+         *
+         * @param[in] image     Input Image (any of ImageF, ImageD, ImageS, ImageI).
+         * @param[in] xInterp   Interpolation scheme to adopt between pixels 
+         * @param[in] kInterp   Interpolation scheme to adopt in k-space
+         * @param[in] dx        Stepsize between pixels in image data table (default value of 
+         *                      `dx = 0.` checks the Image header for a suitable stepsize, sets 
+         *                      to `1.` if none is found). 
+         * @param[in] pad_factor Multiple by which to increase the image size when zero-padding for 
+         *                      the Fourier transform (default `pad_factor = 4`)
+         */
+        template <typename T> 
+        NoiseCorrFunc(
+            const BaseImage<T>& image,
+            boost::shared_ptr<Interpolant2d> xInterp = sbp::defaultXInterpolant2d,
+            boost::shared_ptr<Interpolant2d> kInterp = sbp::defaultKInterpolant2d,
+            double dx=0., double pad_factor=0.);
 
-      NoiseCorrFunc(vector<double> )
+        /// @brief Copy Constructor.
+        NoiseCorrFunc(const NoiseCorrFunc& rhs);
 
+        /// @brief Destructor
+        ~NoiseCorrFunc();
+
+    private:
+        // op= is undefined
+        void operator=(const SBInterpolatedImage& rhs);
+
+        // Most of the SBProfile methods are not available
+        
+    };
 }
 
