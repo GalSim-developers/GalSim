d3cd074ee13411e0370b3d65bb80f3b696a8d509
Barnaby Rowe <barnaby.t.p.rowe@gmail.com>
2012-11-13 17:10:31 +0000

    tidied up debuglogging comments in top of each CorrelatedNoise.c/.h file (#297)

--- include/galsim/CorrelatedNoise.h
+++ include/galsim/CorrelatedNoise.h
@@ -2,11 +2,34 @@
 #ifndef CORRELATEDNOISE_H
 #define CORRELATEDNOISE_H
 
+//#define DEBUGLOGGING
+
+#ifdef DEBUGLOGGING
+#include <fstream>
+std::ostream* dbgout = new std::ofstream("debug.out");
+int verbose_level = 2;
+/*
+ * There are three levels of verbosity which can be helpful when debugging, which are written as
+ * dbg, xdbg, xxdbg (all defined in Std.h).
+ * It's Mike's way to have debug statements in the code that are really easy to turn on and off.
+ *
+ * If DEBUGLOGGING is #defined, then these write out to *dbgout, according to the value of 
+ * verbose_level.
+ * dbg requires verbose_level >= 1
+ * xdbg requires verbose_level >= 2
+ * xxdbg requires verbose_level >= 3
+ * If DEBUGLOGGING is not defined, the all three becomes just `if (false) std::cerr`,
+ * so the compiler parses the statement fine, but trivially optimizes the code away, so there is no
+ * efficiency hit from leaving them in the code.
+ */
+#endif
+
 /**
  * @file CorrelatedNoise.h @brief Contains a class definition for handling the correlation
  * properties of noise in Images.
  */
 
+#include <complex>
 #include "SBInterpolatedImageImpl.h"
 #include "SBInterpolatedImage.h"
 
--- src/CorrelatedNoise.cpp
+++ src/CorrelatedNoise.cpp
@@ -1,3 +1,4 @@
+// -*- c++ -*-
 
 //#define DEBUGLOGGING
 
@@ -5,20 +6,19 @@
 #include <fstream>
 std::ostream* dbgout = new std::ofstream("debug.out");
 int verbose_level = 2;
-// There are three levels of verbosity which can be helpful when debugging,
-// // which are written as dbg, xdbg, xxdbg (all defined in Std.h).
-// // It's Mike's way to have debug statements in the code that are really easy to turn 
-// // on and off.
-// //
-// // If DEBUGLOGGING is #defined, then these write out to *dbgout, according to the value
-// // of verbose_level.
-// // dbg requires verbose_level >= 1
-// // xdbg requires verbose_level >= 2
-// // xxdbg requires verbose_level >= 3
-// //
-// // If DEBUGLOGGING is not defined, the all three becomes just `if (false) std::cerr`,
-// // so the compiler parses the statement fine, but trivially optimizes the code away,
-// // so there is no efficiency hit from leaving them in the code.
+/*
+ * There are three levels of verbosity which can be helpful when debugging, which are written as
+ * dbg, xdbg, xxdbg (all defined in Std.h).
+ * It's Mike's way to have debug statements in the code that are really easy to turn on and off.
+ *
+ * If DEBUGLOGGING is #defined, then these write out to *dbgout, according to the value of 
+ * verbose_level.
+ * dbg requires verbose_level >= 1
+ * xdbg requires verbose_level >= 2
+ * xxdbg requires verbose_level >= 3
+ * If DEBUGLOGGING is not defined, the all three becomes just `if (false) std::cerr`,
+ * so the compiler parses the statement fine, but trivially optimizes the code away, so there is no
+ * efficiency hit from leaving them in the code.
+ */
 #endif
-
 
