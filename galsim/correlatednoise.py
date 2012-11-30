b2946faf365e976b8e1c36374e1f0a665bb93cba
Barnaby Rowe <barnaby.t.p.rowe@gmail.com>
2012-11-30 19:20:20 +0000

    edits, renames to correlated noise (#297)

--- galsim/correlatednoise.py
+++ galsim/correlatednoise.py
@@ -5,8 +5,9 @@ Python layer documentation and functions for handling correlated noise in GalSim
 
 import numpy as np
 from . import galsim
+from . import utilities
 
-class NoiseCorrFunc(GSObject):
+class NoiseCF(galsim.GSObject):
     """A class describing 2D Noise Correlation Functions.
     """
 
@@ -15,15 +16,13 @@ class NoiseCorrFunc(GSObject):
         noise_array = noise_image.array
         # Get the CF using DFTs
         ft_array = np.fft.fft2(noise_array)
-        cf_array = (np.fft.ifft2(ft_array * ft_array.conj())).real / float(np.product(np.shape(ft)))
+        cf_array = (np.fft.ifft2(ft_array * ft_array.conj())).real / \
+            float(np.product(np.shape(ft_array)))
         # Roll CF array to put the centre in image centre.  Remember that numpy stores data [y,x]
-        yxroll = ((cf_array.shape[0] - 1) / 2, (cf_array.shape[1] - 1) / 2)
-        cf_array = np.utilities.roll2d(cf_array, yxroll)
-        cf_image = galsim.ImageViewD(cf_array, xmin=-yxroll[1], ymin=-yxroll[0])
-
-        GSObject.__init__(
-            self, galsim.SBNoiseCF())
-
-def Image_getCorrFunc():
+        yxroll = (cf_array.shape[0] / 2, cf_array.shape[1] / 2)
+        self.cf_array = np.ascontiguousarray(utilities.roll2d(cf_array, yxroll))
+        self.cf_image = galsim.ImageViewD(cf_array, xmin=-yxroll[1], ymin=-yxroll[0])
+        
+#def Image_getNoiseCF():
 
 
