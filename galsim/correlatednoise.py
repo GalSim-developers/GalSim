ef0dbec97ee44c719558edfebfa8aa21bc3b9f14
Barnaby Rowe <barnaby.t.p.rowe@gmail.com>
2012-12-01 23:19:04 +0000

    had a change of heart regarding the name, noticed that the the correlation function is not necessarily for noise.  Also added an image method (#297)

--- galsim/correlatednoise.py
+++ galsim/correlatednoise.py
@@ -4,25 +4,37 @@ Python layer documentation and functions for handling correlated noise in GalSim
 """
 
 import numpy as np
-from . import galsim
+from . import _galsim
+from . import base
 from . import utilities
 
-class NoiseCF(galsim.GSObject):
-    """A class describing 2D Noise Correlation Functions.
+class CorrFunc(base.GSObject):
+    """A class describing 2D Correlation Functions calculated from Images.
     """
 
-    def __init__(self, noise_image):
-        # Build a noise correlation function from the input image of noise
-        noise_array = noise_image.array
-        # Get the CF using DFTs
-        ft_array = np.fft.fft2(noise_array)
-        cf_array = (np.fft.ifft2(ft_array * ft_array.conj())).real / \
-            float(np.product(np.shape(ft_array)))
+    def __init__(self, image):
+        # Build a noise correlation function from the input image, first get the CF using DFTs
+        ft_array = np.fft.fft2(image.array)
+        cf_array = ((np.fft.ifft2(ft_array * ft_array.conj())).real /
+            float(np.product(np.shape(ft_array))))
         # Roll CF array to put the centre in image centre.  Remember that numpy stores data [y,x]
         yxroll = (cf_array.shape[0] / 2, cf_array.shape[1] / 2)
-        self.cf_array = np.ascontiguousarray(utilities.roll2d(cf_array, yxroll))
-        self.cf_image = galsim.ImageViewD(cf_array, xmin=-yxroll[1], ymin=-yxroll[0])
-        
-#def Image_getNoiseCF():
+        cf_array = np.ascontiguousarray(utilities.roll2d(cf_array, yxroll))
+        self.cf_image = _galsim.ImageViewD(cf_array)
+        base.GSObject.__init__(self, _galsim.SBNoiseCF(self.cf_image))
 
+# Make a function for returning Noise correlation
+def Image_getCorrFunc(image):
+    """Returns a CorrFunc instance by calculating the correlation function of image pixels.
+    """
+    return CorrFunc(image.view())
 
+# Then add this Image method to the Image classes
+for Class in _galsim.Image.itervalues():
+    Class.getCorrFunc = Image_getCorrFunc
+
+for Class in _galsim.ImageView.itervalues():
+    Class.getCorrFunc = Image_getCorrFunc
+
+for Class in _galsim.ConstImageView.itervalues():
+    Class.getCorrFunc = Image_getCorrFunc
