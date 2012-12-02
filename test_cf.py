fc4758ab07daaddea5f1c888af57ed90a7024601
Barnaby Rowe <barnaby.t.p.rowe@gmail.com>
2012-12-02 01:48:02 +0000

    updated cf sanity checking script to make nice diagrams of CFs and CV matrices for simple realizations of correlated noise.  Nice. (#297)

--- test_cf.py
+++ test_cf.py
@@ -17,10 +17,44 @@ if __name__ == "__main__":
     oncf = galsim.correlatednoise.CorrFunc(onoise)
     print oncf.SBProfile.xValue(galsim.PositionD(0, 0))
 
+    plt.figure()
     testim = galsim.ImageD(10, 10)
-    out = encf.SBProfile.getCovarianceMatrix(testim.view())
-    plt.pcolor(out.array); plt.colorbar()
-    
+    cv = encf.SBProfile.getCovarianceMatrix(testim.view(), dx=1.)
+    #plt.pcolor(cv.array); plt.colorbar()
+
+    # Construct an image with noise correlated in the y direction
+    plt.figure()
+    ynoise = galsim.ImageViewD(enoise.array[:, :] + np.roll(enoise.array, 1, axis=0))
+    plt.pcolor(ynoise.array); plt.colorbar()
+    plt.title('Noise correlated in y direction')
+
+    yncf = galsim.correlatednoise.CorrFunc(ynoise)
+    yim = galsim.ImageD(IMSIZE, IMSIZE)
+    yncf.draw(yim, dx=1.)
+    plt.figure()
+    plt.pcolor(yim.array); plt.colorbar()
+    plt.title('CorrFunc with noise correlated in y direction')
+    ycv = yncf.SBProfile.getCovarianceMatrix(testim.view(), dx=1.)
+    plt.figure()
+    plt.pcolor(ycv.array); plt.colorbar()
+    plt.title('CorrFunc.getCovarianceMatrix() with noise correlated in y direction')
+
+    # Then construct an image with noise correlated in the x direction
+    plt.figure()
+    xnoise = galsim.ImageViewD(enoise.array[:, :] + np.roll(enoise.array, 1, axis=1))
+    plt.pcolor(xnoise.array); plt.colorbar()
+    plt.title('Noise correlated in x direction')
+
+    xncf = galsim.correlatednoise.CorrFunc(xnoise)
+    xim = galsim.ImageD(IMSIZE, IMSIZE)
+    xncf.draw(xim, dx=1.)
+    plt.figure()
+    plt.pcolor(xim.array); plt.colorbar()
+    plt.title('CorrFunc with noise correlated in x direction')
+    xcv = xncf.SBProfile.getCovarianceMatrix(testim.view(), dx=1.)
+    plt.figure()
+    plt.pcolor(xcv.array); plt.colorbar()
+    plt.title('CorrFunc.getCovarianceMatrix() with noise correlated in x direction')
     plt.show()
 
 
