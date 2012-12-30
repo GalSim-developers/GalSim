83b88064cef826acc2895ddad4c98cc31b8705f2
Rachel Mandelbaum <rmandelb@andrew.cmu.edu>
2012-12-29 21:01:21 -0500

    updated some examples to use InterpolatedImage (#333)

--- examples/Shera.py
+++ examples/Shera.py
@@ -41,16 +41,13 @@ def main(argv):
     e = galsim.Ellipse(s, -(g1*g1+g2*g2), galsim.PositionD(xshift,yshift));
 
     galaxyImg = galsim.fits.read(rootname + "_masknoise.fits")
-    galaxy = galsim.GSObject(galsim.SBInterpolatedImage(galaxyImg, xInterp=l32d, dx=dxHST, pad_factor=1.0))
-    galaxy.setFlux(0.804*1000.*dxSDSS*dxSDSS)
+    galaxy = galsim.InterpolatedImage(galaxyImg, interpolant=l32d, dx=dxHST, flux=0.804*1000.*dxSDSS*dxSDSS)
 
     psf1Img = galsim.fits.read(rootname + ".psf.fits")
-    psf1 = galsim.GSObject(galsim.SBInterpolatedImage(psf1Img, xInterp=l32d, dx=dxHST, pad_factor=2.))
-    psf1.setFlux(1.)
+    psf1 = galsim.InterpolatedImage(psf1Img, interpolant=l32d, dx=dxHST, flux=1.)
 
     psf2Img = galsim.fits.read(rootname + ".sdsspsf.fits")
-    psf2 = galsim.GSObject(galsim.SBInterpolatedImage(psf2Img, xInterp=l32d, dx=dxSDSS, pad_factor=2.))
-    psf2.setFlux(1.)
+    psf2 = galsim.InterpolatedImage(psf2Img, interpolant=l32d, dx=dxSDSS, flux=1.)
 
     outImg = galsim.fits.read(rootname + ".g1_0.02.g2_0.00.fits")
     result = outImg.copy()
