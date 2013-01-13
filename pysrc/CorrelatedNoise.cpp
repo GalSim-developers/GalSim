5043c53f3dea150d29e3589a24c506b4b521469c
Barnaby Rowe <barnaby.t.p.rowe@gmail.com>
2013-01-13 01:24:16 +0000

    added draw wrapping to wrapTempplates in the pysrc (#297)

--- pysrc/CorrelatedNoise.cpp
+++ pysrc/CorrelatedNoise.cpp
@@ -12,21 +12,53 @@ namespace galsim {
         template <typename U, typename W>
         static void wrapTemplates(W & wrapper) {
             wrapper
-                .def(bp::init<const BaseImage<U> &,
-                     boost::shared_ptr<InterpolantXY>,
-                     boost::shared_ptr<InterpolantXY>,
-                     double, double>(
-                         (bp::arg("image"),
-                          bp::arg("xInterp")=bp::object(),
-                          bp::arg("kInterp")=bp::object(),
-                          bp::arg("dx")=0., bp::arg("pad_factor")=0.)
-                     ))
+                .def(
+                    bp::init<const BaseImage<U> &,
+                    boost::shared_ptr<InterpolantXY>,
+                    boost::shared_ptr<InterpolantXY>,
+                    double, double>(
+                        (
+                            bp::arg("image"), bp::arg("xInterp")=bp::object(),
+                            bp::arg("kInterp")=bp::object(),
+                            bp::arg("dx")=0., bp::arg("pad_factor")=0.
+                        )
+                     )
+               )
                .def(
                      "getCovarianceMatrix", (
                          Image<double> (CorrelationFunction::*) (ImageView<U>, double) 
                          const)&CorrelationFunction::getCovarianceMatrix, 
                       (bp::arg("image"), bp::arg("dx")=0.))
-                  ;
+               ;
+               .def("drawShoot", 
+                   (double (CorrelationFunction::*)(
+                       ImageView<U>, double, UniformDeviate, double, double, bool
+                   ) const)&SBProfile::drawShoot, (
+                       bp::arg("image"), bp::arg("N")=0., bp::arg("ud"), bp::arg("gain")=1., 
+                       bp::arg("max_extra_noise")=0., bp::arg("poisson_flux")=true
+                   ),
+                   "Draw object into existing image using photon shooting.\n"
+                   "\n"
+                   "Setting optional integer arg possionFlux != 0 allows profile flux to vary \n"
+                   "according to Poisson statistics for N samples.\n"
+                   "\n"
+                   "Returns total flux of photons that landed inside image bounds."
+                )
+                .def("draw", 
+                     (double (SBProfile::*)(ImageView<U>, double, double) const)&SBProfile::draw, (
+                         bp::arg("image"), bp::arg("gain")=1., bp::arg("wmult")=1.
+                     ),
+                     "Draw in-place and return the summed flux."
+                )
+                .def("drawK", 
+                     (void (SBProfile::*)(
+                         ImageView<U>, ImageView<U>, double, double
+                     ) const)&SBProfile::drawK, (
+                         bp::arg("re"), bp::arg("im"), bp::arg("gain")=1., bp::arg("wmult")=1.
+                     ),
+                     "Draw k-space image (real and imaginary components)."
+                )
+                ;
         }
 
         static void wrap() {
