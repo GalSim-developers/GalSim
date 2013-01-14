93c852b3675c1016037419b6748c746d4b8b08b8
Barnaby Rowe <barnaby.t.p.rowe@gmail.com>
2013-01-14 01:20:30 +0000

    updated comment (#297)

--- include/galsim/SBInterpolatedImageImpl.h
+++ include/galsim/SBInterpolatedImageImpl.h
@@ -110,7 +110,7 @@ namespace galsim {
         double doFillXImage(ImageView<double>& I, double gain) const
         { return fillXImage(I,gain); }
 
-    protected:  // Made protected so that these can be used in the derived NoiseCorrFunc class
+    protected:  // Made protected so that these can be used in the derived CorrelationFunction class
 
         MultipleImageHelper _multi;
         std::vector<double> _wts;
