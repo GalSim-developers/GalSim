e1e7bbeaeb51bf36a1f26b8ad58326a6e4d3b26a
Mike Jarvis <michael@jarvis.net>
2013-01-15 03:24:14 -0500

    Add some debugging lines (#299)

--- galsim/fits.py
+++ galsim/fits.py
@@ -407,12 +407,14 @@ def read(fits, compression='auto'):
     """
     import pyfits     # put this at function scope to keep pyfits optional
     
+    print 'Start read: fits = ',fits
     file_compress, pyfits_compress = parse_compression(compression,fits)
 
     fin = None
     if isinstance(fits, basestring):
         hdus, fin = read_file(fits, file_compress)
         fits = hdus
+    print 'After read_file: fits = ',fits
 
     if isinstance(fits, pyfits.HDUList):
         # Note: Nothing special needs to be done when reading a compressed hdu.
@@ -426,6 +428,7 @@ def read(fits, compression='auto'):
             if len(fits) < 1:
                 raise IOError('Expecting at least one HDU in galsim.read')
             fits = fits[0]
+    print 'fits => ',fits
 
     xmin = fits.header.get("GS_XMIN", 1)
     ymin = fits.header.get("GS_YMIN", 1)
@@ -440,6 +443,10 @@ def read(fits, compression='auto'):
         warnings.warn("Using float")
         Class = _galsim.ImageViewD
         import numpy
+        print 'fits.data = ',fits.data
+        print 'dtype = ',fits.data.dtype
+        print 'dtype.type = ',fits.data.dtype.type
+        print 'shape = ',fits.data.shape
         data = fits.data.astype(numpy.float64)
 
     # Check through byteorder possibilities, compare to native (used for numpy and our default) and
