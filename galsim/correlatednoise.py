c05b374fc4c48c31a5535fdbcf7e242670bde833
Barnaby Rowe <barnaby.t.p.rowe@gmail.com>
2013-01-17 23:27:51 +0000

    made changes to adapt the ImageCorrFunc to be the only obvious means to generate a CorrFunc, and to disable methods I don't think make sense to have access to. (#297)

--- galsim/correlatednoise.py
+++ galsim/correlatednoise.py
@@ -8,32 +8,12 @@ import galsim
 from . import base
 from . import utilities
 
-class CorrFunc(object):
+class _CorrFunc(object):
     """A class describing 2D correlation functions, typically calculated from Images.
 
-    Initialization
-    --------------
-    The constructor for CorrFunc takes a GSObject, which is then reinterpreted as the 
-    correlation function.  However, the typical way to create a CorrFunc is with the 
-    factory function ImageCorrFunc.  See that function for more info.
-
-    Members
-    -------
-    The most useful member is `profile`, which is the internally stored GSObject profile.
-    It can be manipulated using the various methods of GSObject.  So the following are
-    all legal:
-    
-        cf.profile.applyShear(s)
-        cf.profile = cf1.profile + cf2.profile
-        cf.profile /= 2
-        cf.profile.draw(im,dx)
-
-    Methods
-    -------
-    The main way that CorrFunc is used is to add correlated noise to an image.  This is 
-    done with
-
-        cf.addNoiseTo(im)
+    A CorrFunc will not generally be instantiated directly.  It defines the way in which derived
+    classes (currently only the ImageCorrFunc) store the correlation function profile and generates
+    images containing noise with these correlation properties.
     """
     def __init__(self, gsobject):
         if not isinstance(gsobject, base.GSObject):
@@ -49,13 +29,76 @@ class CorrFunc(object):
         # So for now, we start out with store_profile = None.
         self.profile_for_stored = None
 
+        # Delete some of the methods we don't want the use to have access to since they don't make
+        # sense
+        self.profile.applyShift = self._notImplemented
+        self.profile.createShifted = self._notImplemented
+        self.profile.applyDilation = self._notImplemented
+        self.profile.createDilated = self._notImplemented
+        self.profile.setFlux = self._notImplemented
+        self.profile.getFlux = self._notImplemented
+        self.profile.drawShoot = self._notImplemented # this isn't really needed
+        
+        # The statements below replace the self.profile.scaleFlux method with the much more
+        # appropriately named self.profile.scaleVariance method
+        self.profile.scaleVariance = self._scaleVariance
+        self.profile.scaleFlux = self._notImplemented
+
+        # We use a similar pattern to replace the GSObject draw() method with a version that always
+        # uses the "surface brightness" normalization rather than the default "flux"
+        if not hasattr(self.profile, "_drawHidden"): # only do following once or becomes circular...
+            self.profile._drawHidden = self.profile.draw # hide the method for internal use
+        self.profile.draw = self._draw
+
+    # Make add work in the intuitive sense (variances being additive, correlation functions add as
+    # you would expect)
+    def __add__(self, other):
+        ret = self.copy()
+        ret += other
+        return ret
+
+    def __iadd__(self, other):
+        self.profile += other.profile
+        return _CorrFunc(self.profile)
+
+    # Make op* and op*= work to adjust the overall variance of an object
+    def __imul__(self, other):
+        self.profile.scaleVariance(other)
+        return self
+
+    def __mul__(self, other):
+        ret = self.copy()
+        ret *= other
+        return ret
+
+    def __rmul__(self, other):
+        ret = self.copy()
+        ret *= other
+        return ret
+
+    # Likewise for op/ and op/=
+    def __idiv__(self, other):
+        self.profile.scaleVariance(1. / other)
+        return self
+
+    def __div__(self, other):
+        ret = self.copy()
+        ret /= other
+        return ret
+
+    def __itruediv__(self, other):
+        return __idiv__(self, other)
+
+    def __truediv__(self, other):
+        return __div__(self, other)
+
     def copy(self):
         """Returns a copy of the correlation function.
         """
-        return CorrFunc(self.profile.copy())
+        return _CorrFunc(self.profile.copy())
 
     def applyNoiseTo(self, image, dx=0., dev=None, add_to_image=True):
-        """Add noise as a Gaussian random field with this correlation function to an input Image.
+        """Apply noise as a Gaussian random field with this correlation function to an input Image.
 
         If the optional image pixel scale `dx` is not specified, `image.getScale()` is used for the
         input image pixel separation.
@@ -129,7 +172,7 @@ class CorrFunc(object):
             else:
                 newcf.setScale(dx)
             # Then draw this correlation function into an array
-            self.profile.draw(newcf, dx=None) # setting dx=None here uses the newcf image scale set above
+            self.profile.draw(newcf, dx=None) # setting dx=None uses the newcf image scale set above
 
             # Roll to put the origin at the lower left pixel before FT-ing to get the PS...
             rolled_cf_array = utilities.roll2d(
@@ -153,19 +196,46 @@ class CorrFunc(object):
             image = galsim.ImageViewD(np.ascontiguousarray(noise_array.real))
         return image
 
+    # Now I define some methods that are not used by this instance directly, but are used to
+    # redefine the behaviour of the stored profile, or print a method saying that this method is not
+    # implemented
+    def _scaleVariance(self, variance_ratio):
+        """Multiply the overall variance of the correlation function profile by variance_ratio.
+
+        @param variance_ratio The factor by which to scale the variance of the correlation function
+                              profile.
+        """
+        self.profile.SBProfile.scaleFlux(variance_ratio)
+
+    def _notImplemented(self, *args, **kwargs):
+        raise NotImplementedError(
+            "Sorry, this method is not available for profiles which represent correlation "+
+            "functions.")
+
+    def _draw(self, image=None, dx=None, gain=1., wmult=1., add_to_image=False,
+              normalization="surface brightness"):
+        """The draw method for profiles storing correlation functions.
+
+        This is a very mild reimplementation of the draw() method for GSObjects.  The normalization
+        keyword argument default is changed to "surface brightness", as appropriate for this profile
+        which is being used to store a correlation function.
+
+        See the general GSObject draw() method for more information on the other parameters.
+        """
+        return self.profile._drawHidden(
+            image=image, dx=dx, gain=gain, wmult=wmult, normalization=normalization,
+            add_to_image=add_to_image)
+
+
 ###
 # Then we define the ImageCorrFunc, which generates a correlation function by estimating it directly
 # from images:
 #
-def ImageCorrFunc(image, dx=0., interpolant=None):
-    """A factory function for making a CorrFunc from an image:
+class ImageCorrFunc(_CorrFunc):
+    """A class that represents 2D discrete correlation functions calculated from an input Image.
 
-    The correlation function is calculated from its pixel values using the NumPy FFT functions.
-    Optionally, the pixel scale for the input `image` can be specified using the `dx` keyword
-    argument. 
-
-    If `dx` is not set the value returned by `image.getScale()` is used unless this is <= 0, in
-    which case a scale of 1 is assumed.
+    Initialization
+    --------------
 
     Basic example:
 
@@ -193,81 +263,151 @@ def ImageCorrFunc(image, dx=0., interpolant=None):
     The default interpolant if None is set is a galsim.InterpolantXY(galsim.Linear(tol=1.e-4)),
     which uses bilinear interpolation.  Initial tests indicate the favourable performance of this
     interpolant in applications involving correlated pixel noise.
+
+    Attributes
+    ----------
+    The key attribue is `profile`, which is the internally stored GSObject profile.  It can be
+    manipulated using many of the various methods of the GSObject.  The following are all legal:
+    
+        cf.profile.applyShear(s)
+        cf.profile.applyMagnification(m)
+        cf.profile.applyRotation(theta * galsim.degrees)
+        cf.profile.xValue(galsim.PositionD(0., 0.5))
+        cf.profile.kValue(galsim.PositionD(0.5, 0.))
+        cf.profile.draw(im, dx)
+        cf.profile.drawK(im, dk)
+
+    However, some of the GSObject methods are not available, since they do not really make sense
+    for correlation functions.  Calling any of
+
+        cf.profile.applyShift
+        cf.profile.createShifted
+        cf.profile.applyDilation
+        cf.profile.createDilated
+        cf.profile.setFlux
+        cf.profile.scaleFlux
+        cf.profile.getFlux
+        cf.profile.drawShoot
+        cf.profile.scaleFlux
+
+    will raise a `NotImplementedError`.  Note that while the `draw()` method is enabled, it has been
+    reimplemented to make the default normalization keyword take the value 'surface brightness' as
+    is appropriate for rendering correlation functions.
+
+    A new method, which is in fact a reimplmentation of the `cf.profile.scaleFlux()` method, is
+
+        cf.profile.scaleVariance(variance_ratio)
+
+    which scales the overall correlation function, and therefore its total variance, by a scalar
+    factor `variance_ratio`.
+
+    Arithmetic Operators
+    --------------------
+    Addition, multiplication and division operators are defined to work in an intuitive way for
+    correlation functions.
+
+    Addition works simply to add the correlation functions stored in the `.profile` attribute, so
+    that
+
+        >>> cf2 = cf0 + cf1
+        >>> cf2 += cf1
+
+    are functionally equivalent to
+
+        >>> cf2.profile = cf0.profile + cf1.profile
+        >>> cf2.profile += cf1.profile
+
+    but slightly more convenient.  The multiplication and division operators scale the overall
+    correlation function for scalar operands, using the `cf.profile.scaleVariance()` method.
+     
+
+    Methods
+    -------
+    The main way that ImageCorrFunc is used is to add or assign correlated noise to an image.
+    This is done with
+
+        cf.applyNoiseTo(im)
+
+    The correlation function is calculated from its pixel values using the NumPy FFT functions.
+    Optionally, the pixel scale for the input `image` can be specified using the `dx` keyword
+    argument.  See the `applyNoiseTo` docstring for more information.
+
+    If `dx` is not set the value returned by `image.getScale()` is used unless this is <= 0, in
+    which case a scale of 1 is assumed.
+    
     """
-    # Build a noise correlation function (CF) from the input image, using DFTs
+    def __init__(self, image, dx=0., interpolant=None):
 
-    # Calculate the power spectrum then a (preliminary) CF 
-    ft_array = np.fft.fft2(image.array)
-    ps_array = np.abs(ft_array * ft_array.conj())
-    cf_array_prelim = (np.fft.ifft2(ps_array)).real / float(np.product(np.shape(ft_array)))
+        # Build a noise correlation function (CF) from the input image, using DFTs
 
-    # Roll CF array to put the centre in image centre.  Remember that numpy stores data [y,x]
-    cf_array_prelim = utilities.roll2d(
-        cf_array_prelim, (cf_array_prelim.shape[0] / 2, cf_array_prelim.shape[1] / 2))
+        # Calculate the power spectrum then a (preliminary) CF 
+        ft_array = np.fft.fft2(image.array)
+        ps_array = np.abs(ft_array * ft_array.conj())
+        cf_array_prelim = (np.fft.ifft2(ps_array)).real / float(np.product(np.shape(ft_array)))
 
-    # The underlying C++ object is expecting the CF to be represented by an odd-dimensioned 
-    # array with the central pixel denoting the zero-distance correlation (variance), even 
-    # even if the input image was even-dimensioned on one or both sides.
-    # We therefore copy-paste and zero pad the CF calculated above to ensure that these
-    # expectations are met. 
-    #
-    # Determine the largest dimension of the input image, and use it to generate an empty CF 
-    # array for final output, padding by one to make odd if necessary:
-    cf_array = np.zeros((
-        1 + 2 * (cf_array_prelim.shape[0] / 2), 
-        1 + 2 * (cf_array_prelim.shape[1] / 2))) # using integer division
-    # Then put the data from the prelim CF into this array
-    cf_array[0:cf_array_prelim.shape[0], 0:cf_array_prelim.shape[1]] = cf_array_prelim
-    # Then copy-invert-paste data from the leftmost column to the rightmost column, and lowest
-    # row to the uppermost row, if the the original CF had even dimensions in the x and y 
-    # directions, respectively (remembering again that NumPy stores data [y,x] in arrays)
-    if cf_array_prelim.shape[1] % 2 == 0: # first do x
-        lhs_column = cf_array[:, 0]
-        cf_array[:, cf_array_prelim.shape[1]] = lhs_column[::-1] # inverts order as required
-    if cf_array_prelim.shape[0] % 2 == 0: # then do y
-        bottom_row = cf_array[0, :]
-        cf_array[cf_array_prelim.shape[0], :] = bottom_row[::-1] # inverts order as required
+        # Roll CF array to put the centre in image centre.  Remember that numpy stores data [y,x]
+        cf_array_prelim = utilities.roll2d(
+            cf_array_prelim, (cf_array_prelim.shape[0] / 2, cf_array_prelim.shape[1] / 2))
 
-    # Store local copies of the original image, power spectrum and modified correlation function
-    original_image = image.view() # Makes a new object without copying data values
-    original_ps_image = galsim.ImageViewD(np.ascontiguousarray(ps_array))
-    original_cf_image = galsim.ImageViewD(np.ascontiguousarray(cf_array))
+        # The underlying C++ object is expecting the CF to be represented by an odd-dimensioned 
+        # array with the central pixel denoting the zero-distance correlation (variance), even 
+        # even if the input image was even-dimensioned on one or both sides.
+        # We therefore copy-paste and zero pad the CF calculated above to ensure that these
+        # expectations are met. 
+        #
+        # Determine the largest dimension of the input image, and use it to generate an empty CF 
+        # array for final output, padding by one to make odd if necessary:
+        cf_array = np.zeros((
+            1 + 2 * (cf_array_prelim.shape[0] / 2), 
+            1 + 2 * (cf_array_prelim.shape[1] / 2))) # using integer division
+        # Then put the data from the prelim CF into this array
+        cf_array[0:cf_array_prelim.shape[0], 0:cf_array_prelim.shape[1]] = cf_array_prelim
+        # Then copy-invert-paste data from the leftmost column to the rightmost column, and lowest
+        # row to the uppermost row, if the the original CF had even dimensions in the x and y 
+        # directions, respectively (remembering again that NumPy stores data [y,x] in arrays)
+        if cf_array_prelim.shape[1] % 2 == 0: # first do x
+            lhs_column = cf_array[:, 0]
+            cf_array[:, cf_array_prelim.shape[1]] = lhs_column[::-1] # inverts order as required
+        if cf_array_prelim.shape[0] % 2 == 0: # then do y
+            bottom_row = cf_array[0, :]
+            cf_array[cf_array_prelim.shape[0], :] = bottom_row[::-1] # inverts order as required
 
-    # Store the original image scale if set
-    if dx > 0.:
-        original_image.setScale(dx)
-        original_cf_image.setScale(dx)
-    elif image.getScale() > 0.:
-        original_cf_image.setScale(image.getScale())
-    else: # sometimes Images are instantiated with scale=0, in which case we will assume unit
-            # pixel scale
-        original_image.setScale(1.)
-        original_cf_image.setScale(1.)
+        # Store power spectrum and correlation function in an image 
+        original_ps_image = galsim.ImageViewD(np.ascontiguousarray(ps_array))
+        original_cf_image = galsim.ImageViewD(np.ascontiguousarray(cf_array))
 
-    # If interpolant not specified on input, use bilinear
-    if interpolant == None:
-        linear = galsim.Linear(tol=1.e-4)
-        interpolant = galsim.InterpolantXY(linear)
-    else:
-        if isinstance(interpolant, galsim.Interpolant):
-            interpolant = galsim.InterpolantXY(interpolant)
-        elif isinstance(interpolant, galsim.InterpolantXY):
-            interpolant = interpolant
+        # Correctly record the original image scale if set
+        if dx > 0.:
+            original_cf_image.setScale(dx)
+        elif image.getScale() > 0.:
+            original_cf_image.setScale(image.getScale())
+        else: # sometimes Images are instantiated with scale=0, in which case we will assume unit
+              # pixel scale
+            original_image.setScale(1.)
+            original_cf_image.setScale(1.)
+
+        # If interpolant not specified on input, use bilinear
+        if interpolant == None:
+            linear = galsim.Linear(tol=1.e-4)
+            interpolant = galsim.InterpolantXY(linear)
         else:
-            raise RuntimeError(
-                'Specified interpolant is not an Interpolant or InterpolantXY instance!')
+            if isinstance(interpolant, galsim.Interpolant):
+                interpolant = galsim.InterpolantXY(interpolant)
+            elif isinstance(interpolant, galsim.InterpolantXY):
+                interpolant = interpolant
+            else:
+                raise RuntimeError(
+                    'Specified interpolant is not an Interpolant or InterpolantXY instance!')
 
-    # Then initialize...
-    profile = base.InterpolatedImage(original_cf_image, interpolant, dx=original_image.getScale())
-    ret = CorrFunc(profile)
-        
-    # Finally store useful data as a (rootps, dx) tuple for efficient later use:
-    ret.profile_for_stored = profile
-    ret._rootps_store = []
-    ret._rootps_store.append(
-        (np.sqrt(original_ps_image.array), original_cf_image.getScale()))
+        # Then initialize...
+        _CorrFunc.__init__(self, base.InterpolatedImage(
+            original_cf_image, interpolant, dx=original_cf_image.getScale(), normalization="sb"))
 
-    return ret
+        # Finally store useful data as a (rootps, dx) tuple for efficient later use:
+        self.profile_for_stored = self.profile
+        self._rootps_store = []
+        self._rootps_store.append(
+            (np.sqrt(original_ps_image.array), original_cf_image.getScale()))
 
 # Make a function for returning Noise correlations
 def _Image_getCorrFunc(image):
