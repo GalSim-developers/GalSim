d2b1b6429fc45c89743d76c2d9f359bc5589b453
Rachel Mandelbaum <rmandelb@andrew.cmu.edu>
2013-01-31 10:11:34 -0500

    power spectrum tests (#248)

---
+++ devel/external/test_gridshear/plot_pk_test.py
@@ -0,0 +1,122 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# read in theory quantities
+tfile = 'ps.wmap7lcdm.2000.dat'
+tdata = np.loadtxt(tfile)
+tell = tdata[:,0]
+tcell = tdata[:,1]
+tdel = tcell*(tell**2)/(2.*np.pi)
+
+# first do P_E case
+## read in data
+galsim_file = 'output/ps.results.input_pe.dat'
+galsim_data = np.loadtxt(galsim_file)
+ell = galsim_data[:,0]
+galsim_del_ee = (ell**2)*galsim_data[:,1]/(2.*np.pi)
+galsim_del_bb = (ell**2)*galsim_data[:,2]/(2.*np.pi)
+galsim_del_eb = (ell**2)*galsim_data[:,3]/(2.*np.pi)
+sht_file = 'output/ps.results.input_pe.sht.dat'
+sht_data = np.loadtxt(sht_file)
+sht_del_ee = (ell**2)*sht_data[:,1]/(2.*np.pi)
+sht_del_bb = (ell**2)*sht_data[:,2]/(2.*np.pi)
+sht_del_eb = (ell**2)*sht_data[:,3]/(2.*np.pi)
+
+tmp_file = 'output/ps.results.input_pe.halfn.dat'
+tmp_data = np.loadtxt(tmp_file)
+ell_half = tmp_data[:,0]
+tmp_del_ee = (ell_half**2)*tmp_data[:,1]/(2.*np.pi)
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+ax.plot(tell, tdel, color='black', linestyle='dotted', label='theory')
+ax.plot(ell, galsim_del_ee, color='red', label='GalSim EE')
+ax.plot(ell, sht_del_ee, color='red', linestyle='dashed', label='SHT EE')
+ax.plot(ell, galsim_del_bb, color='blue', label='GalSim BB')
+ax.plot(ell, sht_del_bb, color='blue', linestyle='dashed', label='SHT BB')
+#ax.plot(ell, galsim_del_eb, color='green', label='GalSim EB')
+#ax.plot(ell, sht_del_eb, color='green', linestyle='dashed', label='SHT EB')
+ax.set_xscale('log')
+ax.set_yscale('log')
+ax.set_xlabel('ell')
+ax.set_ylabel('ell^2 Cell/2pi')
+ax.set_title('Input P_E = P(k), P_B=0')
+plt.legend(loc='upper left')
+plt.savefig('output/compare_input_pe.eps')
+
+ratio = galsim_del_ee/sht_del_ee
+print ratio
+
+fig = plt.figure()
+ax =fig.add_subplot(111)
+ax.plot(ell, galsim_del_ee, color='red', label='P_EE, orig grid')
+ax.plot(ell_half, tmp_del_ee, color='green', label='P_EE, half grid')
+ax.set_xscale('log')
+ax.set_yscale('log')
+ax.set_xlabel('ell')
+ax.set_ylabel('ell^2 Cell(EE)/2pi')
+ax.set_title('Input P_E = P(k), P_B=0')
+plt.legend(loc='upper left')
+plt.savefig('output/compare_input_pe_halfn.eps')
+
+# next do P_B case
+## read in data
+galsim_file = 'output/ps.results.input_pb.dat'
+galsim_data = np.loadtxt(galsim_file)
+ell = galsim_data[:,0]
+galsim_del_ee = (ell**2)*galsim_data[:,1]/(2.*np.pi)
+galsim_del_bb = (ell**2)*galsim_data[:,2]/(2.*np.pi)
+galsim_del_eb = (ell**2)*galsim_data[:,3]/(2.*np.pi)
+sht_file = 'output/ps.results.input_pb.sht.dat'
+sht_data = np.loadtxt(sht_file)
+sht_del_ee = (ell**2)*sht_data[:,1]/(2.*np.pi)
+sht_del_bb = (ell**2)*sht_data[:,2]/(2.*np.pi)
+sht_del_eb = (ell**2)*sht_data[:,3]/(2.*np.pi)
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+ax.plot(tell, tdel, color='black', linestyle='dotted', label='theory')
+ax.plot(ell, galsim_del_ee, color='red', label='GalSim EE')
+ax.plot(ell, sht_del_ee, color='red', linestyle='dashed', label='SHT EE')
+ax.plot(ell, galsim_del_bb, color='blue', label='GalSim BB')
+ax.plot(ell, sht_del_bb, color='blue', linestyle='dashed', label='SHT BB')
+#ax.plot(ell, galsim_del_eb, color='green', label='GalSim EB')
+#ax.plot(ell, sht_del_eb, color='green', linestyle='dashed', label='SHT EB')
+ax.set_xscale('log')
+ax.set_yscale('log')
+ax.set_xlabel('ell')
+ax.set_ylabel('ell^2 Cell/2pi')
+ax.set_title('Input P_E = 0, P_B=P(k)')
+plt.legend(loc='upper left')
+plt.savefig('output/compare_input_pb.eps')
+
+# first do P_E case
+## read in data
+galsim_file = 'output/ps.results.input_peb.dat'
+galsim_data = np.loadtxt(galsim_file)
+ell = galsim_data[:,0]
+galsim_del_ee = (ell**2)*galsim_data[:,1]/(2.*np.pi)
+galsim_del_bb = (ell**2)*galsim_data[:,2]/(2.*np.pi)
+galsim_del_eb = (ell**2)*galsim_data[:,3]/(2.*np.pi)
+sht_file = 'output/ps.results.input_peb.sht.dat'
+sht_data = np.loadtxt(sht_file)
+sht_del_ee = (ell**2)*sht_data[:,1]/(2.*np.pi)
+sht_del_bb = (ell**2)*sht_data[:,2]/(2.*np.pi)
+sht_del_eb = (ell**2)*sht_data[:,3]/(2.*np.pi)
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+ax.plot(tell, tdel, color='black', linestyle='dotted', label='theory')
+ax.plot(ell, galsim_del_ee, color='red', label='GalSim EE')
+ax.plot(ell, sht_del_ee, color='red', linestyle='dashed', label='SHT EE')
+ax.plot(ell, galsim_del_bb, color='blue', label='GalSim BB')
+ax.plot(ell, sht_del_bb, color='blue', linestyle='dashed', label='SHT BB')
+#ax.plot(ell, galsim_del_eb, color='green', label='GalSim EB')
+#ax.plot(ell, sht_del_eb, color='green', linestyle='dashed', label='SHT EB')
+ax.set_xscale('log')
+ax.set_yscale('log')
+ax.set_xlabel('ell')
+ax.set_ylabel('ell^2 Cell/2pi')
+ax.set_title('Input P_E = P_B = P(k)')
+plt.legend(loc='upper left')
+plt.savefig('output/compare_input_peb.eps')
