c7fb328cbdae00796d4f7a50fcdf77dab903940c
Mike Jarvis <michael@jarvis.net>
2013-02-25 14:28:28 -0500

    Add test_shapelet_properties (#350)

--- tests/test_shapelet.py
+++ tests/test_shapelet.py
@@ -142,24 +142,36 @@ def test_shapelet():
 
 
 def test_shapelet_properties():
-    """Test some basic properties of the SBGaussian profile.
+    """Test some basic properties of the SBShapelet profile.
     """
     import time
     t1 = time.time()
-    psf = galsim.SBGaussian(flux=1, sigma=1)
+
+    # Just a semi-random particular vector of coefficients.
+    order = 4
+    bvec = [1.3,                    # n = 0
+            0.02, 0.03,             # n = 1
+            0.23, -0.19, 0.08,      # n = 2
+            0, 0, 0, 0,             # n = 3
+            0, 0, 0, 0, 0.11]       # n = 4
+    sigma = 1.8
+
+    shapelet = galsim.Shapelet(sigma=sigma, order=order, bvec=bvec)
+
     # Check that we are centered on (0, 0)
-    cen = galsim.PositionD(0, 0)
-    np.testing.assert_equal(psf.centroid(), cen)
+    flux = 1.3 + 0.08 + 0.11
+    np.testing.assert_almost_equal(shapelet.getFlux(), flux, 10)
+    cen = galsim.PositionD(0.02, -0.03) * 2. * sigma / flux
+    np.testing.assert_almost_equal(shapelet.centroid().x, cen.x, 10)
+    np.testing.assert_almost_equal(shapelet.centroid().y, cen.y, 10)
     # Check Fourier properties
-    np.testing.assert_equal(psf.maxK(), 3.7169221888498383)
-    np.testing.assert_almost_equal(psf.stepK(), 0.78539816339744828)
-    np.testing.assert_equal(psf.kValue(cen), 1+0j)
-    # Check input flux vs output flux
-    for inFlux in np.logspace(-2, 2, 10):
-        psfFlux = galsim.SBGaussian(flux=inFlux, sigma=2.)
-        outFlux = psfFlux.getFlux()
-        np.testing.assert_almost_equal(outFlux, inFlux)
-    np.testing.assert_almost_equal(psf.xValue(cen), 0.15915494309189535)
+    np.testing.assert_almost_equal(shapelet.maxK(), 4.6173837118586389, 10)
+    np.testing.assert_almost_equal(shapelet.stepK(), 0.19513374252890903, 10)
+    # Check image values in real and Fourier space
+    zero = galsim.PositionD(0., 0.)
+    np.testing.assert_almost_equal(shapelet.kValue(zero), flux+0j, 10)
+    np.testing.assert_almost_equal(shapelet.xValue(zero), 0.065332121701302717, 10)
+
     t2 = time.time()
     print 'time for %s = %.2f'%(funcname(),t2-t1)
 
@@ -642,8 +654,8 @@ def test_shapelet_shift():
 
 
 if __name__ == "__main__":
-    test_shapelet()
-    #test_shapelet_properties()
+    #test_shapelet()
+    test_shapelet_properties()
     #test_shapelet_radii()
     #test_shapelet_smallshear()
     #test_shapelet_largeshear()
