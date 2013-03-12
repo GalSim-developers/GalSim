07f3ba496a284a1461c59ed47897b2f0992a0605
Barnaby Rowe <barnaby.t.p.rowe@gmail.com>
2013-03-12 10:30:04 -0400

    chaged Joes code to use mysterious g2 sign flip, works now (#304)

--- examples/lensing_example.py
+++ examples/lensing_example.py
@@ -14,23 +14,22 @@ def scale_free_spectrum(k):
 
 def show_shear(E,B,title):
 	R=galsim.lensing.PowerSpectrumRealizer(160, 160, 0.1, E, B)
-	gd=galsim.GaussianDeviate(lseed=int(time.time()))
+	gd=galsim.GaussianDeviate(lseed=1234567)
 
 	g1, g2, kappa = R(gd, get_kappa=True)
 
 	g = (g1**2 + g2**2) ** 0.5
-	theta = 0.5*np.arctan2(g2, g1)
+	theta = 0.5*np.arctan2(-g2, g1)
 	gx = g * np.cos(theta)
 	gy = g * np.sin(theta)
 
-
 	pylab.figure()
-	pylab.imshow(kappa, interpolation='nearest')
+	pylab.pcolor(kappa)
 	pylab.colorbar()
 	pylab.quiver(gx, gy, scale=50, headwidth=0, pivot='middle')
 	pylab.title(title)
         pylab.figure()
-        ke, kb = galsim.lensing.invert_kappa_from_shear(g1, g2)
+        ke, kb = galsim.lensing.invert_kappa_from_shear(g1, -g2)
         pylab.pcolor(ke); pylab.colorbar()
         pylab.title('kappa_E')
         pylab.quiver(gx, gy, scale=50, headwidth=0, pivot='middle')
