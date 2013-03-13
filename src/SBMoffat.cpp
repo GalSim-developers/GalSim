be458967098b8ee3d82b1ddb98639691d71c5842
Mike Jarvis <michael@jarvis.net>
2013-03-13 18:12:42 -0400

    Add unit tests for AutoConvolve (#352)

--- src/SBMoffat.cpp
+++ src/SBMoffat.cpp
@@ -197,12 +197,9 @@ namespace galsim {
         } else {
             _fluxFactor = 1.;
 
-            // Set maxRrD to the radius where surface brightness is kvalue_accuracy
-            // of center value.  (I know this isn't  a kvalue, but the same level 
-            // is probably appropriate here.)
-            // (1+R^2)^-beta = kvalue_accuracy
-            // And ignore the 1+ part of (1+R^2), so
-            maxRrD = std::pow(sbp::kvalue_accuracy,-1./(2.*_beta));
+            // Set maxRrD to the radius where missing fractional flux is xvalue_accuracy
+            // (1+R^2)^(1-beta) = xvalue_accuracy
+            maxRrD = sqrt(std::pow(sbp::xvalue_accuracy,1./(1.-_beta))-1.);
             xdbg<<"Not truncate.  Calculated maxRrD = "<<maxRrD<<"\n";
         }
 
