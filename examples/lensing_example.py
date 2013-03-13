c05d5fc905c6a39d8b64149b2f67d9f7088b9766
Barnaby Rowe <barnaby.t.p.rowe@gmail.com>
2013-03-13 12:24:56 -0400

    added some edits to the lensing_example from working with Rachel (#304)

--- examples/lensing_example.py
+++ examples/lensing_example.py
@@ -24,15 +24,15 @@ def show_shear(E,B,title):
 
     # Make a PowerSpectrum object with the selected E and B power spectra
     print "Initializing PowerSpectrum"
-    my_ps = galsim.PowerSpectrum(E,B)
+    my_ps = galsim.PowerSpectrum(E,B, units=galsim.radians)
     # Get the shears and kappa from the PS
     print "Drawing shears and convergences on a grid"
-    g1, g2, kappa = my_ps.buildGriddedShears(grid_spacing = 0.1, ngrid = 160,
+    g1, g2, kappa = my_ps.buildGriddedShears(grid_spacing = 0.02, ngrid = 120, units=galsim.degrees,
                                              rng = gd, get_kappa=True)
 
     print "Some postprocessing as needed for plotting"
     # Need a sign flip!
-    g2 = -g2
+    g2 = g2
 
     g = (g1**2 + g2**2) ** 0.5
     theta = 0.5*np.arctan2(g2, g1)
@@ -44,19 +44,24 @@ def show_shear(E,B,title):
     pylab.figure()
     pylab.pcolor(kappa)
     pylab.colorbar()
-    pylab.quiver(gx, gy, scale=50, headwidth=0, pivot='middle')
+    magnify = 50.
+    pylab.quiver(magnify * gx, magnify * gy, scale=50, headwidth=0, pivot='middle')
     pylab.title(title)
     pylab.figure()
     ke, kb = galsim.lensing.invert_kappa_from_shear(g1, g2)
     pylab.pcolor(ke); pylab.colorbar()
     pylab.title(title+' kappa_E')
-    pylab.quiver(gx, gy, scale=50, headwidth=0, pivot='middle')
+    pylab.quiver(magnify * gx, magnify * gy, scale=50, headwidth=0, pivot='middle')
     pylab.figure()
     pylab.pcolor(kb); pylab.colorbar()
     pylab.title(title+' kappa_B')
 
-show_shear(scale_free_spectrum, None, "E-mode")
-show_shear(None, scale_free_spectrum, "B-mode")
+    print np.var(g1), np.var(g2)
+    print np.var(ke), np.var(kappa)
+
+tab_ps = galsim.LookupTable(file='./data/cosmo-fid.zmed1.00.out', interpolant='linear')
+show_shear(tab_ps, None, "E-mode")
+#show_shear(None, scale_free_spectrum, "B-mode")
 
 
 pylab.show()
