5d881061ae1cf6b9faac3c86fede913c96de4535
Rachel Mandelbaum <rmandelb@andrew.cmu.edu>
2013-04-04 17:10:31 -0400

    header and attributions (#382)

--- examples/PowerSpectrumEstimator.py
+++ examples/PowerSpectrumEstimator.py
@@ -1,6 +1,27 @@
+# Copyright 2012, 2013 The GalSim developers:
+# https://github.com/GalSim-developers
+#
+# This file is part of GalSim: The modular galaxy image simulation toolkit.
+#
+# GalSim is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# GalSim is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with GalSim.  If not, see <http://www.gnu.org/licenses/>
+#
 import numpy as np
 from numpy import pi
 
+# Note: the code below was developed largely by Joe Zuntz and tweaked by assorted GalSim
+# developers.  This development and testing took place in a separate (private) repository before the
+# code was moved into the GalSim repository.
 class PowerSpectrumEstimator(object):
     """
     Class for estimating the shear power spectrum from gridded shears.
