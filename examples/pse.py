fe1a627fbb64b6cc35a2b3c1b5e88966015a8e2f
Barnaby Rowe <barnaby.t.p.rowe@gmail.com>
2013-04-15 18:58:16 +0100

    harmonized NumPy usage with most common pattern in GalSim (#382)

--- examples/pse.py
+++ examples/pse.py
@@ -29,7 +29,7 @@ class PowerSpectrumEstimator(object):
     Class for estimating the shear power spectrum from gridded shears.
 
     The PowerSpectrumEstimator class can be used even on systems where GalSim is not installed.  It
-    just requires Python v2.6 or 2.7 and Numpy.
+    just requires Python v2.6 or 2.7 and NumPy.
 
     This class stores all the data used in power spectrum estimation that is fixed with the geometry
     of the problem - the binning and spin weighting factors.
