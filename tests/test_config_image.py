f57156200770e703cd19fd3942f9ad1e6e6deb08
Mike Jarvis <michael@jarvis.net>
2013-04-20 22:21:27 -0400

    Add test that Scattered stamp size is indexed by obj_num (which currently fails) (#380)

--- tests/test_config_image.py
+++ tests/test_config_image.py
@@ -42,6 +42,7 @@ def test_scattered():
     import time
     t1 = time.time()
 
+    # Check that the stamps are centered at the correct locations:
     config = {
         'gal' : { 'type' : 'Gaussian', 
                   'half_light_radius' : 2 },
@@ -50,11 +51,69 @@ def test_scattered():
                     'stamp_size' : 7,
                     'pixel_scale' : 1,
                     'center' : { 'type' : 'XY', 'x' : 4, 'y' : 1 },
-                    'nobjects' : 1 }
+                    'nobjects' : 1 },
     }
 
     image, _, _, _  = galsim.config.BuildImage(config)
     image.write('junk.fits')
+
+    # Check that stamp_xsize, stamp_ysize, center use the object count, rather than the 
+    # image count.
+    dx = 1
+    hlr = 4
+    flux = 10
+    size = 40
+
+    config = {
+        'gal' : { 'type' : 'Gaussian', 
+                  'half_light_radius' : hlr,
+                  'flux' : flux 
+                },
+        'image' : { 'type' : 'Scattered',
+                    'size' : size,
+                    'stamp_xsize' : { 'type': 'Sequence', 'first' : 20 },
+                    'stamp_ysize' : { 'type': 'Sequence', 'first' : 20 },
+                    'pixel_scale' : dx,
+                    'center' : { 'type' : 'List',
+                                 'items' : [ galsim.PositionD(14,14),
+                                             galsim.PositionD(33,18),
+                                             galsim.PositionD(11,29) ] 
+                               },
+                    'nobjects' : 3 
+                  }
+    }
+
+    image, _, _, _  = galsim.config.BuildImage(config)
+    image2 = galsim.ImageF(size,size)
+    image2.setZero()
+    image2.scale = dx
+    gal = galsim.Gaussian(half_light_radius=hlr, flux=flux)
+
+    stamp = galsim.ImageF(20,20)
+    stamp.scale = dx
+    gal.draw(stamp)
+    stamp.setCenter(14,14)
+    b = image2.bounds & stamp.bounds
+    image2[b] += stamp[b]
+
+    stamp = galsim.ImageF(21,21)
+    stamp.scale = dx
+    gal.draw(stamp)
+    stamp.setCenter(33,18)
+    b = image2.bounds & stamp.bounds
+    image2[b] += stamp[b]
+
+    stamp = galsim.ImageF(22,22)
+    stamp.scale = dx
+    gal.draw(stamp)
+    stamp.setCenter(11,29)
+    b = image2.bounds & stamp.bounds
+    image2[b] += stamp[b]
+
+    image.write('junk.fits')
+    image2.write('junk2.fits')
+    np.testing.assert_almost_equal(image.array, image2.array)
+
 
     t2 = time.time()
     print 'time for %s = %.2f'%(funcname(),t2-t1)
