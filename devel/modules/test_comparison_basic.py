ccba0f3f9052e605096a0f1bf078dd85ae567125
Barnaby Rowe <barnaby.t.p.rowe@gmail.com>
2013-04-26 15:54:46 +0100

    added module scope docstring about the test script, and a comment (#401)

--- devel/modules/test_comparison_basic.py
+++ devel/modules/test_comparison_basic.py
@@ -2,6 +2,33 @@ import sys
 import logging
 import galsim
 
+"""A simple Python test script to demonstrate use of the galsim.utilities.compare_dft_vs_photon_*
+functions.
+
+This script generates a model galaxy and PSF, and then compares the rendering of this object by both
+photon shooting and DFT methods, by calling the GSObject `drawShoot()` and `draw()` methods
+respectively.
+
+There are two functions that do this in galsim.utilities:
+
+    i)  galsim.utilities.compare_dft_vs_photon_object
+
+    ii) galsim.utilities.compare_dft_vs_photon_config
+
+i) allows the object and optional convolving PSF to be specified directly as GSObject instances.
+However, as these are not picklable, these tests can only run in single core mode.
+
+ii) provides multi-core processing functionality, but requires that the object and optional
+convolving PSF are specified via a `config` dictionary (see, e.g., examples/demo8.py).
+
+The two methods don't provide identical results, because the `object` version uses only one random
+generator sequence to generate all the photons, whereas the `config` version uses a number of
+differently seeded random number generators, one for each image.  One purpose of this script was
+a quick sanity check of their overall consistency, as well as being a demonstration of these testing
+utility functions.
+"""
+
+
 # Make the galaxy and PSF objects elliptical Sersic and Moffat, storing all param vals here
 # in top level scope
 
@@ -104,5 +131,7 @@ def test_comparison_config(np):
 if __name__ == "__main__":
 
     for n_photons in n_photons_test:
+        # First run the config version, then the (slower, single core) object version: see docstring
+        # in module header for more info.
         test_comparison_config(n_photons)
         test_comparison_object(n_photons)
