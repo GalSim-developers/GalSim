4ae18690605c66c48b12b767e6e02760db7e7d98
tomaszkacprzak <tomasz.kacprzak.09@ucl.ac.uk>
2013-04-27 22:25:21 +0100

    updated the plotting script (#343)

--- devel/external/test_photon_vs_fft/photon_vs_fft_plots.py
+++ devel/external/test_photon_vs_fft/photon_vs_fft_plots.py
@@ -14,19 +14,20 @@ import math
 import matplotlib.pyplot as plt
 import galsim
 
-hsm_error_value = -1
-no_psf_value = -1
-dirname_figs = 'figures'
+HSM_ERROR_VALUE = -99
+NO_PSF_VALUE    = -98
 
-
-def plotEllipticityBiases():
+def PlotEllipticityBiases(filename_output):
     """
     This function is a prototype for a plotting function. 
     I am not sure what is the best plots/subplots combination to show what we want to see, so for now 
     let's use this form.
+    Arguments
+    ---------
+    filename_output     file containing results from photon_vs_fft
     """
 
-    data = numpy.loadtxt(config['filename_output'],ndmin=2)
+    data = numpy.loadtxt(filename_output,ndmin=2)
 
     n_test_gals = data.shape[0]
 
@@ -54,7 +55,13 @@ def plotEllipticityBiases():
 
     logger.info('saved figure %s' % filename_fig)
 
-def plotEllipticityBiasesHistogram():
+def PlotEllipticityBiasesHistogram(filename_output):
+    """
+    Plot histogram for differences between photon and FFT images.
+    Arguments
+    ---------
+    filename_output     file containing results from photon_vs_fft
+    """
 
     # id
     # max_diff_over_max_image
@@ -71,7 +78,7 @@ def plotEllipticityBiasesHistogram():
     # hsm_fft_sigma
     # hsm_shoot_sigma
 
-    data = numpy.loadtxt(config['filename_output'])
+    data = numpy.loadtxt(filename_output)
     n_test_gals = data.shape[0]
     n_bins = 25
     n_ticks= 3
@@ -142,89 +149,132 @@ def plotEllipticityBiasesHistogram():
 
     logger.info('saved figure %s' % filename_fig)
 
-def plotStatsGSParams():
+def PlotStatsForParam(config,filename_config,param_name):
+    """
+    Save plots for the results of photon_vs_fft, when param_name is varied.
+    Arguments
+    ---------
+    config               galsim yaml config, which was used to produce the results, read by yaml
+    filename_config      a config file name used to produce the results
+    param_name           varied parameter name, listed under config['vary_params'], for which
+                            to create the plots
+    """
 
+    # get the shortcut to the dict corresponding to current varied parameter
+    param = config['vary_params'][param_name]
 
-    fiducial_value = eval('galsim.GSParams().%s' % config['gsparams']['vary_param'])
-    # logging.info('changing gsparam %s from fiducial %f to %f',config['gsparams']['vary_param'],fiducial_value,new_value)
+    # initialise the arrays for results
+    moments_E1_diff_mean = []
+    moments_E1_diff_stdm = []
+    moments_E1_diff_medn = []
+    moments_E2_diff_mean = []
+    moments_E2_diff_stdm = []
+    moments_E2_diff_medn = []
 
+    hsmcorr_E1_diff_mean = []
+    hsmcorr_E1_diff_stdm = []
+    hsmcorr_E1_diff_medn = []
+    hsmcorr_E2_diff_mean = []
+    hsmcorr_E2_diff_stdm = []
+    hsmcorr_E2_diff_medn = []
 
-    # config['gsparams']['vary_param']
-    vary_params = config['gsparams']
+    moments_sigma_diff_mean = []
+    moments_sigma_diff_stdm = []
+    moments_sigma_diff_medn = []
+    hsmcorr_sigma_diff_mean = []
+    hsmcorr_sigma_diff_stdm = []
+    hsmcorr_sigma_diff_medn = []
 
-    nrows_subplot = len(vary_params)
-    ncols_subplot = len(config['image']['observe_from'])
+    # loop over values changed for the varied parameter
+    for iv,value in enumerate(param['values']):
 
-    for no,observe_from in enumerate(config['image']['observe_from']):
+        # get the filename for the results file
+        filename_out = 'results.%s.%s.%03d.cat' % (filename_config,param_name,iv)
+        # load the results file
+        results = numpy.loadtxt(filename_out)
+        # get the number of galaxies in the results file
+        n_res_all = results.shape[0]
+        # clean the HSM errors
+        for col in range(2,14):
+            select = results[:,col] != HSM_ERROR_VALUE
+            results = results[select,:]
+        n_res = results.shape[0]
 
-        for np,param in enumerate(vary_params):
+        logging.info('opened file %s with %d valid measurements (out of %d) for %s=%e',filename_out,n_res,n_res_all,param_name,iv)
 
-            moments_E1_diff_mean = []
-            moments_E1_diff_stdm = []
-            moments_E1_diff_medn = []
-            moments_E2_diff_mean = []
-            moments_E2_diff_stdm = []
-            moments_E2_diff_medn = []
+        # get all the stats
+        moments_E1_diff_mean.append( numpy.mean(results[:,2] - results[:,4])  )
+        moments_E1_diff_stdm.append( numpy.std(results[:,2] - results[:,4],ddof=1)/numpy.sqrt(n_res)  )
+        moments_E1_diff_medn.append( numpy.median(results[:,2] - results[:,4])  )
+        moments_E2_diff_mean.append( numpy.mean(results[:,3] - results[:,5])  )
+        moments_E2_diff_stdm.append( numpy.std(results[:,3] - results[:,5],ddof=1)/numpy.sqrt(n_res)  )
+        moments_E2_diff_medn.append( numpy.median(results[:,3] - results[:,5])  )
 
-            hsmcorr_E1_diff_mean = []
-            hsmcorr_E1_diff_stdm = []
-            hsmcorr_E1_diff_medn = []
-            hsmcorr_E2_diff_mean = []
-            hsmcorr_E2_diff_stdm = []
-            hsmcorr_E2_diff_medn = []
+        hsmcorr_E1_diff_mean.append( numpy.mean(results[:,6] - results[:,8])  )
+        hsmcorr_E1_diff_stdm.append( numpy.std(results[:,6] - results[:,8],ddof=1)/numpy.sqrt(n_res)  )
+        hsmcorr_E1_diff_medn.append( numpy.median(results[:,6] - results[:,8])  )
+        hsmcorr_E2_diff_mean.append( numpy.mean(results[:,7] - results[:,9])  )
+        hsmcorr_E2_diff_stdm.append( numpy.std(results[:,7] - results[:,9],ddof=1)/numpy.sqrt(n_res)  )
+        hsmcorr_E2_diff_medn.append( numpy.median(results[:,7] - results[:,9])  )
 
-            moments_sigma_diff_mean = []
-            moments_sigma_diff_stdm = []
-            moments_sigma_diff_medn = []
-            hsmcorr_sigma_diff_mean = []
-            hsmcorr_sigma_diff_stdm = []
-            hsmcorr_sigma_diff_medn = []
+        moments_sigma_diff_mean.append( numpy.mean(results[:,10] - results[:,11])  )
+        moments_sigma_diff_stdm.append( numpy.std(results[:,10] - results[:,11],ddof=1)/numpy.sqrt(n_res)  )
+        moments_sigma_diff_medn.append( numpy.median(results[:,10] - results[:,11])  )
+        hsmcorr_sigma_diff_mean.append( numpy.mean(results[:,12] - results[:,13])  )
+        hsmcorr_sigma_diff_stdm.append( numpy.std(results[:,12] - results[:,13],ddof=1)/numpy.sqrt(n_res)  )
+        hsmcorr_sigma_diff_medn.append( numpy.median(results[:,12] - results[:,13])  )
 
-            for nn,new_value in enumerate(param['grid']):
+    # plot figures for moments, hsmcorr, and sigma
+    pylab.figure(1)
+    pylab.title('Weighted moments - uncorrected')
+    pylab.xscale('log')
+    pylab.errorbar(param['values'],moments_E1_diff_mean,yerr=moments_E1_diff_stdm,fmt='b+-')
+    pylab.errorbar(param['values'],moments_E2_diff_mean,yerr=moments_E2_diff_stdm,fmt='rx-')
+    pylab.plot(param['values'],moments_E1_diff_medn,'b+--')
+    pylab.plot(param['values'],moments_E2_diff_medn,'rx--')
+    pylab.ylabel('Ei_fft - Ei_photon')
+    pylab.xlabel(param_name)
+    pylab.xlim([min(param['values'])*0.5, max(param['values'])*1.5])
+    pylab.legend(['mean diff e1','mean diff e2','median diff e1','median diff e2'])
+    filename_fig = 'fig.moments.%s.%s.pdf' % (filename_config,param_name)
+    pylab.savefig(filename_fig)
+    # pylab.show()
+    pylab.close()
+    logger.info('saved figure %s' % filename_fig)
 
-                filename_out = '%s.%s.%s.%d.cat' % (config['filename_output'],observe_from,param['name'],nn)
-                results = numpy.loadtxt(filename_out)
-                n_res = results.shape[0]
+    pylab.figure(2)
+    pylab.title('Weighted moments - corrected')
+    pylab.xscale('log')
+    pylab.errorbar(param['values'],hsmcorr_E1_diff_mean,yerr=hsmcorr_E1_diff_stdm,fmt='b+-')
+    pylab.errorbar(param['values'],hsmcorr_E2_diff_mean,yerr=hsmcorr_E2_diff_stdm,fmt='rx-')
+    pylab.plot(param['values'],hsmcorr_E1_diff_medn,'b+--')
+    pylab.plot(param['values'],hsmcorr_E2_diff_medn,'rx--')
+    pylab.ylabel('Ei_fft - Ei_photon')
+    pylab.xlabel(param_name)
+    pylab.xlim([min(param['values'])*0.5, max(param['values'])*1.5])
+    pylab.legend(['mean diff e1','mean diff e2','median diff e1','median diff e2'])
+    filename_fig = 'fig.hsmcorr.%s.%s.pdf' % (filename_config,param_name)
+    pylab.savefig(filename_fig)
+    # pylab.show()
+    pylab.close()
+    logger.info('saved figure %s' % filename_fig)
 
-                logging.info('opened file %s with %d measurements for %s=%e',filename_out,n_res,config['gsparams']['vary_param'],nn)
-
-                moments_E1_diff_mean.append( numpy.mean(results[:,2] - results[:,4])  )
-                moments_E1_diff_stdm.append( numpy.std(results[:,2] - results[:,4],ddof=1)/numpy.sqrt(n_res)  )
-                moments_E1_diff_medn.append( numpy.median(results[:,2] - results[:,4])  )
-                moments_E2_diff_mean.append( numpy.mean(results[:,3] - results[:,5])  )
-                moments_E2_diff_stdm.append( numpy.std(results[:,3] - results[:,5],ddof=1)/numpy.sqrt(n_res)  )
-                moments_E2_diff_medn.append( numpy.median(results[:,3] - results[:,5])  )
-
-                hsmcorr_E1_diff_mean.append( numpy.mean(results[:,6] - results[:,8])  )
-                hsmcorr_E1_diff_stdm.append( numpy.std(results[:,6] - results[:,8],ddof=1)/numpy.sqrt(n_res)  )
-                hsmcorr_E1_diff_medn.append( numpy.median(results[:,6] - results[:,8])  )
-                hsmcorr_E2_diff_mean.append( numpy.mean(results[:,7] - results[:,9])  )
-                hsmcorr_E2_diff_stdm.append( numpy.std(results[:,7] - results[:,9],ddof=1)/numpy.sqrt(n_res)  )
-                hsmcorr_E2_diff_medn.append( numpy.median(results[:,7] - results[:,9])  )
-
-                moments_sigma_diff_mean.append( numpy.mean(results[:,10] - results[:,11])  )
-                moments_sigma_diff_stdm.append( numpy.std(results[:,10] - results[:,11],ddof=1)/numpy.sqrt(n_res)  )
-                moments_sigma_diff_medn.append( numpy.median(results[:,10] - results[:,11])  )
-                hsmcorr_sigma_diff_mean.append( numpy.mean(results[:,12] - results[:,13])  )
-                hsmcorr_sigma_diff_stdm.append( numpy.std(results[:,12] - results[:,13],ddof=1)/numpy.sqrt(n_res)  )
-                hsmcorr_sigma_diff_medn.append( numpy.median(results[:,12] - results[:,13])  )
-
-            index_subplot += 1
-            pylab.subplot(nrows_subplot,ncols_subplot,index_subplot)
-            pylab.xscale('log')
-            pylab.errorbar(vary_params,moments_E1_diff_mean,yerr=moments_E1_diff_stdm,fmt='b+-')
-            pylab.errorbar(vary_params,moments_E2_diff_mean,yerr=moments_E2_diff_stdm,fmt='rx-')
-            pylab.plot(vary_params,moments_E1_diff_medn,'b+--')
-            pylab.plot(vary_params,moments_E2_diff_medn,'rx--')
-            # pylab.savefig(os.path.join(dirname_figs,'gsparams_test.pdf'))
-            # pylab.title('moments')
-            pylab.ylabel('Ei_fft - Ei_photon')
-            pylab.xlabel(config['gsparams']['vary_param'])
-            pylab.xlim([min(vary_params)*0.5, max(vary_params)*1.5])
-            pylab.show()
-
-
-
+    pylab.figure(3)
+    pylab.title('Measured size')
+    pylab.xscale('log')
+    pylab.errorbar(param['values'],moments_sigma_diff_mean,yerr=moments_sigma_diff_stdm,fmt='b+-')
+    pylab.errorbar(param['values'],hsmcorr_sigma_diff_mean,yerr=hsmcorr_sigma_diff_stdm,fmt='b+-')
+    pylab.plot(param['values'],moments_sigma_diff_medn,'rx--')
+    pylab.plot(param['values'],hsmcorr_sigma_diff_medn,'b+--')
+    pylab.ylabel('sigma_fft - sigma_photon')
+    pylab.xlabel(param_name)
+    pylab.xlim([min(param['values'])*0.5, max(param['values'])*1.5])
+    pylab.legend(['mean diff sigma moments','mean diff sigma hsmcorr','median diff sigma moments','median diff sigma hsmcorr'])
+    filename_fig = 'fig.sigma.%s.%s.pdf' % (filename_config,param_name)
+    pylab.savefig(filename_fig)
+    # pylab.show()
+    pylab.close()
+    logger.info('saved figure %s' % filename_fig)
 
 if __name__ == "__main__":
 
@@ -234,7 +284,6 @@ if __name__ == "__main__":
     # parse arguments
     parser = argparse.ArgumentParser(description=description, add_help=True)
     parser.add_argument('filename_config', type=str, help='yaml config file, see photon_vs_fft.yaml for example.')
-    global args
     args = parser.parse_args()
 
     # set up logger
@@ -243,12 +292,10 @@ if __name__ == "__main__":
 
     # load the configuration file
     filename_config = args.filename_config
-    global config
     config = yaml.load(open(filename_config,'r'))
 
-    # save a figure showing scatter on ellipticity fractional difference between shoot and fft
-    # plotEllipticityBiases()
-    # plotEllipticityBiasesHistogram()
-    plotStatsGSParams()
+    # save a plot for each of the varied parameters
+    for param_name in config['vary_params'].keys():
+        PlotStatsForParam(config,filename_config,param_name)
 
 
