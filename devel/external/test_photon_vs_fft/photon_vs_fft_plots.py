f49e2b224846fd1aba28d8da6dbdd6bdd5296af7
tomaszkacprzak <tomasz.kacprzak.09@ucl.ac.uk>
2013-04-29 17:14:53 +0100

    improved plots (#343)

--- devel/external/test_photon_vs_fft/photon_vs_fft_plots.py
+++ devel/external/test_photon_vs_fft/photon_vs_fft_plots.py
@@ -149,7 +149,7 @@ def PlotEllipticityBiasesHistogram(filename_output):
 
     logger.info('saved figure %s' % filename_fig)
 
-def PlotStatsForParam(config,filename_config,param_name):
+def PlotStatsForParam(config,param_name):
     """
     Save plots for the results of photon_vs_fft, when param_name is varied.
     Arguments
@@ -189,7 +189,7 @@ def PlotStatsForParam(config,filename_config,param_name):
     for iv,value in enumerate(param['values']):
 
         # get the filename for the results file
-        filename_out = 'results.%s.%s.%03d.cat' % (filename_config,param_name,iv)
+        filename_out = 'results.%s.%s.%03d.cat' % (config['filename_config'],param_name,iv)
         # load the results file
         results = numpy.loadtxt(filename_out)
         # get the number of galaxies in the results file
@@ -224,55 +224,61 @@ def PlotStatsForParam(config,filename_config,param_name):
         hsmcorr_sigma_diff_stdm.append( numpy.std(results[:,12] - results[:,13],ddof=1)/numpy.sqrt(n_res)  )
         hsmcorr_sigma_diff_medn.append( numpy.median(results[:,12] - results[:,13])  )
 
+    # yaml is bad at converting lists of floats in scientific notation to floats
+    values_float = map(float,param['values'])
+
+    # set some plot parameters
+    fig_xsize,fig_ysize,legend_ncol,legend_loc = 12,10,2,3
+
     # plot figures for moments, hsmcorr, and sigma
-    pylab.figure(1)
+    pylab.figure(1,figsize=(fig_xsize,fig_ysize))
     pylab.title('Weighted moments - uncorrected')
     pylab.xscale('log')
-    pylab.errorbar(param['values'],moments_E1_diff_mean,yerr=moments_E1_diff_stdm,fmt='b+-')
-    pylab.errorbar(param['values'],moments_E2_diff_mean,yerr=moments_E2_diff_stdm,fmt='rx-')
-    pylab.plot(param['values'],moments_E1_diff_medn,'b+--')
-    pylab.plot(param['values'],moments_E2_diff_medn,'rx--')
+    pylab.errorbar(param['values'],moments_E1_diff_mean,yerr=moments_E1_diff_stdm,fmt='b+-',label='mean (fft-phot) e1')
+    pylab.errorbar(param['values'],moments_E2_diff_mean,yerr=moments_E2_diff_stdm,fmt='rx-',label='mean (fft-phot) e2')
+    pylab.plot(param['values'],moments_E1_diff_medn,'b+--',label='median (fft-phot) e1')
+    pylab.plot(param['values'],moments_E2_diff_medn,'rx--',label='median (fft-phot) e2')
     pylab.ylabel('Ei_fft - Ei_photon')
     pylab.xlabel(param_name)
-    pylab.xlim([min(param['values'])*0.5, max(param['values'])*1.5])
-    pylab.legend(['mean diff e1','mean diff e2','median diff e1','median diff e2'])
-    filename_fig = 'fig.moments.%s.%s.pdf' % (filename_config,param_name)
+    pylab.xlim([min(values_float)*0.5, max(values_float)*1.5])
+    pylab.legend(ncol=legend_ncol,loc=legend_loc,mode="expand")
+    filename_fig = 'fig.moments.%s.%s.png' % (config['filename_config'],param_name)
     pylab.savefig(filename_fig)
-    # pylab.show()
+    if config['debug']: pylab.show()
     pylab.close()
     logger.info('saved figure %s' % filename_fig)
 
-    pylab.figure(2)
+    pylab.figure(2,figsize=(fig_xsize,fig_ysize))
     pylab.title('Weighted moments - corrected')
     pylab.xscale('log')
-    pylab.errorbar(param['values'],hsmcorr_E1_diff_mean,yerr=hsmcorr_E1_diff_stdm,fmt='b+-')
-    pylab.errorbar(param['values'],hsmcorr_E2_diff_mean,yerr=hsmcorr_E2_diff_stdm,fmt='rx-')
-    pylab.plot(param['values'],hsmcorr_E1_diff_medn,'b+--')
-    pylab.plot(param['values'],hsmcorr_E2_diff_medn,'rx--')
+    pylab.errorbar(param['values'],hsmcorr_E1_diff_mean,yerr=hsmcorr_E1_diff_stdm,fmt='b+-',label='mean (fft-phot) e1')
+    pylab.errorbar(param['values'],hsmcorr_E2_diff_mean,yerr=hsmcorr_E2_diff_stdm,fmt='rx-',label='mean (fft-phot) e2')
+    pylab.plot(param['values'],hsmcorr_E1_diff_medn,'b+--',label='median (fft-phot) e1')
+    pylab.plot(param['values'],hsmcorr_E2_diff_medn,'rx--',label='median (fft-phot) e2')
     pylab.ylabel('Ei_fft - Ei_photon')
     pylab.xlabel(param_name)
-    pylab.xlim([min(param['values'])*0.5, max(param['values'])*1.5])
-    pylab.legend(['mean diff e1','mean diff e2','median diff e1','median diff e2'])
-    filename_fig = 'fig.hsmcorr.%s.%s.pdf' % (filename_config,param_name)
+    pylab.xlim([min(values_float)*0.5, max(values_float)*1.5])
+    pylab.legend(ncol=legend_ncol,loc=legend_loc,mode="expand")
+    filename_fig = 'fig.hsmcorr.%s.%s.png' % (config['filename_config'],param_name)
     pylab.savefig(filename_fig)
-    # pylab.show()
+    if config['debug']: pylab.show()
     pylab.close()
     logger.info('saved figure %s' % filename_fig)
 
-    pylab.figure(3)
+    pylab.figure(3,figsize=(fig_xsize,fig_ysize))
     pylab.title('Measured size')
     pylab.xscale('log')
-    pylab.errorbar(param['values'],moments_sigma_diff_mean,yerr=moments_sigma_diff_stdm,fmt='b+-')
-    pylab.errorbar(param['values'],hsmcorr_sigma_diff_mean,yerr=hsmcorr_sigma_diff_stdm,fmt='b+-')
-    pylab.plot(param['values'],moments_sigma_diff_medn,'rx--')
-    pylab.plot(param['values'],hsmcorr_sigma_diff_medn,'b+--')
+    pylab.errorbar(param['values'],moments_sigma_diff_mean,yerr=moments_sigma_diff_stdm,fmt='b+-',label='mean (fft-phot) moments')
+    pylab.errorbar(param['values'],hsmcorr_sigma_diff_mean,yerr=hsmcorr_sigma_diff_stdm,fmt='rx-',label='mean (fft-phot) hsmcorr')
+    pylab.plot(param['values'],moments_sigma_diff_medn,'b+--',label='median (fft-phot) moments')
+    pylab.plot(param['values'],hsmcorr_sigma_diff_medn,'rx--',label='median (fft-phot) hsmcorr')
     pylab.ylabel('sigma_fft - sigma_photon')
     pylab.xlabel(param_name)
-    pylab.xlim([min(param['values'])*0.5, max(param['values'])*1.5])
-    pylab.legend(['mean diff sigma moments','mean diff sigma hsmcorr','median diff sigma moments','median diff sigma hsmcorr'])
-    filename_fig = 'fig.sigma.%s.%s.pdf' % (filename_config,param_name)
+    pylab.xlim([min(values_float)*0.5, max(values_float)*1.5])
+    pylab.legend(ncol=legend_ncol,loc=legend_loc,mode="expand")
+    filename_fig = 'fig.sigma.%s.%s.png' % (config['filename_config'],param_name)
     pylab.savefig(filename_fig)
-    # pylab.show()
+    if config['debug']: pylab.show()
     pylab.close()
     logger.info('saved figure %s' % filename_fig)
 
@@ -284,18 +290,22 @@ if __name__ == "__main__":
     # parse arguments
     parser = argparse.ArgumentParser(description=description, add_help=True)
     parser.add_argument('filename_config', type=str, help='yaml config file, see photon_vs_fft.yaml for example.')
+    parser.add_argument('--debug', action="store_true", help='run with debug verbosity', default=False)
     args = parser.parse_args()
 
-    # set up logger
-    logging.basicConfig(format="%(message)s", level=logging.DEBUG, stream=sys.stdout)
+        # set up logger
+    if args.debug: logger_level = 'logging.DEBUG'
+    else:  logger_level = 'logging.INFO'
+    logging.basicConfig(format="%(message)s", level=eval(logger_level), stream=sys.stdout)
     logger = logging.getLogger("photon_vs_fft") 
 
     # load the configuration file
-    filename_config = args.filename_config
-    config = yaml.load(open(filename_config,'r'))
+    config = yaml.load(open(args.filename_config,'r'))
+    config['debug'] = args.debug
+    config['filename_config'] = args.filename_config
 
     # save a plot for each of the varied parameters
     for param_name in config['vary_params'].keys():
-        PlotStatsForParam(config,filename_config,param_name)
+        PlotStatsForParam(config,param_name)
 
 
