da1078ed54b791f021d847f0c190c467d083009f
Barnaby Rowe <barnaby.t.p.rowe@gmail.com>
2013-05-01 01:15:22 +0100

    put nobs back to 30 (#325)

--- devel/external/test_sersic_highn/plot_sersic_highn_basic.py
+++ devel/external/test_sersic_highn/plot_sersic_highn_basic.py
@@ -20,6 +20,9 @@ err_g1obs = results_tuple[6]
 err_g2obs = results_tuple[7]
 err_sigma = results_tuple[8]
 
+# Urgh changed the sersic n values in the last commit, so this is TEMPORARY!
+test_sersic_highn_basic.SERSIC_N_TEST = [3.5, 4., 4.5, 5., 5.5, 6., 6.5, 7., 7.5, 8.]
+
 ntest = len(test_sersic_highn_basic.SERSIC_N_TEST)
 
 # First do the plots of g1
@@ -68,4 +71,27 @@ plt.savefig(os.path.join('plots', 'sersic_highn_basic_zoomin_g2.png'))
 
 plt.ylim(-YMAX_ZOOMOUT, YMAX_ZOOMOUT)
 plt.savefig(os.path.join('plots', 'sersic_highn_basic_zoomout_g2.png'))
+
+# Then do the plots of sigma
+YMAX_ZOOMOUT = .3   # in arcsec
+YMAX_ZOOMIN = 4.e-4 # in arcsec
+XMIN = 0.
+XMAX = .3 # in arcsec
 plt.clf()
+plt.xlim(XMIN, XMAX)
+for i in range(ntest):
+    if i < 7:
+        fmt='x'
+    else:
+        fmt='o'
+    plt.errorbar(
+        sigma_draw[:, i] * test_sersic_highn_basic.PIXEL_SCALE,
+        delta_sigma[:, i] * test_sersic_highn_basic.PIXEL_SCALE,
+        yerr=err_sigma[:, i] * test_sersic_highn_basic.PIXEL_SCALE, fmt=fmt,
+        label="n = "+str(test_sersic_highn_basic.SERSIC_N_TEST[i]))
+
+plt.ylim(-YMAX_ZOOMIN, YMAX_ZOOMIN)
+plt.xlabel(r'$\sigma$ (DFT) [arcsec]')
+plt.ylabel(r'$\Delta \sigma$ (DFT - Photon) [arcsec]')
+plt.legend()
+plt.show()
