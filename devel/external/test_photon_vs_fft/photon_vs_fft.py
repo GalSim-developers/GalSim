48d1ee9bd657c4836e1352d8e96e7d2366d2d259
tomaszkacprzak <tomasz.kacprzak.09@ucl.ac.uk>
2013-05-01 03:14:06 +0100

    photon_vs_fft.py is using compare_dft_vs_photon_config (#343)

--- devel/external/test_photon_vs_fft/photon_vs_fft.py
+++ devel/external/test_photon_vs_fft/photon_vs_fft.py
@@ -14,18 +14,19 @@ import yaml
 import pdb
 import pylab
 import math
+import copy
 
 HSM_ERROR_VALUE = -99
 NO_PSF_VALUE    = -98
 
-def SaveResults(filename_output,results_all_gals):
+def SaveResults(filename_output,results_pht,results_fft):
     """
-    Save results to file.
-    Arguments
-    ---------
-    filename_output     - file to which results will be written
-    results_all_gals    - list of dictionaries with Photon vs FFT resutls. 
-                            See functon testPhotonVsFft for details of the dict.
+    #brief Save results to file.
+    
+    @filename_output        file to which results will be written
+    @results_pht            dict - result of GetResultsPhoton
+    @results_fft            dict - result of GetResultsFFT
+
     """
 
 
@@ -33,168 +34,96 @@ def SaveResults(filename_output,results_all_gals):
     file_output = open(filename_output,'w')
        
     # write the header
-    output_row_fmt = '%d\t% 2.6f\t% 2.6f\t% 2.6f\t% 2.6f\t% 2.6f\t' + \
-                        '% 2.6f\t% 2.6f\t% 2.6f\t% 2.6f\t% 2.6f\t% 2.6f\t% 2.6f\t% 2.6f\n'
-    output_header = '# id max_diff_over_max_image ' +  \
-                                'E1_moments_fft E2_moments_fft E1_moments_photon E2_moments_photon ' + \
-                                'E1_corr_fft E2_hsm_corr_fft E1_corr_photon E2_corr_photon ' + \
-                                'moments_fft_sigma moments_photon_sigma corr_fft_sigma corr_photon_sigma\n'
-    file_output.write(output_header)
+    output_row_fmt = '%d\t% 2.6e\t% 2.6e\t% 2.6e\t% 2.6e\t% 2.6e\t' + \
+                        '% 2.6e\t% 2.6e\t% 2.6e\t% 2.6e\t% 2.6e\t% 2.6e\t% 2.6e\t% 2.6e\t% 2.6e\t% 2.6e\n'
+    output_header = '# id ' +   'G1_moments_fft G2_moments_fft G1_moments_photon G2_moments_photon ' + \
+                                'G1_hsmcorr_fft G2_hsmcorr_fft G1_hsmcorr_photon G2_hsmcorr_photon ' + \
+                                'moments_fft_sigma moments_photon_sigma ' + \
+                                'err_g1obs err_g2obs err_g1hsm err_g2hsm err_sigma ' + \
+                                '\n'
+    file_output.write(output_header) 
 
     # loop over the results items
-    for res in results_all_gals:
-
-        # write the result item
-        file_output.write(output_row_fmt % (res['ident'], res['max_diff_over_max_image'], 
-            res['moments_fft_e1'], res['moments_fft_e2'], res['moments_phot_e1'], res['moments_phot_e2'],
-            res['hsmcorr_fft_e1'], res['hsmcorr_fft_e2'], res['hsmcorr_phot_e1'], res['hsmcorr_phot_e2'],
-            res['moments_fft_sigma'], res['moments_phot_sigma'], res['hsmcorr_fft_sigma'], res['hsmcorr_phot_sigma']
+    for (i,v) in enumerate(results_pht):     
+        
+        file_output.write(output_row_fmt % (
+            results_fft[i]['ident'] ,
+            results_fft[i]['moments_g1'] ,
+            results_fft[i]['moments_g2'] ,
+            results_pht[i]['moments_g1'] ,
+            results_pht[i]['moments_g2'] ,
+            results_fft[i]['hsmcorr_g1'] ,
+            results_fft[i]['hsmcorr_g2'] ,
+            results_pht[i]['hsmcorr_g1'] ,
+            results_pht[i]['hsmcorr_g2'] ,
+            results_fft[i]['moments_sigma'] ,
+            results_pht[i]['moments_sigma'] ,
+            results_pht[i]['moments_g1err'] ,
+            results_pht[i]['moments_g2err'] ,
+            results_pht[i]['hsmcorr_g1err'] ,
+            results_pht[i]['hsmcorr_g2err'] ,
+            results_pht[i]['moments_sigmaerr'] 
             ))
+
     
     logging.info('saved results file %s' % (filename_output))
 
-
-def GetShapeMeasurements(image_gal_phot, image_gal_fft , image_psf, ident):
+def GetShapeMeasurements(image_gal, image_psf, ident):
     """
-    For a galaxy images created using photon and fft, and PSF image, measure HSM weighted moments 
-    with and without PSF correction. Also measure sizes and maximum pixel differences.
-    Arguments
-    ---------
-    image_gal_phot      image of the galaxy created using photon shooting
-    image_gal_fft       image of the galaxy created using fft
-    image_psf           image of the PSF
-    ident               id of the galaxy, to be saved in the output file
-    
-    Outputs dictionary containing the results of comparison.
-    The dict contains fields:
-    max_diff_over_max_image,moments_fft_e1,moments_fft_e2,moments_phot_e1,moments_phot_e2,
-    hsm_corr_fft_e1,hsm_corr_fft_e2,hsm_corr_phot_e1,hsm_corr_phot_e2,
-    moments_fft_sigma,moments_phot_sigma,hsm_fft_sigma,hsm_phot_sigma,
-    image_fft,image_phot,image_psf,ident
-    If an error occured in HSM measurement, then a error value is written in the fields of this
-    dict.
+    @image_gal - 
+    @image_psf - 
+    @ident - 
     """
 
-    # create a residual image
-    diff_image = image_gal_phot.array - image_gal_fft.array
-    max_diff_over_max_image = abs(diff_image.flatten()).max()/image_gal_fft.array.flatten().max()
-    logger.debug('max(residual) / max(image_fft) = %2.4e ' % ( max_diff_over_max_image )  )
+    HSM_SHEAR_EST = "KSB"
+    NO_PSF_VALUE = -10
 
-    # find adaptive moments
-    try:
-        moments_phot = galsim.FindAdaptiveMom(image_gal_phot)
-        moments_phot_e1 = moments_phot.observed_shape.getE1()
-        moments_phot_e2 = moments_phot.observed_shape.getE2()
-        moments_phot_sigma = moments_phot.moments_sigma 
-    except:
-        logging.error('hsm error in moments measurement for phot image of galaxy %s' % str(ident))
-        moments_phot_e1 = HSM_ERROR_VALUE
-        moments_phot_e2 = HSM_ERROR_VALUE
-        moments_phot_sigma = HSM_ERROR_VALUE
-    try:
-        moments_fft = galsim.FindAdaptiveMom(image_gal_fft)
-        moments_fft_e1 = moments_fft.observed_shape.getE1()
-        moments_fft_e2 = moments_fft.observed_shape.getE2()
-        moments_fft_sigma = moments_fft.moments_sigma 
-    except:
-        logging.error('hsm error in moments measurement for fft image of galaxy %s' % str(ident))
-        moments_fft_e1 = HSM_ERROR_VALUE
-        moments_fft_e2 = HSM_ERROR_VALUE
-        moments_fft_sigma = HSM_ERROR_VALUE
-
-    logger.debug('adaptive moments phot   E1=% 2.6f\t2=% 2.6f\tsigma=%2.6f' % (moments_phot_e1, moments_phot_e2, moments_phot_sigma)) 
-    logger.debug('adaptive moments fft     E1=% 2.6f\t2=% 2.6f\tsigma=%2.6f' % (moments_fft_e1, moments_fft_e2, moments_fft_sigma))
-
-    # find HSM moments
-    if image_psf == None:
-
-        hsm_corr_phot_e1 =  hsm_corr_phot_e2  = hsm_corr_fft_e1 = hsm_corr_fft_e2 =\
-        hsm_fft_sigma = hsm_phot_sigma = \
-        NO_PSF_VALUE 
-    
-    else:
-
-        try:
-            hsmcorr_phot = galsim.EstimateShearHSM(image_gal_phot,image_psf,strict=True)
-            hsmcorr_phot_e1 = hsmcorr_phot.corrected_e1
-            hsmcorr_phot_e2 = hsmcorr_phot.corrected_e2
-            hsmcorr_phot_sigma   = hsmcorr_phot.moments_sigma
-        except:
-            logger.error('hsm error in hsmcorr measurement of phot image of galaxy %s' % str(ident))
-            hsmcorr_phot_e1 = HSM_ERROR_VALUE
-            hsmcorr_phot_e2 = HSM_ERROR_VALUE
-            hsmcorr_phot_sigma   = HSM_ERROR_VALUE
-
-        try:
-            hsmcorr_fft   = galsim.EstimateShearHSM(image_gal_fft,image_psf,strict=True)
-            hsmcorr_fft_e1 = hsmcorr_fft.corrected_e1
-            hsmcorr_fft_e2 = hsmcorr_fft.corrected_e2
-            hsmcorr_fft_sigma   = hsmcorr_fft.moments_sigma
-        except:
-            logger.error('hsm error in hsmcorr measurement of fft image of galaxy %s' % str(ident))
-            hsmcorr_fft_e1 = HSM_ERROR_VALUE
-            hsmcorr_fft_e2 = HSM_ERROR_VALUE
-            hsmcorr_fft_sigma   = HSM_ERROR_VALUE
+    # find adaptive moments  
+    try: moments = galsim.FindAdaptiveMom(image_gal)
+    except: raise RuntimeError('FindAdaptiveMom error')
         
-        logger.debug('hsm corrected moments fft     E1=% 2.6f\tE2=% 2.6f\tsigma=% 2.6f' % (hsmcorr_fft_e1, hsmcorr_fft_e2, hsmcorr_fft_sigma))
-        logger.debug('hsm corrected moments phot    E1=% 2.6f\tE2=% 2.6f\tsigma=% 2.6f' % (hsmcorr_phot_e1, hsmcorr_phot_e2, hsmcorr_phot_sigma))
-          
+    logger.debug('adaptive moments G1=% 2.6f\tG2=% 2.6f\tsigma=%2.6f' % (
+        moments.observed_shape.g1 , moments.observed_shape.g2 , moments.moments_sigma ) )
+    
+    # find HSM moments
+    if image_psf == None: hsmcorr_phot_e1 =  hsmcorr_phot_e2  = NO_PSF_VALUE 
+    else:
+        try: hsmcorr   = galsim.EstimateShearHSM(image_gal,image_psf,strict=True,
+                                                                       shear_est=HSM_SHEAR_EST)
+        except: raise RuntimeError('EstimateShearHSM error')
+        
+    logger.debug('hsm corrected moments     G1=% 2.6f\tG2=% 2.6f' % (
+        hsmcorr.corrected_g1,hsmcorr.corrected_g2) )
+        
     # create the output dictionary
-    result={}
-    result['max_diff_over_max_image'] = max_diff_over_max_image
-    result['moments_fft_e1'] = moments_fft_e1
-    result['moments_fft_e2'] = moments_fft_e2
-    result['moments_phot_e1'] = moments_phot_e1
-    result['moments_phot_e2'] = moments_phot_e2
-    result['hsmcorr_fft_e1'] = hsmcorr_fft_e1
-    result['hsmcorr_fft_e2'] = hsmcorr_fft_e2
-    result['hsmcorr_phot_e1'] = hsmcorr_phot_e1
-    result['hsmcorr_phot_e2'] = hsmcorr_phot_e2
-    result['moments_fft_sigma'] = moments_fft_sigma
-    result['moments_phot_sigma'] = moments_phot_sigma
-    result['hsmcorr_fft_sigma'] = hsmcorr_fft_sigma
-    result['hsmcorr_phot_sigma'] = hsmcorr_phot_sigma
-    result['ident'] = ident
-
+    result = {  'moments_g1' : moments.observed_shape.g1,
+                'moments_g2' : moments.observed_shape.g2,
+                'hsmcorr_g1' : hsmcorr.corrected_g1,
+                'hsmcorr_g2' : hsmcorr.corrected_g2,
+                'moments_sigma' :  moments.moments_sigma, 
+                'ident' : ident}
+    
     return result
 
-
-def RunComparison(config,rebuild_pht,rebuild_fft): 
+def GetResultsFFT(config): 
     """
-    Runs the photon vs FFT comparison. Returns a list of dicts, containing the results of moments 
-    (and other parameters) measured from the FFT and photon images, for each galaxy.
-    
+    @brief get results for all galaxies using photon shooting.
+
     Arguments
     ---------
-    config          the yaml config used to create images
-
-    Outputs a list of dictionaries containing the results of comparison.
+    @config          the yaml config used to create images
+    @return          Outputs a list of dictionaries containing the results of comparison.
     """
 
     # First process the input field:
     galsim.config.ProcessInput(config)
 
-    # Build the image which has dimensions of
-    # x: number of galaxies in the config, y: 2 - one for phot and one for fft
+    # dirty way of getting this number
+    # nobjects = len(galsim.config.GetNObjForMultiFits(config,0,0))
+    # print nobjects
+    nobjects = config['some_variables']['n_gals_in_cat']
 
-    global img_gals_fft, img_gals_pht, img_psfs_fft
-    
-    try:
-
-        if rebuild_fft or img_gals_fft==None:
-            logger.info('building fft image')
-            config['image']['draw_method'] = 'fft'
-            img_gals_fft,img_psfs_fft,_,_ = galsim.config.BuildImage(config=config,make_psf_image=True)
-        if rebuild_pht or img_gals_pht==None:
-            logger.info('building phot image')
-            config['image']['draw_method'] = 'phot'
-            img_gals_pht,img_psfs_pht,_,_ = galsim.config.BuildImage(config=config,make_psf_image=True)
-    except:
-        logging.error('building image failed')
-        return None
-
-    nobjects = galsim.config.GetNObjForImage(config,0)
-    npix = config['image']['stamp_size']
+    config['image']['draw_method'] = 'fft'
 
     # initialise the results dict
     results_all = []
@@ -202,47 +131,86 @@ def RunComparison(config,rebuild_pht,rebuild_fft):
     # measure the photon and fft images
     for i in range(nobjects):
 
-        img_fft =  img_gals_fft[galsim.BoundsI(  1 ,   npix, i*npix+1, (i+1)*npix )]
-        img_pht =  img_gals_pht[galsim.BoundsI(  1 ,   npix, i*npix+1, (i+1)*npix )]
-        img_psf =  img_psfs_fft[galsim.BoundsI(  1 ,   npix, i*npix+1, (i+1)*npix )]
+        obj_num = i * config['some_variables']['n_trials_per_iter']
+        img_gals,img_psfs,_,_,_ = galsim.config.BuildSingleImage( obj_num = obj_num , 
+            config=config , make_psf_image=True )
 
-        if config['debug'] == True: SaveImagePlots(config,i,img_fft,img_pht,img_psf)
+        try: 
+            result = GetShapeMeasurements(img_gals,img_psfs,i)
+        except: 
+            logger.error('failed to get GetShapeMeasurements for galaxy %d' % i)
+            result = {  'moments_g1' : HSM_ERROR_VALUE,
+                        'moments_g2' : HSM_ERROR_VALUE,
+                        'hsmcorr_g1' : HSM_ERROR_VALUE,
+                        'hsmcorr_g2' : HSM_ERROR_VALUE,
+                        'moments_sigma' : HSM_ERROR_VALUE,
+                        'ident' : i }
 
-        # measure and get the resutls
-        results = GetShapeMeasurements(img_fft,img_pht,img_psf,i)
-        results_all.append(results)
+        results_all.append(result)
 
     return results_all
 
-def SaveImagePlots(config,id,img_fft,img_pht,img_psf):
 
-    import pylab
-    pylab.figure()
+def GetResultsPhoton(config): 
+    """
+    @brief get results for all galaxies using photon shooting.
 
-    pylab.subplot(1,4,1)
-    pylab.imshow(img_fft.array,interpolation='nearest')
-    pylab.colorbar()
-    pylab.title('fft image')
-    
-    pylab.subplot(1,4,2)
-    pylab.imshow(img_pht.array,interpolation='nearest')
-    pylab.colorbar()
-    pylab.title('photon image')
-    
-    pylab.subplot(1,4,3)
-    pylab.imshow(img_fft.array-img_pht.array,interpolation='nearest')
-    pylab.colorbar()
-    
-    pylab.subplot(1,4,4)
-    pylab.imshow(img_psf.array,interpolation='nearest')
-    pylab.colorbar()
-    pylab.title('PSF image')
-    
-    filename_fig = 'fig.images.%s.%03d.png' % (config['filename_config'],id)
-    pylab.savefig(filename_fig)
-    logger.debug('saved figure %s' % filename_fig)
+    Arguments
+    ---------
+    @config          the yaml config used to create images
+    @return          Outputs a list of dictionaries containing the results of comparison.
+    """
 
-    pylab.close()
+    # First process the input field:
+    galsim.config.ProcessInput(config)
+
+    # dirty way of getting this number
+    # nobjects = len(galsim.config.GetNObjForMultiFits(config,0,0))
+    nobjects = config['some_variables']['n_gals_in_cat']
+
+    # initialise the results dict
+    results_all = []
+
+    # measure the photon and fft images
+    for i in range(nobjects):
+
+       
+        try:
+            res = galsim.utilities.compare_dft_vs_photon_config(config, gal_num=i, hsm=True,
+                logger=None,
+                abs_tol_ellip = float(config['compare_dft_vs_photon_config']['abs_tol_ellip']),
+                abs_tol_size = float(config['compare_dft_vs_photon_config']['abs_tol_size']),
+                n_trials_per_iter = int(float(config['compare_dft_vs_photon_config']['n_trials_per_iter'])),
+                n_photons_per_trial = int(float(config['compare_dft_vs_photon_config']['n_photons_per_trial']))
+                )
+            result = {  'moments_g1' : res.g1obs_draw - res.delta_g1obs,
+                        'moments_g2' : res.g2obs_draw - res.delta_g2obs,
+                        'hsmcorr_g1' : res.g1hsm_draw - res.delta_g1hsm,
+                        'hsmcorr_g2' : res.g2hsm_draw - res.delta_g2hsm,
+                        'moments_sigma' : res.sigma_draw - res.delta_sigma,
+                        'hsmcorr_g1err' : res.err_g1hsm ,
+                        'hsmcorr_g2err' : res.err_g2hsm ,
+                        'moments_g1err' : res.err_g1obs ,
+                        'moments_g2err' : res.err_g2obs ,
+                        'moments_sigmaerr' : res.err_sigma,
+                        'ident' : i }
+        except:
+            logger.error('failed to get compare_dft_vs_photon_config for galaxy %d' % i)
+            result = {  'moments_g1' : HSM_ERROR_VALUE,
+                        'moments_g2' : HSM_ERROR_VALUE,
+                        'hsmcorr_g1' : HSM_ERROR_VALUE,
+                        'hsmcorr_g2' : HSM_ERROR_VALUE,
+                        'moments_sigma' : HSM_ERROR_VALUE,
+                        'hsmcorr_g1err' : HSM_ERROR_VALUE,
+                        'hsmcorr_g2err' : HSM_ERROR_VALUE,
+                        'moments_g1err' : HSM_ERROR_VALUE,
+                        'moments_g2err' : HSM_ERROR_VALUE,
+                        'moments_sigmaerr' : HSM_ERROR_VALUE,
+                        'ident' : i }
+        
+        results_all.append(result)
+
+    return results_all
 
 def ChangeConfigValue(config,path,value):
     """
@@ -293,9 +261,8 @@ def RunComparisonForVariedParams(config):
     for param_name in config['vary_params'].keys():
 
         # reset the images
-        global img_gals_fft, img_gals_pht, img_psfs_fft
-        (img_gals_fft, img_gals_pht, img_psfs_fft) = (None,None,None)
-
+        results_pht, results_fft = ( None , None )
+        
         # get more info for the parmaeter
         param = config['vary_params'][param_name]
         # loop over all values of the parameter, which will be changed
@@ -306,14 +273,21 @@ def RunComparisonForVariedParams(config):
             ChangeConfigValue(changed_config,param['path'],value)
             logging.info('changed parameter %s to %s' % (param_name,str(value)))
             # run the photon vs fft test on the changed configs
-            results = RunComparison(changed_config,param['rebuild_pht'],param['rebuild_fft'])
-            if results == None:
-                logging.error('failed to get results for these settings')
-                continue
+
+            if param['rebuild_fft'] or results_fft == None:
+                logger.info('getting FFT results for galaxy %d' % iv)
+                changed_config1 = copy.deepcopy(changed_config)
+                results_fft = GetResultsFFT(changed_config1)
+                
+            if param['rebuild_pht'] or results_pht == None:
+                logger.info('getting photon results for galaxy %d' % iv)
+                changed_config2 = copy.deepcopy(changed_config)
+                results_pht = GetResultsPhoton(changed_config2)             
+
             # get the results filename
             filename_results = 'results.%s.%s.%03d.cat' % (config['filename_config'],param_name,iv)
             # save the results
-            SaveResults(filename_results,results)
+            SaveResults(filename_results,results_fft=results_fft,results_pht=results_pht)
             logging.info('saved results for varied parameter %s with value %s, filename %s' % (param_name,str(value),filename_results))
 
 if __name__ == "__main__":
