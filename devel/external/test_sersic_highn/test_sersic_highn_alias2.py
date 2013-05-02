bb29c0bf1aca834ba7d3bf2d03e6b26fd5f2197e
Barnaby Rowe <barnaby.t.p.rowe@gmail.com>
2013-05-02 01:43:05 +0100

    added a script for doing alias threshold test (#325)

---
+++ devel/external/test_sersic_highn/test_sersic_highn_alias2.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+import os
+import logging
+import test_sersic_highn_basic
+
+# Start off with the basic config
+config = test_sersic_highn_basic.config_basic
+config['image']['gsparams']['alias_threshold'] = 2.5e-3
+
+# Output filename
+outfile = os.path.join(
+    "outputs", "sersic_highn_alias2_output_N"+str(test_sersic_highn_basic.NOBS)+".pkl")
+
+# Setup the logging
+logging.basicConfig(level=test_sersic_highn_basic.LOGLEVEL) 
+logger = logging.getLogger("sersic_highn_alias2")
+
+random_seed = 912424534
+
+test_sersic_highn_basic.run_tests(random_seed, outfile, config=config, logger=logger)
