e3711e5f87f218545381e3119628fb072d9ac0e9
Melanie Simet <melanie.simet@gmail.com>
2013-05-09 18:21:40 -0400

    test_interpolants.py in the middle of some bugfixes

--- devel/modules/test_interpolants.py
+++ devel/modules/test_interpolants.py
@@ -34,9 +34,11 @@ first_index = 1
 nitems = 1
 
 # Photon-shoot trial parameters, shamelessly stolen from Barney
-np = int(1E6)
-tol_ellip = 3.e-5
-tol_size = 1.e-4
+np = int(1E5)
+tol_ellip = 3.e-1
+tol_size = 1.0
+#tol_ellip = 3.e-5
+#tol_size = 1.e-4
 
 # Ground-image parameters
 ground_fwhm = 0.65        # arcsec
@@ -58,15 +60,19 @@ ground_filename = 'interpolant_test_output_ground.dat'
 space_filename = 'interpolant_test_output_space.dat'
 ground_original_filename = 'interpolant_test_output_ground_original.dat'
 space_original_filename = 'interpolant_test_output_space_original.dat'
-ground_base_comparison_filename = 'interpolant_test_output_ground_base.dat'
-space_base_comparison_filename = 'interpolant_test_output_space_base.dat'
+ground_base_filename = 'interpolant_test_output_ground_base.dat'
+space_base_filename = 'interpolant_test_output_space_base.dat'
+ground_original_base_filename = 'interpolant_test_output_ground_original_base.dat'
+space_original_base_filename = 'interpolant_test_output_space_original_base.dat'
 
-ground_file = open(ground_filename)
-space_file = open(space_filename)
-ground_original_file = open(ground_original_filename) 
-space_original_file = open(space_original_filename)
-ground_base_comparison_file = open(ground_base_comparison_file)
-space_base_comparison_file = open(space_base_comparison_filename)
+ground_file = open(ground_filename,'w')
+space_file = open(space_filename,'w')
+ground_original_file = open(ground_original_filename,'w') 
+space_original_file = open(space_original_filename,'w')
+ground_base_file = open(ground_base_filename,'w')
+space_base_file = open(space_base_filename,'w')
+ground_original_base_file = open(ground_original_base_filename,'w')
+space_original_base_file = open(space_original_base_filename,'w')
 
 # --- Helper functions to run the main part of the code ---
 def get_config():
@@ -123,9 +129,9 @@ class InterpolationData:
         self.err_g2obs = err_g2obs
         self.err_sigmaobs = err_sigmaobs
         if 'shear' in config['gal']:
-            self.shear = config['gal']['shear']
+            self.shear = [config['gal']['shear']['g1'],config['gal']['shear']['g2']]
         else:
-            self.shear = 0.
+            self.shear = [0., 0.]
         if 'magnification' in config['gal']:
             self.magnification = config['gal']['magnification']
         else:
@@ -141,6 +147,13 @@ class InterpolationData:
         else:
             self.image_type = 'Space'
 
+def CatchAdaptiveMomErrors(obj):
+    try:
+        x = obj.FindAdaptiveMom()
+    except RuntimeError:
+        x = -10.
+    return x
+
 def test_realgalaxy(base_config, shear=None, magnification=None, angle=None, shift=None,
                     interpolant=None, padding=None, seed=None, logger=None):
 # Do something like Barney's compare_dft_vs_photon_config test, only do it for DFT only
@@ -148,7 +161,7 @@ def test_realgalaxy(base_config, shear=None, magnification=None, angle=None, shi
     config = copy.deepcopy(base_config)
     sys.stdout.write('.')
     if shear:
-        config['gal']['shear'] = shear
+        config['gal']['shear'] = {'type': 'G1G2', 'g1': shear[0], 'g2': shear[1]}
     if magnification:
         config['gal']['magnification'] = magnification
     if angle:
@@ -167,12 +180,12 @@ def test_realgalaxy(base_config, shear=None, magnification=None, angle=None, shi
     pass_config = copy.deepcopy(config)
     trial_images = galsim.config.BuildImages(nimages = config['gal']['index']['nitems'], 
         config = config, logger=logger, nproc=config['image']['nproc'])[0] 
-    trial_results = [image.FindAdaptiveMom() for image in trial_images]
+    trial_results = [CatchAdaptiveMomErrors(image) for image in trial_images]
     # Get lists of g1,g2,sigma estimate (this might be quicker using a single list comprehension
     # to get a list of (g1,g2,sigma) tuples, and then unzip with zip(*), but this is clearer)
-    g1obs_list = [res.observed_shape.g1 for res in trial_results] 
-    g2obs_list = [res.observed_shape.g2 for res in trial_results]
-    sigmaobs_list = [res.moments_sigma for res in trial_results]
+    g1obs_list = [-10 if isinstance(res,float) else res.observed_shape.g1 for res in trial_results] 
+    g2obs_list = [-10 if isinstance(res,float) else res.observed_shape.g2 for res in trial_results]
+    sigmaobs_list = [-10 if isinstance(res,float) else res.moments_sigma for res in trial_results]
 
     return InterpolationData(config=pass_config, g1obs=g1obs_list, g2obs=g2obs_list, 
                               sigmaobs = sigmaobs_list)
@@ -224,13 +237,13 @@ def test_realgalaxyoriginal(base_config, shear=None, magnification=None, angle=N
         g1obs_list.append(result.g1obs_draw-result.delta_g1obs)
         g2obs_list.append(result.g2obs_draw-result.delta_g2obs)
         sigmaobs_list.append(result.sigma_draw-result.delta_sigma)
-        err_g1obs_list.append(err_g1obs)
-        err_g2obs_list.append(err_g2obs)
-        err_sigma_list.append(err_sigma)
+        err_g1obs_list.append(result.err_g1obs)
+        err_g2obs_list.append(result.err_g2obs)
+        err_sigma_list.append(result.err_sigma)
         
     return (InterpolationData(config=pass_config, g1obs = g1obs_list, g2obs = g2obs_list, 
         sigmaobs = sigmaobs_list, err_g1obs = err_g1obs_list, err_g2obs = err_g2obs_list, 
-        err_sigmaobs = err_sigmaobs_list), InterpolationData(config=pass_config, 
+        err_sigmaobs = err_sigma_list), InterpolationData(config=pass_config, 
         g1obs = g1obs_list_draw, g2obs = g2obs_list_draw, sigmaobs = sigmaobs_list_draw))
         
 def print_results(base_answer, test_answer, outfile=None):
@@ -259,60 +272,130 @@ def print_results(base_answer, test_answer, outfile=None):
         if image_type==1:
             outfile = ground_base_file
         else:
-            outfile = space_original_file
+            outfile = space_base_file
+    else:
+        raise ValueError('Outfile type %s not understood'%outfile)
             
-    for i in range(len(g1obs)):
-        outfile.write(str(i)+' '+str(interpolant_list.index(test_answer.interpolant))+' '+    
-            str(test_answer.padding)+' '+str(test_answer.shear)+' '+
-            str(test_answer.magnification)+' '+str(test_answer.angle)+' '+
+    for i in range(len(test_answer.g1obs)):
+        outfile.write(str(i)+' '+str(interpolant_list.index(base_answer.interpolant))+' '+    
+            str(base_answer.padding)+' '+str(interpolant_list.index(test_answer.interpolant))+' '+    
+            str(test_answer.padding)+' '+str(test_answer.shear[0])+' '+str(test_answer.shear[1])+
+            ' '+str(test_answer.magnification)+' '+str(test_answer.angle)+' '+
             str(base_answer.g1obs[i])+' '+str(test_answer.g1obs[i]-base_answer.g1obs[i])+' '+
             str(base_answer.g2obs[i])+' '+str(test_answer.g2obs[i]-base_answer.g2obs[i])+' '+
             str(base_answer.sigmaobs[i])+' '+str(test_answer.sigmaobs[i]-base_answer.sigmaobs[i])+
             '\n')
         
-def print_results_original(base_answer, test_answers):
-    pass
+def print_results_original(base_answer, test_answer, outfile=None):
+    if not ((test_answer[0].image_type == test_answer[1].image_type) and
+             (base_answer[0].image_type == base_answer[1].image_type)):
+        raise RuntimeError('Not all input tuples have same config type')
+    if test_answer[0].image_type == 'Ground' and base_answer[0].image_type == 'Ground':
+        image_type = 1
+    elif test_answer[0].image_type == 'Space' and base_answer[0].image_type == 'Space':
+        image_type = 0
+    else:
+        raise RuntimeError('Trying to compare ground-based to space-based images')
+    
+    if ((base_answer[0].interpolant!=test_answer[0].interpolant) or 
+        (base_answer[0].padding!=test_answer[0].padding)) and outfile!='base' and outfile!='Base':
+            raise RuntimeError('Trying to compare different interpolants or paddings')
+    if not (base_answer[0].interpolant==base_answer[1].interpolant and
+             base_answer[0].padding==base_answer[1].padding and
+             test_answer[0].interpolant==test_answer[1].interpolant and
+             test_answer[0].padding==test_answer[1].padding):
+                raise RuntimeError('Not all input tuples have same config type')
+    
+    if (not outfile) or outfile=='original' or outfile=='Original':
+        if image_type==1:
+            outfile = ground_original_file
+        else:
+            outfile = space_original_file
+    elif outfile=='base' or outfile=='Base':
+        if image_type==1:
+            outfile = ground_original_base_file
+        else:
+            outfile = space_original_base_file
+    else:
+        raise ValueError('Outfile type %s not understood'%outfile)
+            
+    for i in range(len(test_answer[0].g1obs)):
+        outfile.write(str(i)+' '+str(interpolant_list.index(base_answer[0].interpolant))+' '+    
+            str(base_answer[0].padding)+' '+str(interpolant_list.index(test_answer[0].interpolant))+
+            ' '+str(test_answer[0].padding)+' '+str(test_answer[0].shear[0])+' '+
+            str(test_answer[0].shear[1])+' '+str(test_answer[0].magnification)+' '+
+            str(test_answer[0].angle)+' '+
+            str(base_answer[0].g1obs[i])+' '+
+            str(base_answer[1].g1obs[i]-base_answer[0].g1obs[i])+' '+
+            str(test_answer[0].g1obs[i]-base_answer[0].g1obs[i])+' '+
+            str(test_answer[1].g1obs[i]-base_answer[0].g1obs[i])+' '+
+            str(base_answer[0].g2obs[i])+' '+
+            str(base_answer[1].g2obs[i]-base_answer[0].g2obs[i])+' '+
+            str(test_answer[0].g2obs[i]-base_answer[0].g2obs[i])+' '+
+            str(test_answer[1].g2obs[i]-base_answer[0].g2obs[i])+' '+
+            str(base_answer[0].sigmaobs[i])+' '+
+            str(base_answer[1].sigmaobs[i]-base_answer[0].sigmaobs[i])+
+            str(test_answer[0].sigmaobs[i]-base_answer[0].sigmaobs[i])+
+            str(test_answer[1].sigmaobs[i]-base_answer[0].sigmaobs[i])+'\n')
 
 def main():
     # Define the config dictionaries we will use for all the following tests
     config_list = get_config()
     
     i=1 # For printing status statements
-    base_list = [] # For comparing the "base" (eg unsheared, unmagnified, unrotated) cases
-    base_list_original = []
     
     # Now, run through the various things we need to test in loops.
     # Right now, test rotation angles separately from shear and magnification
     # (but we can do that nested later if need be - probably with fewer tested angles).
     for base_config in config_list:                     # Ground and space
+        base_list = [] # For comparing the "base" (eg unsheared, unmagnified, unrotated) cases
+        base_list_original = []
         for padding in padding_list:        # Amount of padding
             for interpolant in interpolant_list:        # Possible interpolants
-                base_answer = test_realgalaxy(base_config, seed=rseed, interpolant=interpolant,
-                                            padding=padding)
+                print 'Base test ', i,
+                i+=1
+#                base_answer = test_realgalaxy(base_config, seed=rseed, interpolant=interpolant,
+#                                            padding=padding)
+#                base_list.append(base_answer)
                 base_answer_dft, base_answer_shoot = test_realgalaxyoriginal(base_config, 
                                             interpolant=interpolant, padding=padding, seed=rseed)
-                base_list.append(base_answer)
                 base_list_original.append((base_answer_dft,base_answer_shoot))
+                print ''
                 for angle in angle_list:                        # Possible rotation angles
                     print 'Angle test ', i,
                     i+=1
-                    print_results(base_answer, test_realgalaxy(base_config, angle=angle, 
-                                  interpolant=interpolant, padding=padding, seed=rseed))
+#                    print_results(base_answer, test_realgalaxy(base_config, angle=angle, 
+#                                  interpolant=interpolant, padding=padding, seed=rseed))
                     print_results_original((base_answer_dft, base_answer_shoot), 
                                            test_realgalaxyoriginal(base_config, angle=angle, 
                                            interpolant=interpolant, padding=padding, seed=rseed))
                     print ''
-                i=1
-                for (shear, mag) in zip(shear_list, magnification_list): # Shear and magnification
-                    print 'Shear/magnification test', i,
-                    print_results(base_answer, test_realgalaxy(base_config, shear=shear,
-                                  magnification=magnification, interpolant=interpolant,
-                                  padding=padding, seed=rseed))
+                for (g1, g2, mag) in shear_and_magnification_list: # Shear and magnification
+                    print 'Shear/magnification test', i, g1, g2,
+                    i+=1
+#                    print_results(base_answer, test_realgalaxy(base_config, shear=(g1,g2),
+#                                  magnification=mag, interpolant=interpolant,
+#                                  padding=padding, seed=rseed))
                     print_results_original((base_answer_dft, base_answer_shoot), 
-                                           test_realgalaxyoriginal(base_config, shear=shear,
-                                           magnification=magnification, interpolant=interpolant,
+                                           test_realgalaxyoriginal(base_config, shear=(g1,g2),
+                                           magnification=mag, interpolant=interpolant,
                                            padding=padding, seed=rseed))
-               
+                    print ''
+        for k in range(len(base_list)):
+            for j in range(k+1,len(base_list)):
+#                print_results(base_list[k],base_list[j],outfile='base')
+                print_results_original(base_list_original[k],base_list_original[j],outfile='base')
+
+    ground_file.close()
+    space_file.close()
+    ground_original_file.close()
+    space_original_file.close()
+    ground_base_file.close()
+    space_base_file.close()
+    ground_original_base_file.close()
+    space_original_base_file.close()
+    
+    
         
         
 if __name__ == "__main__":
