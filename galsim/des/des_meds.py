a639ce0151c3c2eaae6be6bfdf8b8b7d4381ad2b
Mike Jarvis <michael@jarvis.net>
2013-05-10 16:51:55 -0400

    Write config builder for des_meds type (#376)

--- galsim/des/des_meds.py
+++ galsim/des/des_meds.py
@@ -25,12 +25,6 @@ MAX_MEMORY = 1e9
 MAX_NCUTOUTS = 11
 EMPTY_START_INDEX = 9999
 EMPTY_JAC = 999
-DUMMY_WEIGHT_VALUE = 1.
-DUMMY_SEG_VALUE = 1.
-
-galsim_image_types = [galsim.ImageD,galsim.ImageF,galsim.ImageI,galsim.ImageS,
-                    galsim.ImageViewD,galsim.ImageViewF,galsim.ImageViewI,
-                    galsim.ImageViewS]
 
 class MultiExposureObject(object):
     """
@@ -48,7 +42,8 @@ class MultiExposureObject(object):
     Constructor parameters:
     @param images               list of images of the object (GalSim images)
     @param weights              list of weight maps (GalSim images)
-    @param segs                 list of segmentation masks (GalSim images)
+    @param badpix               list of bad pixel masks (GalSim images)
+    @param segs                 list of segmentation maps (GalSim images)
     @param jacs                 list of Jacobians of transformation 
                                  row,col->ra,dec tangent plane (u,v) (2x2 numpy arrays)
 
@@ -58,8 +53,13 @@ class MultiExposureObject(object):
     than MAX_NCUTOUTS (default 11).
     """
 
-    def __init__(self,images,weights=None,segs=None,jacs=None):
+    def __init__(self,images,weights=None,badpix=None,segs=None,jacs=None):
 
+        #print 'images = ',images
+        #print 'weights = ',weights
+        #print 'badpix = ',badpix
+        #print 'segs = ',segs
+        #print 'jacs = ',jacs
         if not isinstance(images,list):
             raise TypeError('images should be a list')
 
@@ -78,19 +78,35 @@ class MultiExposureObject(object):
             raise ValueError('box size should be in  [32,48,64,96,128,196,256], is %d' % box_size)
 
 
-        # check if weights, segs and jacs were supplied, if not- create dummy ones
+        # check if weights, segs and jacs were supplied. If not, create sensible values.
         if weights != None:
             self.weights = weights
         else:
-            self.weights = [galsim.ImageD(self.box_size,self.box_size,
-                                          init_value=DUMMY_WEIGHT_VALUE)]*self.n_cutouts
+            self.weights = [galsim.ImageF(self.box_size,self.box_size,init_value=1)]*self.n_cutouts
 
         # check segmaps
         if segs != None:
             self.segs = segs
+            # I think eventually, the meds files will have some more sophisticated pixel map
+            # where the segmentation info and bad pixel info are separately coded.
+            # However, for now, we just set to 0 any bad pixels.
+            # (Not that GalSim has any mechanism yet for generating bad pixels, so this is 
+            # usually a null op, but it's in place for when there is something to do.)
+            if badpix != None:
+                if len(self.segs) != len(badpix):
+                    raise ValueError("segs and badpix are different lengths")
+                for i in range(len(self.segs)):
+                    if (self.segs[i].array.shape != badpix[i].array.shape):
+                        raise ValueError("segs[%d] and badpix[%d] have different shapes."%(i,i))
+                    self.segs[i].array[:,:] &= (badpix[i].array == 0)
+        elif badpix != None:
+            self.segs = badpix
+            # Flip the sense of the bits 0 -> 1, other -> 0
+            # Again, this might need to become more sophisticated at some point...
+            for i in range(len(self.segs)):
+                self.segs[i].array[:,:] = (badpix[i].array == 0)
         else:
-            self.segs = [galsim.ImageD(self.box_size,self.box_size,
-                                       init_value=DUMMY_SEG_VALUE)]*self.n_cutouts
+            self.segs = [galsim.ImageI(self.box_size,self.box_size,init_value=1)]*self.n_cutouts
 
         # check jacs
         if jacs != None:
@@ -116,11 +132,6 @@ class MultiExposureObject(object):
 
             # loop through exposures
             for icutout,cutout in enumerate(ext):
-
-                # all cutouts should be numpy arrays
-                
-                if not any([isinstance(cutout,x) for x in galsim_image_types]):
-                    raise TypeError('cutout %d in %s is not a GalSim Image' % (icutout,extname))
 
                 # get the sizes of array
                 nx=cutout.array.shape[0]
@@ -150,14 +161,14 @@ class MultiExposureObject(object):
                 raise ValueError('Jacobians should be 2x2')
 
 
-def write_meds(filename,objlist,clobber=False):
+def write_meds(file_name,obj_list,clobber=True):
     """
     @brief Writes the galaxy, weights, segmaps images to a MEDS file.
 
     Arguments:
     ----------
-    @param filename:    Name of meds file to be written
-    @param objlist:     List of MultiExposureObjects
+    @param file_name:    Name of meds file to be written
+    @param obj_list:     List of MultiExposureObjects
     """
 
     import numpy
@@ -184,10 +195,10 @@ def write_meds(filename,objlist,clobber=False):
     n_vec = 0
     
     # get number of objects
-    n_obj = len(objlist)
+    n_obj = len(obj_list)
 
     # loop over objects
-    for obj in objlist:
+    for obj in obj_list:
 
         # initialise the start indices for each image
         start_rows = numpy.ones(MAX_NCUTOUTS)*EMPTY_START_INDEX
@@ -305,11 +316,92 @@ def write_meds(filename,objlist,clobber=False):
     seg_cutouts     = pyfits.ImageHDU( vec['seg']   , name='seg_cutouts'    )
 
     # write all
-    hdu_list = pyfits.HDUList([primary,object_data,image_info,metadata,image_cutouts, 
-                                                                weight_cutouts, seg_cutouts])
-    hdu_list.writeto(filename,clobber=clobber)
+    hdu_list = pyfits.HDUList([
+        primary,
+        object_data,
+        image_info,
+        metadata,
+        image_cutouts, 
+        weight_cutouts,
+        seg_cutouts
+    ])
+    hdu_list.writeto(file_name,clobber=clobber)
 
 
+# Now add this to the config framework.
+import galsim.config
 
+# Make this a valid output type:
+galsim.config.process.valid_output_types['des_meds'] = (
+    'galsim.des.BuildMEDS',      # Function that builds the objects using config
+    'galsim.des.GetNObjForMEDS', # Function that calculates the number of objects
+    True,   # Takes nproc argument
+    False,  # Takes *_file_name arguments for psf, weight, badpix
+    False)  # Takes *_hdu arguments for psf, weight, badpix
 
+def BuildMEDS(file_name, config, nproc=1, logger=None, image_num=0, obj_num=0):
+    """
+    Build a meds file as specified in config.
+    
+    @param file_name         The name of the output file.
+    @param config            A configuration dict.
+    @param nproc             How many processes to use.
+    @param logger            If given, a logger object to log progress.
+    @param image_num         If given, the current image_num (default = 0)
+    @param obj_num           If given, the current obj_num (default = 0)
+
+    @return time      Time taken to build file
+    """
+    import time
+    t1 = time.time()
+
+    ignore = [ 'file_name', 'dir', 'nfiles', 'psf', 'weight', 'badpix', 'nproc' ]
+    req = { 'nobjects' : int , 'nstamps_per_object' : int }
+    params = galsim.config.GetAllParams(config['output'],'output',config,ignore=ignore,req=req)[0]
+
+    nobjects = params['nobjects']
+    nstamps_per_object = params['nstamps_per_object']
+    ntot = nobjects * nstamps_per_object
+
+    all_images = galsim.config.BuildImages(
+        ntot, config=config, nproc=nproc, logger=logger, obj_num=obj_num,
+        make_psf_image=False, make_weight_image=True, make_badpix_image=True)
+
+    main_images = all_images[0]
+    weight_images = all_images[2]
+    badpix_images = all_images[3]
+
+    obj_list = []
+    for i in range(nobjects):
+        k1 = i*nstamps_per_object
+        k2 = (i+1)*nstamps_per_object
+        obj = MultiExposureObject(images = main_images[k1:k2], 
+                                  weights = weight_images[k1:k2],
+                                  badpix = badpix_images[k1:k2])
+        obj_list.append(obj)
+
+    write_meds(file_name, obj_list)
+
+    t2 = time.time()
+    return t2-t1
+
+def GetNObjForMEDS(config, file_num, image_num):
+    ignore = [ 'file_name', 'dir', 'nfiles', 'psf', 'weight', 'badpix', 'nproc' ]
+    req = { 'nobjects' : int , 'nstamps_per_object' : int }
+    params = galsim.config.GetAllParams(config['output'],'output',config,ignore=ignore,req=req)[0]
+    config['seq_index'] = file_num
+
+    if 'image' in config and 'type' in config['image']:
+        image_type = config['image']['type'] 
+        if image_type != 'Single':
+            raise AttibuteError("MEDS files are not compatible with image type %s."%image_type)
+
+    nobjects = params['nobjects']
+    nstamps_per_object = params['nstamps_per_object']
+    ntot = nobjects * nstamps_per_object
+
+    # nobj is a list of nobj per image.
+    # The MEDS file is considered to only have a single image, so the list has only 1 element.
+    nobj = [ ntot ]
+    return nobj
 
