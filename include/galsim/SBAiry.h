1f877337410cea35b188ed3d725f78991335135f
barnabytprowe <barnaby.t.p.rowe@gmail.com>
2013-05-19 00:27:30 +0100

    added gsparams to OneDimensionalDeviate in SBAiry (#325)

--- include/galsim/SBAiry.h
+++ include/galsim/SBAiry.h
@@ -55,7 +55,7 @@ namespace galsim {
          *                         image operations and rendering, if different from the default.
          */
         SBAiry(double lam_over_D, double obscuration=0., double flux=1.,
-               boost::shared_ptr<GSParams> gsparams = boost::shared_ptr<GSParams>());
+               boost::shared_ptr<GSParams> gsparams=boost::shared_ptr<GSParams>());
 
         /// @brief Copy constructor
         SBAiry(const SBAiry& rhs);
