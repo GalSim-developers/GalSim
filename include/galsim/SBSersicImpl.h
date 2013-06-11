fff6c569fb75d1b7adbb05bb5c6160ac371950c9
Mike Jarvis <michael@jarvis.net>
2013-06-10 22:38:42 -0400

    Big revamp of SersicInfo and SersicImpl classes #426 to use r0 as the basic dimension rather than re among other things.  Also break up the functionality into more discrete units.

--- include/galsim/SBSersicImpl.h
+++ include/galsim/SBSersicImpl.h
@@ -28,68 +28,12 @@
 
 namespace galsim {
 
-    /** 
-     * @brief Subclass of `SBSersic` which provides the un-normalized radial function.
-     *
-     * Serves as interface to `OneDimensionalDeviate` used for sampling from this 
-     * distribution.
-     */
-    class SersicRadialFunction: public FluxDensity 
-    {
-    public:
-        /**
-         * @brief Constructor
-         *
-         * @param[in] n  Sersic index
-         * @param[in] b  Scale factor which makes radius argument enclose half the flux.
-         */
-        SersicRadialFunction(double n, double b): _invn(1./n), _b(b) {}
-        /**
-         * @brief The un-normalized Sersic function
-         * @param[in] r radius, in units of half-light radius.
-         * @returns Sersic function, normalized to unity at origin
-         */
-        double operator()(double r) const { return std::exp(-_b*std::pow(r,_invn)); } 
-    private:
-        double _invn; ///> 1/n
-        double _b;  ///> radial normalization constant
-    };
-
-    /**
-     * @brief A key for mapping Sersic cache, consisting of a triplet of values
-     * `(n, b, maxRre)`.
-     */
-    struct SersicKey {
-        /**
-         * @param[in] n        Sersic index
-         * @param[in] b        The quantity `(re/r0)^{1/n}`, where r0 is the scale radius
-         * @param[in] maxRre   Maximum radius in units of re, the half-light radius
-         */
-        SersicKey(double _n, double _b, double _maxRre) :
-            n(_n), b(_b), maxRre(_maxRre) {}
-
-        // less operator required for map::find()
-        bool operator<(const SersicKey& rhs) const
-        {
-            return (
-                n == rhs.n ? (
-                    b == rhs.b ? (
-                        maxRre < rhs.maxRre ) :
-                    b < rhs.b ) :
-                n < rhs.n );
-        }
-
-        double n;
-        double b;
-        double maxRre;
-    };
-
     /// @brief A private class that caches the needed parameters for each Sersic index `n`.
     class SersicInfo 
     {
     public:
-        /// @brief Constructor takes SersicKey, which consists of the triplet (n,b,maxRre)
-        SersicInfo(const SersicKey& key, const GSParamsPtr& gsparams);
+        /// @brief Constructor
+        SersicInfo(double n, double trunc, const GSParamsPtr& gsparams);
 
         /// @brief Destructor: deletes photon-shooting classes if necessary
         ~SersicInfo() {}
@@ -106,23 +50,23 @@ namespace galsim {
         /// @brief Looks up the k value for the SBProfile from a lookup table.
         double kValue(double ksq) const;
 
-        double maxK() const { return _maxK; }
-        double stepK() const { return _stepK; }
+        double maxK() const;
+        double stepK() const;
 
-        double getKsqMax() const { return _ksq_max; }
+        /// @brief The half-light radius in units of r0.
+        double getHLR() const;
 
-        /// @brief Returns the maximum relevant R, in units of half-light radius `re`.
-        double getMaxRRe() const { return _maxRre; }
+        /// @brief The fractional flux relateive to the untruncated profile.
+        double getFluxFraction() const;
 
-        /**
-         * @brief Returns the ratio of the actual flux to the specified flux of the object.
-         * Use only when `_truncated` and `_flux_untruncated` are both true.
-         */
-        double getTrueFluxFraction() const { return _flux_fraction; }
-        /**
-         * @brief Returns the ratio of the actual half-light radius `re` to the specified one.
-         */
-        double getTrueReFraction() const { return _re_fraction; }
+        /// @brief The factor to multiple the returned value from xValue
+        double getXNorm() const;
+
+        /// @brief The factor to multiple the returned value from kValue
+        double getKNorm() const;
+
+        /// @brief Calculate scale that has the given HLR and truncation radius in physical units.
+        double calculateScaleForTruncatedHLR(double re, double trunc) const;
 
         /**
          * @brief Shoot photons through unit-size, unnormalized profile
@@ -140,33 +84,40 @@ namespace galsim {
         SersicInfo(const SersicInfo& rhs); ///< Hide the copy constructor.
         void operator=(const SersicInfo& rhs); ///<Hide assignment operator.
 
-        double _n; ///< Sersic index.
-        double _b; ///< scale factor `(re/r0)^{1/n}`, where Sersic is `exp(-b*pow(xsq,inv2n))`
-        double _maxRre; ///< Truncation radius `trunc` in units of half-light radius `re`.
+        // Input variables:
+        double _n;       ///< Sersic index.
+        double _trunc;   ///< Truncation radius `trunc` in units of r0.
+        const GSParamsPtr _gsparams; ///< The GSParams object.
 
-        double _maxRre_sq;
-        double _inv2n;   ///< `1 / (2 * n)`
-        double _maxK;    ///< Value of k beyond which aliasing can be neglected.
-        double _stepK;   ///< Sampling in k space necessary to avoid folding.
-
-        double _norm; ///< Amplitude normalization in Sersic profile `exp(-b*pow(xsq,inv2n))`.
-        double _flux_fraction; ///< Ratio of true flux to the specified flux (for flux_untruncated).
-        double _re_fraction; ///< Ratio of true `re` to the specified `re` (for flux_untruncated).
-        double _kderiv2; ///< Quadratic dependence near k=0.
-        double _kderiv4; ///< Quartic dependence near k=0.
-        double _ksq_min; ///< Minimum ksq to use lookup table.
-        double _ksq_max; ///< Maximum ksq to use lookup table.
+        // Some derived values calculated in the constructor:
+        double _invn;    ///< 1/n
+        double _inv2n;   ///< 1/(2n)
+        double _trunc_sq;  ///< trunc^2
         bool _truncated; ///< True if this Sersic profile is truncated.
+        double _gamma2n; ///< Gamma(2n) = 1/n * int(exp(-r^1/n)*r,r=0..inf)
 
-        Table<double,double> _ft;  ///< Lookup table for Fourier transform of Sersic.
+        // Parameters calculated when they are first needed, and then stored:
+        mutable double _maxk;    ///< Value of k beyond which aliasing can be neglected.
+        mutable double _stepk;   ///< Sampling in k space necessary to avoid folding.
+        mutable double _re;      ///< The HLR in units of r0.
+        mutable double _b;       ///< b = re^(1/n)
+        mutable double _flux;    ///< Flux relative to the untruncated profile.
 
-        /// Function class used for photon shooting
-        boost::shared_ptr<SersicRadialFunction> _radial;  
+        // Parameters for the Hankel transform:
+        mutable Table<double,double> _ft;  ///< Lookup table for fourier transform.
+        mutable double _kderiv2; ///< Quadratic dependence of F near k=0.
+        mutable double _kderiv4; ///< Quartic dependence of F near k=0.
+        mutable double _ksq_min; ///< Minimum ksq to use lookup table.
+        mutable double _ksq_max; ///< Maximum ksq to use lookup table.
 
-        /// Class that does numerical photon shooting
-        boost::shared_ptr<OneDimensionalDeviate> _sampler;   
+        /// Classes used for photon shooting
+        mutable boost::shared_ptr<FluxDensity> _radial;  
+        mutable boost::shared_ptr<OneDimensionalDeviate> _sampler;   
 
-        double findMaxRre(double missing_flux_fraction, double gamma2n);
+        // Helper functions used internally:
+        void buildFT() const;
+        void calculateHLR() const;
+        double calculateMissingFluxRadius(double missing_flux_frac) const;
     };
 
     class SBSersic::SBSersicImpl : public SBProfileImpl
@@ -188,20 +139,20 @@ namespace galsim {
         {
             splits.push_back(0.);
             if (!_truncated) { xmin = -integ::MOCK_INF; xmax = integ::MOCK_INF; }
-            else { xmin = -_maxR; xmax = _maxR; }
+            else { xmin = -_trunc; xmax = _trunc; }
         }
 
         void getYRange(double& ymin, double& ymax, std::vector<double>& splits) const 
         {
             splits.push_back(0.);
             if (!_truncated) { ymin = -integ::MOCK_INF; ymax = integ::MOCK_INF; }
-            else { ymin = -_maxR; ymax = _maxR; }
+            else { ymin = -_trunc; ymax = _trunc; }
         }
 
         void getYRangeX(double x, double& ymin, double& ymax, std::vector<double>& splits) const 
         {
             if (!_truncated) { ymin = -integ::MOCK_INF; ymax = integ::MOCK_INF; }
-            else { ymax = sqrt(_maxR_sq - x*x);  ymin=-ymax; }
+            else { ymax = sqrt(_trunc_sq - x*x);  ymin=-ymax; }
             if (std::abs(x/_re) < 1.e-2) splits.push_back(0.); 
         }
 
@@ -222,13 +173,8 @@ namespace galsim {
         /// @brief Returns the Sersic index n
         double getN() const { return _n; }
         /// @brief Returns the true half-light radius (may be different from the specified value)
-        double getHalfLightRadius() const { return _actual_re; }
-
-        /**
-         * @brief Returns the true scale radius.
-         * Because the internals base their calculation on `_re`, the accuracy of scale radius
-         * are good only to the accuracy set by the solver in `Solve.h`.
-         */
+        double getHalfLightRadius() const { return _re; }
+        /// @brief Returns the scale radius
         double getScaleRadius() const { return _r0; }
 
         // Overrides for better efficiency
@@ -246,34 +192,30 @@ namespace galsim {
                         double y0, double dy, double dyx) const;
 
     private:
-        double _n; ///< Sersic index.
-        double _flux; ///< Actual flux (may differ from that specified at the constructor).
-        double _re;   ///< Half-light radius specified at the constructor.
-        mutable double _r0;   ///< Scale radius specified at the constructor.
-        double _trunc; ///< Truncation radius in same physical units as `_re` (0 if no truncation).
-        bool _flux_untruncated; ///< If true, flux is set to the untruncated Sersic with index `_n`.
+        double _n;     ///< Sersic index.
+        double _flux;  ///< Actual flux (may differ from that specified at the constructor).
+        double _r0;    ///< Scale radius specified at the constructor.
+        double _re;    ///< Half-light radius specified at the constructor.
+        double _trunc;  ///< The truncation radius (if any)
+        bool _truncated; ///< True if this Sersic profile is truncated.
 
-        double _b; ///< The quantity `(_re/_r0)^{1/_n}`
 
-        double _re_sq;
-        double _inv_re;
-        double _inv_re_sq;
-        double _norm; ///< Calculated value: _flux/_re_sq
-        double _actual_re; ///< True half-light radius of object.
-        double _maxRre; ///< Maximum (truncation) `r` in units of `_re`.
-        double _maxRre_sq;
-        double _maxR; ///< Maximum (truncation) radius `r`.
-        double _maxR_sq;
-        double _ksq_max; ///< The ksq_max value from info rescaled with this re value.
-        bool _truncated; ///< Set true if `_trunc > 0`.
+        double _xnorm;     ///< Normalization of xValue relative to what SersicInfo returns.
+        double _knorm;     ///< Normalization of kValue relative to what SersicInfo returns.
+        double _shootnorm; ///< Normalization for photon shooting.
 
-        boost::shared_ptr<SersicInfo> _info; ///< Points to info structure for this SersicKey.
+        double _r0_sq;
+        double _inv_r0;
+        double _inv_r0_sq;
+        double _trunc_sq;
+
+        boost::shared_ptr<SersicInfo> _info; ///< Points to info structure for this n,trunc
 
         // Copy constructor and op= are undefined.
         SBSersicImpl(const SBSersicImpl& rhs);
         void operator=(const SBSersicImpl& rhs);
 
-        static LRUCache<std::pair< SersicKey, GSParamsPtr >, SersicInfo> cache;
+        static LRUCache<boost::tuple< double, double, GSParamsPtr >, SersicInfo> cache;
 
     };
 }
