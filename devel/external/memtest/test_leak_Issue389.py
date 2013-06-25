f15e3508b80ec45184266a75bb5fc01f6895d814
Barnaby Rowe <barnaby.t.p.rowe@gmail.com>
2013-06-24 22:53:52 -0600

    fixed missing import os, sys (#389)

--- devel/external/memtest/test_leak_Issue389.py
+++ devel/external/memtest/test_leak_Issue389.py
@@ -1,6 +1,8 @@
 try:
     import galsim
 except ImportError:
+    import os
+    import sys
     path, filename = os.path.split(__file__)
     sys.path.append(os.path.abspath(os.path.join(path, "..", "..", "..")))
     import galsim
