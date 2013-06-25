91ab648069d1bcd84de06f33ef0ccaa22e83eb03
Barnaby Rowe <barnaby.t.p.rowe@gmail.com>
2013-06-24 22:15:16 -0600

    added python testing script for Barney's seemingly system specific memory issue (#389)

---
+++ devel/external/memtest/test_leak_Issue389.py
@@ -0,0 +1,14 @@
+import galsim
+
+sersic = galsim.Sersic(n=3.1, half_light_radius=0.6)
+
+im = galsim.ImageD(512, 512)
+
+sersic.draw(im, dx=0.03)
+
+interpolated = galsim.InterpolatedImage(im, dx=0.03, k_interpolant=galsim.Lanczos(3))
+interpolated_convolved = galsim.Convolve([interpolated, galsim.Gaussian(1.e-8)])
+
+outimage = galsim.ImageD(512, 512)
+for i in range(7):
+    interpolated_convolved.draw(outimage, dx=0.03)
