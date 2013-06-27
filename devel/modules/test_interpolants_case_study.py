9b6c44bad1fe9cca7f007f1b76eaad7508355c24
Barnaby Rowe <barnaby.t.p.rowe@gmail.com>
2013-06-27 12:11:34 -0600

    added very kludgy case study code for examining a single Sersic n=1 object, can't get it not to work... :( (#389)

---
+++ devel/modules/test_interpolants_case_study.py
@@ -0,0 +1,31 @@
+import galsim
+
+IMSIZE = 512
+G1TEST = 0.1
+G2TEST = 0.
+PIXTEST = 0.03
+
+sersic = galsim.Sersic(n=1, half_light_radius=.7)
+#cosmos_psf = galsim.Airy(lam_over_diam=0.07)
+#pixel = galsim.Pixel(PIXTEST)
+#convolved_sersic = galsim.Convolve([sersic, cosmos_psf, pixel])
+sersic_sheared = sersic.createSheared(g1=G1TEST, g2=G2TEST)
+
+image0 = galsim.ImageD(IMSIZE, IMSIZE)
+image_sheared = galsim.ImageD(IMSIZE, IMSIZE)
+#convolved_sersic.draw(image0, dx=0.03)
+sersic.draw(image0, dx=PIXTEST)
+sersic_sheared.draw(image_sheared, dx=PIXTEST)
+
+interpolated_sersic = galsim.InterpolatedImage(
+    image0, dx=PIXTEST, x_interpolant=galsim.Quintic(), k_interpolant=galsim.Quintic())
+image_test = galsim.ImageD(IMSIZE, IMSIZE)
+interpolated_sersic.applyShear(g1=G1TEST, g2=G2TEST)
+interpolated_sersic = galsim.Convolve([interpolated_sersic, galsim.Gaussian(1.e-8)])
+interpolated_sersic.draw(image_test, dx=PIXTEST)
+
+results_sheared = galsim.hsm.FindAdaptiveMom(image_sheared)
+results_test = galsim.hsm.FindAdaptiveMom(image_test)
+
+print results_sheared.observed_shape.g1, results_sheared.observed_shape.g2
+print results_test.observed_shape.g1, results_test.observed_shape.g2
