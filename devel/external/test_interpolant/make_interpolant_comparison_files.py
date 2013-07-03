15065797744d9040c2e495b8e4c8d590ff93d784
Mike Jarvis <michael@jarvis.net>
2013-07-03 18:41:06 -0400

    Change spline references to use conserve_flux=False (#442)
    Conflicts:
    	devel/external/test_interpolant/.gitignore
    	devel/external/test_interpolant/make_interpolant_comparison_files.py
    	devel/external/test_spline/make_spline_testarrays.py
    	devutils/external/test_spline/make_spline_testarrays.py

--- devel/external/test_interpolant/make_interpolant_comparison_files.py
+++ devel/external/test_interpolant/make_interpolant_comparison_files.py
@@ -1,5 +1,24 @@
 #!/usr/bin/env python
 
+# Copyright 2012, 2013 The GalSim developers:
+# https://github.com/GalSim-developers
+#
+# This file is part of GalSim: The modular galaxy image simulation toolkit.
+#
+# GalSim is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# GalSim is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with GalSim.  If not, see <http://www.gnu.org/licenses/>
+#
+
 import numpy as np
 import galsim
 import os
@@ -10,8 +29,10 @@ import os
 # Uses the NR spline code as preserved in the repo, at commit:
 # 8a9b04085b873f63be4fb56ae4fa5a0ca78a0387
 #
-# Revert to the commit ???
-# and `scons install` to re-run this script.
+# To re-run this script:
+# - First type `git checkout c601a85034f89bba5d608ab6b2944f4f5f168491`
+# - Type `scons install` in the main GalSim directory.
+# - Then type `python make_spline_testarrays.py` in this directory.
 
 # Some arbitrary kx, ky k space values to test
 kxvals = np.array((1.30, 0.71, -4.30)) * np.pi / 2.
@@ -55,7 +76,7 @@ print absoutk
 np.savetxt(os.path.join(dir,'absfKQuintic_test.txt'), absoutk)
 
 # Then make a Lanczos5 interpolant
-interp = galsim.InterpolantXY(galsim.Lanczos(5, conserve_flux=True, tol=1.e-4))
+interp = galsim.InterpolantXY(galsim.Lanczos(5, conserve_flux=False, tol=1.e-4))
 testobj = galsim.SBInterpolatedImage(image.view(), interp, dx=dx)
 for i in xrange(len(kxvals)):
     posk = galsim.PositionD(kxvals[i], kyvals[i])
@@ -64,7 +85,7 @@ print absoutk
 np.savetxt(os.path.join(dir,'absfKLanczos5_test.txt'), absoutk)
 
 # Then make a Lanczos7 interpolant
-interp = galsim.InterpolantXY(galsim.Lanczos(7, conserve_flux=True, tol=1.e-4))
+interp = galsim.InterpolantXY(galsim.Lanczos(7, conserve_flux=False, tol=1.e-4))
 testobj = galsim.SBInterpolatedImage(image.view(), interp, dx=dx)
 for i in xrange(len(kxvals)):
     posk = galsim.PositionD(kxvals[i], kyvals[i])
