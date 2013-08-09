d6a7ac89fefac412d13f4167fcdc6e5956c9479b
Barnaby Rowe <barnaby.t.p.rowe@gmail.com>
2013-08-09 12:22:58 +0100

    enhanced tests, now get interesting results that using the .real of the PS from the CF, calculated by hand, we get the right answer (#430test)

--- junk_test_cf_periodicity.py
+++ junk_test_cf_periodicity.py
@@ -9,6 +9,8 @@ test_image2 = galsim.ImageD(TESTDIM, TESTDIM)
 
 failed1 = 0
 failed2 = 0
+failed3 = 0
+failed4 = 0
 
 TESTSTART = 25
 TESTEND = 125
@@ -18,6 +20,7 @@ for dim in range(TESTSTART, TESTEND):
     noise_image = galsim.ImageViewD(noise_array)
     cn1 = galsim.CorrelatedNoise(rng, noise_image)
     cn2 = galsim.CorrelatedNoise(rng, noise_image, correct_periodicity=False)
+    # First try with (default), then without, periodicity correction
     test_image1.setZero()
     try:
         cn1.applyTo(test_image1)
@@ -27,10 +30,25 @@ for dim in range(TESTSTART, TESTEND):
     try:
         cn2.applyTo(test_image2)
     except RuntimeError:
-        failed2 +=1
+        failed2 += 1
+    # Then try calculating the PS by hand, in the same manner as the CorrelatedNoise internals
+    noiseft = np.fft.fft2(noise_array)
+    ps = np.abs(noiseft)**2
+    cf = np.fft.ifft2(ps)
+    ps_from_cfreal = np.fft.ifft2(cf.real)
+    ps_from_cfabs = np.fft.ifft2(np.abs(cf))
+    if np.any(ps_from_cfreal.real < -1.e-12 * np.mean(ps_from_cfreal).real):
+        import matplotlib.pyplot as plt
+        print ps_from_cfreal
+        print ps_from_cfreal.min()
+        failed3 += 1
+    if np.any(ps_from_cfabs.real < -1.e-12 * np.mean(ps_from_cfabs).real):
+        failed4 += 1
 
-print "With periodicity correction failed    "+str(failed1)+"/"+str(TESTSTART-TESTEND)+" times"
-print "Without periodicity correction failed "+str(failed2)+"/"+str(TESTSTART-TESTEND)+" times"
-    
+print "With periodicity correction failed    "+str(failed1)+"/"+str(TESTEND - TESTSTART)+" times"
+print "Without periodicity correction failed "+str(failed2)+"/"+str(TESTEND - TESTSTART)+" times"
+print "By hand (from real part of PS) failed "+str(failed3)+"/"+str(TESTEND - TESTSTART)+" times"
+print "By hand (from abs() of PSF) failed    "+str(failed4)+"/"+str(TESTEND - TESTSTART)+" times"
+
 
 
