bbc52d92b31187b807f861914f1c5963e10b5509
tomaszkacprzak <tomasz.kacprzak.09@ucl.ac.uk>
2013-08-14 18:28:43 +0100

    speeded up meds saving (#376)

--- galsim/des/des_meds.py
+++ galsim/des/des_meds.py
@@ -261,25 +261,28 @@ def write_meds(file_name, obj_list, clobber=True):
         cat['ncutout'].append(n_cutout)
         cat['box_size'].append(obj.box_size)
         cat['id'].append(obj.id)
-
-        
+      
         # loop over cutouts
         for i in range(n_cutout):
 
             # assign the start row to the end of image vector
             start_rows[i] = n_vec
-
+          
             # append the image vectors
             # if vector not is already initialised
-            if vec['image'] == []:
-                vec['image'] = obj.images[i].array.flatten()
-                vec['seg'] = obj.segs[i].array.flatten()
-                vec['weight'] = obj.weights[i].array.flatten()
-            # if vector already exists
-            else:
-                vec['image'] = numpy.concatenate([vec['image'], obj.images[i].array.flatten()])
-                vec['seg'] = numpy.concatenate([vec['seg'], obj.segs[i].array.flatten()])
-                vec['weight'] = numpy.concatenate([vec['weight'], obj.weights[i].array.flatten()])
+            # if vec['image'] == []:
+            #     vec['image'] = obj.images[i].array.flatten()
+            #     vec['seg'] = obj.segs[i].array.flatten()
+            #     vec['weight'] = obj.weights[i].array.flatten()
+            # # if vector already exists
+            # else:
+            #     vec['image'] = numpy.concatenate([vec['image'], obj.images[i].array.flatten()])
+            #     vec['seg'] = numpy.concatenate([vec['seg'], obj.segs[i].array.flatten()])
+            #     vec['weight'] = numpy.concatenate([vec['weight'], obj.weights[i].array.flatten()])
+
+            vec['image'].append(obj.images[i].array.flatten())
+            vec['seg'].append(obj.segs[i].array.flatten())
+            vec['weight'].append(obj.weights[i].array.flatten())
 
             # append the Jacobian
             dudrow[i] = obj.wcstrans[i].dudrow
@@ -298,7 +301,6 @@ def write_meds(file_name, obj_list, clobber=True):
             # update n_vec to point to the end of image vector
             n_vec = len(vec['image'])
 
-
         # update the start rows fields in the catalog
         cat['start_row'].append(start_rows)
 
@@ -309,6 +311,10 @@ def write_meds(file_name, obj_list, clobber=True):
         cat['dvdcol'].append(dvdcol)
         cat['row0'].append(row0)
         cat['col0'].append(col0)
+
+    vec['image']  = numpy.concatenate(vec['image'])  
+    vec['seg']    = numpy.concatenate(vec['seg'])
+    vec['weight'] = numpy.concatenate(vec['weight'])  
 
     # get the primary HDU
     primary = pyfits.PrimaryHDU()
@@ -428,6 +434,7 @@ def BuildMEDS(file_name, config, nproc=1, logger=None, image_num=0, obj_num=0):
     badpix_images = all_images[3]
 
     obj_list = []
+    logger.debug('building list of MultiExposureObject')
     for i in range(nobjects):
         k1 = i*nstamps_per_object
         k2 = (i+1)*nstamps_per_object
