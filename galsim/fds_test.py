23ff65c0469db8035ca7a590a9dea45dfaf310ff
Mike Jarvis <michael@jarvis.net>
2013-10-23 09:59:12 -0400

    Add fds_test module in case useful in the future (#474)

---
+++ galsim/fds_test.py
@@ -0,0 +1,78 @@
+# I found these routines helpful in tracking down some errors about too many files being open.
+# I've fixed the errors, but I'll leave this module here in case it is useful for someone else
+# down the road.  I lifted the code from the following StackOverflow answers:
+# http://stackoverflow.com/questions/4814970/subprocess-check-output-doesnt-seem-to-exist-python-2-6-5
+
+# To use it do the following:
+#
+#     import galsim.fds_test as fds
+#
+#     ...
+#
+#     try:
+#        ... Code that might raise OSError ...
+#     except OSError as e:
+#        print 'Caught ',e
+#        fds.printOpenFiles()
+#        raise
+#
+# Of course, you can also do fds.printOpenFiles() elsewhere too for information.
+#
+# You can also keep track of the number of open files and pipes with:
+#
+#     print 'files, pipes = ',fds.openFiles()
+
+
+
+import __builtin__
+openfiles = set()
+oldfile = __builtin__.file
+class newfile(oldfile):
+    def __init__(self, *args):
+        self.x = args[0]
+        print "### OPENING %s ###" % str(self.x)            
+        oldfile.__init__(self, *args)
+        openfiles.add(self)
+
+    def close(self):
+        print "### CLOSING %s ###" % str(self.x)
+        oldfile.close(self)
+        openfiles.remove(self)
+
+oldopen = __builtin__.open
+def newopen(*args):
+    return newfile(*args)
+__builtin__.file = newfile
+__builtin__.open = newopen
+
+def getOpenFiles(do_print=False):
+    '''
+    return the number of open files and pipes for current process
+
+    .. warning: will only work on UNIX-like os-es.
+    '''
+    import subprocess
+    import os
+
+    pid = os.getpid()
+    # check_output is py2.7 only:
+    #procs = subprocess.check_output( [ "lsof", '-w', '-Ff', "-p", str( pid ) ] )
+    p = subprocess.Popen(['lsof', '-w', '-Ff', '-p', str(pid)], 
+                         stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    procs = p.communicate()[0]
+    p.stdout.close()
+    p.stderr.close()
+
+    procs = filter( 
+            lambda s: s and s[ 0 ] == 'f' and s[1: ].isdigit(),
+            procs.split( '\n' ) )
+    if do_print:
+        print 'procs = ',procs
+        print 'nprocs = ',len(procs)
+    return len(openfiles), len(procs) - len(openfiles)
+
+def printOpenFiles():
+    print "### %d OPEN FILES: [%s]" % (len(openfiles), ", ".join(f.x for f in openfiles))
+    nopen = getOpenFiles(do_print=True)
+    print "files, pipes = %d, %d"%nopen
+
