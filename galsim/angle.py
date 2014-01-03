cca27606cff1101bdecaa9b79f3c93b13d6cc4c8
Mike Jarvis <michael@jarvis.net>
2014-01-03 01:15:20 -0500

    Add some more useful functionality to the Angle and CelestialCoord classes (#364)

--- galsim/angle.py
+++ galsim/angle.py
@@ -73,7 +73,7 @@ There are five built-in AngleUnits which are always available for use:
 Radian access method
 --------------------
 
-Since extracting the value in radians is extremely common, we have an accessor method to do this 
+Since extracting the value in radians is extremely common, we have an accessor method to do this
 quickly:
 
     >>> x = theta.rad()
@@ -116,7 +116,7 @@ If you want your angles to be wrapped to [-pi,pi) radians, you can do this by ca
 
     >>> theta = theta.wrap()
 
-This could be appropriate before testing for the equality of two angles for example, or 
+This could be appropriate before testing for the equality of two angles for example, or
 calculating the difference between them.
 
 """
@@ -132,9 +132,36 @@ def __repr__(self):
 def __neg__(self):
     return -1. * self
 
+def _make_dms_string(decimal):
+    if decimal >= 0:
+        sign = '+'
+    else:
+        sign = '-'
+        decimal = -decimal
+    h = int(decimal)
+    decimal -= h
+    decimal *= 60.
+    m = int(decimal)
+    decimal -= m
+    decimal *= 60.
+    s = int(decimal)
+    decimal -= s
+    decimal *= 1.e8
+    return '%s%02d%02d%02d.%08d'%(sign,h,m,s,decimal)
+
+def hms(self):
+    """Return an HMS representation of the angle as a string: (+/-)hhmmss.decimal"""
+    return _make_dms_string(self / galsim.hours)
+
+def dms(self):
+    """Return a DMS representation of the angle as a string: (+/-)ddmmss.decimal"""
+    return _make_dms_string(self / galsim.degrees)
+
 galsim.Angle.__str__ = __str__
 galsim.Angle.__repr__ = __repr__
 galsim.Angle.__neg__ = __neg__
+galsim.Angle.hms = hms
+galsim.Angle.dms = dms
 
 def get_angle_unit(unit):
     """Convert a string into the corresponding AngleUnit
@@ -159,7 +186,7 @@ def parse_dms(s):
     """Convert a string of the form ddmmss.decimal into decimal degrees."""
 
     sign = 1
-    if s[0] == '-': 
+    if s[0] == '-':
         sign = -1
         s = s[1:]
     elif s[0] == '+':
@@ -174,7 +201,7 @@ def parse_dms(s):
 def HMS_Angle(str):
     """Convert a string of the form hhmmss.decimal into an Angle.
 
-    There may be an initial + or - (or neither), then 2 digits for the hours, two for the 
+    There may be an initial + or - (or neither), then 2 digits for the hours, two for the
     minutes, and two for the seconds.  Then there may be a decimal point followed by more
     digits.
 
@@ -185,7 +212,7 @@ def HMS_Angle(str):
 def DMS_Angle(str):
     """Convert a string of the form ddmmss.decimal into an Angle.
 
-    There may be an initial + or - (or neither), then 2 digits for the degrees, two for the 
+    There may be an initial + or - (or neither), then 2 digits for the degrees, two for the
     minutes, and two for the seconds.  Then there may be a decimal point followed by more
     digits.
 
@@ -195,16 +222,34 @@ def DMS_Angle(str):
 
 
 class CelestialCoord(object):
-    """This is a lightweight wrapper of two Angles, ra and dec.  
+    """This is class defines a position on the celestial sphere, normally given by
+    two angles, ra and dec.
 
     Mostly this class exists to enforce the units when a position is really a location on
     the celestial sphere rather than using PositionD as we normally do for positions.
+    In that role, it can be considered a lightweight wrapper around two angles, ra and dec.
+    They are accessible as coord.ra and coord.dec.
 
-    Also, most of the arithmetic defined for PositionD is invalid on the sphere, so this 
-    prevents you from doing things like `coord1 - coord2` for (ra,dec) positions.
+    However, there are a few useful functions that we provide.
 
-    There is a `distanceTo` method which returns the great-circle angle between two
-    positions, but there isn't anything else in terms of arithmetic.
+    The distance between two coordinate positions can be calculated with
+
+            d = coord.distanceTo(other_coord)
+
+    There are two tangent plane projections you can use:
+        - a Lambert projection, which preserves area
+        - a stereographic projection, which preserves angles
+        - a Postel projection, which preserves distances from the tangent point
+    See the project and deproject functions for details.
+
+    You can also precess a coordinate from one epoch to another and get the galaxy
+    coordinates with
+
+            coord_1950 = coord2000.precess(2000, 1950)
+            el, b = coord.getGalaxyPos()
+
+    We don't use either of these for anything within GalSim, but I had the code to do it
+    lying around, so I included it here.
 
     Initialization
     --------------
@@ -222,11 +267,23 @@ class CelestialCoord(object):
             raise TypeError("dec must be a galsim.Angle")
         self._ra = ra
         self._dec = dec
+        self._x = None  # Indicate that x,y,z are not set yet.
 
     @property
     def ra(self): return self._ra
     @property
     def dec(self): return self._dec
+
+    def _set_aux(self):
+        if self._x is None:
+            import math
+            self._cosdec = math.cos(self._dec.rad())
+            self._sindec = math.sin(self._dec.rad())
+            self._cosra = math.cos(self._ra.rad())
+            self._sinra = math.sin(self._ra.rad())
+            self._x = self._cosdec * self._cosra
+            self._y = -self._cosdec * self._sinra
+            self._z = self._sindec
 
     def distanceTo(self, other):
         """Returns the great circle distance between this coord and another one.
@@ -241,39 +298,299 @@ class CelestialCoord(object):
         # y = cos(dec) sin(ra)
         # z = sin(dec)
 
-        # Note: if this is a common operation, it is faster to compute x,y,z for each point
-        # once in the constructor.  However, I don't think that is currently the case for 
-        # any use case in GalSim.  Really, this function is kind of just for grins...  :)
-        import math
-        cosdec1 = math.cos(self._dec.rad())
-        sindec1 = math.sin(self._dec.rad())
-        cosra1 = math.cos(self._ra.rad())
-        sinra1 = math.sin(self._ra.rad())
-        cosdec2 = math.cos(other._dec.rad())
-        sindec2 = math.sin(other._dec.rad())
-        cosra2 = math.cos(other._ra.rad())
-        sinra2 = math.sin(other._ra.rad())
-
-        x1 = cosdec1 * cosra1
-        y1 = cosdec1 * sinra1
-        z1 = sindec1
-        x2 = cosdec2 * cosra2
-        y2 = cosdec2 * sinra2
-        z2 = sindec2
+        self._set_aux()
+        other._set_aux()
 
         # The the direct distance between the two points is
         #
         # d^2 = (x1-x2)^2 + (y1-y2)^2 + (z1-z2)^2
 
-        dsq = (x1-x2)**2 + (y1-y2)**2 + (z1-z2)**2
+        dsq = (self._x-other._x)**2 + (self._y-other._y)**2 + (self._z-other._z)**2
 
         # This direct distance can then be converted to a great circle distance via
         #
         # sin(theta/2) = d/2
 
+        import math
         theta = 2. * math.asin(0.5 * math.sqrt(dsq))
         return theta * galsim.radians
 
+    def angleBetween(self, coord1, coord2):
+        """Find the open angle at the location of the current coord between coord1 and coord2."""
+        # Call A = coord1, B = coord2, C = self
+        # Then we are looking for the angle ACB.
+        # If we treat each coord as a (x,y,z) vector, then we can use the following spherical
+        # trig identities:
+        #
+        # (A x C) . B = sina sinb sinC
+        # (A x C) . (B x C) = sina sinb cosC
+        #
+        # Then we can just use atan2 to find C, and atan2 automatically gets the sign right.
+        # And we only need 1 trig call, assuming that x,y,z are already set up, which is often
+        # the case.
+
+        self._set_aux()
+        coord1._set_aux()
+        coord2._set_aux()
+
+        AxC = ( coord1._y * self._z - coord1._z * self._y ,
+                coord1._z * self._x - coord1._x * self._z ,
+                coord1._x * self._y - coord1._y * self._x )
+        BxC = ( coord2._y * self._z - coord2._z * self._y ,
+                coord2._z * self._x - coord2._x * self._z ,
+                coord2._x * self._y - coord2._y * self._x )
+        sinC = AxC[0] * coord2._x + AxC[1] * coord2._y + AxC[2] * coord2._z
+        cosC = AxC[0] * BxC[0] + AxC[1] * BxC[1] + AxC[2] * BxC[2]
+        import math
+        C = math.atan2(sinC, cosC)
+        return C * galsim.radians
+
+    def project(self, other, projection='lambert'):
+        """Use the currect coord as the center point of a tangent plane projection to project
+        the other coordinate onto that plane.
+
+        This function return the position (u,v) in the Euclidean coordinate system defined by
+        a tangent plane projection around the current coordinate, with +v pointing north and
+        +u pointing west.
+
+        There are currently three options for the projection, which you can specify with the
+        optional `projection` keyword argument:
+
+            'lambert' (the default) uses a Lambert azimuthal projection, which preserves
+                    area, but not angles.  For more information, see
+                    http://mathworld.wolfram.com/LambertAzimuthalEqual-AreaProjection.html
+            'stereographic' uses a stereographic proejection, which preserves angles, but
+                    not area.  For more information, see
+                    http://mathworld.wolfram.com/StereographicProjection.html
+            'postel' uses a Postel equidistant proejection, which preserves distances from
+                    the projection point, but not area or angles.  For more information, see
+                    http://mathworld.wolfram.com/AzimuthalEquidistantProjection.html
+
+        The distance or angle errors increase with distance from the projection point of course.
+
+        Returns (u,v) in arcsec as a PositionD object.
+        """
+        # The equations are given at the above mathworld websites.  They are the same except
+        # for the definition of k:
+        #
+        # x = k cos(dec) sin(ra-ra0)
+        # y = k ( cos(dec0) sin(dec) - sin(dec0) cos(dec) cos(ra-ra0) )
+        #
+        # Lambert:
+        #   k = sqrt( 2 ( 1 + sin(dec0) sin(dec) + cos(dec0) cos(phi) cos(ra-ra0) )^-1 )
+        # Stereographic:
+        #   k = 2 ( 1 + sin(dec0) sin(dec) + cos(dec0) cos(phi) cos(ra-ra0) )^-1
+        # Postel:
+        #   k = c / sin(c)
+        #   where cos(c) = sin(dec0) sin(dec) + cos(dec0) cos(dec) cos(ra-ra0)
+
+        if projection not in [ 'lambert', 'stereographic', 'postel' ]:
+            raise ValueError('Unknown projection ' + projection)
+
+        self._set_aux()
+        other._set_aux()
+
+        # cosdra = cos(ra - ra0) = cosra cosra0 + sinra sinra0
+        cosdra = self._cosra * other._cosra + self._sinra * other._sinra
+
+        # sindra = -sin(ra - ra0);
+        # Note: - sign here is to make +x correspond to -ra,
+        #       so x increases for decreasing ra.
+        #       East is to the left on the sky!
+        # sindra = -sinra cosra0 + cosra sinra0
+        sindra = -self._cosra * other._sinra + self._sinra * other._cosra
+
+        # Calculate k according to which projection we are using
+        cosc = self._sindec * other._sindec + self._cosdec * other._cosdec * cosdra
+        if projection == 'postel':
+            import math
+            c = math.acos(cosc)
+            k = c / math.sin(c)
+        else:
+            k = 2. / (1. + cosc)
+            if projection == 'lambert':
+                import math
+                k = math.sqrt(k)
+
+        x = k * other._cosdec * sindra
+        y = k * ( self._cosdec * other._sindec - self._sindec * other._cosdec * cosdra )
+
+        # Convert to arcsec
+        x = x * galsim.radians / galsim.arcsec
+        y = y * galsim.radians / galsim.arcsec
+
+        return galsim.PositionD(x,y)
+
+    def deproject(self, pos, projection='lambert'):
+        """Do the reverse process from the project function.
+
+        i.e. This takes in a position (u,v) as a PositionD object and returns the
+        corresponding celestial coordinate, using the current coordinate as the center
+        point of the tangent plane projection.
+        """
+        # The inverse equations are also given at the same web sites:
+        #
+        # sin(dec) = cos(c) sin(dec0) + v sin(c) cos(dec0)/r
+        # tan(ra-ra0) = u sin(c) / (r cos(dec0) cos(c) - v sin(dec0) sin(c))
+        #
+        # where
+        #
+        # r = sqrt(u^2+v^2)
+        # c = 2 sin^(-1)(r/2) for lambert
+        # c = 2 tan^(-1)(r/2) for stereographic
+        # c = sqrt(x^2+y^2)   for postel
+
+        if projection not in [ 'lambert', 'stereographic', 'postel' ]:
+            raise ValueError('Unknown projection ' + projection)
+
+        # Convert from arcsec to radians
+        u = pos.x * galsim.arcsec / galsim.radians
+        v = pos.y * galsim.arcsec / galsim.radians
+
+        import math
+        # Compute r, c
+        # r = sqrt(u*u + v*v)
+        # c = 2 * arctan(r/2) for num == 1
+        # c = 2 * arcsin(r/2) for num == 2
+        # Note that we can rewrite the formulae as:
+        #
+        # sin(dec) = cos(c) sin(dec0) + v (sin(c)/r) cos(dec0)
+        # tan(ra-ra0) = u (sin(c)/r) / (cos(dec0) cos(c) - v sin(dec0) (sin(c)/r))
+        #
+        # which means we only need cos(c) and sin(c)/r
+        rsq = u*u + v*v
+        if projection == 'lambert':
+            # c = 2 * arcsin(r/2)
+            # Some trig manipulations reveal:
+            # cos(c) = 1 - r^2/2
+            # sin(c) = r sqrt(4-r^2) / 2
+            cosc = 1. - rsq/2.
+            sinc_over_r = math.sqrt(4.-rsq) / 2.
+        elif projection == 'stereographic':
+            # c = 2 * arctan(r/2)
+            # Some trig manipulations reveal:
+            # cos(c) = (4-r^2) / (4+r^2)
+            # sin(c) = 4r / (4+r^2)
+            cosc = (4.-rsq) / (4.+rsq)
+            sinc_over_r = 4. / (4.+rsq)
+        else:
+            r = math.sqrt(rsq)
+            if r == 0.:
+                cosc = 1
+                sinc_over_r = 1
+            else:
+                cosc = math.cos(r)
+                sinc_over_r = math.sin(r)/r
+
+        # Compute sindec, tandra
+        self._set_aux()
+        sindec = cosc * self._sindec + v * sinc_over_r * self._cosdec
+        # Remember the - sign so +dra is -u.  East is left.
+        tandra_num = -u * sinc_over_r
+        tandra_denom = self._cosdec * cosc - v * self._sindec * sinc_over_r
+
+        dec = math.asin(sindec) * galsim.radians
+        ra = self.ra + math.atan2(tandra_num, tandra_denom) * galsim.radians
+
+        return CelestialCoord(ra,dec)
+
+    def precess(self, from_epoch, to_epoch):
+        """This function precesses equatorial ra and dec from one epoch to another.
+           It is adapted from a set if fortran subroutines found in precess.f,
+           which  were based on (a) pages 30-34 fo the Eplanatory Supplement
+           to the AE, (b) Lieske, et al. (1977) A&A 58, 1-16, and
+           (c) Lieske (1979) A&A 73, 282-284.
+        """
+        if from_epoch == to_epoch: return self
+
+        # t0, t below correspond to Lieske's big T and little T
+        t0 = (_epoch-2000.)/100.;
+        t = (newepoch-_epoch)/100.;
+        t02 = t0*t0;
+        t2 = t*t;
+        t3 = t2*t;
+
+        # a,b,c below correspond to Lieske's zeta_A, z_A and theta_A
+        a = ( (2306.2181 + 1.39656*t0 - 0.000139*t02) * t +
+              (0.30188 - 0.000344*t0) * t2 + 0.017998 * t3 ) * galsim.arcsec
+        b = ( (2306.2181 + 1.39656*t0 - 0.000139*t02) * t +
+              (1.09468 + 0.000066*t0) * t2 + 0.018203 * t3 ) * galsim.arcsec
+        c = ( (2004.3109 - 0.85330*t0 - 0.000217*t02) * t +
+              (-0.42665 - 0.000217*t0) * t2 - 0.041833 * t3 ) * galsim.arcsec
+        import math
+        cosa = math.cos(a.rad())
+        sina = math.sin(a.rad())
+        cosb = math.cos(b.rad())
+        sinb = math.sin(b.rad())
+        cosc = math.cos(c.rad())
+        sinc = math.sin(c.rad())
+
+        # This is the precession rotation matrix:
+        xx = cosa*cosc*cosb - sina*sinb;
+        yx = -sina*cosc*cosb - cosa*sinb;
+        zx = -sinc*cosb;
+        xy = cosa*cosc*sinb + sina*cosb;
+        yy = -sina*cosc*sinb + cosa*cosb;
+        zy = -sinc*sinb;
+        xz = cosa*sinc;
+        yz = -sina*sinc;
+        zz = cosc;
+
+        # Perform the rotation:
+        self._set_aux()
+        x2 = xx*self._x + yx*self._y + zx*self._z,
+        y2 = xy*self._x + yy*self._y + zy*self._z,
+        z2 = xz*self._x + yz*self._y + zz*self._z,
+
+        new_dec = math.atan2(z2,math.sqrt(x2**2+y2**2)) * galsim.radians
+        new_ra = math.atan2(y2,x2) * galsim.radians
+        new_coord = CelestialCoord(new_ra,new_dec)
+        # Since we already knwo these, might as well set them.
+        new_coord._x = x2
+        new_coord._y = y2
+        new_coord._z = z2
+        return new_coord
+
+    def getGalaxyPos(self, epoch=2000.):
+        """Get the galaxy longitude and latitude corresponding to this position.
+
+        It returns the longitude and latitude as a tuple (el, b).  They are each given
+        as galsim.Angle instances.
+
+        The formulae are implemented in terms of the 1950 coordinates, so it needs to
+        precess from the current epoch to 1950.  The current epoch is assumed to be 2000
+        by default, but you may also specify a different value with the epoch parameter.
+        """
+        # cos(b) cos(el-33) = cos(dec) cos(ra-282.25)
+        # cos(b) sin(el-33) = sin(dec) sin(62.6) + cos(dec) sin(ra-282.25) cos(62.6)
+        #            sin(b) = sin(dec) sin(62.6) - cos(dec) sin(ra-282.25) sin(62.6)
+        import math
+        el0 = 33. * galsim.degrees
+        r0 = 282.25 * galsim.degrees
+        d0 = 62.6 * galsim.degrees
+        cosd0 = math.cos(d0.rad());
+        sind0 = math.sin(d0.rad());
+
+        temp = self.precess(epoch, 1950.);
+        d = temp.dec
+        r = temp.ra
+        cosd = math.cos(d.rad())
+        sind = math.sin(d.rad())
+        cosr = math.cos(r.rad() - r0.rad())
+        sinr = math.sin(r.rad() - r0.rad())
+
+        cbcl = cosd*cosr;
+        cbsl = sind*sind0 + cosd*sinr*cosd0;
+        sb = sind*cosd0 - cosd*sinr*sind0;
+
+        b = math.asin(sb) * galsim.radians;
+        cl = cbcl/cosb;
+        sl = cbsl/cosb;
+        el = math.atan2(sl,cl) * galsim.radians + el0;
+
+        return (el, b)
+
     def copy(self): return CelestialCoord(self._ra, self._dec)
 
+    def __repr__(self): return 'CelestialCoord('+repr(self._ra)+','+repr(self._dec)+')'
 
