e27ef0fbaabd969795490fbdb8e93e4218740938
jmeyers314 <jmeyers314@gmail.com>
2014-01-31 14:12:12 -0800

    update GK error estimate and add documentation (#467)

--- galsim/integ.py
+++ galsim/integ.py
@@ -17,7 +17,8 @@
 # along with GalSim.  If not, see <http://www.gnu.org/licenses/>
 #
 """@file integ.py
-A Python layer version of the C++ int1d function in galim::integ
+Includes a Python layer version of the C++ int1d function in galim::integ,
+and python image integrators for use in galsim.chromatic
 """
 
 from . import _galsim
@@ -54,22 +55,52 @@ def int1d(func, min, max, rel_err=1.e-6, abs_err=1.e-12):
     else:
         raise RuntimeError(result)
 
-def midpoint_int_image(f_image, a, b, N=100):
-    """Integrate an f_image, which generates an image as a function of a single variable,
-    (e.g., wavelength in the chromatic modules), using the midpoint rule.
+def midpoint_int_image(f_image, a, b, N=250):
+    """Integrate a datacube over one dimension using the midpoint rule.
+
+    Most probably this will be used as part of galsim.chromatic to integrate a chromatic surface
+    brightness profile over wavelength.
+
+    @param f_image     Function that accepts a single parameter and returns a galsim.Image
+    @param a           Lower limit of integral
+    @param b           Upper limit of integral
+    @param N           Number of intervals over which to evaluate the integrand
+    @returns           Integrated image
     """
     h = (b*1.0 - a)/N
     w = [a + h * (i+0.5) for i in range(N)]
     images = [f_image(i) for i in w]
     return h*reduce(lambda x, y: x+y, images)
 
-def trapezoidal_int_image(f_image, a, b, N=100):
+def trapezoidal_int_image(f_image, a, b, N=250):
+    """Integrate a datacube over one dimension using the trapezoidal rule.
+
+    Most probably this will be used as part of galsim.chromatic to integrate a chromatic surface
+    brightness profile over wavelength.
+
+    @param f_image     Function that accepts a single parameter and returns a galsim.Image
+    @param a           Lower limit of integral
+    @param b           Upper limit of integral
+    @param N           Number of intervals over which to evaluate the integrand
+    @returns           Integrated image
+    """
     h = (b*1.0 - a)/N
     w = [a + h * i for i in range(N+1)]
     images = [f_image(i) for i in w]
     return 0.5*h*(images[0] + 2.0*reduce(lambda x, y: x+y, images[1:-1]) + images[-1])
 
-def simpsons_int_image(f_image, a, b, N=100):
+def simpsons_int_image(f_image, a, b, N=250):
+    """Integrate a datacube over one dimension using Simpson's rule.
+
+    Most probably this will be used as part of galsim.chromatic to integrate a chromatic surface
+    brightness profile over wavelength.
+
+    @param f_image     Function that accepts a single parameter and returns a galsim.Image
+    @param a           Lower limit of integral
+    @param b           Upper limit of integral
+    @param N           Number of intervals over which to evaluate the integrand
+    @returns           Integrated image
+    """
     if N%2 == 1:
         N += 1
     h = (b*1.0 - a)/N
@@ -80,7 +111,8 @@ def simpsons_int_image(f_image, a, b, N=100):
                     + 2.0*reduce(lambda x, y: x+y, images[2:-2:2])
                     + images[-1])
 
-#Node locations and weights for Gaussian-Kronrod quadrature
+# Node locations and weights for Gaussian7-Kronrod15 quadrature
+# Stolen from http://www.advanpix.com/2011/11/07/gauss-kronrod-quadrature-nodes-weights/
 GK_nodes = [-0.9914553711208126392068547,
             -0.9491079123427585245261897,
             -0.8648644233597690727897128,
@@ -127,17 +159,35 @@ G_weights = [0.0,
              0.1294849661688696932706114,
              0.0]
 
-def gauss_kronrod_image_rule(f, a, b):
+def gauss_kronrod_image_rule(f_image, a, b):
+    """Gauss-Kronrod quadrature rule for integrating over one dimension of a datacube.
+
+    Uses 7 point Gaussian and 15 point Kronrod quadrature rules to estimate both a value and error
+    of an integral over one dimension of a datacube.
+
+    The paper Gonnet2010 was helpful here: http://arxiv.org/pdf/1003.4629.pdf
+
+    @param f_image     Function that accepts a single parameter and returns a galsim.Image
+    @param a           Lower limit of integral
+    @param b           Upper limit of integral
+    @returns           Integrated galsim.Image and pixel-by-pixel error estimate galsim.Image
+    """
     z = [(b-a)/2.0 * n + (a+b)/2.0 for n in GK_nodes]
-    fz = [f(i) for i in z]
+    fz = [f_image(i) for i in z]
     GArray = np.zeros_like(fz[0].array)
     KArray = np.zeros_like(fz[0].array)
+    IArray = np.zeros_like(fz[0].array)
     for i in xrange(15):
         GArray += fz[i].array * G_weights[i]
         KArray += fz[i].array * K_weights[i]
     GArray *= (b-a)/2.0
     KArray *= (b-a)/2.0
-    errArray = (200 * abs(GArray-KArray))**1.5 #need citation
+
+    # Gonnet2010 cites Piessens+1983 (QUADPACK, QAG algorithm) for the following error estimate:
+    for i in xrange(15):
+        IArray += np.abs(fz[i].array - KArray/(b-a)) * K_weights[i]
+    errArray = IArray * np.minimum(1, (200 * abs(GArray - KArray) / IArray)**1.5)
+
     QImage = fz[0].copy()
     errImage = fz[0].copy()
     QImage.setZero()
@@ -146,27 +196,56 @@ def gauss_kronrod_image_rule(f, a, b):
     errImage.array[:] = errArray
     return QImage, errImage
 
-def globally_adaptive_GK_int_image(f, a, b, rel_err=1.e-3, maxiter=100, verbose=False):
-    SImage, errImage = gauss_kronrod_image_rule(f, a, b)
-    err = errImage.array.sum()
+def globally_adaptive_GK_int_image(f_image, a, b, rel_err=1.e-4, maxiter=1000, verbose=False):
+    """Integrate a datacube over one dimension using a globally adaptive Gauss-Kronrod rule.
+
+    Most probably this will be used as part of galsim.chromatic to integrate a chromatic surface
+    brightness profile over wavelength.
+
+    The paper Gonnet2010 was helpful here: http://arxiv.org/pdf/1003.4629.pdf
+
+    The trickiest part of the algorithm is how to decide what to use as the scalar error
+    estimate for each interval.  Settled on using the maximum estimated single pixel error from
+    the GK rule.  Using the maximum has the benefit that the sum of max errors over subintervals
+    is an upper bound on the max error for the sum over subintervals. The global scale used is the
+    total flux in the integrated image.
+
+    @param f_image     Function that accepts a single parameter and returns a galsim.Image
+    @param a           Lower limit of integral
+    @param b           Upper limit of integral
+    @param rel_err     Target relative error
+    @param maxiter     Maximum number of times to divide an interval
+    @param verbose     Print out number of iterations and function evaluations used
+    @returns           Integrated galsim.Image and pixel-by-pixel error estimate galsim.Image
+    """
+    SImage, errImage = gauss_kronrod_image_rule(f_image, a, b)
+    shape = SImage.array.shape
+    #err = np.sqrt(np.mean(((errImage.array)**2)))
+    err = errImage.array.max()
     heap = [(a, b, SImage, errImage, err)]
     iter_=0
-    while ((err / SImage.array.sum()) > rel_err) and iter_ < maxiter:
+    # Could also try SImage.array.max() as a global scale below
+    while ((err / np.abs(SImage.array).sum()) > rel_err) and iter_ < maxiter:
         errs = [heap[i][4] for i in range(len(heap))]
         max_ = max(errs)
         k = errs.index(max_)
         a = heap[k][0]
         b = heap[k][1]
         m = (a*1.0 + b)/2.0
-        SImageLeft, errImageLeft = gauss_kronrod_image_rule(f, a, m)
-        SImageRight, errImageRight = gauss_kronrod_image_rule(f, m, b)
+        SImageLeft, errImageLeft = gauss_kronrod_image_rule(f_image, a, m)
+        SImageRight, errImageRight = gauss_kronrod_image_rule(f_image, m, b)
         SImage = SImage - heap[k][2] + SImageLeft + SImageRight
         errImage = errImage - heap[k][3] + errImageLeft + errImageRight
         del heap[k]
-        heap.append((a, m, SImageLeft, errImageLeft, errImageLeft.array.sum()))
-        heap.append((m, b, SImageRight, errImageRight, errImageRight.array.sum()))
-        err = errImage.array.sum()
+        # errLeft = np.sqrt(np.mean(((errImageLeft.array)**2)))
+        # errRight = np.sqrt(np.mean(((errImageRight.array)**2)))
+        errLeft = errImageLeft.array.max()
+        errRight = errImageRight.array.max()
+        heap.append((a, m, SImageLeft, errImageLeft, errLeft))
+        heap.append((m, b, SImageRight, errImageRight, errRight))
+        # err = np.sqrt(np.mean(((errImage.array)**2)))
+        err = errImage.array.max()
         iter_ += 1
     if verbose:
-        print 'GAGK iter: {}  N_eval: {}'.format(iter_, iter_*15)
+        print 'GAGK iter: {}  N_eval: {}'.format(iter_, (iter_+1)*15)
     return SImage
