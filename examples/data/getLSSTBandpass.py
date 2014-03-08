478980233d02c4cee6d470b7d19d3036adf869c8
jmeyers314 <jmeyers314@gmail.com>
2014-03-07 21:40:41 -0800

    Add copyright statement and more detailed file descriptions (#467)

--- examples/data/getLSSTBandpass.py
+++ examples/data/getLSSTBandpass.py
@@ -1,4 +1,25 @@
-# Grab LSST Bandpasses from the web, and then thin with rel_err = 1.e-5
+# Copyright 2012-2014 The GalSim developers:
+# https://github.com/GalSim-developers
+#
+# This file is part of GalSim: The modular galaxy image simulation toolkit.
+#
+# GalSim is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# GalSim is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with GalSim.  If not, see <http://www.gnu.org/licenses/>
+"""@file getLSSTBandpass.py
+Grab LSST Bandpasses from the web, and then thin with rel_err = 1.e-5.  Note that the outputs of
+this script, which are the files GALSIM_DIR/examples/data/LSST_?.dat, are already included in the
+repository.  This script just lets users know where these files came from and how they were altered.
+"""
 
 import galsim
 import urllib2
