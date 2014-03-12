45cab9e7537d9c19041c03b4202c15ceea613f7f
jmeyers314 <jmeyers314@gmail.com>
2014-03-11 19:22:37 -0700

    Some extra commentary and a couple doc fixes for demo12 (#467)

--- examples/demo12.py
+++ examples/demo12.py
@@ -28,7 +28,7 @@ level chromatic objects has not been written yet.
 This script introduces the chromatic objects module galsim.chromatic, which handles wavelength-
 dependent profiles.  Three uses of this module are demonstrated:
 
-1) A chromatic object representing an Exponential galaxy with an early-type SED at redshift 0.8 is
+1) A chromatic object representing a De Vaucouleurs galaxy with a early-type SED at redshift 0.8 is
 created.  The galaxy is then drawn using the six LSST filter throughput curves to demonstrate that
 the galaxy is a g-band dropout.
 
@@ -46,9 +46,9 @@ New features introduced in this demo:
 - SED = galsim.SED(wave, flambda)
 - SED2 = SED.atRedshift(redshift)
 - bandpass = galsim.Bandpass(filename)
-- bandpass2 = bandpass.truncate(relative_throughput=X)
+- bandpass2 = bandpass.truncate(relative_throughput)
 - bandpass3 = bandpass2.thin(rel_err)
-- gal = galsim.Chromatic(GSObject, wave, photons)
+- gal = galsim.Chromatic(GSObject, SED)
 - gal = GSObject * SED
 - obj = galsim.Add([list of ChromaticObjects])
 - ChromaticObject.draw(bandpass)
@@ -81,15 +81,17 @@ def main(argv):
     SEDs = {}
     for SED_name in SED_names:
         SED_filename = os.path.join(datapath, '{}.sed'.format(SED_name))
-        # Wavelengths read are in nanometers, flux in flambda.  This is important for the
-        # differential chromatic refraction routines below, which assume wavelengths are in
-        # nanometers. Without the DCR routines, then it's enough to just match the wavelength
-        # units of the SED and the bandpass.
+        # Here we create some galsim.SED objects to hold star or galaxy spectra.  The most
+        # convenient way to create realistic spectra is to read them in from a two-column ASCII
+        # file, where the first column is wavelength and the second column is flux. Wavelengths in
+        # the example SED files are in Angstroms, flux in flambda.  The default wavelength type for
+        # galsim.SED is nanometers, however, so we need to override by specifying
+        # `wave_type = 'Ang'`.
         SED = galsim.SED(SED_filename, wave_type='Ang')
         # The normalization of SEDs affects how many photons are eventually drawn into an image.
         # One way to control this normalization is to specify the flux density in photons per nm
-        # at a particular wavelength.  For example, here we normalize such that photon density is
-        # 1 photon per nm at 500 nm.
+        # at a particular wavelength.  For example, here we normalize such that the photon density
+        # is 1 photon per nm at 500 nm.
         SEDs[SED_name] = SED.withFluxDensity(target_flux_density=1.0, wavelength=500)
     logger.debug('Successfully read in SEDs')
 
@@ -98,12 +100,18 @@ def main(argv):
     filters = {}
     for filter_name in filter_names:
         filter_filename = os.path.join(datapath, 'LSST_{}.dat'.format(filter_name))
-        # Columns required to be 1) Wavelength in nm, 2) Dimensionless throughput
+        # Here we create some galsim.Bandpass objects to represent the filters we're observing
+        # through.  These include the entire imaging system throughput including the atmosphere,
+        # reflective and refractive optics, filters, and the CCD quantum efficiency.  These are
+        # also conveniently read in from two-column ASCII files where the first column is
+        # wavelength and the second column is dimensionless flux. The example filter files have
+        # units of nanometers and dimensionless throughput, which is exactly what galsim.Bandpass
+        # expects, so we just specify the filename.
         filters[filter_name] = galsim.Bandpass(filter_filename)
-        # and thin out the wavelength sampling
-        # rel_err here specifies the relative error when integrating over just the filter
-        # (however, this is not necessarily the relative error when integrating over the
-        #  filter times an SED)
+        # For speed, we can thin out the wavelength sampling of the filter a bit.
+        # In the following line, `rel_err` specifies the relative error when integrating over just
+        # the filter (however, this is not necessarily the relative error when integrating over the
+        # filter times an SED)
         filters[filter_name] = filters[filter_name].thin(rel_err=1e-4)
     logger.debug('Read in filters')
 
@@ -120,7 +128,7 @@ def main(argv):
     logger.info('')
     logger.info('Starting part A: chromatic De Vaucouleurs galaxy')
     redshift = 0.8
-    mono_gal = galsim.Exponential(half_light_radius=0.5)
+    mono_gal = galsim.DeVaucouleurs(half_light_radius=0.5)
     SED = SEDs['CWW_E_ext'].atRedshift(redshift)
     gal = galsim.Chromatic(mono_gal, SED)
 
@@ -160,7 +168,7 @@ def main(argv):
     # make a bulge ...
     mono_bulge = galsim.DeVaucouleurs(half_light_radius=0.5)
     bulge_SED = SEDs['CWW_E_ext'].atRedshift(redshift)
-    # Here's a shortcut for creating a chromatic version of a GSObject:
+    # The `*` operator can be used as a shortcut for creating a chromatic version of a GSObject:
     bulge = mono_bulge * bulge_SED
     bulge.applyShear(g1=0.12, g2=0.07)
     logger.debug('Created bulge component')
@@ -203,7 +211,8 @@ def main(argv):
     mono_gal = galsim.Exponential(half_light_radius=0.5)
     SED = SEDs['CWW_Im_ext'].atRedshift(redshift)
     # Here's another way to set the normalization of the SED.  If we want 50 counts to be drawn
-    # when observing an object with this SED through the g-band, for instance, then we do:
+    # when observing an object with this SED through the LSST g-band filter, for instance, then we
+    # can do:
     SED = SED.withFlux(50.0, filters['g'])
     # The flux drawn through other bands, which sample different parts of the SED and have different
     # throughputs, will, of course, be different.
@@ -220,32 +229,31 @@ def main(argv):
     # Kolmogorov turbulence in the atmosphere leads to a seeing size (e.g., FWHM) that scales with
     # wavelength to the (-0.2) power.
     #
-    # These effects are both implemented in the ChroamticAtmosphere class as perturbations to a
-    # fiducial PSF at some fiducial wavelength.
+    # The ChromaticAtmosphere function will attach both of these effects to a fiducial PSF at
+    # some fiducial wavelength.
 
-    # First we define a monochromatic PSF that will be the basis for perturbation as a function of
-    # wavelength
+    # First we define a monochromatic PSF to be the fiducial PSF.
     PSF_500 = galsim.Moffat(beta=2.5, fwhm=0.5)
     # Then we use ChromaticAtmosphere to manipulate this fiducial PSF as a function of wavelength.
-    # We also specify the wavelength of our fiducial PSF, and the zenith_angle of the observation
-    # so that the DCR can be computed.  We can also optionally specify the parallactic angle, which
-    # is the position angle of the zenith (measured from North through East).  A parallactic angle
-    # of 0.0 implies that the zenith is North (up), and a parallactic angle of 90 degrees indicates
-    # that the zenith is to the East (left).
-
-    # We can use dcr.zenith_parallactic_angles to compute these angles for an object at a given
-    # celestial coordinate and hour angle, and an observer at a particular latitude.  For example,
-    # the coordinates of M101 are:
+    # ChromaticAtmosphere also needs to know the wavelength of the fiducial PSF, and the location
+    # and orientation of the object with respect to the zenith.  This final piece of information
+    # can be specified in several ways (see the ChromaticAtmosphere docstring for all of them).
+    # Here are a couple ways: let's pretend our object is located near M101 on the sky, we observe
+    # it 1 hour before it transits and we're observing from Mauna Kea.
     ra = galsim.HMS_Angle("14:03:13") # hours : minutes : seconds
     dec = galsim.DMS_Angle("54:20:57") # degrees : minutes : seconds
     m101 = galsim.CelestialCoord(ra, dec)
-    latitude = 19.8207 * galsim.degrees
-    # Let's say we observed this position an hour before it transits, i.e. hour angle = -1 hr.
-    HA = -1.0 * galsim.hours
-    # Then we can compute the zenith angle and parallactic angle of this observation:
+    latitude = 19.8207 * galsim.degrees # latitude of Mauna Kea
+    HA = -1.0 * galsim.hours # Hour angle = one hour before transit
+
+    # Then we can compute the zenith angle and parallactic angle (which is is the position angle
+    # of the zenith measured from North through East) of this object:
     za, pa = galsim.dcr.zenith_parallactic_angles(m101, HA=HA, latitude=latitude)
     # And then finally, create the chromatic PSF
     PSF = galsim.ChromaticAtmosphere(PSF_500, 500.0, zenith_angle=za, parallactic_angle=pa)
+    # We could have also just passed `m101`, `latitude` and `HA` to ChromaticAtmosphere directly:
+    PSF = galsim.ChromaticAtmosphere(PSF_500, 500.0, obj_coord=m101, latitude=latitude, HA=HA)
+    # and proceed like normal.
 
     # convolve with galaxy and pixel to create final profile
     pix = galsim.Pixel(pixel_scale)
