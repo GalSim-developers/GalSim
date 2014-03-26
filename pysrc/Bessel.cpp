9656445e9af419e6bcb29d9a5167ce16f237e67c
Rachel Mandelbaum <rmandelb@andrew.cmu.edu>
2014-03-25 21:53:47 -0400

    add missing file, oops (#387)

---
+++ pysrc/Bessel.cpp
@@ -0,0 +1,47 @@
+/* -*- c++ -*-
+ * Copyright 2012-2014 The GalSim developers:
+ * https://github.com/GalSim-developers
+ *
+ * This file is part of GalSim: The modular galaxy image simulation toolkit.
+ *
+ * GalSim is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * GalSim is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with GalSim.  If not, see <http://www.gnu.org/licenses/>
+ */
+#ifndef __INTEL_COMPILER
+#if defined(__GNUC__) && __GNUC__ >= 4 && (__GNUC__ >= 5 || __GNUC_MINOR__ >= 8)
+#pragma GCC diagnostic ignored "-Wunused-local-typedefs"
+#endif
+#endif
+
+#define BOOST_NO_CXX11_SMART_PTR
+#include "boost/python.hpp"
+#include "boost/math/special_functions/bessel.hpp"
+
+#include <iostream>
+
+namespace bp = boost::python;
+
+namespace galsim {
+    namespace {
+        float besselj(double v, double x)
+        {
+            return boost::math::cyl_bessel_j(v,x);
+        }
+    }
+
+    void pyExportBesselJ() {
+        bp::def("besselj",
+                &besselj, bp::args("v","x"),
+                "Return the value of Bessel J_v(x).");
+    }
+} // namespace galsim
