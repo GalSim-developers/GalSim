d79c97f5ee253cf70c46e31b8d40ccfebae9291d
Barnaby Rowe <barnaby.t.p.rowe@gmail.com>
2014-04-04 18:10:33 +0200

    moved code to NPhelper.h (#486)

--- pysrc/NumpyHelper.h
+++ pysrc/NumpyHelper.h
@@ -278,6 +278,48 @@ static void CheckNumpyArray(const bp::object& array, int ndim, bool isConst,
     }
 }
 
+template <typename T>
+class array_scalar_converter {
+public:
+
+  static PyTypeObject const * get_pytype() {
+    // This implementation depends on the fact that get_builtin returns pointers to objects
+    // NumPy has declared statically, and that the typeobj member also refers to a static
+    // object.  That means we don't need to do any reference counting.
+    // In fact, I'm somewhat concerned that increasing the reference count of any of these
+    // might cause leaks, because I don't think Boost.Python ever decrements it, but it's
+    // probably a moot point if everything is actually static.
+    return reinterpret_cast<PyArray_Descr*>(dtype::get_builtin<T>().ptr())->typeobj;
+  }
+
+  static void * convertible(PyObject * obj) {
+    if (obj->ob_type == get_pytype()) {
+      return obj;
+    } else { 
+      return 0;
+    }
+  }
+
+  static void convert(PyObject * obj, pyconv::rvalue_from_python_stage1_data* data) {
+    void * storage = reinterpret_cast<pyconv::rvalue_from_python_storage<T>*>(data)->storage.bytes;
+    // We assume std::complex is a "standard layout" here and elsewhere; not guaranteed by
+    // C++03 standard, but true in every known implementation (and guaranteed by C++11).
+    PyArray_ScalarAsCtype(obj, reinterpret_cast<T*>(storage));
+    data->convertible = storage;
+  }
+
+  static void declare() {
+    pyconv::registry::push_back(
+      &convertible, &convert, python::type_id<T>()
+#ifndef BOOST_PYTHON_NO_PY_SIGNATURES
+      , &get_pytype
+#endif
+    );
+  }
+
+};
+
+
 } // namespace galsim
 
 #endif
