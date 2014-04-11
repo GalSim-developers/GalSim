7e9577a03fde83b005c7ab3e768883b2a0bf8666
Rachel Mandelbaum <rmandelb@andrew.cmu.edu>
2014-04-11 18:46:14 -0400

    pse.py docstring cleanup (#526)

--- galsim/pse.py
+++ galsim/pse.py
@@ -39,8 +39,8 @@ class PowerSpectrumEstimator(object):
     This class stores all the data used in power spectrum estimation that is fixed with the geometry
     of the problem - the binning and spin weighting factors.
 
-    The only public method is `estimate()`, which is called with 2D g1 and g2 arrays on a square
-    grid.  It assumes the flat sky approximation (where ell and k are interchangeable), and rebins
+    The only public method is estimate(), which is called with 2D `g1` and `g2` arrays on a square
+    grid.  It assumes the flat sky approximation (where `ell` and `k` are interchangeable), and rebins
     the observed ell modes into a user-defined number of logarithimic bins in ell.  Given that the
     grid parameters are precomputed and stored when the PowerSpectrumEstimator is initialized,
     computation of the PS for multiple sets of shears corresponding to the same grid setup can
@@ -71,25 +71,25 @@ class PowerSpectrumEstimator(object):
         # same grid geometry, we can reuse the same PowerSpectrumEstimator object.
         ell, P_e, P_b, P_eb = my_pse.estimate(g1, g2)
 
-    The output NumPy arrays ell, P_e, P_b, and P_eb contain the effective ell value, the E-mode
-    auto-power spectrum, the B-mode auto-power spectrum, and the EB cross-power spectrum.  The units
-    are inverse radians for ell, and radians^2 for the output power spectra.
+    The output NumPy arrays `ell`, `P_e`, `P_b`, and `P_eb` contain the effective ell value, the
+    E-mode auto-power spectrum, the B-mode auto-power spectrum, and the EB cross-power spectrum.
+    The units are inverse radians for ell, and radians^2 for the output power spectra.
 
     Some important notes:
 
-    1) Power spectrum estimation requires a weight function which decides how the averaging
-    is done across ell within each bin.  By default that weighting is flat in ell using an
-    analytic calculation of the area in ell space, but this is easy to change with the `_bin_power`
-    function.  (Note this area averaged bin weighting is only approximate for the higher frequency
-    bins in which the lower ell edge is greater than pi * ngrid / grid_size, due to the annular
-    ell region being cut off by the square grid edges beyond this value.)  A keyword allows for
+    1) Power spectrum estimation requires a weight function which decides how the averaging is done
+    across ell within each bin.  By default that weighting is flat in ell using an analytic
+    calculation of the area in ell space, but this is easy to change with the `_bin_power` function.
+    (Note this area averaged bin weighting is only approximate for the higher frequency bins in
+    which the lower `ell` edge is greater than `pi * ngrid / grid_size`, due to the annular `ell`
+    region being cut off by the square grid edges beyond this value.)  A keyword allows for
     weighting by the power itself, but use of this functionality requires the GalSim software
     package.
 
     2) This is the power spectrum of the gridded *data*, not the underlying field - we do not
     account for the effects of the finite grid (basically, ignoring all the reasons why power
     spectrum estimation is hard - see devel/modules/lensing_engine.pdf in the GalSim repository).
-    Users must account for the contribution of noise in g1, g2 and any masking.
+    Users must account for the contribution of noise in `g1`, `g2` and any masking.
 
     3) The binning is currently fixed as uniform in log(ell).
 
@@ -103,10 +103,10 @@ class PowerSpectrumEstimator(object):
         PowerSpectrumEstimator can be used to estimate the power spectrum quickly for many sets of
         shears at gridded positions.
 
-        @param N            The number of pixels along each side of the grid. [default: 100]
+        @param N             The number of pixels along each side of the grid. [default: 100]
         @param sky_size_deg  The total grid width (in one dimension) in degrees. [default: 10]
-        @param nbin         The number of evenly-spaced logarithmic ell bins to use for estimating
-                            the power spectrum. [default: 15]
+        @param nbin          The number of evenly-spaced logarithmic `ell` bins to use for estimating
+                             the power spectrum. [default: 15]
         """
         # Set up the scales of the sky and pixels
         self.N = N
@@ -174,7 +174,7 @@ class PowerSpectrumEstimator(object):
         return P/count
 
     def estimate(self, g1, g2, weight_EE=False, weight_BB=False, theory_func=None):
-        """Compute the EE, BB, and EB power spectra of two 2D arrays g1 and g2.
+        """Compute the EE, BB, and EB power spectra of two 2D arrays `g1` and `g2`.
 
         For example usage, see the docstring for the PowerSpectrumEstimator class.
 
