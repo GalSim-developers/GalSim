cffac20fa99b4ba71e46d9ec7815c8a8ff110a99
Barnaby Rowe <barnaby.t.p.rowe@gmail.com>
2014-07-02 22:32:21 +0100

    fixed references to aliasing that remained in docstring for folding_threshold GSParams (#562)

--- galsim/base.py
+++ galsim/base.py
@@ -2164,20 +2164,20 @@ to change.
                             which is often a sign of an error in the user's code. However, if
                             you have the memory to handle it, you can raise this limit to
                             allow the calculation to happen. [default: 4096]
-@param folding_threshold    This sets a maximum amount of real space aliasing that is allowed,
+@param folding_threshold    This sets a maximum amount of real space folding that is allowed,
                             an effect caused by the periodic nature of FFTs.  FFTs implicitly
                             use periodic boundary conditions, and a profile specified on a
                             finite grid in Fourier space corresponds to a real space image
-                            that will have some overlap with the neighboring (aliased) copies
-                            of the real space profile.  As the step size in k increases, the
-                            spacing between neighboring aliases in real space decreases,
-                            increasing the amount of overlapping flux.  `folding_threshold` is
-                            used to set an appropriate step size in k to allow at most this
-                            fraction of the flux to be aliased.  This parameter is also
-                            relevant when you let GalSim decide how large an image to use for
-                            your object.  The image is made to be large enough that at most a
-                            fraction `folding_threshold` of the total flux is allowed to fall off
-                            the edge of the image.  [default: 5.e-3]
+                            that will have some overlap with the neighboring copies of the real
+                            space profile.  As the step size in k increases, the spacing between
+                            neighboring aliases in real space decreases, increasing the amount of
+                            folded, overlapping flux.  `folding_threshold` is used to set an
+                            appropriate step size in k to allow at most this fraction of the flux
+                            to be folded.
+                            This parameter is also relevant when you let GalSim decide how large
+                            an image to use for your object.  The image is made to be large enough
+                            that at most a fraction `folding_threshold` of the total flux is
+                            allowed to fall off the edge of the image. [default: 5.e-3]
 @param stepk_minimum_hlr    In addition to the above constraint for aliasing, also set stepk
                             such that pi/stepk is at least `stepk_minimum_hlr` times the
                             profile's half-light radius (for profiles that have a well-defined
