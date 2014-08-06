813cfda0befbd66c93234fabcf5bca01e4232e2a
Barnaby Rowe <barnaby.t.p.rowe@gmail.com>
2014-08-06 15:21:39 +0100

    style edits where being picky improves legibility, additional comment or two, 100 char stuff (#524)

--- tests/test_cdmodel.py
+++ tests/test_cdmodel.py
@@ -18,9 +18,7 @@
 import numpy as np
 import os
 import sys
-
 from galsim_test_helpers import *
-
 try:
     import galsim
     from galsim.cdmodel import *
@@ -34,9 +32,9 @@ except ImportError:
 def test_simplegeometry():
     """Test charge deflection model for image with charges in only the central pixel(s).
     """
-    size=50
-    center=25
-    shiftcoeff=0.1
+    size = 50
+    center = 25
+    shiftcoeff = 0.1
     
     # create otherwise empty image with central pixel at one
     i0 = galsim.Image(size,size,dtype=np.float32,init_value=0)
@@ -54,15 +52,14 @@ def test_simplegeometry():
     it.setValue(center,center  ,1)
     it.setValue(center,center+1,1)
 
+    # set up models, images
     cdr0   = PowerLawCD(2,shiftcoeff,0,0,0,0,0,0)
     i0cdr0 = cdr0.applyForward(i0)
-
     cdt0   = PowerLawCD(2,0,shiftcoeff,0,0,0,0,0)
     i0cdt0 = cdt0.applyForward(i0)
-    
     cdrx   = PowerLawCD(2,0,0,shiftcoeff,0,0,0,0)
     cdtx   = PowerLawCD(2,0,0,0,shiftcoeff,0,0,0)
-    
+
     # these should do something
     ircdtx = cdtx.applyForward(ir)
     itcdrx = cdrx.applyForward(it)
@@ -135,7 +132,7 @@ def test_simplegeometry():
     import time
     u = galsim.UniformDeviate(int(time.time()))
     
-    cdnull   = PowerLawCD(2,0,0,shiftcoeff*u(),shiftcoeff*u(),shiftcoeff*u(),shiftcoeff*u(),0)
+    cdnull = PowerLawCD(2, 0, 0, shiftcoeff*u(), shiftcoeff*u(), shiftcoeff*u(), shiftcoeff*u(), 0)
     i0cdnull = cdnull.applyForward(i0)
         
     # setting all pixels to 0 that we expect to be not 0...
@@ -173,29 +170,25 @@ def test_simplegeometry():
 def test_fluxconservation():
     """Test flux conservation of charge deflection model for galaxy and flat image.
     """
-
-    galflux=30000.
-    galsigma=3.
-    noise=1.
-    shiftcoeff=5.e-5
-    alpha=0.3
-    
-    size=50
+    galflux = 30000.
+    galsigma = 3.
+    noise = 1.
+    shiftcoeff = 5.e-5
+    alpha = 0.3
+    size = 50
 
     gal = galsim.Gaussian(flux=galflux, sigma=galsigma)
     image = gal.drawImage(scale=1.)
     image.addNoise(galsim.GaussianNoise(sigma=noise))
-    
-    flat = galsim.Image(size,size,dtype=np.float32,init_value=0)
-    flat.fill(100)
-    
-    cd   = PowerLawCD(
+
+    flat = galsim.Image(size, size, dtype=np.float32, init_value=0)
+    flat.fill(100)    
+    cd = PowerLawCD(
         2, shiftcoeff, shiftcoeff, shiftcoeff/2., shiftcoeff/2., shiftcoeff/2., shiftcoeff/2.,
         alpha)
-    
     imagecd = cd.applyForward(image)
     flatcd  = cd.applyForward(flat)
-    
+    # Then test
     np.testing.assert_almost_equal(
         image.array.sum(), imagecd.array.sum(), 2,
         "Galaxy image flux is not left invariant by charge deflection")
@@ -206,47 +199,34 @@ def test_fluxconservation():
 def test_forwardbackward():
     """Test invariance (to first order) under forward-backward transformation.
     """
-    galflux=30000.
-    galsigma=3.
-    noise=1.
-    shiftcoeff=1.e-5
-    alpha=0.3
-    
-    size=50
+    galflux = 30000.
+    galsigma = 3.
+    noise = 1.
+    shiftcoeff = 1.e-5
+    alpha = 0.3
+    size = 50
 
     gal = galsim.Gaussian(flux=galflux, sigma=galsigma)
     maxflux = gal.xValue(0,0)
-    #print maxflux
     image = gal.drawImage(scale=1.)
     
     cimage = galsim.Image(image.getBounds()) # used for normalization later   
     cimage.fill(1.)
     cimage = cimage+image
     cimage = cimage*maxflux*maxflux*shiftcoeff*shiftcoeff
-    
+
     image.addNoise(galsim.GaussianNoise(sigma=noise))    
-    
-    #image.write("image.fits")
-    #cimage.write("imagec.fits")
-    
-    cd   = PowerLawCD(
+    cd = PowerLawCD(
         2, shiftcoeff, 2.*shiftcoeff, shiftcoeff/2., 2.*shiftcoeff/3., shiftcoeff/2.,
         shiftcoeff/3., alpha)
     
     imagecd = cd.applyForward(image)
     imagecddc = cd.applyBackward(imagecd)
     
-    #imagecd.write("imagecd.fits")
-    #imagecddc.write("imagecddc.fits")
-    
     # residual after forward-backward should be of order a^2 q qmax^2
-    
-    imageres = (imagecddc-image)/cimage
-    #imageres.write("imageres.fits")
-    
+    imageres = (imagecddc - image) / cimage    
     maxres = imageres.array.max()
     minres = imageres.array.min()
-    
     assert maxres<10, ("maximum positive residual of forward-backward transformation is too large")
     assert minres>-10, ("maximum negative residual of forward-backward transformation is too large")
     
@@ -254,25 +234,18 @@ def test_exampleimage():
     """Test application of model compared to an independent implementation that was run on the
     example image.
     """
-    
     #n, r0, t0, rx, tx, r, t, alpha
     cd = PowerLawCD(5, 2.e-7, 1.e-7, 1.25e-7, 1.25e-7, 0.75e-7, 0.5e-7, 0.3)
-      # model used externally to bring cdtest1 to cdtest2
-
+    # model used externally to bring cdtest1 to cdtest2
     image_orig  = galsim.fits.read("fits_files/cdtest1.fits") # unprocessed image
     image_proc  = galsim.fits.read("fits_files/cdtest2.fits") # image with cd model applied with
                                                               # other library
-    
     image_plcd  = cd.applyForward(image_orig)
-    
-    #image_plcd.write("cdtest_intern.fits")
-    #image_proc.write("cdtest_extern.fits")
-
-    np.testing.assert_array_almost_equal(image_proc.array, image_plcd.array, 1,
-                                   "externally and internally processed image not equal") 
+    np.testing.assert_array_almost_equal(
+        image_proc.array, image_plcd.array, 1, "externally and internally processed image unequal") 
                                    # I checked that the remaining differences are numerical noise
 
-    
+
 if __name__ == "__main__":
     test_simplegeometry()
     test_fluxconservation()
