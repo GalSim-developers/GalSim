d53b1c20a3b22ff494033ab3b6e17e6ea19f67b4
jmeyers314 <jmeyers314@gmail.com>
2014-12-09 13:13:47 -0800

    documentation for nx,ny,bounds (#603)

--- galsim/base.py
+++ galsim/base.py
@@ -904,16 +904,17 @@ class GSObject(object):
         If drawImage() will be creating the image from scratch for you, then there are several
         ways to control the size of the new image.  If the `nx` and `ny` keywords are present, then
         an image with these numbers of pixels on a side will be created.  Similarly, if the `bounds`
-        keyword is present, then an image with the specified bounds will be created.  In the absence
-        of `nx`, `ny`, and `bounds`, drawImage will decide a good size to use based on the size of
-        the object being drawn.  Basically, it will try to use an area large enough to include at
-        least 99.5% of the flux.  (Note: the value 0.995 is really `1 - folding_threshold`.  You can
-        change the value of `folding_threshold` for any object via GSParams.  See `help(GSParams)`
-        for more details.)  You can set the pixel scale of the constructed image with the `scale`
-        parameter, or set a WCS function with `wcs`.  If you do not provide either `scale` or `wcs`,
-        then drawImage() will default to using the Nyquist scale for the current object.  You can
-        also set the data type used in the new Image with the `dtype` parameter that has the same
-        options as for the Image constructor.
+        keyword is present, then an image with the specified bounds will be created.  Note that it
+        is an error to provide existing Image when also specifying `nx`, `ny`, or `bounds`. In the
+        absence of `nx`, `ny`, and `bounds`, drawImage will decide a good size to use based on the
+        size of the object being drawn.  Basically, it will try to use an area large enough to
+        include at least 99.5% of the flux.  (Note: the value 0.995 is really
+        `1 - folding_threshold`.  You can change the value of `folding_threshold` for any object via
+        GSParams.  See `help(GSParams)` for more details.)  You can set the pixel scale of the
+        constructed image with the `scale` parameter, or set a WCS function with `wcs`.  If you do
+        not provide either `scale` or `wcs`, then drawImage() will default to using the Nyquist
+        scale for the current object.  You can also set the data type used in the new Image with
+        the `dtype` parameter that has the same options as for the Image constructor.
 
         There are several different possible methods drawImage() can use for rendering the image.
         This is set by the `method` parameter.  The options are:
@@ -1036,9 +1037,12 @@ class GSObject(object):
                             image's pixel scale.
                             If `scale` is None and `image` is None, then use the Nyquist scale.
                             If `scale <= 0` (regardless of `image`), then use the Nyquist scale.
+                            If `scale > 0` and `image` is given, then override `image.scale` with
+                            the value given as a keyword.
                             [default: None]
-        @param wcs          If provided, use this as the wcs for the image.  At most one of `scale`
-                            or `wcs` may be provided. [default: None]
+        @param wcs          If provided, use this as the wcs for the image (possibly overriding any
+                            existing `image.wcs`.  At most one of `scale` or `wcs` may be provided.
+                            [default: None]
         @param dtype        The data type to use for an automatically constructed image.  Only
                             valid if `image` is None. [default: None, which means to use
                             numpy.float32]
