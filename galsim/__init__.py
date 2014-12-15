5de7e554cf41628009ede5eed7582b66359b3d8e
Arun Kannawadi <arunkannawadi@cmu.edu>
2014-12-15 15:09:12 -0500

    calling detectors (#555)

--- galsim/__init__.py
+++ galsim/__init__.py
@@ -146,9 +146,7 @@ from lensing_ps import PowerSpectrum
 from nfw_halo import NFWHalo, Cosmology
 
 # Detector effects
-from detectors import applyNonlinearity
-from detectors import addReciprocityFailure
-from detectors import applyIPC
+from . import detectors
 
 # Packages we intentionally keep separate.  E.g. requires galsim.fits.read(...)
 from . import fits
