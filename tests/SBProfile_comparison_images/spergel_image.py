3aabc9c8699b50fa2c2fef7ddb996f4e10b0737c
jmeyers314 <jmeyers314@gmail.com>
2014-12-19 19:16:24 -0800

    Spergel radii tests (#616)

--- tests/SBProfile_comparison_images/spergel_image.py
+++ tests/SBProfile_comparison_images/spergel_image.py
@@ -25,7 +25,7 @@ hlr = 1.0
 
 hdulist = fits.HDUList()
 
-for nu in [-0.9, -0.3, 0.0, 0.85]:
+for nu in [-0.9, -0.5, 0.0, 0.85]:
     img = radial_profile(nu, hlr)(np.sqrt(xs**2 + ys**2))
     print nu, c(nu)
     output_file = "spergel_nu{:.2f}.fits".format(nu)
