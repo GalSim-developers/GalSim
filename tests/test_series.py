53dd8a7eb1cff86c7a66039ee076b43126872b0d
jmeyers314 <jmeyers314@gmail.com>
2015-02-25 16:36:05 -0800

    kValue and convergence unit tests (#628)

--- tests/test_series.py
+++ tests/test_series.py
@@ -62,6 +62,31 @@ def test_spergelseries_decomposeA():
     """
     pass
 
+def test_spergelseries_kValue():
+    """ Test that SpergelSeries.kValue() agrees with some Mathematica computations.
+    This is essentially testing the handling of the internal .Delta, .ellip, and .phi0 parameters
+    directly, without relying on the _decomposeA method.
+    """
+
+    #                  inputs                      output
+    #       [   nu,    D, eps, phi,    x,    y,      kval]
+    vals = [
+            [-0.85,  0.1, 0.1, 1.0,  1.1, -3.4, 0.686564 ],
+            [ -0.5, -0.1, 0.2, 1.1, -2.3,  3.3, 0.243808 ],
+            [  0.0, 0.23, 0.2, 0.2,  0.3, 10.0, 0.015526 ],
+            [  1.1, 0.01, 0.5, 0.1,  5.2, -2.7, 0.0003938]
+    ]
+
+
+    for nu, Delta, epsilon, phi0, x, y, kval in vals:
+        gal = galsim.SpergelSeries(nu=nu, scale_radius=1.0, jmax=5)
+        # Set these by hand instead of through .dilate, .shear, etc...
+        gal.ellip = epsilon
+        gal.phi0 = phi0
+        gal.Delta = Delta
+        gal.scale_radius = 1.0
+        np.testing.assert_almost_equal(gal.kValue(x,y), kval, 6)
+    
 # def test_spergelseries_coeff():
 #     """Check a_jq coefficient by generating a_jmn coefficients and forming the appropriate sum.
 #     """
@@ -92,18 +117,74 @@ def test_spergelseries_decomposeA():
 def test_spergelseries():
     """Test that SpergelSeries converges to Spergel.
     """
-    pass
+    # Need a PSF to handle super peaky nu < 0 Spergel profiles.  Otherwise the required fft size
+    # explodes.
+    psf = galsim.Gaussian(fwhm=0.5)
+    # Higher |e| profiles require more terms to converge, so include jmax as a param to vary.
+    #       [   nu,    e1, e2,  mu, jmax]
+    vals = [
+            [-0.85,  0.1, 0.1, 0.9, 5],
+            [ -0.5, -0.1, 0.2, 1.1, 5],   #Misses decimal=5 for 0.4% of pixels
+            [  0.0, 0.23, 0.2, 0.7, 6],   #Misses decimal=5 for 4% of pixels
+            [  1.1, 0.01, 0.5, 0.2, 7]    #Misses decimal=5 for 0.6% of pixels
+    ]
+    for nu, e1, e2, mu, jmax in vals:
+        gal_exact = galsim.Spergel(nu=nu, half_light_radius=1.0).shear(e1=e1, e2=e2).dilate(mu)
+        gal_series = (galsim.SpergelSeries(nu=nu, half_light_radius=1.0, jmax=jmax)
+                      .shear(e1=e1, e2=e2).dilate(mu))
+        obj_exact = galsim.Convolve(gal_exact, psf)
+        obj_series = galsim.SeriesConvolution(gal_series, psf)
+        im_exact = obj_exact.drawImage(nx=32, ny=32, scale=0.2)
+        im_series = obj_series.drawImage(nx=32, ny=32, scale=0.2)
+        if False:
+            import matplotlib.pyplot as plt
+            plt.subplot(121)
+            plt.imshow(im_exact.array)
+            plt.subplot(122)
+            plt.imshow(im_series.array)
+            plt.show()
+        np.testing.assert_array_almost_equal(im_exact.array, im_series.array, 4)
+            
 
 def test_spergelseries_dilate():
-    """ Check that SpergelSeries(nu=nu, scale_radius=1.0).dilate(1.1) gives the same results as
-    SpergelSeries(nu=nu, scale_radius=1.1)
+    """ Check that SpergelSeries with .scale_radius = mu gives same image as SpergelSeries with
+    .scale_radius = 1.0 and .Delta = (1-mu**2).
     """
-    pass
+    # Need a PSF to handle super peaky nu < 0 Spergel profiles.  Otherwise the required fft size
+    # explodes.
+    psf = galsim.Gaussian(fwhm=0.5)
+    #                  inputs
+    #       [   nu,    D, eps, phi, jmax]
+    vals = [
+            [-0.85,  0.2, 0.1, 0.9, 5],
+            [ -0.5, -0.2, 0.2, 1.1, 5],
+            [  0.0, 0.23, 0.2, 0.7, 6],
+            [  1.1,  0.2, 0.5, 0.2, 7]
+    ]
+    for nu, Delta, eps, phi, jmax in vals:
+        mu = np.sqrt(1-Delta)
+        gal_direct = galsim.SpergelSeries(nu=nu, half_light_radius=1.0, jmax=jmax)
+        # Set appropriate attributes directly, instead of through .decomposeA()
+        gal_direct.ellip = eps
+        gal_direct.phi0 = phi
+        gal_direct.scale_radius = mu
+        gal_direct.Delta = 0.0
+        gal_dilate = galsim.SpergelSeries(nu=nu, half_light_radius=1.0, jmax=jmax)
+        gal_dilate.ellip = eps
+        gal_dilate.phi0 = phi
+        gal_dilate.scale_radius = 1.0
+        gal_dilate.Delta = Delta
+        obj_direct = galsim.SeriesConvolution(gal_direct, psf)
+        obj_dilate = galsim.SeriesConvolution(gal_dilate, psf)
+        im_direct = obj_direct.drawImage(nx=32, ny=32, scale=0.2)
+        im_dilate = obj_dilate.drawImage(nx=32, ny=32, scale=0.2)
+        np.testing.assert_almost_equal(im_direct.array, im_dilate.array, 5)
 
 if __name__ == "__main__":
     test_spergelet()
     test_series_draw()
     test_series_gsobject_convolution()
     test_spergelseries_decomposeA()
+    test_spergelseries_kValue()
     test_spergelseries()
     test_spergelseries_dilate()
