6d20ba2584665129c008b149cfc0effad2eddf58
Mike Jarvis <michael@jarvis.net>
2015-03-02 10:02:53 -0500

    Delete the tarball by default (#590)

--- bin/galsim_download_cosmos.py
+++ bin/galsim_download_cosmos.py
@@ -38,18 +38,18 @@ def parse_args():
     # rather than the full 4 GB file.  Right now, this just downloads the larger catalog.
 
     # Short description strings common to both parsing mechanisms
-    version_str = "GalSim Version %s"%galsim.version
-    description = "galsim_download_cosmos will download the COSMOS RealGalaxy catalog "
-    description += "and place it in the GalSim share directory so it can be used as "
-    description += "the default file_name for the RealGalaxyCatalog class. "
-    description += "See https://github.com/GalSim-developers/GalSim/wikiealGalaxy%20Data "
-    description += "for details about where this program is downloading from."
+    description = "This program will download the COSMOS RealGalaxy catalog and images\n"
+    description += "and place them in the GalSim share directory so they can be used as\n "
+    description += "the default files for the RealGalaxyCatalog class.\n"
+    description += "See https://github.com/GalSim-developers/GalSim/wiki/RealGalaxy%20Data\n"
+    description += "for more details about the files being downloaded."
+    epilog = "Note: The unpacked files total almost 6 GB in size!\n"
     
     try:
         import argparse
         
         # Build the parser and add arguments
-        parser = argparse.ArgumentParser(description=description, add_help=True)
+        parser = argparse.ArgumentParser(description=description, epilog=epilog, add_help=True)
         parser.add_argument(
             '-v', '--verbosity', type=int, action='store', default=2, choices=(0, 1, 2, 3),
             help='integer verbosity level: min=0, max=3 [default=2]')
@@ -58,26 +58,23 @@ def parse_args():
             help='force overwriting the current file if one exists')
         parser.add_argument(
             '-q', '--quiet', action='store_const', default=False, const=True,
-            help="don't ask about re-downloading an existing file.")
+            help="don't ask about re-downloading an existing file. (implied by verbosity=0)")
         parser.add_argument(
             '-u', '--unpack', action='store_const', default=False, const=True,
             help='re-unpack the tar file if not downloading')
         parser.add_argument(
-            '--version', action='store_const', default=False, const=True,
-            help='show the version of GalSim')
+            '-s', '--save', action='store_const', default=False, const=True,
+            help="save the tarball after unpacking.")
         args = parser.parse_args()
-
-        if args.version:
-            print version_str
 
     except ImportError:
         # Use optparse instead
         import optparse
 
         # Usage string not automatically generated for optparse, so generate it
-        usage = """usage: galsim_download_cosmos [-h] [-v {0,1,2,3}] [-f] [-q] [-u] [--version] """
+        usage = """usage: galsim_download_cosmos [-h] [-v {0,1,2,3}] [-f] [-q] [-u] [-d]"""
         # Build the parser
-        parser = optparse.OptionParser(usage=usage, description=description)
+        parser = optparse.OptionParser(usage=usage, description=description, epilog=epilog)
         # optparse only allows string choices, so take verbosity as a string and make it int later
         parser.add_option(
             '-v', '--verbosity', type="choice", action='store', choices=('0', '1', '2', '3'),
@@ -87,19 +84,19 @@ def parse_args():
             help='force overwriting the current file if one exists')
         parser.add_argument(
             '-q', '--quiet', action='store_const', default=False, const=True,
-            help="don't ask about re-downloading an existing file.")
+            help="don't ask about re-downloading an existing file. (implied by verbosity=0)")
         parser.add_argument(
             '-u', '--unpack', action='store_const', default=False, const=True,
             help='Re-unpack the tar file if not downloading')
-        parser.add_option(
-            '--version', action='store_const', default=False, const=True,
-            help='show the version of GalSim')
+        parser.add_argument(
+            '-s', '--save', action='store_const', default=False, const=True,
+            help="save the tarball after unpacking.")
 
         # Remembering to convert to an integer type
         args.verbosity = int(args.verbosity) 
 
-        if args.version:
-            print version_str
+    if args.verbosity == 0:
+        args.quiet = True
 
     # Return the args
     return args
@@ -158,12 +155,15 @@ def main():
     share_dir = galsim.meta_data.share_dir
     target = os.path.join(share_dir, file_name)
 
+    unpack_dir = target[:-len('.tar.gz')]
+
     logger.info('Downloading from url:\n  %s',url)
-    logger.info('Target location is %s',target)
+    logger.info('Target location is:\n  %s',target)
 
     # See how large the file to be downloaded is.
     u = urllib2.urlopen(url)
     meta = u.info()
+    logger.debug("\nMeta information about url:\n%s",str(meta))
     file_size = int(meta.getheaders("Content-Length")[0]) / 1024**2
     logger.info("\nSize of %s: %d MBytes" , file_name, file_size)
 
@@ -177,22 +177,41 @@ def main():
                         existing_file_size)
         elif file_size == existing_file_size:
             if args.quiet:
-                logger.info("Target file already exists.")
+                logger.info("Target file already exists.  Not re-downloading.")
                 do_download = False
             else:
-                yn = query_yes_no("Target file already exists.  Overwrite?", default='no')
+                q = "Target file already exists.  Overwrite?"
+                yn = query_yes_no(q, default='no')
                 if yn == 'no':
                     do_download = False
         else:
-            print "Target file already exists, but it seems to be corrupt."
-            print "Size of existing file = %d MBytes   "%(existing_file_size),
+            logger.warn("Target file already exists, but it seems to be incomplete.")
             if args.quiet:
-                logger.info("Re-downloading.")
+                logger.warn("Size of existing file = %d MBytes.  Re-downloading.",
+                            existing_file_size)
             else:
-                yn = query_yes_no("Re-download?", default='yes')
+                q = "Size of existing file = %d MBytes.  Re-download?"%(existing_file_size)
+                yn = query_yes_no(q, default='yes')
                 if yn == 'no':
                     do_download = False
-
+    elif os.path.isdir(unpack_dir):
+        logger.info("")
+        if args.force:
+            logger.info("Target file has already been downloaded and unpacked.  "+
+                        "Forced re-download.")
+        else:
+            if args.quiet:
+                logger.info("Target file has already been downloaded and unpacked.  "+
+                            "Not re-downloading.")
+                do_download = False
+                args.save = True  # Don't try to re-delete it!
+            else:
+                q = "Target file has already been downloaded and unpacked.  Re-download?"
+                yn = query_yes_no(q, default='no')
+                if yn == 'no':
+                    do_download = False
+                    args.save = True
+ 
     # The next bit is based on one of the answers here: (by PabloG)
     # http://stackoverflow.com/questions/22676/how-do-i-download-a-file-over-http-using-python
     # The progress bar feature in that answer is important here, since this will take a while,
@@ -221,7 +240,7 @@ def main():
         logger.info("Download complete.")
 
     if do_download or args.unpack:
-        logger.info("Unpacking the tarball")
+        logger.info("Unpacking the tarball...")
         with tarfile.open(target) as tar:
             if args.verbosity >= 3:
                 tar.list(verbose=True)
@@ -230,6 +249,9 @@ def main():
             tar.extractall(share_dir)
         logger.info("Extracted contents of tar file.")
 
+    if not args.save:
+        logger.info("Removing the tarball to save space")
+        os.remove(target)
 
 if __name__ == "__main__":
     main()
