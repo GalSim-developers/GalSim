7cfa86dd4d710112620be3594d151f28b9122450
Rachel Mandelbaum <rmandelb@andrew.cmu.edu>
2015-04-16 14:13:54 -0400

    typo (#218)

--- galsim/chromatic.py
+++ galsim/chromatic.py
@@ -1198,7 +1198,7 @@ class ChromaticTransformation(ChromaticObject):
 
     Typically, you do not need to construct a ChromaticTransformation object explicitly.
     This is the type returned by the various transformation methods of ChromaticObject such as
-    shear(), rotat(), shift(), transform(), etc.  All the various transformations can be described
+    shear(), rotate(), shift(), transform(), etc.  All the various transformations can be described
     as a combination of transform() and shift(), which are described by (dudx,dudy,dvdx,dvdy) and 
     (dx,dy) respectively.
 
