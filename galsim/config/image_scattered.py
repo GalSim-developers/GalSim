GalSim-developers/GalSim

commit 6705f4ebe2f2c3a0e9aa2d33e4c58af810525454

Save nimages and nobj so extra processing doesn't need to rerun the calculation. (#654)

galsim/config/output.py
@@ -134,7 +134,7 @@ def BuildFiles(nfiles, config, file_num=0, logger=None):
     else:
         if logger and logger.isEnabledFor(logging.WARN):
             if nfiles_written > 1 and nproc != 1:
-                logger.warn('Total time for %d files with %d processes = %f sec', 
+                logger.warn('Total time for %d files with %d processes = %f sec',
                             nfiles_written,nproc,t2-t1)
             logger.warn('Done building files')
 
@@ -144,7 +144,7 @@ output_ignore = [ 'file_name', 'dir', 'nfiles', 'nproc', 'skip', 'noclobber', 'r
 def BuildFile(config, file_num=0, image_num=0, obj_num=0, logger=None):
     """
     Build an output file as specified in config.
-    
+
     @param config           A configuration dict.
     @param file_num         If given, the current file_num. [default: 0]
     @param image_num        If given, the current image_num. [default: 0]
@@ -162,10 +162,12 @@ def BuildFile(config, file_num=0, image_num=0, obj_num=0, logger=None):
     if logger and logger.isEnabledFor(logging.DEBUG):
         logger.debug('file %d: seed = %d',file_num,seed)
 
-    # The GetNImagesForFile function performs some basic setup, so in the interest of avoiding
-    # code duplication, call it here, even though we don't really need the output.  (Although,
-    # we do got ahead and use it in the debug logging, since we have it.)
+    # Put these values in the config dict so we won't have to run them again later if
+    # we need them.  e.g. ExtraOuput processing uses these.
     nimages = GetNImagesForFile(config, file_num)
+    nobj = GetNObjForFile(config,file_num,image_num)
+    config['nimages'] = nimages
+    config['nobj'] = nobj
 
     output = config['output']
     output_type = output['type']
@@ -244,9 +246,9 @@ def GetNImagesForFile(config, file_num):
     else:
         output_type = 'Fits'
     nim_func = valid_output_types[output_type]['nim']
- 
+
     return nim_func(config, file_num)
- 
+
 
 def GetNObjForFile(config, file_num, image_num):
     """
@@ -267,7 +269,7 @@ def GetNObjForFile(config, file_num, image_num):
         galsim.config.ProcessInput(config, file_num=file_num)
         nobj = [ galsim.config.GetNObjForImage(config, image_num+j) for j in range(nimages) ]
     return nobj
- 
+
 
 def SetupConfigFileNum(config, file_num, image_num, obj_num):
     """Do the basic setup of the config dict at the file processing level.
@@ -366,7 +368,7 @@ def RetryIO(func, args, ntries, file_name, logger):
 def BuildFits(config, file_num, image_num, obj_num, ignore, logger):
     """
     Build a regular fits file as specified in config.
-    
+
     @param config           A configuration dict.
     @param file_num         The current file_num.
     @param image_num        The current image_num.

galsim/config/extra.py
@@ -107,7 +107,7 @@ def SetupExtraOutput(config, file_num=0, logger=None):
             if init_func is None:
                 # Make the output_obj list the right length now to avoid issues with multiple
                 # processes trying to append at the same time.
-                nimages = galsim.config.GetNImagesForFile(config, file_num)
+                nimages = config['nimages']
                 for i in range(nimages): output_obj.append(None)
             if logger and logger.isEnabledFor(logging.DEBUG):
                 logger.debug('file %d: Setup output %s object',file_num,key)
@@ -170,9 +170,7 @@ def ProcessExtraOutputsForImage(config, logger=None):
                     image_num = config['image_num']
                     start_image_num = config['start_image_num']
                     start_obj_num = config['start_obj_num']
-                    nobj = galsim.config.GetNObjForFile(config,file_num,start_image_num)
-                    # GetNObjForFile changed index_key, so make sure to set it back.
-                    config['index_key'] = 'image_num'
+                    nobj = config['nobj']
                     k = image_num - start_image_num
                     for i in range(k):
                         start_obj_num += nobj[i]

commit f5e1d5b86739f9d5cd5e3a7b656c612ae7a15fac

Fix a subtle error introduced by commit 6705f4eb (#654)

galsim/config/image_scattered.py
@@ -40,12 +40,7 @@ def SetupScattered(config, image_num, obj_num, ignore, logger):
         logger.debug('image %d: Building Scattered: image, obj = %d,%d',
                      image_num,image_num,obj_num)
 
-    if 'nobjects' not in config['image']:
-        nobjects = galsim.config.ProcessInputNObjects(config)
-        if nobjects is None:
-            raise AttributeError("Attribute nobjects is required for image.type = Scattered")
-    else:
-        nobjects = galsim.config.ParseValue(config['image'],'nobjects',config,int)[0]
+    nobjects = GetNObjScattered(config, image_num)
     if logger and logger.isEnabledFor(logging.DEBUG):
         logger.debug('image %d: nobj = %d',image_num,nobjects)
     config['nobjects'] = nobjects

galsim/config/output.py
@@ -69,6 +69,9 @@ def BuildFiles(nfiles, config, file_num=0, logger=None):
         SetupConfigFileNum(config, file_num, image_num, obj_num)
         seed = galsim.config.SetupConfigRNG(config)
 
+        # Get the number of objects in each image for this file.
+        nobj = GetNObjForFile(config,file_num,image_num)
+
         # Process the input fields that might be relevant at file scope:
         galsim.config.ProcessInput(config, file_num=file_num, logger=logger, file_scope_only=True)
 
@@ -88,8 +91,6 @@ def BuildFiles(nfiles, config, file_num=0, logger=None):
             file_name = GetFilename(output, config, default_ext)
             jobs.append( (kwargs, (file_num, file_name)) )
 
-        # Get the number of objects in each image for this file.
-        nobj = GetNObjForFile(config,file_num,image_num)
         # nobj is a list of nobj for each image in that file.
         # So len(nobj) = nimages and sum(nobj) is the total number of objects
         # This gets the values of image_num and obj_num ready for the next loop.
@@ -164,8 +165,8 @@ def BuildFile(config, file_num=0, image_num=0, obj_num=0, logger=None):
 
     # Put these values in the config dict so we won't have to run them again later if
     # we need them.  e.g. ExtraOuput processing uses these.
-    nimages = GetNImagesForFile(config, file_num)
     nobj = GetNObjForFile(config,file_num,image_num)
+    nimages = len(nobj)
     config['nimages'] = nimages
     config['nobj'] = nobj
 

galsim/config/process.py
@@ -325,6 +325,7 @@ def SetupConfigRNG(config, seed_offset=0):
 
     @param config           The configuration dict.
     @param seed_offset      An offset to use relative to what config['image']['random_seed'] gives.
+                            [default: 0]
 
     @returns the seed used to initialize the RNG.
     """
@@ -347,6 +348,12 @@ def SetupConfigRNG(config, seed_offset=0):
 
     index_key = config['index_key']
 
+    # If we are starting a new file, clear out the existing rngs.
+    if index_key == 'file_num':
+        for key in ['seed', 'rng', 'obj_num_rng', 'image_num_rng', 'file_num_rng']:
+            if key in config:
+                del config[key]
+
     if 'random_seed' in config['image']:
         config['index_key'] = 'obj_num'
         seed = galsim.config.ParseValue(config['image'], 'random_seed', config, int)[0]
@@ -355,9 +362,14 @@ def SetupConfigRNG(config, seed_offset=0):
     else:
         seed = 0
 
-    config['seed'] = seed
-    rng = galsim.BaseDeviate(seed)
-    config['rng'] = rng
+    # Normally, the file_num rng and the image_num rng can be the same.  So if seed
+    # comes out the same as what we already built, then just use the existing file_num_rng.
+    if index_key == 'image_num' and 'file_num_rng' in config and seed == config.get('seed',None):
+        rng = config['file_num_rng']
+    else:
+        config['seed'] = seed
+        rng = galsim.BaseDeviate(seed)
+        config['rng'] = rng
 
     # Also save this rng as 'file_num_rng' or 'image_num_rng' or 'obj_num_rng' according
     # to whatever the index_key is.