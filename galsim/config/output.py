f241f393360aa48ebec5d4ed4624763f1eec2dcd
Mike Jarvis <michael@jarvis.net>
2016-01-26 19:34:30 -0500

    Switch output registration to use the inheritance model (#691)

--- galsim/config/output.py
+++ galsim/config/output.py
@@ -27,9 +27,8 @@ import logging
 
 # This module-level dict will store all the registered output types.
 # See the RegisterOutputType function at the end of this file.
-# The keys are the (string) names of the output types, and the values will be dicts that keep track
-# of the functions to call at different stages of processing to construct and write the output
-# file(s).
+# The keys are the (string) names of the output types, and the values will be builder objects
+# will perform the different stages of processing to construct and write the output file(s).
 valid_output_types = {}
 
 
@@ -92,10 +91,9 @@ def BuildFiles(nfiles, config, file_num=0, logger=None):
         if file_num >= first_file_num:
             # Get the file_name here, in case it needs to create directories, which is not
             # safe to do with multiple processes. (At least not without extra code in the
-            # GetFilename function...)
+            # getFilename function...)
             output_type = output['type']
-            default_ext = valid_output_types[output_type]['ext']
-            file_name = GetFilename(output, config, default_ext)
+            file_name = valid_output_types[output_type].getFilename(output, config, logger)
             jobs.append( (kwargs, (file_num, file_name)) )
 
         # nobj is a list of nobj for each image in that file.
@@ -188,9 +186,10 @@ def BuildFile(config, file_num=0, image_num=0, obj_num=0, logger=None):
     galsim.config.ProcessInput(config, file_num=file_num, logger=logger)
     galsim.config.SetupExtraOutput(config, file_num=file_num, logger=logger)
 
+    builder = valid_output_types[output_type]
+
     # Get the file name
-    default_ext = valid_output_types[output_type]['ext']
-    file_name = GetFilename(output, config, default_ext)
+    file_name = builder.getFilename(output, config, logger)
 
     # Check if we ought to skip this file
     if 'skip' in output and galsim.config.ParseValue(output, 'skip', config, bool)[0]:
@@ -207,18 +206,16 @@ def BuildFile(config, file_num=0, image_num=0, obj_num=0, logger=None):
         t2 = time.time()
         return file_name, 0
 
-    if logger: 
+    if logger:
         if logger.isEnabledFor(logging.DEBUG):
             logger.debug('file %d: file_name = %s',file_num,file_name)
         else:
             logger.warn('Start file %d = %s', file_num, file_name)
 
-    build_func = valid_output_types[output_type]['build']
     ignore = output_ignore + galsim.config.valid_extra_outputs.keys()
-    data = build_func(config, file_num, image_num, obj_num, ignore, logger)
+    data = builder.buildImages(output, config, file_num, image_num, obj_num, ignore, logger)
 
-    can_add_hdus = valid_output_types[output_type]['hdus']
-    if can_add_hdus:
+    if builder.canAddHdus():
         data = data + galsim.config.BuildExtraOutputHDUs(config,logger,len(data))
 
     if 'retry_io' in output:
@@ -228,9 +225,8 @@ def BuildFile(config, file_num=0, image_num=0, obj_num=0, logger=None):
     else:
         ntries = 1
 
-    write_func = valid_output_types[output_type]['write']
     args = (data, file_name)
-    RetryIO(write_func, args, ntries, file_name, logger)
+    RetryIO(builder.writeFile, args, ntries, file_name, logger)
     if logger:
         logger.debug('file %d: Wrote %s to file %r',file_num,output_type,file_name)
 
@@ -249,13 +245,13 @@ def GetNImagesForFile(config, file_num):
 
     @returns the number of images
     """
-    if 'output' in config and 'type' in config['output']:
-        output_type = config['output']['type']
+    output = config['output']
+    if 'type' in config['output']:
+        output_type = output['type']
     else:
         output_type = 'Fits'
-    nim_func = valid_output_types[output_type]['nim']
 
-    return nim_func(config, file_num)
+    return valid_output_types[output_type].getNImages(output, config, file_num)
 
 
 def GetNObjForFile(config, file_num, image_num):
@@ -322,39 +318,23 @@ def SetupConfigFileNum(config, file_num, image_num, obj_num):
         raise AttributeError("Invalid output.type=%s."%output_type)
 
 
-def SetDefaultExt(config, ext):
-    """Set a default ext if appropriate"""
-    if ext is not None:
-        if ( isinstance(config,dict) and 'type' in config and 
-            config['type'] == 'NumberedFile' and 'ext' not in config ):
-            config['ext'] = ext
+def SetDefaultExt(config, default_ext):
+    """Set a default ext in a config 'file_name' field if appropriate.
 
-
-def GetFilename(config, base, default_ext=None):
-    """Get the file_name for the current file being worked on.
+    @param config           The configuration dict for the item that might need to be given
+                            a default 'ext' value.
+    @param default_ext      The default extension to set in the config dict if one is not set.
     """
-    if 'file_name' in config:
-        SetDefaultExt(config['file_name'],default_ext)
-        file_name = galsim.config.ParseValue(config, 'file_name', base, str)[0]
-    elif 'root' in base and default_ext is not None:
-        # If a file_name isn't specified, we use the name of the config file + '.fits'
-        file_name = base['root'] + default_ext
-    else:
-        raise AttributeError("No file_name specified and unable to generate it automatically.")
-
-    # Prepend a dir to the beginning of the filename if requested.
-    if 'dir' in config:
-        dir = galsim.config.ParseValue(config, 'dir', base, str)[0]
-        if dir and not os.path.isdir(dir): os.makedirs(dir)
-        file_name = os.path.join(dir,file_name)
-
-    return file_name
+    if default_ext is not None:
+        if ( isinstance(config,dict) and 'type' in config and
+            config['type'] == 'NumberedFile' and 'ext' not in config ):
+            config['ext'] = default_ext
 
 
 # A helper function to retry io commands
 def RetryIO(func, args, ntries, file_name, logger):
     for itry in range(ntries):
-        try: 
+        try:
             ret = func(*args)
         except IOError as e:
             if itry == ntries-1:
@@ -373,64 +353,115 @@ def RetryIO(func, args, ntries, file_name, logger):
     return ret
 
 
-def BuildFits(config, file_num, image_num, obj_num, ignore, logger):
+class OutputBuilder(object):
+    """A base class for building and writing the output objects.
+
+    The base class defines the call signatures of the methods that any derived class should follow.
+    It also includes the implementation of the default output type: Fits.
     """
-    Build a regular fits file as specified in config.
 
-    @param config           A configuration dict.
-    @param file_num         The current file_num.
-    @param image_num        The current image_num.
-    @param obj_num          The current obj_num.
-    @param ignore           A list of parameters that are allowed to be in config['output']
-                            that we can ignore here.  i.e. it won't be an error if these
-                            parameters are present.
-    @param logger           If given, a logger object to log progress.
+    # A class attribute that sub-classes may override.
+    default_ext = '.fits'
 
-    @returns the image in a list with one item: [ image ]
-    """
-    # There are no extra parameters to get, so just check that there are no invalid parameters
-    # in the config dict.
-    galsim.config.CheckAllParams(config['output'], ignore=ignore)
+    def getFilename(self, config, base, logger):
+        """Get the file_name for the current file being worked on.
 
-    image = galsim.config.BuildImage(config, logger=logger, image_num=image_num, obj_num=obj_num)
-    return [ image ]
+        Note that the base class defines a default extension = '.fits'.
+        This can be overridden by subclasses by changing the default_ext property.
 
-def GetNImagesFits(config, file_num):
-    """
-    Get the number of images for a Fits file type.  i.e. 1.
+        @param config           The configuration dict for the output type.
+        @param base             The base configuration dict.
+        @param image_num        The current image_num.
+        @param obj_num          The current obj_num.
+        @param ignore           A list of parameters that are allowed to be in config['output']
+                                that we can ignore here.  i.e. it won't be an error if these
+                                parameters are present.
+        @param logger           If given, a logger object to log progress.
 
-    @param config           The configuration dict.
-    @param file_num         The current file number.
+        @returns the filename to build.
+        """
+        if 'file_name' in config:
+            SetDefaultExt(config['file_name'], self.default_ext)
+            file_name = galsim.config.ParseValue(config, 'file_name', base, str)[0]
+        elif 'root' in base and self.default_ext is not None:
+            # If a file_name isn't specified, we use the name of the config file + '.fits'
+            file_name = base['root'] + self.default_ext
+        else:
+            raise AttributeError("No file_name specified and unable to generate it automatically.")
 
-    @returns 1
-    """
-    return 1
+        # Prepend a dir to the beginning of the filename if requested.
+        if 'dir' in config:
+            dir = galsim.config.ParseValue(config, 'dir', base, str)[0]
+            if dir and not os.path.isdir(dir): os.makedirs(dir)
+            file_name = os.path.join(dir,file_name)
 
-def RegisterOutputType(output_type, build_func, write_func, nimages_func,
-                       extra_hdus=False, default_ext='.fits'):
+        return file_name
+
+    def buildImages(self, config, base, file_num, image_num, obj_num, ignore, logger):
+        """Build the images for output.
+
+        In the base class, this function just calls BuildImage to build the single image to
+        put in the output file.  So the returned list only has one item.
+
+        @param config           The configuration dict for the output field.
+        @param base             The base configuration dict.
+        @param file_num         The current file_num.
+        @param image_num        The current image_num.
+        @param obj_num          The current obj_num.
+        @param ignore           A list of parameters that are allowed to be in config that we can
+                                ignore here.  i.e. it won't be an error if they are present.
+        @param logger           If given, a logger object to log progress.
+
+        @returns a list of the images built
+        """
+        # There are no extra parameters to get, so just check that there are no invalid parameters
+        # in the config dict.
+        galsim.config.CheckAllParams(config, ignore=ignore)
+
+        image = galsim.config.BuildImage(base, image_num, obj_num, logger=logger)
+        return [ image ]
+
+    def getNImages(self, config, base, file_num):
+        """Returns the number of images to be built.
+
+        In the base class, we only build a single image, so it returns 1.
+
+        @param config           The configuration dict for the output field.
+        @param base             The base configuration dict.
+        @param file_num         The current file number.
+
+        @returns the number of images to build.
+        """
+        return 1
+
+    def writeFile(self, data, file_name):
+        """Write the data to a file.
+
+        @param data             The data to write.  Usually a list of images returned by
+                                buildImages, but possibly with extra HDUs tacked onto the end
+                                from the extra output items.
+        @param file_name        The file_name to write to.
+        """
+        galsim.fits.writeMulti(data,file_name)
+
+    def canAddHdus(self):
+        """Returns whether it is permissible to add extra HDUs to the end of the data list.
+
+        In the base class, this returns True.
+        """
+        return True
+
+
+def RegisterOutputType(output_type, builder):
     """Register an output type for use by the config apparatus.
 
     @param output_type      The name of the type in config['output']
-    @param build_func       The function to call for building the necessary data.
-                            The call signature is:
-                                data = Build(config, file_num, image_num, obj_num, ignore, logger)
-    @param write_func       The function to use for writing the data to a file.
-                            The call signature is:
-                                Write(data, file_name)
-    @param nimages_func     A function that returns the number of images that will be built.
-                            The call signature is 
-                                nimages = GetNImages(config, file_num)
-    @param extra_hdus       Whether extra hdus can be added to the end of the data for the extra
-                            output items. [default: False]
-    @param default_ext      The default file extension if none is given. [default: '.fits']
+    @param builder          A class to use for building this output type.  It should be a
+                            subclass of OutputBuilder.
     """
-    valid_output_types[output_type] = {
-        'build' : build_func,
-        'write' : write_func,
-        'nim' : nimages_func,
-        'hdus' : extra_hdus,
-        'ext' : default_ext
-    }
+    # Make a concrete instance of the builder.
+    valid_output_types[output_type] = builder()
 
-RegisterOutputType('Fits', BuildFits, galsim.fits.writeMulti, GetNImagesFits, extra_hdus=True)
+# The base class is also the builder for type = Fits.
+RegisterOutputType('Fits', OutputBuilder)
 
