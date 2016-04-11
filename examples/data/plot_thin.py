6ef24686c6ae40399451b9f5b8cb19b073991fba
Josh Meyers <jmeyers314@gmail.com>
2016-04-11 15:08:36 -0700

    restore plot_thin.py to working version

--- examples/data/plot_thin.py
+++ examples/data/plot_thin.py
@@ -24,18 +24,17 @@ from matplotlib.backends.backend_pdf import PdfPages
 pp = PdfPages('thin.pdf')
 
 for file_name in glob.glob('*.dat') + glob.glob('*.sed'):
-# for file_name in glob.glob('*.sed'):
     plt.clf()
     x,f = np.loadtxt(file_name, unpack=True)
-    print "{0} raw size = {1}".format(file_name,len(x))
     plt.plot(x,f, color='black', label='raw')
-    x1,f1 = galsim.utilities.thin_tabulated_values2(x,f,rel_err=1.e-4)
+    x1,f1 = galsim.utilities.thin_tabulated_values(x,f,rel_err=1.e-4)
     plt.plot(x1,f1, color='blue', label='rel_err = 1.e-4')
-    x2,f2 = galsim.utilities.thin_tabulated_values2(x,f,rel_err=1.e-3)
+    x2,f2 = galsim.utilities.thin_tabulated_values(x,f,rel_err=1.e-3)
     plt.plot(x2,f2, color='green', label='rel_err = 1.e-3')
-    x3,f3 = galsim.utilities.thin_tabulated_values2(x,f,rel_err=1.e-2)
+    x3,f3 = galsim.utilities.thin_tabulated_values(x,f,rel_err=1.e-2)
     plt.plot(x3,f3, color='red', label='rel_err = 1.e-2')
     plt.legend(loc='upper right')
+    print "{0} raw size = {1}".format(file_name,len(x))
     print "    thinned sizes = {0}, {1}, {2}".format(len(x1),len(x2),len(x3))
 
     pp.savefig()
