e815df6c7e5a2077d70e6004063908a3e594f5f7
Rachel Mandelbaum <rmandelb@andrew.cmu.edu>
2016-05-17 17:11:15 -0400

    add default_thin_trunc as separate kwarg (#745)

--- galsim/wfirst/wfirst_bandpass.py
+++ galsim/wfirst/wfirst_bandpass.py
@@ -25,7 +25,7 @@ import galsim
 import numpy as np
 import os
 
-def getBandpasses(AB_zeropoint=True, exptime=None, **kwargs):
+def getBandpasses(AB_zeropoint=True, exptime=None, default_thin_trunc=False, **kwargs):
     """Utility to get a dictionary containing the WFIRST bandpasses used for imaging.
 
     This routine reads in a file containing a list of wavelengths and throughput for all WFIRST
@@ -40,7 +40,7 @@ def getBandpasses(AB_zeropoint=True, exptime=None, **kwargs):
     The bandpasses can be either truncated or thinned before setting the zero points, by passing in
     the keyword arguments that need to get propagated through to the Bandpass.thin() and/or
     Bandpass.truncate() routines.  Or, if the user wishes to thin and truncate using the defaults
-    for those two routines, they can pass in `default_thin_trunc=True` option.
+    for those two routines, they can use `default_thin_trunc=True`.
 
     By default, the routine will set an AB zeropoint using the WFIRST effective diameter and default
     exposure time.  Setting the zeropoint can be avoided by setting `AB_zeropoint=False`; changing
@@ -61,15 +61,18 @@ def getBandpasses(AB_zeropoint=True, exptime=None, **kwargs):
         >>> wfirst_bandpasses = galsim.wfirst.getBandpasses()
         >>> f184_bp = wfirst_bandpasses['F184']
 
-    @param AB_zeropoint     Should the routine set an AB zeropoint before returning the bandpass?
-                            If False, then it is up to the user to set a zero point.  [default:
-                            True]
-    @param exptime          Exposure time to use for setting the zeropoint; if None, use the default
-                            WFIRST exposure time, taken from galsim.wfirst.exptime.  [default: None]
-
-    It's also possible to pass through keyword arguments for the Bandpass.thin() and
-    Bandpass.truncate() routines, or use `default_thin_trunc=True` to use both of those routines at
-    their default settings.
+    @param AB_zeropoint       Should the routine set an AB zeropoint before returning the bandpass?
+                              If False, then it is up to the user to set a zero point.  [default:
+                              True]
+    @param exptime            Exposure time to use for setting the zeropoint; if None, use the
+                              default WFIRST exposure time, taken from galsim.wfirst.exptime.
+                              [default: None]
+    @param default_thin_trunc Use the default thinning and truncation options?  Users who wish to
+                              use no thinning and truncation of bandpasses, or who want control over
+                              the level of thinning and truncation, should have this be False.
+                              [default: False]
+    @params **kwargs          Other kwargs are passed to either `bandpass.thin()` or 
+                              `bandpass.truncate()` as appropriate.
 
     @returns A dictionary containing bandpasses for all WFIRST imaging filters.
     """
@@ -96,14 +99,14 @@ def getBandpasses(AB_zeropoint=True, exptime=None, **kwargs):
     thin_kwargs = ['rel_err', 'trim_zeros', 'preserve_range', 'fast_search']
     tmp_truncate_dict = {}
     tmp_thin_dict = {}
-    if 'default_thin_trunc' in kwargs.keys():
-        if len(kwargs) > 1:
-            raise ValueError("Cannot pass default_thin_trunc with other kwargs!")
-        if kwargs['default_thin_trunc']:
-            pass
-        else:
-            del kwargs['default_thin_trunc']
-    else:
+    if default_thin_trunc:
+        if len(kwargs) > 0:
+            import warnings
+            warnings.warn('default_thin_trunc is true, but other arguments have been passed'
+                          ' to getBandpasses().  Using the other arguments and ignoring'
+                          ' default_thin_trunc.')
+            default_thin_trunc = False
+    if len(kwargs) > 0:
         if any(x in kwargs.keys() for x in truncate_kwargs):
             for key in kwargs.keys():
                 if key in truncate_kwargs:
@@ -127,9 +130,9 @@ def getBandpasses(AB_zeropoint=True, exptime=None, **kwargs):
         bp = galsim.Bandpass(galsim.LookupTable(wave, data[bp_name]), wave_type='nm')
 
         # Use any arguments related to truncation, thinning, etc.
-        if len(tmp_truncate_dict) > 0 or 'default_thin_trunc' in kwargs.keys():
+        if len(tmp_truncate_dict) > 0 or default_thin_trunc:
             bp = bp.truncate(**tmp_truncate_dict)
-        if len(tmp_thin_dict) > 0 or 'default_thin_trunc' in kwargs.keys():
+        if len(tmp_thin_dict) > 0 or default_thin_trunc:
             bp = bp.thin(**tmp_thin_dict)
 
         # Set the zeropoint if requested by the user:
