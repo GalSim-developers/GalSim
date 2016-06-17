7460a10e633b4a353d075f660c815c88a063fc7a
Mike Jarvis <michael@jarvis.net>
2016-06-17 08:58:41 -0400

    Add missing 23.5 to real_galaxy_catalog_example.fits (#534)

--- tests/test_config_image.py
+++ tests/test_config_image.py
@@ -20,6 +20,7 @@ from __future__ import print_function
 import numpy as np
 import os
 import sys
+import logging
 
 from galsim_test_helpers import *
 
@@ -371,7 +372,7 @@ def test_njobs():
             'cosmos_catalog' : {
                 'min_hlr' : '@psf.items.1.sigma',
                 'dir' : os.path.join('..','examples','data'),
-                'file_name' : 'real_galaxy_catalog_example.fits',
+                'file_name' : 'real_galaxy_catalog_23.5_example.fits',
             },
         },
         'output' : {
@@ -384,12 +385,14 @@ def test_njobs():
             },
         },
     }
-    galsim.config.Process(config)
+    logging.basicConfig(format="%(message)s", level=logging.INFO, stream=sys.stdout)
+    logger = logging.getLogger()
+    galsim.config.Process(config, logger=logger)
 
     # Repeat with 2 jobs
     config['output']['file_name']['root'] = 'test_two_jobs_'
-    galsim.config.Process(config, njobs=2, job=1)
-    galsim.config.Process(config, njobs=2, job=2)
+    galsim.config.Process(config, njobs=2, job=1, logger=logger)
+    galsim.config.Process(config, njobs=2, job=2, logger=logger)
 
     # Check that the images are equal:
     one00 = galsim.fits.read('test_one_job_00.fits', dir='output')
