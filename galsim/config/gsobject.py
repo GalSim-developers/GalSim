7fde567e53bf9af23ad84f1fd43fbfd5fdc71244
Mike Jarvis <michael@jarvis.net>
2016-09-05 18:24:00 -0400

    Use defaults for req, opt, single if the user doesn't supply them. (#782)

--- galsim/config/gsobject.py
+++ galsim/config/gsobject.py
@@ -243,11 +243,12 @@ def _BuildSimple(config, base, ignore, gsparams, logger):
     if logger:
         logger.debug('obj %d: BuildSimple for type = %s',base['obj_num'],type_name)
 
+    req = init_func._req_params if hasattr(init_func,'_req_params') else {}
+    opt = init_func._opt_params if hasattr(init_func,'_opt_params') else {}
+    single = init_func._single_params if hasattr(init_func,'_single_params') else []
     kwargs, safe = galsim.config.GetAllParams(config, base,
-                                              req = init_func._req_params,
-                                              opt = init_func._opt_params,
-                                              single = init_func._single_params,
-                                              ignore = ignore)
+                                              req=req, opt=opt, single=single,
+                                              ignore=ignore)
     if gsparams: kwargs['gsparams'] = galsim.GSParams(**gsparams)
 
     if init_func._takes_rng:
