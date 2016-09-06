e720f40a17bb7a00427927ae763f961a72afce19
Bryan Gillis <brg@roe.ac.uk>
2016-09-06 12:23:46 +0100

    Adding pickle test using scale_h_over_r

--- tests/test_inclined_exponential.py
+++ tests/test_inclined_exponential.py
@@ -276,6 +276,7 @@ def test_pickle():
     
     do_pickle(galsim.InclinedExponential(inclination=0.1*galsim.radians,scale_radius=3.0,scale_height=0.3))
     do_pickle(galsim.InclinedExponential(inclination=0.1*galsim.radians,scale_radius=3.0))
+    do_pickle(galsim.InclinedExponential(inclination=0.1*galsim.radians,scale_radius=3.0,scale_h_over_r=0.2))
     do_pickle(galsim.InclinedExponential(inclination=0.1*galsim.radians,scale_radius=3.0,scale_height=0.3,
                                          flux=10.0))
     do_pickle(galsim.InclinedExponential(inclination=0.1*galsim.radians,scale_radius=3.0,scale_height=0.3,
