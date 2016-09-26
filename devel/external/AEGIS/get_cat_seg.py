eb6c3e33ae7b61c0efe3a72725a439b6215e834e
sowmyakth <sowmya.spacebug@gmail.com>
2016-09-26 11:38:36 -0700

    fits fake values changed to 9999

--- devel/external/AEGIS/get_cat_seg.py
+++ devel/external/AEGIS/get_cat_seg.py
@@ -2,7 +2,7 @@
 Reduces the full catalog to objects that have postage stamps for each segment.
 Then adds a few columns that are required for the main catalog. If a redshift 
 and photometric catalog are provided, then objects from main catalog are matched
-to the photometric and redshift catlog, and redhsift and magnitude values are 
+to the photometric and redshift catlog, and redshift and magnitude values are 
 saved. 
 
 Note: If no photomteric or redshift catalog, set the input argument for the 
@@ -12,7 +12,6 @@ need to be changed to their names in the input.
 Since paramteric fits were not done for the galaxies, the parameter values 
 are manually set. If the fits step is performed read the file below and save that
 to the catalog, replacing the fake values set here.
-
 """
 
 import subprocess
@@ -60,20 +59,21 @@ def get_cat_seg(args):
     temp.rename_column('ALPHA_J2000', 'RA')
     temp.rename_column('DELTA_J2000', 'DEC')
 
+    # Use KD Tree to match objects in catalogs that are within 1 arcsecond
     c_x = temp['RA']*np.cos(np.radians(temp['DEC']))
     c_y = temp['DEC']
     c_pts = zip(c_x, c_y)
     tolerance = 1/3600.
-    #If a photometric catalog is given
+    # If a photometric catalog is given
     if args.phot_cat_file_name is not 'None':
         phot_cat = Table.read(args.phot_cat_file_name, format="fits")
-        #### set column names as based on the photometric catalog  
+        #### Set column names as based on the photometric catalog.  
         p_ra = 'ACSRA_'+f_str
         p_dec = 'ACSDEC_'+f_str
         p_x = phot_cat[p_ra]*np.cos(np.radians(phot_cat[p_dec]))
         p_y = phot_cat[p_dec]
-        p_tree=spatial.KDTree(zip(p_x, p_y)) 
-        # Objects in catalog that exist in photometric catalog
+        p_tree = spatial.KDTree(zip(p_x, p_y)) 
+        # Objects in catalog that are matched to photometric catalog.
         s = p_tree.query(c_pts, distance_upper_bound=tolerance)
         ch_q, = np.where(s[0]!= np.inf)
         c_p = ch_q
@@ -82,23 +82,22 @@ def get_cat_seg(args):
         temp['ACS_' + f_str + 'BESTER'][c_p] = phot_cat['ACS_' + f_str + 'BESTER'][p_c]
         temp['ACSID'][c_p] = phot_cat['ACSID'][p_c]
         temp['ACSTILE'][c_p] = phot_cat['ACSTILE'][p_c]
-    #If a reshift catalog is given
+    #If a redshift catalog is given
     if args.phot_cat_file_name is not 'None':
-        #Only values with ZQUALITY >3 can be trusted
+        # Only values with ZQUALITY >3 can be trusted.
         z_cat_temp = Table.read(args.z_cat_file_name, format="fits")
-        #### set column names as based on the redshift catalog     
+        #### set column names as based on the redshift catalog.     
         z_cat = z_cat_temp[z_cat_temp['ZQUALITY']>3]
         z_x = z_cat['RA']*np.cos(np.radians(z_cat['DEC']))
         z_y = z_cat['DEC']
         z_tree=spatial.KDTree(zip(z_x, z_y))
-        #Objects in catalog that exist in redshift catalog
+        # Objects in catalog that are matched to redshift catalog.
         s = z_tree.query(c_pts, distance_upper_bound=tolerance)
         ch_q, = np.where(s[0]!= np.inf)
         c_z = ch_q
         z_c = s[1][ch_q]
         temp['zphot'][c_z] = z_cat['ZBEST'][z_c]
         temp['zphot_err'][c_z] = z_cat['ZERR'][z_c]
-
 
     print " Adding required columns for selection catalog"
     # Add columns for selection catalog. 
@@ -131,18 +130,18 @@ def get_cat_seg(args):
         temp['average_mask_adjacent_pixel_count'][idx] = avg_flux
         temp['peak_image_pixel_count'][idx] = peak_val
 
-    print " Adding columns for fits catalog information"
-    # Add columns for selection catalog. If fits is permormed, read here
-    #If fits is performed change script below.
-    fit_mad_s = np.zeros(len(temp))
-    fit_mad_b= np.zeros(len(temp))
-    fit_dvc_btt = np.zeros(len(temp))
-    use_bulgefit = np.zeros(len(temp))
-    viable_sersic = np.zeros(len(temp))
-    fit_status = [[1]*5]*len(temp)
-    sersicfit = [[0]*8]*len(temp)
-    bulgefit = [[0]*16]*len(temp)
-    hlr = [[0]*3]*len(temp)
+    print " Adding columns for parametric fits catalog information"
+    # Values manually set.
+    # If fits is performed change script below.
+    fit_mad_s = np.ones(len(temp))*9999
+    fit_mad_b= np.ones(len(temp))*9999
+    fit_dvc_btt = np.ones(len(temp))*9999
+    use_bulgefit = np.ones(len(temp))*9999
+    viable_sersic = np.ones(len(temp))*9999
+    fit_status = [[9999.]*5]*len(temp)
+    sersicfit = [[9999.]*8]*len(temp)
+    bulgefit = [[9999.]*16]*len(temp)
+    hlr = [[9999.]*3]*len(temp)
     names = ('fit_mad_s', 'fit_mad_b', 'fit_dvc_btt', 'use_bulgefit')
     names+= ('viable_sersic', 'fit_status', 'sersicfit', 'bulgefit', 'hlr')
     dtype =('f8', 'f8', 'f8','i4')
@@ -151,7 +150,6 @@ def get_cat_seg(args):
     tab+= [viable_sersic, fit_status, sersicfit, bulgefit, hlr]
     temp2 = Table(tab, names=names, dtype=dtype)
     temp = hstack([temp,temp2])
-
 
     print "Catalog with pstamps saved at ", new_cat_name
     temp.write(new_cat_name, format='fits')  
