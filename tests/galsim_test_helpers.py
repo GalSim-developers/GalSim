15f8ce541e940d055d02bc767ecfd4a1833fe8b5
Mike Jarvis <michael@jarvis.net>
2016-10-09 18:53:48 -0400

    Change how timer gets fname to work with nosetests -s

--- tests/galsim_test_helpers.py
+++ tests/galsim_test_helpers.py
@@ -461,7 +461,7 @@ def timer(f):
         t0 = time.time()
         result = f(*args, **kwargs)
         t1 = time.time()
-        fname = inspect.stack()[1][4][0].split('(')[0].strip()
+        fname = repr(f).split()[1]
         print('time for %s = %.2f' % (fname, t1-t0))
         return result
     return f2
