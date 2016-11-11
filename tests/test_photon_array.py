70aa8a88e4c664558f506bbf5693853e7ab045ea
Mike Jarvis <michael@jarvis.net>
2016-11-11 15:59:46 -0500

    Run through basic tests of FRatioAngles using thre kinds of seeds (#823)

--- tests/test_photon_array.py
+++ tests/test_photon_array.py
@@ -195,43 +195,50 @@ def test_photon_angles():
     # expectations can be evaluated precisely
     fratio = 1.2
     obscuration = 0.2
-    assigner = galsim.FRatioAngles(fratio, obscuration, seed)
-    assigner.applyTo(photon_array)
 
-    dxdz = photon_array.getDXDZArray()
-    dydz = photon_array.getDYDZArray()
+    # rng can be None, an existing BaseDeviate, or an integer
+    for rng in [ None, ud, 12345 ]:
+        assigner = galsim.FRatioAngles(fratio, obscuration, rng)
+        assigner.applyTo(photon_array)
+
+        dxdz = photon_array.getDXDZArray()
+        dydz = photon_array.getDYDZArray()
+
+        phi = np.arctan2(dydz,dxdz)
+        tantheta = np.sqrt(np.square(dxdz) + np.square(dydz))
+        sintheta = np.sin(np.arctan(tantheta))
+
+        # Check that the values are within the ranges expected
+        # (The test on phi really can't fail, because it is only testing the range of the
+        # arctan2 function.)
+        np.testing.assert_array_less(-phi, np.pi, "Azimuth angles outside possible range")
+        np.testing.assert_array_less(phi, np.pi, "Azimuth angles outside possible range")
+
+        fov_angle = np.arctan(0.5 / fratio)
+        obscuration_angle = obscuration * fov_angle
+        np.testing.assert_array_less(-sintheta, -np.sin(obscuration_angle),
+                                     "Inclination angles outside possible range")
+        np.testing.assert_array_less(sintheta, np.sin(fov_angle),
+                                     "Inclination angles outside possible range")
 
     # Compare these slopes with the expected distributions (uniform in azimuth 
     # over all azimiths and uniform in sin(inclination) over the range of
     # allowed inclinations
-    phi = np.arctan2(dydz,dxdz)
-    tantheta = np.sqrt(np.square(dxdz) + np.square(dydz))
-    sintheta = np.sin(np.arctan(tantheta))
-
+    # Only test this for the last one, so we make sure we have a deterministic result.
+    # (The above tests should be reliable even for the default rng.)
     phi_histo, phi_bins = np.histogram(phi, bins=100)
     sintheta_histo, sintheta_bins = np.histogram(sintheta, bins=100)
-
     phi_ref = float(np.sum(phi_histo))/phi_histo.size
     sintheta_ref = float(np.sum(sintheta_histo))/sintheta_histo.size
 
     chisqr_phi = np.sum(np.square(phi_histo - phi_ref)/phi_ref) / phi_histo.size
     chisqr_sintheta = np.sum(np.square(sintheta_histo - sintheta_ref) /
-                      sintheta_ref) / sintheta_histo.size
+                             sintheta_ref) / sintheta_histo.size
+
+    print('chisqr_phi = ',chisqr_phi)
+    print('chisqr_sintheta = ',chisqr_sintheta)
     assert 0.9 < chisqr_phi < 1.1, "Distribution in azimuth is not nearly uniform"
     assert 0.9 < chisqr_sintheta < 1.1, "Distribution in sin(inclination) is not nearly uniform"
-
-    # Also check that the values are within the ranges expected
-    # (The test on phi really can't fail, because it is only testing the range of the
-    # arctan2 function.)
-    np.testing.assert_array_less(-phi, np.pi, "Azimuth angles outside possible range")
-    np.testing.assert_array_less(phi, np.pi, "Azimuth angles outside possible range")
-
-    fov_angle = np.arctan(0.5 / fratio)
-    obscuration_angle = obscuration * fov_angle
-    np.testing.assert_array_less(-sintheta, -np.sin(obscuration_angle),
-                                 "Inclination angles outside possible range")
-    np.testing.assert_array_less(sintheta, np.sin(fov_angle),
-                                 "Inclination angles outside possible range")
 
     # Try some invalid inputs
     try:
