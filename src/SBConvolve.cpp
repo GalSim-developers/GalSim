GalSim-developers/GalSim

commit a82ef3572d75325cc292ebabfb008054f638cfe8

Delay some calculations in SBTransform and SBConvolve until they are needed (#820)

src/SBConvolve.cpp
@@ -25,9 +25,9 @@
 
 namespace galsim {
 
-    SBConvolve::SBConvolve(const std::list<SBProfile>& slist, bool real_space,
+    SBConvolve::SBConvolve(const std::list<SBProfile>& plist, bool real_space,
                            const GSParamsPtr& gsparams) :
-        SBProfile(new SBConvolveImpl(slist,real_space,gsparams)) {}
+        SBProfile(new SBConvolveImpl(plist,real_space,gsparams)) {}
 
     SBConvolve::SBConvolve(const SBConvolve& rhs) : SBProfile(rhs) {}
 
@@ -82,23 +82,23 @@ namespace galsim {
         return oss.str();
     }
 
-    SBConvolve::SBConvolveImpl::SBConvolveImpl(const std::list<SBProfile>& slist, bool real_space,
+    SBConvolve::SBConvolveImpl::SBConvolveImpl(const std::list<SBProfile>& plist, bool real_space,
                                                const GSParamsPtr& gsparams) :
-        SBProfileImpl(gsparams ? gsparams : GetImpl(slist.front())->gsparams),
-        _real_space(real_space)
+        SBProfileImpl(gsparams ? gsparams : GetImpl(plist.front())->gsparams),
+        _real_space(real_space),
+        _x0(0.), _y0(0.), _isStillAxisymmetric(true), _fluxProduct(1.),
+        _maxk(0.), _stepk(0.)
     {
-        for (ConstIter sptr = slist.begin(); sptr!=slist.end(); ++sptr)
-            add(*sptr);
-        initialize();
+        for(ConstIter it=plist.begin(); it!=plist.end(); ++it) add(*it);
     }
 
-    void SBConvolve::SBConvolveImpl::add(const SBProfile& rhs)
+    void SBConvolve::SBConvolveImpl::add(const SBProfile& sbp)
     {
         dbg<<"Start SBConvolveImpl::add.  Adding item # "<<_plist.size()+1<<std::endl;
 
         // Add new terms(s) to the _plist:
-        assert(GetImpl(rhs));
-        const SBProfileImpl* p = GetImpl(rhs);
+        assert(GetImpl(sbp));
+        const SBProfileImpl* p = GetImpl(sbp);
         const SBConvolveImpl* sbc = dynamic_cast<const SBConvolveImpl*>(p);
         const SBAutoConvolve::SBAutoConvolveImpl* sbc2 =
             dynamic_cast<const SBAutoConvolve::SBAutoConvolveImpl*>(p);
@@ -106,65 +106,59 @@ namespace galsim {
             dynamic_cast<const SBAutoCorrelate::SBAutoCorrelateImpl*>(p);
         if (sbc) {
             dbg<<"  (Item is really "<<sbc->_plist.size()<<" items.)"<<std::endl;
-            // If rhs is an SBConvolve, copy its list here
-            for (ConstIter pptr = sbc->_plist.begin(); pptr!=sbc->_plist.end(); ++pptr) {
-                if (!pptr->isAnalyticK() && !_real_space)
-                    throw SBError("SBConvolve requires members to be analytic in k");
-                if (!pptr->isAnalyticX() && _real_space)
-                    throw SBError("Real_space SBConvolve requires members to be analytic in x");
-                _plist.push_back(*pptr);
-            }
+            // If sbp is an SBConvolve, copy its list here
+            for (ConstIter pptr = sbc->_plist.begin(); pptr!=sbc->_plist.end(); ++pptr) add(*pptr);
         } else if (sbc2) {
             dbg<<"  (Item is really AutoConvolve.)"<<std::endl;
-            // If rhs is an SBAutoConvolve, put two of its item here:
+            // If sbp is an SBAutoConvolve, put two of its item here:
             const SBProfile& obj = sbc2->getAdaptee();
-            if (!obj.isAnalyticK() && !_real_space)
-                throw SBError("SBConvolve requires members to be analytic in k");
-            if (!obj.isAnalyticX() && _real_space)
-                throw SBError("Real_space SBConvolve requires members to be analytic in x");
-            _plist.push_back(obj);
-            _plist.push_back(obj);
+            add(obj);
+            add(obj);
         } else if (sbc3) {
             dbg<<"  (Item is really AutoCorrelate items.)"<<std::endl;
-            // If rhs is an SBAutoCorrelate, put its item and 180 degree rotated verion here:
+            // If sbp is an SBAutoCorrelate, put its item and 180 degree rotated verion here:
             const SBProfile& obj = sbc3->getAdaptee();
-            if (!obj.isAnalyticK() && !_real_space)
-                throw SBError("SBConvolve requires members to be analytic in k");
-            if (!obj.isAnalyticX() && _real_space)
-                throw SBError("Real_space SBConvolve requires members to be analytic in x");
-            _plist.push_back(obj);
+            add(obj);
             SBProfile temp = obj.rotate(180. * degrees);
-            _plist.push_back(temp);
+            add(temp);
         } else {
-            if (!rhs.isAnalyticK() && !_real_space)
+            if (!sbp.isAnalyticK() && !_real_space)
                 throw SBError("SBConvolve requires members to be analytic in k");
-            if (!rhs.isAnalyticX() && _real_space)
+            if (!sbp.isAnalyticX() && _real_space)
                 throw SBError("Real-space SBConvolve requires members to be analytic in x");
-            _plist.push_back(rhs);
+            _plist.push_back(sbp);
         }
+        _x0 += sbp.centroid().x;
+        _y0 += sbp.centroid().y;
+        _isStillAxisymmetric = _isStillAxisymmetric && sbp.isAxisymmetric();
+        _fluxProduct *= sbp.getFlux();
     }
 
-    void SBConvolve::SBConvolveImpl::initialize()
+    double SBConvolve::SBConvolveImpl::maxK() const
     {
-        _x0 = _y0 = 0.;
-        _fluxProduct = 1.;
-        _minMaxK = 0.;
-        _isStillAxisymmetric = true;
-
-        _netStepK = 0.;  // Accumulate Sum 1/stepk^2
-        for(ConstIter it=_plist.begin(); it!=_plist.end(); ++it) {
-            double maxk = it->maxK();
-            double stepk = it->stepK();
-            dbg<<"SBConvolve component has maxK, stepK = "<<maxk<<" , "<<stepk<<std::endl;
-            _fluxProduct *= it->getFlux();
-            _x0 += it->centroid().x;
-            _y0 += it->centroid().y;
-            if ( _minMaxK<=0. || maxk < _minMaxK) _minMaxK = maxk;
-            _netStepK += 1./(stepk*stepk);
-            _isStillAxisymmetric = _isStillAxisymmetric && it->isAxisymmetric();
+        if (_maxk == 0.) {
+            for(ConstIter it=_plist.begin(); it!=_plist.end(); ++it) {
+                double it_maxk = it->maxK();
+                dbg<<"SBConvolve component has maxK = "<<it_maxk<<std::endl;
+                if (_maxk <= 0. || it_maxk < _maxk) _maxk = it_maxk;
+            }
+            dbg<<"Net maxK = "<<_maxk<<std::endl;
         }
-        _netStepK = 1./sqrt(_netStepK);  // Convert to (Sum 1/stepk^2)^(-1/2)
-        dbg<<"Net maxK, stepK = "<<_minMaxK<<" , "<<_netStepK<<std::endl;
+        return _maxk;
+    }
+
+    double SBConvolve::SBConvolveImpl::stepK() const
+    {
+        if (_stepk == 0.) {
+            for(ConstIter it=_plist.begin(); it!=_plist.end(); ++it) {
+                double it_stepk = it->stepK();
+                dbg<<"SBConvolve component has stepK = "<<it_stepk<<std::endl;
+                _stepk += 1./(it_stepk*it_stepk);  // Accumulate Sum 1/stepk^2
+            }
+            _stepk = 1./sqrt(_stepk);  // Convert to (Sum 1/stepk^2)^(-1/2)
+            dbg<<"Net stepK = "<<_stepk<<std::endl;
+        }
+        return _stepk;
     }
 
     double SBConvolve::SBConvolveImpl::xValue(const Position<double>& pos) const

include/galsim/SBConvolveImpl.h
@@ -29,7 +29,7 @@ namespace galsim {
     {
     public:
 
-        SBConvolveImpl(const std::list<SBProfile>& slist, bool real_space,
+        SBConvolveImpl(const std::list<SBProfile>& plist, bool real_space,
                        const GSParamsPtr& gsparams);
         ~SBConvolveImpl() {}
 
@@ -47,8 +47,8 @@ namespace galsim {
         bool hasHardEdges() const { return false; }
         bool isAnalyticX() const { return _real_space; }
         bool isAnalyticK() const { return true; }    // convolvees must all meet this
-        double maxK() const { return _minMaxK; }
-        double stepK() const { return _netStepK; }
+        double maxK() const;
+        double stepK() const;
 
         void getXRange(double& xmin, double& xmax, std::vector<double>& splits) const
         {
@@ -129,19 +129,14 @@ namespace galsim {
         typedef std::list<SBProfile>::const_iterator ConstIter;
 
         std::list<SBProfile> _plist; ///< list of profiles to convolve
+        bool _real_space; ///< Whether to do convolution as an integral in real space.
         double _x0; ///< Centroid position in x.
         double _y0; ///< Centroid position in y.
         bool _isStillAxisymmetric; ///< Is output SBProfile shape still circular?
-        double _minMaxK; ///< Minimum maxK() of the convolved SBProfiles.
-        double _netStepK; ///< Minimum stepK() of the convolved SBProfiles.
-        double _sumMinX; ///< sum of minX() of the convolved SBProfiles.
-        double _sumMaxX; ///< sum of maxX() of the convolved SBProfiles.
-        double _sumMinY; ///< sum of minY() of the convolved SBProfiles.
-        double _sumMaxY; ///< sum of maxY() of the convolved SBProfiles.
         double _fluxProduct; ///< Flux of the product.
-        bool _real_space; ///< Whether to do convolution as an integral in real space.
 
-        void initialize();
+        mutable double _maxk; ///< Minimum maxK() of the convolved SBProfiles.
+        mutable double _stepk; ///< Minimum stepK() of the convolved SBProfiles.
 
         // Copy constructor and op= are undefined.
         SBConvolveImpl(const SBConvolveImpl& rhs);

include/galsim/SBTransformImpl.h
@@ -43,8 +43,8 @@ namespace galsim {
         bool isAnalyticX() const { return _adaptee.isAnalyticX(); }
         bool isAnalyticK() const { return _adaptee.isAnalyticK(); }
 
-        double maxK() const { return _maxk; }
-        double stepK() const { return _stepk; }
+        double maxK() const;
+        double stepK() const;
 
         void getXRange(double& xmin, double& xmax, std::vector<double>& splits) const;
 
@@ -108,15 +108,17 @@ namespace galsim {
         double _ampScaling;  ///< Amount to scale amplitude by
         double _fluxScaling;  ///< Amount to scale flux by (= absdet * ampScaling)
         double _invdet;  ///< Inverse determinant of `M` matrix.
-        double _maxk;
-        double _stepk;
         bool _stillIsAxisymmetric; ///< Is output SBProfile shape still circular?
-        double _xmin, _xmax, _ymin, _ymax; ///< Ranges propagated from adaptee
-        double _coeff_b, _coeff_c, _coeff_c2; ///< Values used in getYRangeX(x,ymin,ymax);
-        std::vector<double> _xsplits, _ysplits; ///< Good split points for the intetegrals
         bool _zeroCen;
+        double _major, _minor;
 
-        void initialize();
+        mutable double _maxk;
+        mutable double _stepk;
+        mutable double _xmin, _xmax, _ymin, _ymax; ///< Ranges propagated from adaptee
+        mutable double _coeff_b, _coeff_c, _coeff_c2; ///< Values used in getYRangeX(x,ymin,ymax);
+        mutable std::vector<double> _xsplits, _ysplits; ///< Good split points for the intetegrals
+
+        void setupRanges() const;
 
         /**
          * @brief Forward coordinate transform with `M` matrix.

src/SBTransform.cpp
@@ -79,49 +79,42 @@ namespace galsim {
         const Position<double>& cen, double ampScaling,
         const GSParamsPtr& gsparams) :
         SBProfileImpl(gsparams ? gsparams : GetImpl(adaptee)->gsparams),
-        _adaptee(adaptee), _mA(mA), _mB(mB), _mC(mC), _mD(mD), _cen(cen), _ampScaling(ampScaling)
+        _adaptee(adaptee), _mA(mA), _mB(mB), _mC(mC), _mD(mD), _cen(cen), _ampScaling(ampScaling),
+        _maxk(0.), _stepk(0.), _xmin(0.), _xmax(0.), _ymin(0.), _ymax(0.)
     {
-        dbg<<"Start TransformImpl (1)\n";
+        dbg<<"Start TransformImpl\n";
         dbg<<"matrix = "<<_mA<<','<<_mB<<','<<_mC<<','<<_mD<<std::endl;
         dbg<<"cen = "<<_cen<<", ampScaling = "<<_ampScaling<<std::endl;
 
-        // All the actual initialization is in a separate function so we can share code
-        // with the other constructor.
-        initialize();
-    }
-
-    void SBTransform::SBTransformImpl::initialize()
-    {
-        dbg<<"Start SBTransformImpl initialize\n";
         // First check if our adaptee is really another SBTransform:
         assert(GetImpl(_adaptee));
-        const SBTransformImpl* sbd = dynamic_cast<const SBTransformImpl*>(GetImpl(_adaptee));
-        dbg<<"sbd = "<<sbd<<std::endl;
-        if (sbd) {
+        const SBTransformImpl* sbt = dynamic_cast<const SBTransformImpl*>(GetImpl(_adaptee));
+        dbg<<"sbt = "<<sbt<<std::endl;
+        if (sbt) {
             dbg<<"wrapping another transformation.\n";
             // We are transforming something that's already a transformation.
             dbg<<"this transformation = "<<
                 _mA<<','<<_mB<<','<<_mC<<','<<_mD<<','<<
                 _cen<<','<<_ampScaling<<std::endl;
             dbg<<"adaptee transformation = "<<
-                sbd->_mA<<','<<sbd->_mB<<','<<sbd->_mC<<','<<sbd->_mD<<','<<
-                sbd->_cen<<','<<sbd->_ampScaling<<std::endl;
+                sbt->_mA<<','<<sbt->_mB<<','<<sbt->_mC<<','<<sbt->_mD<<','<<
+                sbt->_cen<<','<<sbt->_ampScaling<<std::endl;
             dbg<<"adaptee getFlux = "<<_adaptee.getFlux()<<std::endl;
             // We are transforming something that's already a transformation.
             // So just compound the affine transformaions
             // New matrix is product (M_this) * (M_old)
             double mA = _mA; double mB=_mB; double mC=_mC; double mD=_mD;
-            _cen += Position<double>(mA*sbd->_cen.x + mB*sbd->_cen.y,
-                                     mC*sbd->_cen.x + mD*sbd->_cen.y);
-            _mA = mA*sbd->_mA + mB*sbd->_mC;
-            _mB = mA*sbd->_mB + mB*sbd->_mD;
-            _mC = mC*sbd->_mA + mD*sbd->_mC;
-            _mD = mC*sbd->_mB + mD*sbd->_mD;
-            _ampScaling *= sbd->_ampScaling;
+            _cen += Position<double>(mA*sbt->_cen.x + mB*sbt->_cen.y,
+                                     mC*sbt->_cen.x + mD*sbt->_cen.y);
+            _mA = mA*sbt->_mA + mB*sbt->_mC;
+            _mB = mA*sbt->_mB + mB*sbt->_mD;
+            _mC = mC*sbt->_mA + mD*sbt->_mC;
+            _mD = mC*sbt->_mB + mD*sbt->_mD;
+            _ampScaling *= sbt->_ampScaling;
             dbg<<"this transformation => "<<
                 _mA<<','<<_mB<<','<<_mC<<','<<_mD<<','<<
                 _cen<<','<<_ampScaling<<std::endl;
-            _adaptee = sbd->_adaptee;
+            _adaptee = sbt->_adaptee;
         } else {
             dbg<<"wrapping a non-transformation.\n";
             dbg<<"this transformation = "<<
@@ -152,25 +145,12 @@ namespace galsim {
             && (_mA==_mD)
             && (_cen.x==0.) && (_cen.y==0.); // Need pure rotation
 
-        // Calculate maxK, stepK:
+        // Needed for maxK, stepK:
         double h1 = hypot( _mA+_mD, _mB-_mC);
         double h2 = hypot( _mA-_mD, _mB+_mC);
-        double major = 0.5*std::abs(h1+h2);
-        double minor = 0.5*std::abs(h1-h2);
-        if (major < minor) std::swap(major,minor);
-        _maxk = _adaptee.maxK() / minor;
-        _stepk = _adaptee.stepK() / major;
-
-        // If we have a shift, we need to further modify stepk
-        //     stepk = Pi/R
-        // R <- R + |shift|
-        // stepk <- Pi/(Pi/stepk + |shift|)
-        if (_cen.x != 0. || _cen.y != 0.) {
-            double shift = sqrt( _cen.x*_cen.x + _cen.y*_cen.y );
-            dbg<<"stepk from adaptee = "<<_stepk<<std::endl;
-            _stepk = M_PI / (M_PI/_stepk + shift);
-            dbg<<"shift = "<<shift<<", stepk -> "<<_stepk<<std::endl;
-        }
+        _major = 0.5*std::abs(h1+h2);
+        _minor = 0.5*std::abs(h1-h2);
+        if (_major < _minor) std::swap(_major,_minor);
 
         xdbg<<"Transformation init\n";
         xdbg<<"matrix = "<<_mA<<','<<_mB<<','<<_mC<<','<<_mD<<std::endl;
@@ -178,9 +158,58 @@ namespace galsim {
         xdbg<<"_invdet = "<<_invdet<<std::endl;
         xdbg<<"_absdet = "<<_absdet<<std::endl;
         xdbg<<"_ampScaling = "<<_ampScaling<<std::endl;
-        xdbg<<"major, minor = "<<major<<", "<<minor<<std::endl;
-        xdbg<<"maxK() = "<<_maxk<<std::endl;
-        xdbg<<"stepK() = "<<_stepk<<std::endl;
+        xdbg<<"major, minor = "<<_major<<", "<<_minor<<std::endl;
+
+        // The scale factor for the flux is absdet * ampScaling.
+        _fluxScaling = _absdet * _ampScaling;
+        xdbg<<"_fluxScaling -> "<<_fluxScaling<<std::endl;
+
+        // Figure out which function we need for kValue and kValueNoPhase
+        if (std::abs(_fluxScaling-1.) < this->gsparams->kvalue_accuracy) {
+            xdbg<<"fluxScaling = "<<_fluxScaling<<" = 1, so use NoDet version.\n";
+            _kValueNoPhase = &SBTransform::SBTransformImpl::_kValueNoPhaseNoDet;
+        } else {
+            xdbg<<"fluxScaling = "<<_fluxScaling<<" != 1, so use WithDet version.\n";
+            _kValueNoPhase = &SBTransform::SBTransformImpl::_kValueNoPhaseWithDet;
+        }
+        if (_cen.x == 0. && _cen.y == 0.) {
+            _zeroCen = true;
+            _kValue = _kValueNoPhase;
+        } else {
+            _zeroCen = false;
+            _kValue = &SBTransform::SBTransformImpl::_kValueWithPhase;
+        }
+    }
+
+    double SBTransform::SBTransformImpl::maxK() const
+    {
+        // The adaptee's maxk can be slow (e.g. high-n Sersic), so delay this calculation
+        // until we actually need it.
+        if (_maxk == 0.) _maxk = _adaptee.maxK() / _minor;
+        return _maxk;
+    }
+
+    double SBTransform::SBTransformImpl::stepK() const
+    {
+        if (_stepk == 0.) {
+            _stepk = _adaptee.stepK() / _major;
+            // If we have a shift, we need to further modify stepk
+            //     stepk = Pi/R
+            // R <- R + |shift|
+            // stepk <- Pi/(Pi/stepk + |shift|)
+            if (_cen.x != 0. || _cen.y != 0.) {
+                double shift = sqrt( _cen.x*_cen.x + _cen.y*_cen.y );
+                dbg<<"stepk from adaptee = "<<_stepk<<std::endl;
+                _stepk = M_PI / (M_PI/_stepk + shift);
+                dbg<<"shift = "<<shift<<", stepk -> "<<_stepk<<std::endl;
+            }
+        }
+        return _stepk;
+    }
+
+    void SBTransform::SBTransformImpl::setupRanges() const
+    {
+        if (_xmin != 0. || _xmax != 0.) return;
 
         // Calculate the values for getXRange and getYRange:
         if (_adaptee.isAxisymmetric()) {
@@ -334,30 +363,12 @@ namespace galsim {
                 }
             }
         }
-        // The scale factor for the flux is absdet * ampScaling.
-        _fluxScaling = _absdet * _ampScaling;
-        xdbg<<"_fluxScaling -> "<<_fluxScaling<<std::endl;
-
-        // Figure out which function we need for kValue and kValueNoPhase
-        if (std::abs(_fluxScaling-1.) < this->gsparams->kvalue_accuracy) {
-            xdbg<<"fluxScaling = "<<_fluxScaling<<" = 1, so use NoDet version.\n";
-            _kValueNoPhase = &SBTransform::SBTransformImpl::_kValueNoPhaseNoDet;
-        } else {
-            xdbg<<"fluxScaling = "<<_fluxScaling<<" != 1, so use WithDet version.\n";
-            _kValueNoPhase = &SBTransform::SBTransformImpl::_kValueNoPhaseWithDet;
-        }
-        if (_cen.x == 0. && _cen.y == 0.) {
-            _zeroCen = true;
-            _kValue = _kValueNoPhase;
-        } else {
-            _zeroCen = false;
-            _kValue = &SBTransform::SBTransformImpl::_kValueWithPhase;
-        }
     }
 
     void SBTransform::SBTransformImpl::getXRange(
         double& xmin, double& xmax, std::vector<double>& splits) const
     {
+        setupRanges();
         xmin = _xmin; xmax = _xmax;
         splits.insert(splits.end(),_xsplits.begin(),_xsplits.end());
     }
@@ -365,6 +376,7 @@ namespace galsim {
     void SBTransform::SBTransformImpl::getYRange(
         double& ymin, double& ymax, std::vector<double>& splits) const
     {
+        setupRanges();
         ymin = _ymin; ymax = _ymax;
         splits.insert(splits.end(),_ysplits.begin(),_ysplits.end());
     }
@@ -372,6 +384,7 @@ namespace galsim {
     void SBTransform::SBTransformImpl::getYRangeX(
         double x, double& ymin, double& ymax, std::vector<double>& splits) const
     {
+        setupRanges();
         xxdbg<<"Transformation getYRangeX for x = "<<x<<std::endl;
         if (_adaptee.isAxisymmetric()) {
             std::vector<double> splits0;

commit c4f85d43a8d9ca884954d24a41e5f4c4fc2bc04e

Fix bug introduced in a82ef35. (Only noticed by name==main tests) (#873)

src/SBConvolve.cpp
@@ -127,11 +127,11 @@ namespace galsim {
             if (!sbp.isAnalyticX() && _real_space)
                 throw SBError("Real-space SBConvolve requires members to be analytic in x");
             _plist.push_back(sbp);
+            _x0 += sbp.centroid().x;
+            _y0 += sbp.centroid().y;
+            _isStillAxisymmetric = _isStillAxisymmetric && sbp.isAxisymmetric();
+            _fluxProduct *= sbp.getFlux();
         }
-        _x0 += sbp.centroid().x;
-        _y0 += sbp.centroid().y;
-        _isStillAxisymmetric = _isStillAxisymmetric && sbp.isAxisymmetric();
-        _fluxProduct *= sbp.getFlux();
     }
 
     double SBConvolve::SBConvolveImpl::maxK() const