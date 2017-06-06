4f86c2a60c96203c2cf818dc1af18dbed720c732
Rachel Mandelbaum <rmandelb@andrew.cmu.edu>
2017-06-06 10:58:18 -0400

    update various WFIRST module parameters based on feedback from Chris (#675)

--- galsim/wfirst/__init__.py
+++ galsim/wfirst/__init__.py
@@ -74,7 +74,7 @@ Currently, the module includes the following numbers:
                         normalization factor already, and allows users to choose an exposure time or
                         use the default WFIRST exposure time.
 
-    read_noise - A total of 10e-.  This comes from 20 e- per correlated double sampling (CDS) and a
+    read_noise - A total of 8.5e-.  This comes from 20 e- per correlated double sampling (CDS) and a
                  5 e- floor, so the CDS read noise dominates.  The source of CDS read noise is the
                  noise introduced when subtracting a single pair of reads; this can be reduced by
                  averaging over multiple reads.  Also, this read_noise value might be reduced
@@ -201,21 +201,21 @@ import numpy as np
 
 gain = 1.0
 pixel_scale = 0.11  # arcsec / pixel
-diameter = 2.36  # meters
-obscuration = 0.3
+diameter = 2.37  # meters
+obscuration = 0.32
 collecting_area = np.pi * diameter**2 / 4.0 * (1.0 - obscuration**2) * 1e4  # cm^2
-exptime = 168.1  # s
-dark_current = 0.015
-nonlinearity_beta = -3.57e-7
+exptime = 140.25  # s
+dark_current = 0.015 # e-/pix/s
+nonlinearity_beta = -6.e-7
 reciprocity_alpha = 0.0065
-read_noise = 10.0
+read_noise = 8.5 # e-
 n_dithers = 6
-thermal_backgrounds = {'J129': 0.06,
-                       'F184': 1.18,
-                       'Y106': 0.06,
-                       'Z087': 0.06,
-                       'H158': 0.08,
-                       'W149': 0.06}
+thermal_backgrounds = {'J129': 0.023, # e-/pix/s
+                       'F184': 0.179,
+                       'Y106': 0.023,
+                       'Z087': 0.023,
+                       'H158': 0.022,
+                       'W149': 0.023}
 
 pupil_plane_file = os.path.join(galsim.meta_data.share_dir,
                                 "WFIRST-AFTA_Pupil_Mask_C5_20141010_PLT.fits.gz")
@@ -231,7 +231,7 @@ ipc_kernel = np.array([ [0.001269938, 0.015399776, 0.001199862], \
                         [0.001270391, 0.016129619, 0.001200137] ])
 ipc_kernel /= np.sum(ipc_kernel)
 ipc_kernel = galsim.Image(ipc_kernel)
-persistence_coefficients = np.array([0.2246,0.0225,0.0085,0.0043,0.0025,0.0016,0.0011,0.0008])/100.
+persistence_coefficients = np.array([0.045707683,0.014959818,0.009115737,0.00656769,0.005135571,0.004217028,0.003577534,0.003106601])/100.
 n_sca = 18
 n_pix_tot = 4096
 n_pix = 4088
