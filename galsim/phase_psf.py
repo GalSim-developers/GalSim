3bf655b3cec2c0383c09a3845d886b677f99c21b
Mike Jarvis <michael@jarvis.net>
2017-09-28 17:20:38 -0400

    Fix error in PhasePSF getstate (#809f)

--- galsim/phase_psf.py
+++ galsim/phase_psf.py
@@ -559,7 +559,7 @@ class Aperture(object):
     def __getstate__(self):
         # Let unpickled object reconstruct cached values on-the-fly instead of including them in the
         # pickle.
-        d = self.__dict__
+        d = self.__dict__.copy()
         for k in ['_rho', '_u', '_v', '_rsqr']:
             d.pop(k, None)
         return d
