ceb23fcbfebcd31dbf5b4ad50739da168054934e
Mike Jarvis <michael@jarvis.net>
2017-10-02 20:02:06 -0400

    remove some unneccessary wrapping bits (#809f)

--- pysrc/SBProfile.cpp
+++ pysrc/SBProfile.cpp
@@ -51,35 +51,13 @@ namespace galsim {
                         bp::arg("shoot_accuracy"),
                         bp::arg("allowed_flux_variation"),
                         bp::arg("range_division_for_extrema"),
-                        bp::arg("small_fraction_of_flux"))
-                    )
-                )
-                .def_readonly("minimum_fft_size", &GSParams::minimum_fft_size)
-                .def_readonly("maximum_fft_size", &GSParams::maximum_fft_size)
-                .def_readonly("folding_threshold", &GSParams::folding_threshold)
-                .def_readonly("stepk_minimum_hlr", &GSParams::stepk_minimum_hlr)
-                .def_readonly("maxk_threshold", &GSParams::maxk_threshold)
-                .def_readonly("kvalue_accuracy", &GSParams::kvalue_accuracy)
-                .def_readonly("xvalue_accuracy", &GSParams::xvalue_accuracy)
-                .def_readonly("table_spacing", &GSParams::table_spacing)
-                .def_readonly("realspace_relerr", &GSParams::realspace_relerr)
-                .def_readonly("realspace_abserr", &GSParams::realspace_abserr)
-                .def_readonly("integration_relerr", &GSParams::integration_relerr)
-                .def_readonly("integration_abserr", &GSParams::integration_abserr)
-                .def_readonly("shoot_accuracy", &GSParams::shoot_accuracy)
-                .def_readonly("allowed_flux_variation", &GSParams::allowed_flux_variation)
-                .def_readonly("range_division_for_extrema", &GSParams::range_division_for_extrema)
-                .def_readonly("small_fraction_of_flux", &GSParams::small_fraction_of_flux)
-                .def(bp::self == bp::other<GSParams>())
-                .enable_pickling()
-                ;
+                        bp::arg("small_fraction_of_flux"))));
         }
     };
 
 
     struct PySBProfile
     {
-
         template <typename U, typename W>
         static void wrapTemplates(W & wrapper) {
             // We don't need to wrap templates in a separate function, but it keeps us
@@ -100,78 +78,26 @@ namespace galsim {
         }
 
         static void wrap() {
-            static char const * doc =
-                "\n"
-                "SBProfile is an abstract base class representing all of the 2d surface\n"
-                "brightness that we know how to draw.  Every SBProfile knows how to\n"
-                "draw an Image<float> of itself in real and k space.  Each also knows\n"
-                "what is needed to prevent aliasing or truncation of itself when drawn.\n"
-                "\n"
-                "Note that when you use the SBProfile::draw() routines you will get an\n"
-                "image of **surface brightness** values in each pixel, not the flux\n"
-                "that fell into the pixel.  To get flux, you must multiply the image by\n"
-                "(dx*dx).\n"
-                "\n"
-                "drawK() routines are normalized such that I(0,0) is the total flux.\n"
-                "\n"
-                "Currently we have the following possible implementations of SBProfile:\n"
-                "Basic shapes: SBBox, SBGaussian, SBExponential, SBAiry, SBSersic,\n"
-                "              SBMoffat, SBKolmogorov, SBSpergel\n"
-                "SBInterpolatedImage: a representation of some arbitrary image\n"
-                "SBShapelet: an object represented as a shapelets decomposition\n"
-                "SBTransform: affine transformation of another SBProfile\n"
-                "SBAdd: sum of SBProfiles\n"
-                "SBConvolve: convolution of other SBProfiles\n"
-                "SBDeconvolve: deconvolution of an SBProfile\n"
-                "\n"
-                "==== Drawing routines ====\n"
-                "Grid on which SBProfile is drawn has pitch dx, which is taken from the\n"
-                "image's scale parameter.\n"
-                "\n"
-                "Note that in an FFT the image may be calculated internally on a\n"
-                "larger grid than the provided image to avoid folding.\n"
-                ;
-
-            bp::class_<SBProfile> pySBProfile("SBProfile", doc, bp::no_init);
+            bp::class_<SBProfile> pySBProfile("SBProfile", bp::no_init);
             pySBProfile
-                .def(bp::init<>())
-                .def(bp::init<const SBProfile &>())
-                .def("getGSParams", &SBProfile::getGSParams)
-                .def("xValue", &SBProfile::xValue,
-                     "Return value of SBProfile at a chosen 2d position in real space.\n"
-                     "May not be implemented for derived classes (e.g. SBConvolve) that\n"
-                     "require an FFT to determine real-space values.")
-                .def("kValue", &SBProfile::kValue,
-                     "Return value of SBProfile at a chosen 2d position in k-space.")
-                .def("maxK", &SBProfile::maxK, "Value of k beyond which aliasing can be neglected")
-                .def("nyquistDx", &SBProfile::nyquistDx,
-                     "Image pixel spacing that does not alias maxK")
-                .def("getGoodImageSize", &SBProfile::getGoodImageSize,
-                     "A good image size for drawing the SBProfile")
-                .def("stepK", &SBProfile::stepK,
-                     "Sampling in k space necessary to avoid folding of image in x space")
+                .def("xValue", &SBProfile::xValue)
+                .def("kValue", &SBProfile::kValue)
+                .def("maxK", &SBProfile::maxK)
+                .def("stepK", &SBProfile::stepK)
                 .def("isAxisymmetric", &SBProfile::isAxisymmetric)
                 .def("hasHardEdges", &SBProfile::hasHardEdges)
-                .def("isAnalyticX", &SBProfile::isAnalyticX,
-                     "True if real-space values can be determined immediately at any position "
-                     "without DFT.")
-                .def("isAnalyticK", &SBProfile::isAnalyticK,
-                     "True if k-space values can be determined immediately at any position "
-                     "without DFT.")
+                .def("isAnalyticX", &SBProfile::isAnalyticX)
+                .def("isAnalyticK", &SBProfile::isAnalyticK)
                 .def("centroid", &SBProfile::centroid)
                 .def("getFlux", &SBProfile::getFlux)
                 .def("getPositiveFlux", &SBProfile::getPositiveFlux)
                 .def("getNegativeFlux", &SBProfile::getNegativeFlux)
                 .def("maxSB", &SBProfile::maxSB)
                 .def("shoot", &SBProfile::shoot, bp::args("pa", "u"))
-                .def("__repr__", &SBProfile::repr)
-                .def("serialize", &SBProfile::serialize)
-                .enable_pickling()
                 ;
             wrapTemplates<float>(pySBProfile);
             wrapTemplates<double>(pySBProfile);
         }
-
     };
 
     void pyExportSBProfile()
