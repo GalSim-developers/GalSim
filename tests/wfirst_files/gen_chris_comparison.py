3d689f6f6723132753c8413562948d81a7c9f737
Mike Jarvis <michael@jarvis.net>
2018-04-21 17:36:46 -0400

    Update copyright to 2018.  Closes Issue #944

--- tests/wfirst_files/gen_chris_comparison.py
+++ tests/wfirst_files/gen_chris_comparison.py
@@ -1,3 +1,21 @@
+# Copyright (c) 2012-2018 by the GalSim developers team on GitHub
+# https://github.com/GalSim-developers
+#
+# This file is part of GalSim: The modular galaxy image simulation toolkit.
+# https://github.com/GalSim-developers/GalSim
+#
+# GalSim is free software: redistribution and use in source and binary forms,
+# with or without modification, are permitted provided that the following
+# conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice, this
+#    list of conditions, and the disclaimer given in the accompanying LICENSE
+#    file.
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions, and the disclaimer given in the documentation
+#    and/or other materials provided with the distribution.
+#
+
 # imports, etc.
 import galsim
 import galsim.wfirst as wf
