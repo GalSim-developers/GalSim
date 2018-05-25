610b3e6b2ea4e98f3e09b8c14f20fc03165a2cf0
Erin Sheldon <erin.sheldon@gmail.com>
2018-05-25 12:03:48 -0400

    test coverage for sending a profile to RandomWalk

--- tests/test_randwalk.py
+++ tests/test_randwalk.py
@@ -36,7 +36,7 @@ def test_randwalk_defaults():
     npoints=100
     hlr = 8.0
     rng = galsim.BaseDeviate(1234)
-    rw=galsim.RandomWalk(npoints, hlr, rng=rng)
+    rw=galsim.RandomWalk(npoints, half_light_radius=hlr, rng=rng)
 
     assert rw.npoints==npoints,"expected npoints==%d, got %d" % (npoints, rw.npoints)
     assert rw.input_half_light_radius==hlr,\
@@ -81,18 +81,26 @@ def test_randwalk_valid_inputs():
     seed=35
     rng=galsim.UniformDeviate(seed)
 
-    rw=galsim.RandomWalk(npoints, hlr, flux=flux, rng=rng)
+    args = (npoints,)
+    kw1 = {'half_light_radius':hlr,'flux':flux,'rng':rng}
+    prof=galsim.Exponential(half_light_radius=hlr, flux=flux)
+    kw2 = {'profile':prof, 'rng':rng}
 
-    assert rw.npoints==npoints,"expected npoints==%d, got %d" % (npoints, rw.npoints)
-    assert rw.input_half_light_radius==hlr,\
-        "expected hlr==%g, got %g" % (hlr, rw.input_half_light_radius)
-    assert rw.flux==flux,\
-        "expected flux==%g, got %g" % (flux, rw.flux)
+    for kw in (kw1, kw2):
+        rw=galsim.RandomWalk(*args, **kw)
 
-    pts=rw.points
-    nobj=len(pts)
-    assert nobj == npoints==npoints,"expected %d objects, got %d" % (npoints, nobj)
-    assert pts.shape == (npoints,2),"expected (%d,2) shape for points, got %s" % (npoints, pts.shape)
+        assert rw.npoints==npoints,"expected npoints==%d, got %d" % (npoints, rw.npoints)
+        assert rw.input_half_light_radius==hlr,\
+            "expected hlr==%g, got %g" % (hlr, rw.input_half_light_radius)
+        assert rw.flux==flux,\
+            "expected flux==%g, got %g" % (flux, rw.flux)
+
+        d=rw.points
+        nobj=len(d)
+        assert nobj == npoints==npoints,"expected %d objects, got %d" % (npoints, nobj)
+
+        pts=rw.points
+        assert pts.shape == (npoints,2),"expected (%d,2) shape for points, got %s" % (npoints, pts.shape)
 
 @timer
 def test_randwalk_invalid_inputs():
@@ -107,10 +115,33 @@ def test_randwalk_invalid_inputs():
     hlr = 8.0
     rng=37
 
+    # support old style where hlr was required. Still works
+    # because we put the keyword second
     args=(npoints, hlr)
     kwargs={'rng':rng}
     with assert_raises(TypeError):
         galsim.RandomWalk(*args, **kwargs)
+
+    # new style where hlr is a keyword.
+    args=(npoints)
+    kwargs={'rng':rng, 'half_light_radius':hlr}
+    with assert_raises(TypeError):
+        galsim.RandomWalk(*args, **kwargs)
+
+    # sending a profile
+    args=(npoints)
+    exp_profile=galsim.Exponential(half_light_radius=hlr, flux=3)
+    kwargs={'rng':rng, 'profile':exp_profile}
+    with assert_raises(TypeError):
+        galsim.RandomWalk(*args, **kwargs)
+
+    # try sending wrong type for profile
+    args=(npoints)
+    bad_profile=3.5
+    kwargs={'rng':rng, 'profile':bad_profile}
+    with assert_raises(TypeError):
+        galsim.RandomWalk(*args, **kwargs)
+
 
     # try sending wrong type for npoints
     npoints=[35]
@@ -170,24 +201,35 @@ def test_randwalk_repr():
     npoints=100
     hlr = 8.0
     flux=1
-    rw=galsim.RandomWalk(npoints, hlr, flux=flux)
+    rw1=galsim.RandomWalk(
+        npoints,
+        half_light_radius=hlr,
+        flux=flux,
+    )
+    rw2=galsim.RandomWalk(
+        npoints,
+        profile=galsim.Exponential(half_light_radius=hlr, flux=flux),
+    )
 
-    # just make sure str() works, don't require eval to give
-    # a consistent object back
-    st=str(rw)
+    for rw in (rw1, rw2):
 
-    # require eval(repr(rw)) to give a consistent object back
 
-    new_rw = eval(repr(rw))
+        # just make sure str() works, don't require eval to give
+        # a consistent object back
+        st=str(rw)
 
-    assert new_rw.npoints == rw.npoints,\
-        "expected npoints=%d got %d" % (rw.npoints,new_rw.npoints)
+        # require eval(repr(rw)) to give a consistent object back
 
-    mess="expected input_half_light_radius=%.16g got %.16g"
-    assert new_rw.input_half_light_radius == rw.input_half_light_radius,\
-        mess % (rw.input_half_light_radius,new_rw.input_half_light_radius)
-    assert new_rw.flux == rw.flux,\
-        "expected flux=%.16g got %.16g" % (rw.flux,new_rw.flux)
+        new_rw = eval(repr(rw))
+
+        assert new_rw.npoints == rw.npoints,\
+            "expected npoints=%d got %d" % (rw.npoints,new_rw.npoints)
+
+        mess="expected input_half_light_radius=%.16g got %.16g"
+        assert new_rw.input_half_light_radius == rw.input_half_light_radius,\
+            mess % (rw.input_half_light_radius,new_rw.input_half_light_radius)
+        assert new_rw.flux == rw.flux,\
+            "expected flux=%.16g got %.16g" % (rw.flux,new_rw.flux)
 
 @timer
 def test_randwalk_config():
@@ -196,38 +238,53 @@ def test_randwalk_config():
     explicit constructor
     """
 
-    gal_config = {
+    hlr=2.0
+    flux=np.pi
+    gal_config1 = {
         'type':'RandomWalk',
         'npoints':100,
-        'half_light_radius':2.0,
-        'flux':np.pi,
+        'half_light_radius':hlr,
+        'flux':flux,
     }
-    config={
-        'gal':gal_config,
-        'rng':galsim.BaseDeviate(31415),
+    gal_config2 = {
+        'type':'RandomWalk',
+        'npoints':100,
+        'profile':galsim.Exponential(half_light_radius=hlr,flux=flux),
+        # TODO: this doesn't work
+        #'profile': {
+        #    'type': 'Exponential',
+        #    'half_light_radius': 2.0,
+        #}
     }
 
-    rwc = galsim.config.BuildGSObject(config, 'gal')[0]
+    for gal_config in (gal_config1, gal_config2):
+        config={
+            'gal':gal_config,
+            'rng':galsim.BaseDeviate(31415),
+        }
 
-    rw = galsim.RandomWalk(
-        gal_config['npoints'],
-        gal_config['half_light_radius'],
-        flux=gal_config['flux'],
-    )
+        rwc = galsim.config.BuildGSObject(config, 'gal')[0]
 
-    assert rw.npoints==rwc.npoints,\
-        "expected npoints==%d, got %d" % (rw.npoints, rwc.npoints)
+        rw = galsim.RandomWalk(
+            gal_config['npoints'],
+            half_light_radius=hlr,
+            flux=flux,
+        )
 
-    assert rw.input_half_light_radius==rwc.input_half_light_radius,\
-        "expected hlr==%g, got %g" % (rw.input_half_light_radius, rw.input_half_light_radius)
+        assert rw.npoints==rwc.npoints,\
+            "expected npoints==%d, got %d" % (rw.npoints, rwc.npoints)
 
-    nobj=len(rw.points)
-    nobjc=len(rwc.points)
-    assert nobj==nobjc,"expected %d objects, got %d" % (nobj,nobjc)
+        assert rw.input_half_light_radius==rwc.input_half_light_radius,\
+            "expected hlr==%g, got %g" % (rw.input_half_light_radius, rw.input_half_light_radius)
 
-    pts=rw.points
-    ptsc=rwc.points
-    assert pts.shape == ptsc.shape, "expected %s shape for points, got %s" % (pts.shape,ptsc.shape)
+        nobj=len(rw.points)
+        nobjc=len(rwc.points)
+        assert nobj==nobjc,"expected %d objects, got %d" % (nobj,nobjc)
+
+        pts=rw.points
+        ptsc=rwc.points
+        assert (pts.shape == ptsc.shape),\
+                "expected %s shape for points, got %s" % (pts.shape,ptsc.shape)
 
 
 @timer
@@ -235,6 +292,8 @@ def test_randwalk_hlr():
     """
     Create a random walk galaxy and test that the half light radius
     is consistent with the requested value
+
+    Note for DeV profile we don't test npoints=3 because it fails
     """
 
     # for checking accuracy, we need expected standard deviation of
@@ -255,20 +314,31 @@ def test_randwalk_hlr():
     # number of trials
     ntrial_vals=[100]*len(npt_vals)
 
-    for ipts,npoints in enumerate(npt_vals):
+    profs = [
+        galsim.Gaussian(half_light_radius=hlr),
+        galsim.Exponential(half_light_radius=hlr),
+        galsim.DeVaucouleurs(half_light_radius=hlr),
+    ]
+    for prof in profs:
+        for ipts,npoints in enumerate(npt_vals):
 
-        ntrial=ntrial_vals[ipts]
+            # DeV profile will fail for npoints==3
+            if isinstance(prof,galsim.DeVaucouleurs) and npoints==3:
+                continue
 
-        hlr_calc=np.zeros(ntrial)
-        for i in range(ntrial):
-            rw=galsim.RandomWalk(npoints, hlr)
-            hlr_calc[i] = rw.calculateHLR()
+            ntrial=ntrial_vals[ipts]
 
-        mn=hlr_calc.mean()
+            hlr_calc=np.zeros(ntrial)
+            for i in range(ntrial):
+                #rw=galsim.RandomWalk(npoints, hlr)
+                rw=galsim.RandomWalk(npoints, profile=prof)
+                hlr_calc[i] = rw.calculateHLR()
 
-        std_check=np.interp(npoints, interp_npts, interp_std*hlr)
-        mess="hlr for npoints: %d outside of expected range" % npoints
-        assert abs(mn-hlr) < nstd*std_check, mess
+            mn=hlr_calc.mean()
+
+            std_check=np.interp(npoints, interp_npts, interp_std*hlr)
+            mess="hlr for npoints: %d outside of expected range" % npoints
+            assert abs(mn-hlr) < nstd*std_check, mess
 
 if __name__ == "__main__":
     test_randwalk_defaults()
