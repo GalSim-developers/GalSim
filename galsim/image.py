249efb1c91e84613ba086114a1557e34ab10b284
Mike Jarvis <michael@jarvis.net>
2018-07-03 17:02:25 -0400

    Add no cover to another line codecov thinks isn't covered. (#821)

--- galsim/image.py
+++ galsim/image.py
@@ -1607,7 +1607,7 @@ def check_image_consistency(im1, im2, integer=False):
     if integer and not im1.isinteger:
         raise GalSimValueError("Image must have integer values.",im1)
     if isinstance(im2, Image):
-        if im1.array.shape != im2.array.shape:
+        if im1.array.shape != im2.array.shape: # pragma: no cover  (covered, but codecov says it's not.)
             raise GalSimIncompatibleValuesError( "Image shapes are inconsistent", im1=im1, im2=im2)
         if integer and not im2.isinteger:
             raise GalSimValueError("Image must have integer values.",im2)
