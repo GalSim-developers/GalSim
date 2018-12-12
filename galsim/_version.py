4c239b81055e7bbf2df23fd8f6c4eba6e04848f3
Mike Jarvis <michael@jarvis.net>
2018-12-12 14:52:37 -0500

    Update master to version 2.2

--- galsim/_version.py
+++ galsim/_version.py
@@ -15,5 +15,5 @@
 #    this list of conditions, and the disclaimer given in the documentation
 #    and/or other materials provided with the distribution.
 #
-__version__ = '2.1'
+__version__ = '2.2'
 __version_info__ = tuple(map(int, __version__.split('.')))
