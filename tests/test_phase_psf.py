6737b690f0aa2477397f8fcff866d8e7390a4d21
Josh Meyers <jmeyers314@gmail.com>
2019-04-01 09:55:38 -0700

    Add shared memory test

--- tests/test_phase_psf.py
+++ tests/test_phase_psf.py
@@ -921,6 +921,7 @@ def test_speedup():
     print("Time for geometric approximation draw: {:6.4f}s".format(t1-t0))
     assert (t1-t0) < 0.1, "Photon-shooting took too long ({0} s).".format(t1-t0)
 
+
 @timer
 def test_instantiation_check():
     """Check that after instantiating, drawing with the other method will emit a warning.
@@ -1080,6 +1081,76 @@ def test_withGSP():
     assert optPSF.stepk != optPSF2.stepk
     assert optPSF.drawImage().bounds != optPSF2.drawImage().bounds
 
+
+# Need a non-local picklable function for next test...
+class DummyWork(object):
+    @staticmethod
+    def work(i, atm):
+        rng = galsim.UniformDeviate(i+10)
+        theta = (rng()*galsim.degrees, rng()*galsim.degrees)
+        psf = atm.makePSF(exptime=1.0, diam=1.1, lam=1000, theta=theta)
+        return psf.drawImage(nx=32, ny=32, scale=0.2, n_photons=10000, method='phot', rng=rng)
+
+
+@timer
+def test_shared_memory():
+    """Test that shared memory hooks to AtmosphericScreen work.
+    """
+    # Make the atmosphere
+    seed = 12345
+    r0_500 = 0.15  # m
+    L0 = 20.0  # m
+    nlayers = 6
+    screen_size = 51.2  # m
+    # screen_size = 819.2  # m
+    screen_scale = 0.1 # m
+    max_speed = 20  # m/s
+    rng = galsim.BaseDeviate(seed)
+    u = galsim.UniformDeviate(rng)
+    Ellerbroek_alts = [0.0, 2.58, 5.16, 7.73, 12.89, 15.46]  # km
+    Ellerbroek_weights = [0.652, 0.172, 0.055, 0.025, 0.074, 0.022]
+    Ellerbroek_interp = galsim.LookupTable(
+            Ellerbroek_alts,
+            Ellerbroek_weights,
+            interpolant='linear')
+    alts = np.max(Ellerbroek_alts)*np.arange(nlayers)/(nlayers-1)
+    weights = Ellerbroek_interp(alts)
+    weights /= sum(weights)
+    spd = []  # Wind speed in m/s
+    dirn = [] # Wind direction in radians
+    r0_500s = [] # Fried parameter in m at a wavelength of 500 nm.
+    for i in range(nlayers):
+        spd.append(u()*max_speed)
+        dirn.append(u()*360*galsim.degrees)
+        r0_500s.append(r0_500*weights[i]**(-3./5))
+    rng2 = rng.duplicate()
+    atm = galsim.Atmosphere(r0_500=r0_500, L0=L0, speed=spd, direction=dirn, altitude=alts, rng=rng,
+                            screen_size=screen_size, screen_scale=screen_scale)
+
+    workObj = DummyWork()
+
+    import multiprocessing as mp
+    mp = mp.get_context(None)
+    with mp.Pool(
+        4,
+        initializer=galsim.phase_screens.initWorker,
+        initargs=galsim.phase_screens.initWorkerArgs()
+    ) as pool:
+        resultsParallel = []
+        for i in range(100):
+            resultsParallel.append(pool.apply_async(workObj.work, (i, atm)))
+        for r in resultsParallel:
+            r.wait()
+    resultsParallel = [r.get() for r in resultsParallel]
+
+    # Serial comparison
+    resultsSerial = []
+    for i in range(100):
+        resultsSerial.append(workObj.work(i, atm))
+
+    assert resultsSerial == resultsParallel
+
+
 if __name__ == "__main__":
     test_aperture()
     test_atm_screen_size()
@@ -1099,3 +1170,4 @@ if __name__ == "__main__":
     test_instantiation_check()
     test_gc()
     test_withGSP()
+    test_shared_memory()
