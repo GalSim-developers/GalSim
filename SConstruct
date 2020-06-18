06083b54b3af4bca8fc4d8d1d26391b1e5a2c90a
Mike Jarvis <michael@jarvis.net>
2020-06-17 21:48:57 -0400

    2020

--- SConstruct
+++ SConstruct
@@ -1,7 +1,7 @@
 from __future__ import print_function
 # vim: set filetype=python et ts=4 sw=4:
 
-# Copyright (c) 2012-2019 by the GalSim developers team on GitHub
+# Copyright (c) 2012-2020 by the GalSim developers team on GitHub
 # https://github.com/GalSim-developers
 #
 # This file is part of GalSim: The modular galaxy image simulation toolkit.
--- devel/lsst/focus_depth.py
+++ devel/lsst/focus_depth.py
@@ -1,3 +1,22 @@
+# Copyright (c) 2012-2020 by the GalSim developers team on GitHub
+# https://github.com/GalSim-developers
+#
+# This file is part of GalSim: The modular galaxy image simulation toolkit.
+# https://github.com/GalSim-developers/GalSim
+#
+# GalSim is free software: redistribution and use in source and binary forms,
+# with or without modification, are permitted provided that the following
+# conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice, this
+#    list of conditions, and the disclaimer given in the accompanying LICENSE
+#    file.
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions, and the disclaimer given in the documentation
+#    and/or other materials provided with the distribution.
+#
+
+#
 # Compute LSST focus curves including effects of Silicon absorption length and
 # refraction, and the fast f/1.2 beam.
 # Compare this to figure 8 from O'Connor++06 "Study of silicon sensor thickness
