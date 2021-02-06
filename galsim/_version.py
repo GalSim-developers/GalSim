6c23cf2c573abcdb15a9e495dfb727418204b35f
Mike Jarvis <michael@jarvis.net>
2021-02-05 23:20:41 -0500

    v2.2.5

--- galsim/_version.py
+++ galsim/_version.py
@@ -15,5 +15,5 @@
 #    this list of conditions, and the disclaimer given in the documentation
 #    and/or other materials provided with the distribution.
 #
-__version__ = '2.2.4'
+__version__ = '2.2.5'
 __version_info__ = tuple(map(int, __version__.split('.')))
