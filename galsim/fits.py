d727377455633dec3d181cedcfb363cc5d4e72f9
Mike Jarvis <michael@jarvis.net>
2022-08-23 19:26:10 -0700

    Only verify/fix if hdu.data raises an error

--- galsim/fits.py
+++ galsim/fits.py
@@ -321,8 +321,14 @@ def _check_hdu(hdu, pyfits_compress, header_only=False):
     # Astropy will automatically fix any errors it finds by just accessing the header and data.
     hdu.header
     if not header_only:
-        hdu.verify('silentfix')
-        hdu.data
+        try:
+            hdu.data
+        except pyfits.VerifyError:  # pragma: no cover
+            # If just reading the data raises a VerifyError, try again with fix.
+            # This seems to be OS-specific, and probably astropy-version specific.
+            # But it's at least possible for this to happen and the verify step to help.
+            hdu.verify('fix')
+            hdu.data
 
     # Check that the specified compression is right for the given hdu type.
     if pyfits_compress:
