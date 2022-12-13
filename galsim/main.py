d144bfb760aedbaa08ae8e5279b32944d9cbbe45
Sidney Mau <sidneymau@gmail.com>
2022-12-13 14:09:51 -0800

    Add --log_format to galsim executable

--- galsim/main.py
+++ galsim/main.py
@@ -52,6 +52,9 @@ def parse_args(command_args):
         '-v', '--verbosity', type=int, action='store', default=1, choices=(0, 1, 2, 3),
         help='integer verbosity level: min=0, max=3 [default=1]')
     parser.add_argument(
+        '--log_format', type=str, action='store', default="%(message)s",
+        help='logger format string [default="%(message)s"]')
+    parser.add_argument(
         '-l', '--log_file', type=str, action='store', default=None,
         help='filename for storing logging output [default is to stream to stdout]')
     parser.add_argument(
@@ -154,11 +157,11 @@ def make_logger(args):
     # Setup logging to go to sys.stdout or (if requested) to an output file
     if args.log_file is None:
         handler = logging.StreamHandler(sys.stdout)
-        handler.setFormatter(logging.Formatter("%(message)s"))
+        handler.setFormatter(logging.Formatter(args.log_format))
         handler.setLevel(level)
     else:
         handler = logging.FileHandler(args.log_file, mode='w')
-        handler.setFormatter(logging.Formatter("%(message)s"))
+        handler.setFormatter(logging.Formatter(args.log_format))
         handler.setLevel(level)
     logger.addHandler(handler)
     return logger
