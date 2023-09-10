8bd033634f9d9bfd91ade3074af6b1ef8a92c07d
Mike Jarvis <michael@jarvis.net>
2023-09-10 13:32:28 -0400

    Save RSS function in Silicon.cpp

--- src/Silicon.cpp
+++ src/Silicon.cpp
@@ -48,6 +48,27 @@
 #include "Image.h"
 #include "PhotonArray.h"
 
+#ifdef DEBUGLOGGING
+// This function was used to help track down a memory leak.  Saving it for future resuse.
+
+#include<mach/mach.h>
+
+double RSS() {
+    struct task_basic_info t_info;
+    mach_msg_type_number_t t_info_count = TASK_BASIC_INFO_COUNT;
+
+    if (KERN_SUCCESS != task_info(mach_task_self(),
+                                  TASK_BASIC_INFO, (task_info_t)&t_info,
+                                  &t_info_count))
+    {
+        return -1;
+    }
+
+    // resident size is in t_info.resident_size;
+    // virtual size is in t_info.virtual_size;
+    return double(t_info.resident_size) / (1 << 30);  // 2^30 == GB
+}
+#endif
 
 namespace galsim {
     // std::min and std::max are not supported in GPU offloaded code, so define our own
