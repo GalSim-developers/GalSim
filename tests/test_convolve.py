a31673d69779b7a01e554187cb09bd16409a9d26
beckermr <becker.mr@gmail.com>
2023-10-04 16:13:32 -0500

    TST more test fixes for jax

--- tests/test_convolve.py
+++ tests/test_convolve.py
@@ -497,9 +497,9 @@ def test_deconvolve():
 
     cen = galsim.PositionD(0,0)
     np.testing.assert_equal(inv_psf.centroid, cen)
-    np.testing.assert_almost_equal(inv_psf.flux, 1./psf.flux)
+    np.testing.assert_array_almost_equal(inv_psf.flux, 1./psf.flux)
     # This doesn't really have any meaning, but this is what we've assigned to a deconvolve max_sb.
-    np.testing.assert_almost_equal(inv_psf.max_sb, -psf.max_sb / psf.flux**2)
+    np.testing.assert_array_almost_equal(inv_psf.max_sb, -psf.max_sb / psf.flux**2)
 
     check_basic(inv_psf, "Deconvolve(Moffat)", do_x=False)
 
