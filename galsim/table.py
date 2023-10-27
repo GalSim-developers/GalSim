1f6d6d1adcdbbb4ff10645839ff1445fe3c60e74
Mike Jarvis <michael@jarvis.net>
2023-10-27 00:33:19 -0400

    Try to avoid lambda in mul_sed the same way we do in mul_bandpass

--- galsim/table.py
+++ galsim/table.py
@@ -391,11 +391,13 @@ class LookupTable:
     def _check_range(self, x):
         slop = (self.x_max - self.x_min) * 1.e-6
         if np.min(x,initial=self.x_min) < self.x_min - slop:
+            xx = np.array(x)
             raise GalSimRangeError("x value(s) below the range of the LookupTable.",
-                                   x, self.x_min, self.x_max)
+                                   xx[xx<self.x_min], self.x_min, self.x_max) from None
         if np.max(x,initial=self.x_max) > self.x_max + slop:  # pragma: no branch
+            xx = np.array(x)
             raise GalSimRangeError("x value(s) above the range of the LookupTable.",
-                                   x, self.x_min, self.x_max)
+                                   xx[xx>self.x_max], self.x_min, self.x_max) from None
 
     def getArgs(self):
         return self.x
