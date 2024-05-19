1e26418121ac86e71caa9dea9042602c37cf89cd
Eli Rykoff <erykoff@stanford.edu>
2024-05-19 14:40:07 -0700

    Add copy_if_needed shim for numpy1/2 compatibility.

--- galsim/utilities.py
+++ galsim/utilities.py
@@ -55,6 +55,20 @@ from .position import parse_pos_args
 from ._utilities import *
 
 
+# Numpy array constructor compatibility shim.
+if np.lib.NumpyVersion(np.__version__) >= "2.0.0":
+    copy_if_needed = None
+elif np.lib.NumpyVersion(np.__version__) < "1.28.0":
+    copy_if_needed = False
+else:
+    # 2.0.0 dev versions, handle cases where copy may or may not exist
+    try:
+        np.array([1]).__array__(copy=None)
+        copy_if_needed = None
+    except TypeError:
+        copy_if_needed = False
+
+
 def roll2d(image, shape):
     """Perform a 2D roll (circular shift) on a supplied 2D NumPy array, conveniently.
 
