ac22edd4d9464118567c9716d1f8c2416bcb271f
Mike Jarvis <michael@jarvis.net>
2025-09-05 14:40:20 -0400

    Let pytest run from root dir

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,11 @@
+import pytest
+
 def pytest_addoption(parser):
     parser.addoption(
         "--run_slow", action="store_true", default=False, help="Run slow tests"
     )
+
+# cf. https://stackoverflow.com/questions/62044541/change-pytest-working-directory-to-test-case-directory
+@pytest.fixture(autouse=True)
+def change_test_dir(request, monkeypatch):
+    monkeypatch.chdir(request.fspath.dirname)
