c3a36c85df9dc3f7cf07c07f10ee1510e3abeba8
Arun Kannawadi <arunkannawadi@astro.princeton.edu>
2026-02-21 22:13:45 -0500

    Define a convenient getBandpass function

--- galsim/roman/__init__.py
+++ galsim/roman/__init__.py
@@ -99,7 +99,7 @@ charge_diffusion = 0.1
 # Maxinum allowed angle from the telecope solar panels to the sun in degrees.
 max_sun_angle = 36.
 
-from .roman_bandpass import getBandpasses
+from .roman_bandpass import getBandpass, getBandpasses
 from .roman_backgrounds import getSkyLevel
 from .roman_psfs import getPSF
 from .roman_wcs import getWCS, findSCA, allowedPos, bestPA, convertCenter
